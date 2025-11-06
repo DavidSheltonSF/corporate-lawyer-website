0a35f1e49ec37358a7c905e07fed6edf0512843a
DavidSheltonSF <david.faria012@gmail.com>
2025-11-06 14:45:21 -0300

    refactor(Navbar): add justify-center Tailwind class to the ul

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,7 +6,7 @@ export function Navbar(){
       </a>
     </div>
     <div>
-      <ul className="flex items-center gap-[24px] text-color-white text-[24px]">
+      <ul className="flex items-center justify-center gap-[24px] text-color-white text-[24px]">
         <a href="#">Início</a>
         <a href="#">Contato</a>
         <a href="#">Blog</a>
