8f2a6edf25414e75eea658e38ce0e806218f2693
DavidSheltonSF <david.faria012@gmail.com>
2025-11-07 18:45:17 -0300

    feat(components): add DynamicSection to show content dynamicaly accordingly the selected nav item

---
+++ app/components/DynamicSection.tsx
@@ -0,0 +1,23 @@
+import { Activity, ReactNode, useContext } from 'react';
+
+interface Props {
+  index: number;
+  title: string;
+  selectedSection: number;
+  style: string;
+  children: ReactNode;
+}
+
+export function DynamicSection({ index, title, selectedSection, style, children }: Props) {
+
+  const isSelected = index === selectedSection;
+
+  return (
+    <Activity mode={isSelected ? 'visible' : 'hidden'}>
+      <section className={style}>
+        <h1 className='font-bold text-[3rem] mt-[40px]'>{title}</h1>
+        {children}
+      </section>
+    </Activity>
+  );
+}
