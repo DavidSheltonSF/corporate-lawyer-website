4278e9f1897854009b72fa412a152d118bf917cc
DavidSheltonSF <david.faria012@gmail.com>
2025-11-07 18:43:37 -0300

    refactor(SubNavbar): enhance component reusing
    -  Added itemsNames property to render the nav idems dynamicaly;
    
    - Added selectedSection state and setSelectedSection function property to control wich nav item is selected
    
    - Added map function to map through itemsNames to render nav items dynamicaly

--- app/components/SubNavbar.tsx
+++ app/components/SubNavbar.tsx
@@ -1,17 +1,33 @@
 'use client';
 
-import { useState } from 'react';
 import { SubNavbarItem } from './SubNavbarItem';
 
-export function SubNavbar() {
-  const [selected, setSelected] = useState(0);
+interface Props {
+  itemsNames: string[];
+  selectedSection: number;
+  setSelectedSection: any;
+}
+
+export function SubNavbar(props: Props) {
+  
+  const { itemsNames, selectedSection, setSelectedSection } = props;
 
   return (
     <nav className="flex items-center justify-center w-full h-[56px] bg-color-primary">
       <ul className="flex items-center justify-center gap-[24px] h-[90%]">
-        <SubNavbarItem index={0} name="Sobre nós" selected={selected} setSelected={setSelected} />
-        <SubNavbarItem index={1} name="Serviços" selected={selected} setSelected={setSelected} />
-        <SubNavbarItem index={2} name="Equipe" selected={selected} setSelected={setSelected} />
+        {itemsNames
+          ? itemsNames.map((itemName, index) => {
+              return (
+                <SubNavbarItem
+                  key={`subnav-item${index}`}
+                  index={index}
+                  name={itemName}
+                  selected={selectedSection}
+                  setSelected={setSelectedSection}
+                />
+              );
+            })
+          : ''}
       </ul>
     </nav>
   );
