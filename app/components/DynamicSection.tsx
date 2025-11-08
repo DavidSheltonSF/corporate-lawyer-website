2570186a1d9f9d8055aff3d68021f2fc03d94fe8
DavidSheltonSF <david.faria012@gmail.com>
2025-11-08 16:51:46 -0300

    refactor(DynamicSection): make it a flex element

--- app/components/DynamicSection.tsx
+++ app/components/DynamicSection.tsx
@@ -12,7 +12,7 @@ export function DynamicSection({ index, title, selectedSection, children }: Prop
 
   return (
     <Activity mode={isSelected ? 'visible' : 'hidden'}>
-      <section className={`appear-animation text-white text-2xl h-full w-[80%] `}>
+      <section className={`flex flex-col appear-animation text-white text-2xl h-full w-[80%] `}>
         <h1 className="font-bold text-[3rem] my-[40px]">{title}</h1>
         {children}
       </section>
