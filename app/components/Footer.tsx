4f3a4d12dcf0f3a5c2856c716c309be967ecc1a5
DavidSheltonSF <david.faria012@gmail.com>
2025-11-09 15:07:06 -0300

    feat(components): add Footer component

---
+++ app/components/Footer.tsx
@@ -0,0 +1,23 @@
+export function Footer() {
+  return (
+    <footer>
+      <div className="flex flex-col h-[50vh] bg-color-primary">
+        <div className="flex justify-center items-center h-[65%]">
+          <img className="size-[240px]" src="website-logo.png" alt="" />
+        </div>
+        <div className="flex justify-center items-center gap-[24px] h-[15%] border-y border-color-secondary">
+          <a href="#">
+            <img className="size-[40px]" src="icons/instagram.svg" alt="" />
+          </a>
+          <a href="#">
+            <img className="size-[40px]" src="icons/facebook.png" alt="" />
+          </a>
+        </div>
+        <div className="flex justify-center items-center gap-[24px] h-[20%] font-bold text-[var(--white-color)] text-xl">
+          <a href="#">privacidade</a>
+          <a href="#">Contate-nos</a>
+        </div>
+      </div>
+    </footer>
+  );
+}
