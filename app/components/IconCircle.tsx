79abf609dc1a115b9f4019804599513e3676dcf4
DavidSheltonSF <david.faria012@gmail.com>
2025-11-08 21:07:41 -0300

    feat(componens): add IconCircle to abstract Services emblems logic

---
+++ app/components/IconCircle.tsx
@@ -0,0 +1,24 @@
+interface Props {
+  iconPath: string;
+  isOverParent?: boolean;
+  size: string;
+}
+
+export function IconCircle(props: Props) {
+  const { iconPath, isOverParent, size } = props;
+
+  return (
+    <div
+      className={`flex items-center justify-center rounded-full bg-color-secondary  overflow-hidden mt-[16px] cursor-pointer${
+        isOverParent ? 'shadow-[var(--bright-yellow-shadow)]' : ''
+      }`}
+      style={{ width: size, height: size }}
+    >
+      <img
+        className={`size-[68%] hover:invert  ${isOverParent ? 'invert' : ''}`}
+        src={iconPath}
+        alt=""
+      />
+    </div>
+  );
+}
