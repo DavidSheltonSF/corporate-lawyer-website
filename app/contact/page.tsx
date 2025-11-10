4aa0365733f9dbd85467ce9a34b295f5c1dd76c1
DavidSheltonSF <david.faria012@gmail.com>
2025-11-10 15:30:32 -0300

    refactor(Contact page): set the background color to black

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,7 +2,7 @@ import { HeroSection } from '../components/HeroSection';
 
 export default function Contact() {
   return (
-    <div className="flex flex-col min-h-screen items-center bg-zinc-50 font-sans bg-color-white">
+    <div className="flex flex-col min-h-screen items-center bg-zinc-50 font-sans bg-color-black">
       <HeroSection additionalStyles="h-[320px] items-end" background="var(--blue-gradient)">
         <div className="flex flex-col ml-[160px] w-[936px] text-color-white gap-[16px] pb-[40px]">
           <h1 className="text-[64px] font-bold">Contate-nos</h1>
