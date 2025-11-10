bb54dac769a173918b1ae0ce32892c72d379bd43
DavidSheltonSF <david.faria012@gmail.com>
2025-11-10 13:57:05 -0300

    refactor(Home page): replace the png image in the hero section with a webp version for better performance

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen items-center bg-zinc-50 font-sans bg-color-white">
       <header className="w-full">
-        <div className="flex items-end bg-[url(/hero-image.png)] w-full h-[728px] bg-cover bg-center">
+        <div className="flex items-end bg-[url(/hero-image.webp)] w-full h-[728px] bg-cover bg-center">
           <h1 className="text-[64px] font-bold text-white w-[936px] ml-[160px] mb-[40px]">
             ESCRITÓRIO DE ADVOCACIA MEDEIROS E SANTIAGO
           </h1>
