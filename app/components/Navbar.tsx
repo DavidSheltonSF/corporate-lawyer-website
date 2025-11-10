829b629d8afa91a9ca9b52da7bd3fcfed8f812dc
DavidSheltonSF <david.faria012@gmail.com>
2025-11-10 12:40:49 -0300

    refactor(Navbar): replace the png logo image with a webp version for a better performance.

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,7 +5,7 @@ export function Navbar() {
     <nav className="absolute top-0 flex justify-between items-center bg-[var(--black-color)]/48 w-full h-[80px] px-[40px]">
       <div>
         <a href="#">
-          <img className="size-[72px]" src="./website-logo-with-circle.png" alt="website-logo" />
+          <img className="size-[72px]" src="./website-logo-with-circle-72px.webp" alt="website-logo" />
         </a>
       </div>
       <div>
