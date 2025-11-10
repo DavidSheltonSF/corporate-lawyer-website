8ab964a03d6ae701c3bec07ad46ddae1d1cd9787
DavidSheltonSF <david.faria012@gmail.com>
2025-11-10 12:40:35 -0300

    refactor(Footer): replace the png logo image with a webp version for a better performance.

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,7 +3,7 @@ export function Footer() {
     <footer>
       <div className="flex flex-col h-[50vh] bg-color-primary">
         <div className="flex justify-center items-center h-[65%]">
-          <img className="size-[240px]" src="website-logo.png" alt="" />
+          <img className="size-[240px]" src="website-logo.webp" alt="" />
         </div>
         <div className="flex justify-center items-center gap-[24px] h-[15%] border-y border-color-secondary">
           <a href="#">
