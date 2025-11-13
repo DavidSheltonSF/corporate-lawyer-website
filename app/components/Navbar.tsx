c26c796c7f70e79e95620197ee1541ed27eea0cf
DavidSheltonSF <david.faria012@gmail.com>
2025-11-13 16:16:12 -0300

    refactor(Navbar): make it compatible with both large and small screens

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,21 +1,36 @@
+'use client'
+
 import { NavbarItem } from './NavbarItem';
 
 export function Navbar() {
+
+  function toggleMenu() {
+    const menu = document.querySelector('.navbar-menu');
+    menu?.classList.toggle('active');
+  }
+
   return (
-    <nav className="absolute top-0 flex justify-between items-center bg-[var(--black-color)]/48 w-full h-[80px] px-[40px]">
+    <nav className="absolute top-0 flex justify-between items-center bg-[var(--black-color)]/48 w-full h-[80px] px-[40px] max-md:px-[24px]">
       <div>
         <a href="#">
-          <img className="size-[72px]" src="./website-logo-with-circle-72px.webp" alt="website-logo" />
+          <img
+            className="size-[72px]"
+            src="./website-logo-with-circle-72px.webp"
+            alt="website-logo"
+          />
         </a>
       </div>
-      <div>
-        <ul className="flex items-center justify-center gap-[24px] text-color-white text-[24px]">
+      <div className="navbar-menu max-md:absolute max-md:w-full max-md:left-0 max-md:top-[100%] max-md:bg-[var(--black-color)]/48 max-md:h-0 max-md:overflow-hidden active">
+        <ul className="flex max-md:flex-col items-center justify-center gap-[24px] text-color-white text-[24px]">
           <NavbarItem name="Início" link="/" />
           <NavbarItem name="Contato" link="/contact" />
           <NavbarItem name="Blog" link="#" />
           <NavbarItem name="Página do Cliente" link="#" />
         </ul>
       </div>
+      <button className='min-lg:hidden' onClick={toggleMenu}>
+        <img className="size-[48px]" src="icons/menu.svg" alt="" />
+      </button>
     </nav>
   );
 }
