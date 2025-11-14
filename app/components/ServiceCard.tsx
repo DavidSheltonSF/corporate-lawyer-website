195d461af2619fcb780b77c4c7d4d7ab1e51e186
DavidSheltonSF <david.faria012@gmail.com>
2025-11-14 15:25:13 -0300

    refator(ServiceCard components): make compatible with small screens

--- app/components/ServiceCard.tsx
+++ app/components/ServiceCard.tsx
@@ -21,17 +21,16 @@ export function ServiceCard(props: Props) {
 
   return (
     <article
-      className="flex flex-col w-[calc((100%-160px)/3)] h-[440px] shrink-0"
+      className="flex flex-col w-[calc((100%-160px)/3)] max-md:w-full h-[440px] shrink-0"
       onMouseOver={handleMouseOver}
       onMouseLeave={handleMouseLeave}
     >
       <header className="flex justify-center">
-        <IconCircle iconPath={image} size="144px" isOverParent={isOver}/>
-
+        <IconCircle iconPath={image} additionalStyles="size-[144px] max-md:size-[116px]" isOverParent={isOver} />
       </header>
       <main className="flex flex-col items-center text-center gap-[16px]">
         <h1 className="text-3xl font-bold mt-[16px]">{title}</h1>
-        <p className="w-[92%] text-xl">{content}</p>
+        <p className="w-[92%] text-xl max-md:text-lg">{content}</p>
       </main>
     </article>
   );
