7546317703116c4343e726682a0c8af3da036989
DavidSheltonSF <david.faria012@gmail.com>
2025-11-15 19:52:51 -0300

    refactor(IconCircle): enhance the machanic that makes the image color be inverted on mouse over
    Added hangleMouseOver and handleMouseOut to add ou remove invertedColor style to the icon-image

--- app/components/IconCircle.tsx
+++ app/components/IconCircle.tsx
@@ -5,16 +5,28 @@ interface Props {
 }
 
 export function IconCircle(props: Props) {
-  const { iconPath, isOverParent , additionalStyles} = props;
+  const { iconPath, isOverParent, additionalStyles } = props;
+
+  function handleMouseOver(e: React.MouseEvent<HTMLElement>) {
+    const iconImage = e.currentTarget.querySelector('.icon-image');
+    iconImage?.classList.add('invertedImage');
+  }
+
+  function handleMouseOut(e: React.MouseEvent<HTMLElement>) {
+    const iconImage = e.currentTarget.querySelector('.icon-image');
+    iconImage?.classList.remove('invertedImage');
+  }
 
   return (
     <div
       className={`flex items-center justify-center rounded-full bg-color-secondary  overflow-hidden mt-[16px] cursor-pointer ${
         isOverParent ? 'shadow-[var(--bright-yellow-shadow)]' : ''
       } ${additionalStyles}`}
+      onMouseOver={handleMouseOver}
+      onMouseOut={handleMouseOut}
     >
       <img
-        className={`size-[68%] hover:invert  ${isOverParent ? 'invert' : ''}`}
+        className={`icon-image size-[68%] hover:invert  ${isOverParent ? 'invert' : ''}`}
         src={iconPath}
         alt=""
       />
