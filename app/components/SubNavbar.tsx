cc6896fd547e077e609d46baa47f90b237237a13
DavidSheltonSF <david.faria012@gmail.com>
2025-11-15 19:35:16 -0300

    refactor(SubNavbar): set height: 32px to large screens

--- app/components/SubNavbar.tsx
+++ app/components/SubNavbar.tsx
@@ -12,7 +12,7 @@ export function SubNavbar(props: Props) {
   const { itemsNames, selectedSection, setSelectedSection } = props;
 
   return (
-    <nav className="flex items-center justify-center w-full h-[56px] bg-color-primary">
+    <nav className="flex items-center justify-center w-full h-[56px] lg:h-[32px] bg-color-primary">
       <ul className="flex items-center justify-center gap-[24px] h-[90%]">
         {itemsNames
           ? itemsNames.map((itemName, index) => {
