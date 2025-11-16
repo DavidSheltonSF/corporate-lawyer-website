e47b128c73e689cae109d84bc5b316c804335b86
DavidSheltonSF <david.faria012@gmail.com>
2025-11-15 21:29:11 -0300

    refactor(ServiceCard):  modify the calculation used to set its width to calc(100%-112px)/3 instead of calc(100%-160px)/3 , since the gap between the elements will be 56px instead of 80px

--- app/components/ServiceCard.tsx
+++ app/components/ServiceCard.tsx
@@ -21,7 +21,7 @@ export function ServiceCard(props: Props) {
 
   return (
     <article
-      className="flex flex-col w-full h-[440px] lg:h-auto lg:w-[calc((100%-160px)/3)] shrink-0"
+      className="flex flex-col w-full h-[440px] lg:h-auto lg:w-[calc((100%-112px)/3)] shrink-0"
       onMouseOver={handleMouseOver}
       onMouseLeave={handleMouseLeave}
     >
