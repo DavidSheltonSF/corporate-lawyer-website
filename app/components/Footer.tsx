158af9dacec29ce9a76d4f8857b9422defadee6b
DavidSheltonSF <david.faria012@gmail.com>
2025-11-15 21:57:11 -0300

    refactor(Footer): ehance style on large screens

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,17 +3,17 @@ export function Footer() {
     <footer>
       <div className="flex flex-col h-[50vh] bg-color-primary">
         <div className="flex justify-center items-center h-[65%]">
-          <img className="size-[240px]" src="website-logo.webp" alt="" />
+          <img className="size-[240px] lg:size-[160px]" src="website-logo.webp" alt="" />
         </div>
-        <div className="flex justify-center items-center gap-[24px] h-[15%] border-y border-color-secondary">
+        <div className="flex justify-center items-center gap-[24px] lg:gap-[16px] h-[15%] border-y border-color-secondary">
           <a href="#">
-            <img className="size-[40px]" src="icons/instagram.svg" alt="" />
+            <img className="size-[40px] lg:size-[24px]" src="icons/instagram.svg" alt="" />
           </a>
           <a href="#">
-            <img className="size-[40px]" src="icons/facebook.png" alt="" />
+            <img className="size-[40px] lg:size-[24px]" src="icons/facebook.png" alt="" />
           </a>
         </div>
-        <div className="flex justify-center items-center gap-[24px] h-[20%] font-bold text-[var(--white-color)] text-xl">
+        <div className="flex justify-center items-center gap-[24px] h-[20%] font-bold text-[var(--white-color)] text-[1rem]">
           <a href="#">privacidade</a>
           <a href="#">Contate-nos</a>
         </div>
