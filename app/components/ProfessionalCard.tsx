10f5239eacbaf3da0266d9d64680eddb77eb7769
DavidSheltonSF <david.faria012@gmail.com>
2025-11-15 21:54:06 -0300

    refactor(ProfessionalCard): enhance responsiveness

--- app/components/ProfessionalCard.tsx
+++ app/components/ProfessionalCard.tsx
@@ -25,9 +25,9 @@ export function ProfessionalCard(props: ProfessionalCardProps) {
     });
   }
   return (
-    <article className="flex flex-col bg-[#4D3B00] w-full lg:w-[480px] rounded-xl  border border-[var(--secondary-color)]">
+    <article className="flex flex-col items-between justify-center bg-[#4D3B00] w-full lg:w-[480px] rounded-xl  border border-[var(--secondary-color)] ">
       <main className="flex flex-col lg:flex-row">
-        <div className="flex justify-center items-center lg:h-full lg:w-[30%] p-[16px]">
+        <div className="flex justify-center items-center lg:h-full p-[16px]">
           <img className="size-full lg:size-[120px] rounded-xl" src={imagePath} alt="" />
         </div>
         <div className="flex lg:w-[70%] flex-col gap-[8px] px-[16px] pb-[16px]">
@@ -35,14 +35,14 @@ export function ProfessionalCard(props: ProfessionalCardProps) {
           <p className="w-full text-[4vw] lg:text-[0.7rem]">{description}</p>
         </div>
       </main>
-      <footer className="flex flex-1 border-t border-[var(--secondary-color)] lg:text-[1.5vw]">
-        <div className="hidden md:flex flex-col border-r border-[var(--secondary-color)] h-full w-[50%] p-[16px]">
+      <footer className="flex h-fit border-t border-[var(--secondary-color)] lg:text-[0.8rem]">
+        <div className="hidden md:flex flex-col gap-[8px] border-r border-[var(--secondary-color)] w-[50%] pl-[16px] py-[8px]">
           <h4 className="font-bold">Especialidades</h4>
-          <span className="flex items-center gap-[16px] mt-auto">{renderSpecializations()}</span>
+          <span className="flex items-center gap-[16px]">{renderSpecializations()}</span>
         </div>
-        <div className="flex md:flex-col p-[16px] w-full md:w-50">
+        <div className="flex md:flex-col gap-[8px] px-[16px] py-[8px] w-full md:w-[50%]">
           <h4 className="hidden md:block font-bold">Contato</h4>
-          <span className="flex gap-[16px] m-auto md:m-0 md:mt-auto items-center">
+          <span className="flex gap-[16px] m-auto md:ml-0 items-center">
             <a href={emailLink}>
               <img className="size-[56px] lg:size-[32px]" src="icons/email.svg" alt="" />
             </a>
