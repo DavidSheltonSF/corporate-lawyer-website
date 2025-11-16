d14d89e37eeeed88e638afacf63996daf3f7f1d3
DavidSheltonSF <david.faria012@gmail.com>
2025-11-16 16:29:36 -0300

    fix(DynamicSection): make  the text higher on large screens

--- app/components/DynamicSection.tsx
+++ app/components/DynamicSection.tsx
@@ -14,7 +14,7 @@ export function DynamicSection({ index, title, selectedSection, additionalStyle,
   return (
     <Activity mode={isSelected ? 'visible' : 'hidden'}>
       <section className={`flex flex-col appear-animation text-white w-full h-full ${additionalStyle}`}>
-        <h1 className="font-bold text-[3rem] lg:text-[2rem] my-[40px]">{title}</h1>
+        <h1 className="font-bold text-[3rem] my-[40px]">{title}</h1>
         {children}
       </section>
     </Activity>
