72b64d282495b21c0bedca20539bd5c69c73d4a5
DavidSheltonSF <david.faria012@gmail.com>
2025-11-17 14:44:03 -0300

    feat(data): add constant servicesDetails to store details about the services provided

---
+++ app/data/servicesDetails.tsx
@@ -0,0 +1,15 @@
+ export const servicesDetails = [
+   {
+     title: 'Família',
+     services: [
+       'Ação de divórcio (consensual e litigioso)',
+       'Pensão alimentícia',
+       'Guarda e visitas de filhos',
+       'Reconhecimento e dissolução de união estável',
+       'Regulamentação de convivência familiar',
+       'Partilha de bens',
+       'Investigação de paternidade',
+       'Adoção',
+     ],
+   },
+ ];
