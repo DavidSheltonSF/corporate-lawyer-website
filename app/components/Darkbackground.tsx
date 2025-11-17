eb40727e3621697a2a38d25e301960cd2789f1ab
DavidSheltonSF <david.faria012@gmail.com>
2025-11-17 14:40:09 -0300

    feat(components): add DarkBackground
    It will be used do highlight modals

---
+++ app/components/Darkbackground.tsx
@@ -0,0 +1,5 @@
+import { ReactNode } from "react";
+
+export function Darkbackground({ children }: { children: ReactNode }) {
+  return <div className="absolute flex w-full h-full bg-color-black-dark opacity-[50%]">{children}</div>;
+}
