803931f2686a3211236761e21fa8c1270b1093ca
DavidSheltonSF <david.faria012@gmail.com>
2025-11-18 17:16:12 -0300

    refactor(components): make ServiceDetailsModal work properly
    Removed  title and services props
    
    Added modalIsOpen, setModalIsOpen and modalData props
    
    Set z-index: 9999 to bring the element front over all other elements

--- app/components/ServiceDetailsModal.tsx
+++ app/components/ServiceDetailsModal.tsx
@@ -1,32 +1,35 @@
-import { useState } from "react";
-import { ServiceDetails } from "../types/ServiceDetails";
+interface Props {
+  modalIsOpen: boolean;
+  setModalIsOpen: any;
+  modalData: Record<string, any>;
+}
 
-
-export function ServiceDetailsModal({title, services}: ServiceDetails) {
-
-  const [displayed, setDisplayed] = useState(true);
-
+export function ServiceDetailsModal({ modalIsOpen, setModalIsOpen, modalData }: Props) {
   function handleClickOk() {
-    setDisplayed(false)
+    setModalIsOpen(false);
   }
+
+  const { title, services } = modalData;
 
   return (
     <div
-      className={`flex flex-col fixed left-[50%] translate-x-[-50%] top-[10vh] lg:top-[15vh]  w-[80%] lg:w-[30rem] min-h-[25rem] rounded-xl p-[16px] text-color-white font-bold bg-color-black-dark border border-color-secondary ${!displayed ? 'hidden' : ''}`}
+      className={`flex z-9999 flex-col fixed left-[50%] translate-x-[-50%] top-[10vh] lg:top-[15vh]  w-[80%] lg:w-[30rem] min-h-[25rem] rounded-xl p-[16px] text-color-white font-bold bg-color-black-dark border border-color-secondary ${
+        !modalIsOpen ? 'hidden' : ''
+      }`}
     >
       <h3 className="font-bold text-2xl self-center">{title}</h3>
       <div className="flex flex-col gap-[8px] my-[16px] size-full scroll-hidden p-[8px] rounded-xl">
         <p className="text-xl">Prestamos os seguintes serviços:</p>
         <ul className="flex list-disc list-inside flex-col gap-[8px]">
-         {
-          services.map((service, index) => {
-            return <li key={index}>{service}</li>
-          })
-         }
+          {services.map((service: any, index: number) => {
+            return <li key={index}>{service}</li>;
+          })}
         </ul>
       </div>
-      <button className="self-end w-full lg:w-[88px] rounded-full lg:rounded-md bg-color-secondary text-color-black font-bold py-[8px] cursor-pointer hover:brightness-150"
-      onClick={handleClickOk}>
+      <button
+        className="self-end w-full lg:w-[88px] rounded-full lg:rounded-md bg-color-secondary text-color-black font-bold py-[8px] cursor-pointer hover:brightness-150"
+        onClick={handleClickOk}
+      >
         Ok
       </button>
     </div>
