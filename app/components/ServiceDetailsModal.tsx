371617573bcef08f25f8f0dd4636bac211093253
DavidSheltonSF <david.faria012@gmail.com>
2025-11-20 16:09:16 -0300

    refactor(ServiceDetailsModal): make it find the service data using serviceAreaId property
    Removed modalData property
    
    Added serviceAreaId property

--- app/components/ServiceDetailsModal.tsx
+++ app/components/ServiceDetailsModal.tsx
@@ -1,18 +1,18 @@
+import { servicesDetails } from '../data/servicesDetails';
+
 interface Props {
-  modalData: Record<string, any>;
+  serviceAreaId: string;
 }
 
-export function ServiceDetailsModal({modalData }: Props) {
-
-  const { title, services } = modalData;
-
+export function ServiceDetailsModal({ serviceAreaId }: Props) {
+  const area = servicesDetails[serviceAreaId];
   return (
     <div>
-      <h3 className="font-bold text-2xl self-center">{title}</h3>
+      <h3 className="font-bold text-2xl self-center">{area?.title}</h3>
       <div className="flex flex-col gap-[8px] my-[16px] size-full scroll-hidden p-[8px] rounded-xl">
         <p className="text-xl">Prestamos os seguintes serviços:</p>
         <ul className="flex list-disc list-inside flex-col gap-[8px]">
-          {services.map((service: any, index: number) => {
+          {area?.services.map((service: any, index: number) => {
             return <li key={index}>{service}</li>;
           })}
         </ul>
