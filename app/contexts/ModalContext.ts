ff762010db1a81d5453d1c2c2179f86f89df9928
DavidSheltonSF <david.faria012@gmail.com>
2025-11-20 16:35:18 -0300

    refactor(ModalContext): add ModalContextType to the context

--- app/contexts/ModalContext.ts
+++ app/contexts/ModalContext.ts
@@ -1,3 +1,10 @@
-import { createContext } from "react";
+import { createContext, Dispatch, SetStateAction } from 'react';
 
-export const ModalContext = createContext({});
+type ModalContextType = {
+  modalIsOpen: boolean | null;
+  setModalIsOpen: Dispatch<SetStateAction<boolean>> | null;
+  serviceAreaId: string | null;
+  setServiceAreaId: Dispatch<SetStateAction<string>> | null;
+};
+
+export const ModalContext = createContext<ModalContextType | undefined>(undefined);
