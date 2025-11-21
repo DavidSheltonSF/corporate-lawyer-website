a51b23788bc7f6cd993bf6ac280653bcfc371fc2
DavidSheltonSF <david.faria012@gmail.com>
2025-11-21 14:02:30 -0300

    refactor(contact page): reduce the HeroSection's height to fit better with the content inside

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,7 +3,7 @@ import { HeroSection } from '../components/HeroSection';
 export default function Contact() {
   return (
     <div className="flex flex-col min-h-screen items-center bg-zinc-50 font-sans bg-color-black">
-      <HeroSection additionalStyles="h-[320px] items-end" background="var(--blue-gradient)">
+      <HeroSection additionalStyles="h-[280px] items-end" background="var(--blue-gradient)">
         <div className="flex flex-col gap-[16px] justify-end w-full mb-[40px] px-[40px] lg:pl-[160px] h-fit text-color-white">
           <h1 className="text-3xl lg:text-5xl font-bold ">Contate-nos</h1>
           <p className="lg:text-[1.5rem] lg:w-[50%]">
