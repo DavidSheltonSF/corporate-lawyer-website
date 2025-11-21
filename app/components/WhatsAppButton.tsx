d73d7e203098b3d091072bc816212db9012ef213
DavidSheltonSF <david.faria012@gmail.com>
2025-11-21 15:10:29 -0300

    feat(components): add WhatsAppButton to redirect users to the WhatsApp chat

---
+++ app/components/WhatsAppButton.tsx
@@ -0,0 +1,17 @@
+interface Props {
+  whatsAppNumber?: string;
+}
+
+export function WhatsAppButton({ whatsAppNumber }: Props) {
+  return (
+    <div className="fixed right-[24px] bottom-[24px] size-[64px] bg-green-400 rounded-full hover:brightness-120">
+      <a
+        className="flex size-full items-center justify-center cursor-pointer"
+        href={`https://wa.me/${whatsAppNumber}`}
+        target="_blank"
+      >
+        <img className="size-[70%]" src="/icons/whatsapp.svg" alt="" />
+      </a>
+    </div>
+  );
+}
