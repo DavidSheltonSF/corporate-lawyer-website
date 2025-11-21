5cf71d458fa001cde7d3bfcdc9da1094a8e86c3b
DavidSheltonSF <david.faria012@gmail.com>
2025-11-21 13:59:44 -0300

    refactor(contact page): remove the black background from the content wrapper inside HeroSection

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,7 +4,7 @@ export default function Contact() {
   return (
     <div className="flex flex-col min-h-screen items-center bg-zinc-50 font-sans bg-color-black">
       <HeroSection additionalStyles="h-[320px] items-end" background="var(--blue-gradient)">
-        <div className="flex flex-col gap-[16px] justify-end w-full mb-[40px] px-[40px] lg:pl-[160px] bg-[var(--black-color)]/40 h-fit text-color-white">
+        <div className="flex flex-col gap-[16px] justify-end w-full mb-[40px] px-[40px] lg:pl-[160px] h-fit text-color-white">
           <h1 className="text-3xl lg:text-5xl font-bold ">Contate-nos</h1>
           <p className="lg:text-[1.5rem] lg:w-[50%]">
             Precisa de apoio jurítico ou consultorria? Envie-nos uma mensagem e entraremos em
