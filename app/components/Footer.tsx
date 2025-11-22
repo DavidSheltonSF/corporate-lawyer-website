b2dd892f2688ab02fd5b025cd0202af6ffcab7d0
DavidSheltonSF <david.faria012@gmail.com>
2025-11-22 10:18:34 -0300

    chore(Footer): set all images loading to lazy

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,14 +3,14 @@ export function Footer() {
     <footer>
       <div className="flex flex-col h-[50vh] bg-color-primary">
         <div className="flex justify-center items-center h-[65%]">
-          <img className="size-[240px]" src="website-logo.webp" alt="" />
+          <img className="size-[240px]" src="website-logo.webp" alt="" loading="lazy" />
         </div>
         <div className="flex justify-center items-center gap-[24px] lg:gap-[16px] h-[15%] border-y border-color-secondary">
           <a href="#">
-            <img className="size-[40px]" src="icons/instagram.svg" alt="" />
+            <img className="size-[40px]" src="icons/instagram.svg" alt="" loading="lazy" />
           </a>
           <a href="#">
-            <img className="size-[40px]" src="icons/facebook.png" alt="" />
+            <img className="size-[40px]" src="icons/facebook.png" alt="" loading="lazy" />
           </a>
         </div>
         <div className="flex justify-center items-center gap-[24px] h-[20%] font-bold text-[var(--white-color)] text-[1rem]">
