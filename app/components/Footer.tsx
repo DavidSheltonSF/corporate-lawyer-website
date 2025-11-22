14749d88e1433ff9ba12e9b99424d12cb912e5cf
DavidSheltonSF <david.faria012@gmail.com>
2025-11-22 10:57:26 -0300

    chore(Footer): add alternative text to the website logo and social media icons

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,14 +3,24 @@ export function Footer() {
     <footer>
       <div className="flex flex-col h-[50vh] bg-color-primary">
         <div className="flex justify-center items-center h-[65%]">
-          <img className="size-[240px]" src="website-logo.webp" alt="" loading="lazy" />
+          <img className="size-[240px]" src="website-logo.webp" alt="Medeiros e Santiago" loading="lazy" />
         </div>
         <div className="flex justify-center items-center gap-[24px] lg:gap-[16px] h-[15%] border-y border-color-secondary">
           <a href="#">
-            <img className="size-[40px]" src="icons/instagram.svg" alt="" loading="lazy" />
+            <img
+              className="size-[40px]"
+              src="icons/instagram.svg"
+              alt="Go to our Instagram page"
+              loading="lazy"
+            />
           </a>
           <a href="#">
-            <img className="size-[40px]" src="icons/facebook.png" alt="" loading="lazy" />
+            <img
+              className="size-[40px]"
+              src="icons/facebook.png"
+              alt="Go to our Facebook page"
+              loading="lazy"
+            />
           </a>
         </div>
         <div className="flex justify-center items-center gap-[24px] h-[20%] font-bold text-[var(--white-color)] text-[1rem]">
