50c14970df33f367db4e4e7751ccfaf618430669
DavidSheltonSF <david.faria012@gmail.com>
2025-11-22 19:27:46 -0300

    refactor(CarouselButton): improve styling and responsiveness
    - Set button hidden on small screens
    - Set button size to 56px on small screens
    - Set button size to 80px on medium and large screens

--- app/components/CarouselButton.tsx
+++ app/components/CarouselButton.tsx
@@ -3,14 +3,14 @@ import { ReactEventHandler, ReactNode } from 'react';
 interface Props {
   children: ReactNode;
   inert?: boolean;
-  onClickHandler: ReactEventHandler
+  onClickHandler: ReactEventHandler;
 }
 
 export function CarouselButton({ onClickHandler, inert, children }: Props) {
   return (
     <button
       inert={inert}
-      className={`flex items-center justify-center rounded-md size-[80px] border border-color-secondary bg-black/50 hover:bg-white transition-[background-color] duration-300  cursor-pointer inert:opacity-50 inert:saturate-0`}
+      className={`rounded-md hidden md:block border-color-secondary bg-black/50 hover:bg-white transition-[background-color] duration-300 cursor-pointer inert:opacity-50 inert:saturate-0 size-[56px] md:size-[80px]`}
       onClick={onClickHandler}
     >
       <div>{children}</div>
