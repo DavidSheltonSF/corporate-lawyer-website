26888ec7710599fbebfddb54fe0c01bc25756bd5
DavidSheltonSF <david.faria012@gmail.com>
2025-11-23 14:10:38 -0300

    refactor(Navbar): wrap the website logo with the <Link> tag instead of <a> tag to avoid page refreshing when user clicks it.

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import { NavbarItem } from './NavbarItem';
 import { NavbarMobileMenuButton } from './NavbarMobileMenuButton';
 import { NavbarList } from './NavbarList';
+import Link from 'next/link';
 
 export function Navbar() {
   const [menuIsOpen, setMenuIsOpen] = useState(false);
@@ -11,13 +12,13 @@ export function Navbar() {
   return (
     <nav className="absolute top-0 flex justify-between items-center bg-[var(--black-color)]/48 w-full h-auto py-[4px] px-[24px] lg:px-[40px]">
       <div>
-        <a href="/">
+        <Link href="/">
           <img
             className="size-[56px]"
             src="./website-logo-with-circle-72px.webp"
             alt="Medeiros e Santiago"
           />
-        </a>
+        </Link>
       </div>
       <NavbarList menuIsOpen={menuIsOpen}>
         <NavbarItem name="Início" link="/" />
