ac36eb4ed347b27cf46f13f946d4f6676e0ca130
DavidSheltonSF <david.faria012@gmail.com>
2025-11-23 14:45:34 -0300

    refactor(data): enhance professionalsInfo typing and consistency
    - make professionalsInfo a ProfessionalCardProps[]  and rename emailLink
    - Renamed emailLink and whatsAppLink properties to email and whatsAppNumber respectively

--- app/data/professionalsInfo.ts
+++ app/data/professionalsInfo.ts
@@ -1,12 +1,14 @@
- export const professionalsInfo = [
+import { ProfessionalCardProps } from "../components/ProfessionalCard";
+
+ export const professionalsInfo: ProfessionalCardProps[] = [
    {
      imagePath: 'photos/carla-224px.webp',
      imageAlternativeText: 'Dra. Carla Medeiros, advogada, sorrindo em sua mesa',
      title: 'Dr. Carla Medeiros',
      description: `Profissional dedicada à defesa dos direitos do trabalhador e do segurado, atuando com precisão em causas previdenciárias e trabalhistas. Sempre comprometida em garantir benefícios, solucionar conflitos e oferecer orientação clara e humanizada.`,
      specializations: ['trabalhista', 'previdenciario'],
-     emailLink: '#',
-     whatsAppLink: '#',
+     email: '#',
+     whatsAppNumber: '#',
    },
    {
      imagePath: 'photos/flavia-224px.webp',
@@ -14,7 +16,7 @@
      title: 'Dr. Flávia Santiago',
      description: `Especialista em Direito de Família e Direito Cível, com foco em resolver conflitos de forma sensível, estratégica e eficiente. Atua em divórcios, guarda, pensão, contratos e questões patrimoniais, sempre priorizando o bem-estar e a segurança jurídica do cliente.`,
      specializations: ['familia', 'civil'],
-     emailLink: '#',
-     whatsAppLink: '#',
+     email: '#',
+     whatsAppNumber: '#',
    },
  ];
