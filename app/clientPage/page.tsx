923e11fb6bf6b1eba356a6ad98876ca3138651d4
DavidSheltonSF <david.faria012@gmail.com>
2025-11-28 17:58:26 -0300

    refactor(clientPage): remove login form make it show the user name in a hero section

--- app/clientPage/page.tsx
+++ app/clientPage/page.tsx
@@ -1,43 +1,31 @@
-import { Form } from '@/components/Form';
 import { HeroSection } from '@/components/HeroSection';
-import { InputForm } from '@/components/InputForm';
+import { getUserInformation } from '@/lib/getUserInformation';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 
-export default function ClientPage() {
+export default async function ClientPage() {
+  const cookieStore = await cookies();
+  const auth = cookieStore.get('authentication');
+  const token = auth?.value;
+  if (!token) {
+    redirect('/clientLogin');
+  }
+  const user = await getUserInformation(token);
+
+  if (!user) {
+    console.log('Some thing went wrong');
+    redirect('/clientLogin');
+  }
+
   return (
     <div className="bg-color-black">
       <HeroSection
         background="var(--blue-gradient)"
-        title="Página do Cliente"
-        description="Clientes cadastrados podem consultar o andamento dos seus processos diretamente na plataforma."
+        title={`Bem vindo(a) ${user.role === 'lawyer' ? 'Dra' : ''} ${user.firstName}`}
         additionalStyles="h-[280px]"
       />
       <main>
-        <section className="flex items-center justify-center h-[90vh] lg:h-[60vh]">
-          <Form >
-            <InputForm
-              id="input-email"
-              name="email"
-              iconPath="/icons/email-primary-light.svg"
-              label="Email"
-              type='email'
-              required={true}
-            />
-            <InputForm
-              id="input-password"
-              name="password"
-              iconPath="/icons/lock-primary-light.svg"
-              label="Password"
-              type='password'
-              required={true}
-            />
-            <button
-              type="submit"
-              className="w-full bg-[var(--primary-color-light)] rounded-full font-bold h-[40px] hover:brightness-124 transition-all duration-[300ms] cursor-pointer"
-            >
-              Entrar
-            </button>
-          </Form>
-        </section>
+        <section className="flex items-center justify-center h-[90vh] lg:h-[60vh]"></section>
       </main>
     </div>
   );
