bf9a4bc3878d023b5f1c979c38b55f74abb75101
DavidSheltonSF <david.faria012@gmail.com>
2025-11-28 17:53:03 -0300

    feat(actions): add login to abstract user login logic

---
+++ actions/login.ts
@@ -0,0 +1,20 @@
+'use server';
+
+import { authenticateUser } from '@/lib/authenticateUser';
+import { isString } from '@/lib/isString';
+import { cookies } from 'next/headers';
+
+export async function login(
+  formData: FormData
+): Promise<{ error: boolean; message: string }> {
+  const token = await authenticateUser(formData);
+
+  if (!token || !isString(token)) {
+    return { error: true, message: 'Email or password is incorrect' };
+  }
+
+  const cookiesStore = await cookies();
+  cookiesStore.set('authentication', token, { httpOnly: true });
+
+  return { error: false, message: '' };
+}
