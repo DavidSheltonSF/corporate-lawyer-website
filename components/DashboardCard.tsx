80473861e85c39673a521800484197f64b134872
DavidSheltonSF <david.faria012@gmail.com>
2025-11-29 16:28:42 -0300

    feat(components): add DashboardCard with title and  children properties

---
+++ components/DashboardCard.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from 'react';
+
+interface Props {
+  title: string;
+  children: ReactNode;
+}
+
+export function DashboardCard({ title, children }: Props) {
+  return (
+    <div
+      className="flex flex-col bg-color-primary-light h-[160px] rounded-xl py-[16px]"
+      style={{
+        width: 'calc((100% - 120px) / 4)',
+      }}
+    >
+      <h1 className="ml-[24px] font-bold text-[1.5rem]">{title}</h1>
+      <div className="flex flex-col gap-[16px] bg-color-primary h-[90%] px-[24px] py-[8px]">
+        {children}
+      </div>
+    </div>
+  );
+}
