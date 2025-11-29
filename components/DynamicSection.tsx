36f334f0894c6deaaf6aa574d7d8bae6fab1ac3a
DavidSheltonSF <david.faria012@gmail.com>
2025-11-28 23:48:59 -0300

    feat(components): add DynamicSection to separete content inside DynamicSections

--- components/DynamicSection.tsx
+++ components/DynamicSection.tsx
@@ -1,30 +1,8 @@
-import { Activity, ReactNode } from 'react';
+import { ReactNode } from 'react';
 
 interface Props {
-  index: number;
-  title: string;
-  selectedSection: number;
-  additionalStyle?: string;
   children: ReactNode;
 }
-
-export function DynamicSection({
-  index,
-  title,
-  selectedSection,
-  additionalStyle,
-  children,
-}: Props) {
-  const isSelected = index === selectedSection;
-
-  return (
-    <Activity mode={isSelected ? 'visible' : 'hidden'}>
-      <section
-        className={`flex flex-col fade-in-animation text-white w-full h-full ${additionalStyle}`}
-      >
-        <h1 className="font-bold text-[3rem] my-[40px]">{title}</h1>
-        {children}
-      </section>
-    </Activity>
-  );
+export function DynamicSection({ children }: Props) {
+  return <section>{children}</section>;
 }
