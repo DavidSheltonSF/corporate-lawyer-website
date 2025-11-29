ba02739e72c3721c5e451c51b24168950df6cfe6
DavidSheltonSF <david.faria012@gmail.com>
2025-11-28 23:51:05 -0300

    refactor(Home page): extract DynamicSection component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 'use client';
 import { useState } from 'react';
-import { DynamicSection } from '../components/DynamicSection';
-import { SubNavbar } from '../components/SubNavbar';
 import { ServiceCard } from '../components/ServiceCard';
 import { DraggableCarousel } from '../components/DraggableCarousel';
 import { ProfessionalCard } from '../components/ProfessionalCard';
@@ -12,10 +10,10 @@ import { ServiceDetailsModal } from '../components/ServiceDetailsModal';
 import { ModalWindow } from '../components/ModalWindow';
 import { ModalContext } from '../contexts/ModalContext';
 import { Carousel } from '../components/Carousel';
+import { DynamicSections } from '@/components/DynamicSections';
+import { DynamicSection } from '@/components/DynamicSection';
 
 export default function Home() {
-  const [selectedSection, setSelectedSection] = useState(0);
-
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [serviceAreaId, setServiceAreaId] = useState('');
 
@@ -33,70 +31,56 @@ export default function Home() {
 
       <main>
         <ModalContext value={{ modalIsOpen, setModalIsOpen, serviceAreaId, setServiceAreaId }}>
-          <section className="flex flex-col items-center w-full min-h-[90vh] h-auto pb-[80px]">
-            <SubNavbar
-              itemsNames={['Sobre nós', 'Serviços', 'Equipe']}
-              selectedSection={selectedSection}
-              setSelectedSection={setSelectedSection}
-            />
-            <div className="flex justify-center items-center w-[80%] h-full">
-              <DynamicSection
-                index={0}
-                title="Sobre Nós"
-                selectedSection={selectedSection}
-                additionalStyle="text-[1.5rem]"
-              >
-                <div className="flex flex-col gap-[32px] lg:w-[70%]">
-                  <div className="flex flex-col gap-[16px]">
-                    <h3 className="text-[1.7rem] text-color-secondary font-bold">
-                      Somos experiêntes
-                    </h3>
-                    <p>
-                      Com mais de 5 anos prestando atendimento com empatia, ética e dedicação, somos
-                      um escritório de advocacia comprometido em oferecer soluções jurídicas
-                      eficazes e humanizadas.
-                    </p>
-                  </div>
-                  <div className="flex flex-col gap-[16px]">
-                    <h3 className="text-[1.7rem] ] text-color-secondary font-bold">
-                      Diversificados
-                    </h3>
-                    <p>
-                      Atuamos em diversas áreas do Direito, com destaque para Direito de Família,
-                      Previdenciário, Trabalhista e Civil, sempre buscando garantir os melhores
-                      resultados para cada cliente, de forma personalizada e transparente.
-                    </p>
-                  </div>
-                  <div className="flex flex-col gap-[16px]">
-                    <h3 className="text-[1.7rem] text-color-secondary font-bold">
-                      Também somos adaptáveis
-                    </h3>
-                    <p>
-                      Além disso, somos um escritório moderno e adaptável, que utiliza tecnologias
-                      inovadoras para agilizar atendimentos, acompanhar processos e oferecer suporte
-                      jurídico de excelência, onde quer que você esteja.
-                    </p>
-                  </div>
+          <DynamicSections sectionsNames={['Sobre Nós', 'Serviços', 'Equipe']}>
+            <DynamicSection>
+              <div className="flex flex-col gap-[32px] lg:w-[70%] text-[1.5rem]">
+                <div className="flex flex-col gap-[16px]">
+                  <h3 className="text-[1.7rem] text-color-secondary font-bold">
+                    Somos experiêntes
+                  </h3>
+                  <p>
+                    Com mais de 5 anos prestando atendimento com empatia, ética e dedicação, somos
+                    um escritório de advocacia comprometido em oferecer soluções jurídicas eficazes
+                    e humanizadas.
+                  </p>
                 </div>
-              </DynamicSection>
-              <DynamicSection index={1} title="Serviços" selectedSection={selectedSection}>
-                <div className="flex flex-col gap-[32px] w-full h-full flex-1">
-                  <Carousel>
-                    <ServiceCard serviceDetails={servicesDetails['familia']} />
-                    <ServiceCard serviceDetails={servicesDetails['trabalhista']} />
-                    <ServiceCard serviceDetails={servicesDetails['civil']} />
-                    <ServiceCard serviceDetails={servicesDetails['previdenciario']} />
-                  </Carousel>
+                <div className="flex flex-col gap-[16px]">
+                  <h3 className="text-[1.7rem] ] text-color-secondary font-bold">Diversificados</h3>
+                  <p>
+                    Atuamos em diversas áreas do Direito, com destaque para Direito de Família,
+                    Previdenciário, Trabalhista e Civil, sempre buscando garantir os melhores
+                    resultados para cada cliente, de forma personalizada e transparente.
+                  </p>
                 </div>
-              </DynamicSection>
-              <DynamicSection index={2} title="Equipe" selectedSection={selectedSection}>
-                <div className="flex flex-col gap-[40px]">
-                  <ProfessionalCard {...professionalsInfo[0]} />
-                  <ProfessionalCard {...professionalsInfo[1]} />
+                <div className="flex flex-col gap-[16px]">
+                  <h3 className="text-[1.7rem] text-color-secondary font-bold">
+                    Também somos adaptáveis
+                  </h3>
+                  <p>
+                    Além disso, somos um escritório moderno e adaptável, que utiliza tecnologias
+                    inovadoras para agilizar atendimentos, acompanhar processos e oferecer suporte
+                    jurídico de excelência, onde quer que você esteja.
+                  </p>
                 </div>
-              </DynamicSection>
-            </div>
-          </section>
+              </div>
+            </DynamicSection>
+            <DynamicSection>
+              <div className="flex flex-col gap-[32px] w-full h-full flex-1">
+                <Carousel>
+                  <ServiceCard serviceDetails={servicesDetails['familia']} />
+                  <ServiceCard serviceDetails={servicesDetails['trabalhista']} />
+                  <ServiceCard serviceDetails={servicesDetails['civil']} />
+                  <ServiceCard serviceDetails={servicesDetails['previdenciario']} />
+                </Carousel>
+              </div>
+            </DynamicSection>
+            <DynamicSection>
+              <div className="flex flex-col gap-[40px]">
+                <ProfessionalCard {...professionalsInfo[0]} />
+                <ProfessionalCard {...professionalsInfo[1]} />
+              </div>
+            </DynamicSection>
+          </DynamicSections>
         </ModalContext>
       </main>
     </div>
---
+++ components/DynamicSections.tsx
@@ -0,0 +1,35 @@
+import { Children, ReactElement, ReactNode, useState } from 'react';
+import { SubNavbar } from './SubNavbar';
+import { DynamicSectionWrapper } from './DynamicSectionWrapper';
+import { DynamicSection } from './DynamicSection';
+
+interface Props {
+  sectionsNames: string[];
+  children: ReactElement<typeof DynamicSection> | ReactElement<typeof DynamicSection>[];
+}
+
+export function DynamicSections({ sectionsNames, children }: Props) {
+  const [selectedSection, setSelectedSection] = useState(0);
+  return (
+    <section className="flex flex-col items-center w-full min-h-[90vh] h-auto pb-[80px]">
+      <SubNavbar
+        itemsNames={sectionsNames}
+        selectedSection={selectedSection}
+        setSelectedSection={setSelectedSection}
+      />
+      <div className="flex justify-center items-center w-[80%] h-full">
+        {Children.map(children, (child, index) => {
+          return (
+            <DynamicSectionWrapper
+              index={index}
+              title={sectionsNames[index]}
+              selectedSection={selectedSection}
+            >
+              {child}
+            </DynamicSectionWrapper>
+          );
+        })}
+      </div>
+    </section>
+  );
+}
