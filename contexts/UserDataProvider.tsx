766ae9fa573d16b19b873bf41a83e7ce18f87b7d
DavidSheltonSF <david.faria012@gmail.com>
2025-12-01 01:08:32 -0300

    feat(context): add UserDataContext and its provider

---
+++ contexts/UserDataProvider.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import { ReactNode } from 'react';
+import { UserDataContext } from './UserDataContext';
+import { UserProps } from '@/types/UserProps';
+import { WithId } from '@/types/WIthId';
+
+interface Props {
+  userData: WithId<UserProps>;
+  children: ReactNode;
+}
+
+export function UserDataProvider({ userData, children }: Props) {
+  return <UserDataContext.Provider value={{ userData }}>{children}</UserDataContext.Provider>;
+}
