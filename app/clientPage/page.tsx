e489cb9befbca056330c9335e8a05b00c6dc38da
DavidSheltonSF <david.faria012@gmail.com>
2025-12-01 01:10:59 -0300

    refactor(clientPage): provide user data by context

--- app/clientPage/page.tsx
+++ app/clientPage/page.tsx
@@ -1,8 +1,11 @@
+import CaseSearchSection from '@/components/CaseSearchSection';
 import { DashboardCard } from '@/components/DashboardCard';
 import { DashboardCardInfo } from '@/components/DashboardCardInfo';
+import { DropDownButton } from '@/components/DropdownButton';
 import { DynamicSection } from '@/components/DynamicSection';
 import { DynamicSections } from '@/components/DynamicSections';
 import { HeroSection } from '@/components/HeroSection';
+import { UserDataProvider } from '@/contexts/UserDataProvider';
 import { getTokenFromCookies } from '@/lib/getTokenFromCookies';
 import { getUserInformation } from '@/lib/getUserInformation';
 import { redirect } from 'next/navigation';
@@ -31,32 +34,33 @@ export default async function ClientPage() {
         title={`Bem vindo(a) ${user?.role === 'lawyer' ? 'Dra' : ''} ${user?.firstName}`}
         additionalStyles="h-[280px]"
       />
-      <main>
-        <DynamicSections sectionsNames={['Geral', 'Processos']}>
-          <DynamicSection>
-            <div className="flex gap-[40px]">
-              <DashboardCard title="Processos">
-                <DashboardCardInfo name="Em andamento" value={1} />
-                <DashboardCardInfo name="Encerrados" value={2} />
-              </DashboardCard>
-              <DashboardCard title="Atendimento">
-                <DashboardCardInfo name="Data" value="2 de jan. de 2026" />
-              </DashboardCard>
-              <DashboardCard title="Próxima audiência">
-                <DashboardCardInfo name="Data" value="15 de jan. de 2026" />
-              </DashboardCard>
-              <DashboardCard title='Documentação'>
-                <DashboardCardInfo name="status" value="pendente"/>
-              </DashboardCard>
-            </div>
-          </DynamicSection>
-          <DynamicSection>
-            <div>
-              <h1>Processos</h1>
-            </div>
-          </DynamicSection>
-        </DynamicSections>
-      </main>
+
+      <UserDataProvider userData={user}>
+        <main>
+          <DynamicSections sectionsNames={['Geral', 'Processos']}>
+            <DynamicSection>
+              <div className="flex gap-[40px]">
+                <DashboardCard title="Processos">
+                  <DashboardCardInfo name="Em andamento" value={1} />
+                  <DashboardCardInfo name="Encerrados" value={2} />
+                </DashboardCard>
+                <DashboardCard title="Atendimento">
+                  <DashboardCardInfo name="Data" value="2 de jan. de 2026" />
+                </DashboardCard>
+                <DashboardCard title="Próxima audiência">
+                  <DashboardCardInfo name="Data" value="15 de jan. de 2026" />
+                </DashboardCard>
+                <DashboardCard title="Documentação">
+                  <DashboardCardInfo name="status" value="pendente" />
+                </DashboardCard>
+              </div>
+            </DynamicSection>
+            <DynamicSection>
+              <CaseSearchSection />
+            </DynamicSection>
+          </DynamicSections>
+        </main>
+      </UserDataProvider>
     </div>
   );
 }
