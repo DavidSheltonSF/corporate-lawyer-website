42d67d242586139d22a9281315d9c6212662b2af
DavidSheltonSF <david.faria012@gmail.com>
2025-12-02 23:19:40 -0300

    test(api): add pagination to api/cases/[clientId] route
    - Read page and limit queries
    - Apply pagination

--- app/api/cases/[clientId]/route.ts
+++ app/api/cases/[clientId]/route.ts
@@ -1,8 +1,19 @@
+import { paginate } from '@/helpers/paginate';
 import { fakeCases } from '../../fakeDatabase/cases';
 
 export async function GET(req: Request, context: { params: Promise<{ clientId: string }> }) {
   const { clientId } = await context.params;
+  const { searchParams } = new URL(req.url);
   const cases = fakeCases.filter((cas) => cas.clientId === clientId);
 
-  return Response.json(cases);
+  const page = Number(searchParams.get('page')) || 1;
+  const limit = Number(searchParams.get('limit')) || 5;
+  const response = {
+    data: paginate(cases, page, limit),
+    page,
+    limit,
+    total: cases.length,
+    totalPages: Math.ceil(cases.length / limit),
+  };
+  return Response.json(response);
 }
