816b011aafa16a8fe046c0e85af45e35a0a72dbd
DavidSheltonSF <david.faria012@gmail.com>
2025-12-02 23:36:19 -0300

    feat(components): add pagination to CaseSearchContainer
    - Made it a relative parent
    - Addeed pageIndex to useEffect list
    - Set its height to 130vh
    - Added pageIndex and totalPage states
    - Added buttons to set pageIndex at the bottom of the component

--- components/CaseSearchContainer.tsx
+++ components/CaseSearchContainer.tsx
@@ -17,10 +17,15 @@ interface Props {
 
 export function CaseSearchContainer({ query, queryType, userData }: Props) {
   const [cases, setCases] = useState<WithId<CaseProps>[] | null>(null);
+  const [pageIndex, setPageIndex] = useState(1);
+  const [totalPage, setTotalPage] = useState(0);
 
   useEffect(() => {
     async function loadCases() {
-      const casesData = await getClientCases(userData.id);
+      const casesPagination = await getClientCases(userData.id, pageIndex, 4);
+      const casesData = casesPagination.data;
+      console.log(casesData);
+      setTotalPage(casesPagination.totalPages);
       let filteredData = null;
       switch (queryType) {
         case CaseQueryTypeEnum.num_processo:
@@ -37,17 +42,36 @@ export function CaseSearchContainer({ query, queryType, userData }: Props) {
       setCases(filteredData);
     }
     loadCases();
-  }, [query, queryType]);
+  }, [query, queryType, pageIndex]);
 
   const renderCases = cases?.map((cas, index) => {
     return <CaseCard key={index} caseData={cas} />;
   });
   return (
-    <div className="flex flex-col gap-[32px] mt-[88px] min-h-[50vh] w-full ">
+    <div className="relative flex flex-col gap-[32px] mt-[88px] h-[130vh] w-full">
       <Activity mode={!cases || cases.length === 0 ? 'visible' : 'hidden'}>
         <h1 className="text-3xl">Nenhum caso encontrado</h1>
       </Activity>
       {renderCases}
+      <div className="flex justify-center border absolute bottom-[24px] left-[50%] translate-x-[-50%] w-[80%]">
+        <div className="flex gap-[16px]">
+          {Array.from({ length: totalPage }).map((page, index) => {
+            return (
+              <div
+                className={`flex justify-center items-center text-2xl bg-color-primary text-color-white size-[56px] rounded-lg ${
+                  pageIndex === index + 1 ? 'brightness-180' : ''
+                }`}
+                key={index}
+                onClick={() => {
+                  setPageIndex(index + 1);
+                }}
+              >
+                {index + 1}
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
