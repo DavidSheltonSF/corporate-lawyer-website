e2f7cfbd21ea208124cd4255c259fb7a87ae0a0d
DavidSheltonSF <david.faria012@gmail.com>
2025-12-03 12:06:20 -0300

    refactor(getClientCases): add additionalQuery param to filter client cases

--- lib/getClientCases.ts
+++ lib/getClientCases.ts
@@ -1,12 +1,31 @@
+import { CaseQueryTypeEnum } from '@/components/CaseQueryTypeEnum';
 import { CasesPaginationProps } from '@/types/CasesPaginationProps';
 
 export async function getClientCases(
   clientId: string,
   page: number,
-  limit: number
+  limit: number,
+  additionalQuery?: {
+    value: string;
+    type: CaseQueryTypeEnum;
+  }
 ): Promise<CasesPaginationProps> {
   try {
-    const response = await fetch(`/api/cases/${clientId}?page=${page}&limit=${limit}`);
+    let baseRoute = '/api/cases/';
+    let queryString = `${clientId}?page=${page}&limit=${limit}`;
+
+    switch (additionalQuery?.type) {
+      case CaseQueryTypeEnum.num_processo:
+        queryString += `&processNumber=${additionalQuery.value}`;
+        break;
+      case CaseQueryTypeEnum.titulo:
+        queryString += `&title=${additionalQuery.value}`;
+
+      default:
+        break;
+    }
+
+    const response = await fetch(`${baseRoute}/${queryString}`);
 
     const clientCases = await response.json();
 
