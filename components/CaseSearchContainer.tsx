84c57e395b7b6c7f6952a6900fcfc05f16e10273
DavidSheltonSF <david.faria012@gmail.com>
2025-12-03 12:08:13 -0300

    refactor(CaseSearchContainer): remove filtering, since cases will be filtered by the API by query params

--- components/CaseSearchContainer.tsx
+++ components/CaseSearchContainer.tsx
@@ -6,8 +6,6 @@ import { WithId } from '@/types/WIthId';
 import { Activity, useEffect, useState } from 'react';
 import { CaseProps } from '@/types/CaseProps';
 import { CaseQueryTypeEnum } from './CaseQueryTypeEnum';
-import { filterCasesByTitle } from '@/lib/filterCasesByTitle';
-import { filterCasesByProcessNumber } from '@/lib/filterCasesByProcessNumber';
 import { CaseCardSkeleton } from './CaseCardSkeleton';
 
 interface Props {
@@ -24,24 +22,16 @@ export function CaseSearchContainer({ query, queryType, userData }: Props) {
 
   useEffect(() => {
     async function loadCases() {
-      setLoading(true)
-      const casesPagination = await getClientCases(userData.id, pageIndex, 4);
+      setLoading(true);
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      const casesPagination = await getClientCases(userData.id, pageIndex, 4, {
+        type: queryType,
+        value: query,
+      });
       const casesData = casesPagination.data;
       setTotalPage(casesPagination.totalPages);
-      let filteredData = null;
-      switch (queryType) {
-        case CaseQueryTypeEnum.num_processo:
-          filteredData = filterCasesByProcessNumber(casesData, query);
-          break;
 
-        case CaseQueryTypeEnum.titulo:
-          filteredData = filterCasesByTitle(casesData, query);
-          break;
-
-        default:
-          break;
-      }
-      setCases(filteredData);
+      setCases(casesData);
       setLoading(false);
     }
     loadCases();
@@ -56,7 +46,7 @@ export function CaseSearchContainer({ query, queryType, userData }: Props) {
   });
   return (
     <div className="relative flex flex-col gap-[32px] mt-[88px] h-[130vh] w-full">
-      <Activity mode={!loading &&  (!cases || cases.length === 0) ? 'visible' : 'hidden'}>
+      <Activity mode={!loading && (!cases || cases.length === 0) ? 'visible' : 'hidden'}>
         <h1 className="text-3xl">Nenhum caso encontrado</h1>
       </Activity>
       {loading ? renderCaseSkeletons : renderCases}
