1491e81f6bfcbf6e59d8061929e6efe7fdf48590
DavidSheltonSF <david.faria012@gmail.com>
2025-12-03 09:30:30 -0300

    feat(case searching): make skeletons be displayed while the cards are not loaded

--- components/CaseSearchContainer.tsx
+++ components/CaseSearchContainer.tsx
@@ -8,6 +8,7 @@ import { CaseProps } from '@/types/CaseProps';
 import { CaseQueryTypeEnum } from './CaseQueryTypeEnum';
 import { filterCasesByTitle } from '@/lib/filterCasesByTitle';
 import { filterCasesByProcessNumber } from '@/lib/filterCasesByProcessNumber';
+import { CaseCardSkeleton } from './CaseCardSkeleton';
 
 interface Props {
   query: string;
@@ -19,9 +20,11 @@ export function CaseSearchContainer({ query, queryType, userData }: Props) {
   const [cases, setCases] = useState<WithId<CaseProps>[] | null>(null);
   const [pageIndex, setPageIndex] = useState(1);
   const [totalPage, setTotalPage] = useState(0);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function loadCases() {
+      setLoading(true)
       const casesPagination = await getClientCases(userData.id, pageIndex, 4);
       const casesData = casesPagination.data;
       setTotalPage(casesPagination.totalPages);
@@ -39,6 +42,7 @@ export function CaseSearchContainer({ query, queryType, userData }: Props) {
           break;
       }
       setCases(filteredData);
+      setLoading(false);
     }
     loadCases();
   }, [query, queryType, pageIndex]);
@@ -46,12 +50,16 @@ export function CaseSearchContainer({ query, queryType, userData }: Props) {
   const renderCases = cases?.map((cas, index) => {
     return <CaseCard key={index} caseData={cas} />;
   });
+
+  const renderCaseSkeletons = Array.from({ length: 4 }).map((page, index) => {
+    return <CaseCardSkeleton key={index} />;
+  });
   return (
     <div className="relative flex flex-col gap-[32px] mt-[88px] h-[130vh] w-full">
-      <Activity mode={!cases || cases.length === 0 ? 'visible' : 'hidden'}>
+      <Activity mode={!loading &&  (!cases || cases.length === 0) ? 'visible' : 'hidden'}>
         <h1 className="text-3xl">Nenhum caso encontrado</h1>
       </Activity>
-      {renderCases}
+      {loading ? renderCaseSkeletons : renderCases}
       <div className="flex justify-center border absolute bottom-[24px] left-[50%] translate-x-[-50%] w-[80%]">
         <div className="flex gap-[16px]">
           {Array.from({ length: totalPage }).map((page, index) => {
