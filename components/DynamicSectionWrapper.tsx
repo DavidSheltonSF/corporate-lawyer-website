03b49ebd312f6f8875f541218dda7f3adc1d0181
DavidSheltonSF <david.faria012@gmail.com>
2025-12-03 14:16:46 -0300

    refactor(DynamicSectionWrapper): replace the Activity bulit-in component with conditional rendering to render the section only if it is selected
    - Before this change dynamic sections where just hidden if not selected, so their states were not reset when they was displayed again.

--- components/DynamicSectionWrapper.tsx
+++ components/DynamicSectionWrapper.tsx
@@ -1,4 +1,4 @@
-import { Activity, ReactNode } from 'react';
+import { ReactNode } from 'react';
 
 interface Props {
   index: number;
@@ -7,20 +7,15 @@ interface Props {
   children: ReactNode;
 }
 
-export function DynamicSectionWrapper({
-  index,
-  title,
-  selectedSection,
-  children,
-}: Props) {
+export function DynamicSectionWrapper({ index, title, selectedSection, children }: Props) {
   const isSelected = index === selectedSection;
 
   return (
-    <Activity mode={isSelected ? 'visible' : 'hidden'}>
+    isSelected && (
       <section className="flex flex-col fade-in-animation text-white w-full h-full">
         <h1 className="font-bold text-[3rem] my-[40px]">{title}</h1>
         {children}
       </section>
-    </Activity>
+    )
   );
 }
