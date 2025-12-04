a14d5f8d49390f98dbdff5131269894c54a02273
DavidSheltonSF <david.faria012@gmail.com>
2025-12-04 14:10:07 -0300

    refactor(CaseSearchSection): enhance searching logic and component structure
    - Received pageIndex, totalPage and loading states from its children
    
    - Renamed loading to casesLoading for consistency
    
    -  Defined a useEffect to load case cards when page loads and when padeIndex changes
    
    - Defined a useEffect to load cases when query changes and query is not a void string
    
    - Defined a useEffect to reset query when queryType changes
    
    - Added Pagination component

--- components/CaseSearchSection.tsx
+++ components/CaseSearchSection.tsx
@@ -1,31 +1,68 @@
 'use client';
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { CaseSearchBar } from './CaseSearchBar';
 import { CaseSearchContainer } from './CaseSearchContainer';
 import { UserDataContext } from '@/contexts/UserDataContext';
 import { CaseQueryTypeEnum } from './CaseQueryTypeEnum';
+import { getClientCases } from '@/lib/getClientCases';
+import { CaseProps } from '@/types/CaseProps';
+import { WithId } from '@/types/WIthId';
+import { Pagination } from './Pagination';
 
 export default function CaseSearchSection() {
   const [query, setQuery] = useState('');
   const [queryType, setQueryType] = useState<CaseQueryTypeEnum>(CaseQueryTypeEnum.num_processo);
+  const [pageIndex, setPageIndex] = useState(1);
+  const [totalPage, setTotalPage] = useState(0);
+  const [cases, setCases] = useState<WithId<CaseProps>[]>([]);
+  const [casesLoading, setCasesLoading] = useState(false);
+
+  async function loadCases() {
+    setCasesLoading(true);
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    const casesPagination = await getClientCases(userData.id, pageIndex, 4, {
+      type: queryType,
+      value: query,
+    });
+    const casesData = casesPagination.cases;
+    setTotalPage(casesPagination.totalPages);
+
+    setCases(casesData);
+    setCasesLoading(false);
+  }
+
+  // Load case cards when component loads or page index changes
+  useEffect(() => {
+    loadCases();
+  }, [pageIndex]);
+
+  // Load case cards and reset query when query
+  useEffect(() => {
+    if (!query) return;
+    loadCases();
+    setQuery('');
+  }, [query]);
+
+  // Reset query when queryType changes
+  useEffect(() => {
+    setQuery('');
+  }, [queryType]);
 
   const context = useContext(UserDataContext);
-    if (!context) {
-      return <div>User data not found</div>;
-    }
+  if (!context) {
+    return <div>User data not found</div>;
+  }
 
-    const userData = context.userData
+  const userData = context.userData;
 
   return (
-    <section>
+    <section className="relative">
       <div className="flex flex-col">
-        <CaseSearchBar
-          setQuery={setQuery}
-          queryType={queryType}
-          setQueryType={setQueryType}
-        />
+        <CaseSearchBar setQuery={setQuery} queryType={queryType} setQueryType={setQueryType} />
       </div>
-      <CaseSearchContainer query={query} queryType={queryType} userData={userData}/>
+      <CaseSearchContainer loading={casesLoading} cases={cases} />
+      <Pagination totalPage={totalPage} pageIndex={pageIndex} setPageIndex={setPageIndex} />
     </section>
   );
 }
