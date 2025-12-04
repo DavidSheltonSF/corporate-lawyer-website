ebb3ff1115c1688492d9d13c03e024175eb193d7
DavidSheltonSF <david.faria012@gmail.com>
2025-12-04 19:59:08 -0300

    fix(fechLawyers): import WithId

--- lib/fetchLawyers.ts
+++ lib/fetchLawyers.ts
@@ -1,6 +1,6 @@
-import { WithId } from '@/types/WIthId';
 import { UserProps } from '@/types/UserProps';
 import { fetchUser } from './fetchUser';
+import { WithId } from '@/types/WIthId';
 
 export async function fetchLawyers(ids: string[]): Promise<WithId<UserProps>[]> {
   const promises = ids.map((id) => fetchUser(id));
