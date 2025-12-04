6c8912a1ed2967a69194742da2192ab6f8b7c456
DavidSheltonSF <david.faria012@gmail.com>
2025-12-04 20:00:28 -0300

    chore: move fechUsers.ts to /services folder and update references

--- services/fetchLawyers.ts
+++ services/fetchLawyers.ts
@@ -1,6 +1,6 @@
-import { UserProps } from '@/types/UserProps';
-import { fetchUser } from '../services/fetchUser';
 import { WithId } from '@/types/WIthId';
+import { UserProps } from '@/types/UserProps';
+import { fetchUser } from './fetchUser';
 
 export async function fetchLawyers(ids: string[]): Promise<WithId<UserProps>[]> {
   const promises = ids.map((id) => fetchUser(id));
