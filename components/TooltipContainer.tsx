792cc1ed989d3c1615e7c5c124f1aeb929347acb
DavidSheltonSF <david.faria012@gmail.com>
2025-12-04 22:12:05 -0300

    refactor(TooltipContainer): make its width and height fits its content

--- components/TooltipContainer.tsx
+++ components/TooltipContainer.tsx
@@ -9,7 +9,7 @@ interface Props {
 }
 export function TooltipContainer({ label, tooltipLabelProps, additionalStyle, children }: Props) {
   return (
-    <div className={`relative group ${additionalStyle}`}>
+    <div className={`relative group w-fit h-fit ${additionalStyle}`}>
       {children}
       <TooltipLabel tooltipLabelProps={tooltipLabelProps}>{label}</TooltipLabel>
     </div>
