fd2ca8aa99a08e2eb08399e7b176dd8f4be3f2ab
DavidSheltonSF <david.faria012@gmail.com>
2025-12-06 21:34:11 -0300

    feat(DashBoardSection): fetch client cases and display the number of cases in progress and closed in a DashBoardCard component

--- components/DashBoardSection.tsx
+++ components/DashBoardSection.tsx
@@ -2,17 +2,36 @@ import { DashboardCard } from './DashboardCard';
 import { DashboardCardInfo } from './DashboardCardInfo';
 import { WithId } from '@/types/WIthId';
 import { UserProps } from '@/types/UserProps';
+import { fetchClientCases } from '@/services/fetchClientCases';
+import { CaseStatusEnum } from '@/types/CaseProps';
 
 interface Props {
   userData: WithId<UserProps>;
 }
 
 export async function DashBoardSection({ userData }: Props) {
+  const inProgressCasesPromise = fetchClientCases(userData.id, {
+    page: 1,
+    limit: 4,
+    status: CaseStatusEnum.em_andamento,
+  });
+
+  const closedCasesPromise = fetchClientCases(userData.id, {
+    page: 1,
+    limit: 4,
+    status: CaseStatusEnum.encerrado,
+  });
+
+  const [inProgressCases, closedCases] = await Promise.all([
+    inProgressCasesPromise,
+    closedCasesPromise,
+  ]);
+
   return (
     <div className="flex gap-[40px]">
       <DashboardCard title="Processos">
-        <DashboardCardInfo name="Em andamento" value={0} />
-        <DashboardCardInfo name="Encerrados" value={0} />
+        <DashboardCardInfo name="Em andamento" value={inProgressCases?.total} />
+        <DashboardCardInfo name="Encerrados" value={closedCases?.total} />
       </DashboardCard>
       <DashboardCard title="Atendimento">
         <DashboardCardInfo name="Data" value="2 de jan. de 2026" />
