cbc055a052afa57816d2787d55238db53389cbd3
DavidSheltonSF <david.faria012@gmail.com>
2025-12-08 18:06:02 -0300

    chore(frontend/services/authenticateUser): change the API url to the real backend and add wrap the function code with try-catch structure

--- frontend/services/authenticateUser.ts
+++ frontend/services/authenticateUser.ts
@@ -1,21 +1,26 @@
 import { API_URL } from '@/config/api';
 
 export async function authenticateUser(formData: FormData): Promise<string | null> {
-  const email = formData.get('email');
-  const password = formData.get('password');
+  try {
+    const email = formData.get('email');
+    const password = formData.get('password');
 
-  const response = await fetch(`${API_URL}/api/auth`, {
-    method: 'POST',
-    body: JSON.stringify({ email, password }),
-  });
+    const response = await fetch(`${API_URL}/auth`, {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
 
-  const data = await response.json();
-  console.log(data);
-
-  if (!data || data.status > 300) {
-    console.log(data.message);
-    return null;
+    if (!response.ok) {
+      const message = await response.text();
+      throw Error(message)
+    }
+      
+    const data = await response.json();
+    return data.token;
+    
+  } catch (error) {
+    console.log(error)
+    throw Error('Something went wrongg')
   }
-
-  return data.token;
 }
