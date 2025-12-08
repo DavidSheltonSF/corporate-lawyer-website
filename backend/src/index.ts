7f3d2dfca3af3b3b25add0e699fab6dfabb8c707
DavidSheltonSF <david.faria012@gmail.com>
2025-12-08 18:01:15 -0300

    feat(backend): add /me and /cases/:clientId routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,11 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import { fakeUserDatabase } from './fakeDatabase/users';
+import { fakeCases } from './fakeDatabase/cases';
+import { filterCasesByTitle } from './helpers/filterCasesByTitle';
+import { filterCasesByProcessNumber } from './helpers/filterCasesByProcessNumber';
+import { getCaseLawyers } from './helpers/getCaseLawyers';
+import { paginate } from './helpers/paginate';
 
 const app = express();
 
@@ -11,15 +16,33 @@ app.use(cors());
 const port = 3080;
 
 app.get('/', (req: Request, res: Response) => {
-  res.status(200).json({
+  res.status(200).send({
     message: 'Everything is working',
+  });
+});
+
+app.get('/me', (req: Request, res: Response) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.send(400).send({
+      status: 401,
+      message: 'Token missing',
+    });
+  }
+
+  const email = token.split('-')[0];
+
+  const user = fakeUserDatabase.find((user) => user.email === email);
+
+  return res.status(200).json({
+    user,
   });
 });
 
 app.post('/auth', (req: Request, res: Response) => {
   try {
     const body = req.body;
-    console.log(body);
 
     if (!body) {
       return res.status(400).send({
@@ -46,6 +69,59 @@ app.post('/auth', (req: Request, res: Response) => {
       message: 'Something went wron in the server side',
     });
   }
+});
+
+app.get('/cases/:clientId', (req: Request, res: Response) => {
+  const { clientId } = req.params;
+  const { status, title, processNumber } = req.query;
+
+  const page = req.query.page || 1;
+  const limit = req.query.limit || 4;
+
+  if (title && processNumber) {
+    return res.status(400).send({
+      status: 400,
+      message: "You must send ONLY 'title' OR 'processNumber', not both.",
+    });
+  }
+
+  let casesByQuery = null;
+
+  const casesByClientId = fakeCases.filter((cas) => cas.clientId === clientId);
+  casesByQuery = casesByClientId.slice();
+
+  if (status) {
+    casesByQuery = casesByQuery.filter((cas) => cas.status === status);
+  }
+
+  if (title) {
+    casesByQuery = filterCasesByTitle(casesByQuery, `${title}`);
+  }
+
+  if (processNumber) {
+    casesByQuery = filterCasesByProcessNumber(casesByQuery, `${processNumber}`);
+  }
+
+  let cases = casesByQuery || casesByClientId;
+  cases = cases.map((cas) => {
+    const lawyers = getCaseLawyers(cas.lawyerIds);
+
+    return { ...cas, lawyers };
+  });
+
+  const pagination = {
+    cases: paginate(cases, Number(page), Number(limit)),
+    page,
+    limit,
+    total: cases.length,
+    totalPages: Math.ceil(cases.length / Number(limit)),
+  };
+
+  const response: any = {
+    status: 200,
+    data: pagination,
+  };
+  return res.status(200).send(response);
 });
 
 app.listen(port, () => {
