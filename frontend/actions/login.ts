1a430ca1e9454d30f75197baa77c7e89068cabd0
DavidSheltonSF <david.faria012@gmail.com>
2025-12-08 19:18:17 -0300

    refactor(actions/login:=: enhance error handling
    - Added try-catch to catch any thrown errors
    - Returns a message if eny error was thrown

--- frontend/actions/login.ts
+++ frontend/actions/login.ts
@@ -5,14 +5,19 @@ import { isString } from '@/lib/isString';
 import { cookies } from 'next/headers';
 
 export async function login(formData: FormData): Promise<{ error: boolean; message: string }> {
-  const token = await authenticateUser(formData);
+  try {
+    const token = await authenticateUser(formData);
 
-  if (!token || !isString(token)) {
-    return { error: true, message: 'Email or password is incorrect' };
+    if (!token || !isString(token)) {
+      return { error: true, message: 'Email or password is incorrect' };
+    }
+
+    const cookiesStore = await cookies();
+    cookiesStore.set('authentication', token, { httpOnly: true });
+
+    return { error: false, message: '' };
+  } catch (error) {
+    console.log(error);
+    return { error: true, message: 'Something went wrong in the server' };
   }
-
-  const cookiesStore = await cookies();
-  cookiesStore.set('authentication', token, { httpOnly: true });
-
-  return { error: false, message: '' };
 }
