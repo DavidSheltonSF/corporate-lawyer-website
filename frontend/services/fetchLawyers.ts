00c718160b6066b8dacbd4c3686babfdbb240705
DavidSheltonSF <david.faria012@gmail.com>
2025-12-08 18:56:04 -0300

    chore: delete fetchLawyers service, since it is not used anymore

--- frontend/services/fetchLawyers.ts
+++
@@ -1,9 +0,0 @@
-import { WithId } from '@/types/WithId';
-import { UserProps } from '@/types/UserProps';
-import { fetchUserById } from './fetchUserById';
-
-export async function fetchLawyers(ids: string[]): Promise<WithId<UserProps>[]> {
-  const promises = ids.map((id) => fetchUserById(id));
-
-  return Promise.all(promises);
-}
