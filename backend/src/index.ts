0e2d6c22387c7ce605c1e0dde234eb2ac9b3ee92
DavidSheltonSF <david.faria012@gmail.com>
2025-12-07 22:08:47 -0300

    feat(backend): add /auth endpoint
    Currently, this endpoinds makes a fake authentication

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
+import { fakeUserDatabase } from './fakeDatabase/users';
 
 const app = express();
 
@@ -13,6 +14,38 @@ app.get('/', (req: Request, res: Response) => {
   res.status(200).json({
     message: 'Everything is working',
   });
+});
+
+app.post('/auth', (req: Request, res: Response) => {
+  try {
+    const body = req.body;
+    console.log(body);
+
+    if (!body) {
+      return res.status(400).send({
+        message: 'Body request is missing',
+      });
+    }
+
+    const { email, password } = body;
+    const user = fakeUserDatabase.find((user) => user.email === email);
+
+    if (!user || user.password !== password) {
+      return res.status(401).send({
+        message: 'Invalid email or password',
+      });
+    }
+
+    const fakeToken = email + '-token';
+    return res.status(200).send({
+      token: fakeToken,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      message: 'Something went wron in the server side',
+    });
+  }
 });
 
 app.listen(port, () => {
