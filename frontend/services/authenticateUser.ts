fcd01cb6eec0e37a3872d821fa59e65b03c8d80b
DavidSheltonSF <david.faria012@gmail.com>
2025-12-08 19:17:17 -0300

    refactor(authenticateUser): enhance error handling
    - Removed try-catch that was wrapping the function's source code
    -  Made function returns null if the API returned unnauthorized (401)
    - Made function throws error if the response is not ok and the status is not 401

--- frontend/services/authenticateUser.ts
+++ frontend/services/authenticateUser.ts
@@ -1,26 +1,25 @@
 import { API_URL } from '@/config/api';
 
 export async function authenticateUser(formData: FormData): Promise<string | null> {
-  try {
-    const email = formData.get('email');
-    const password = formData.get('password');
+  const email = formData.get('email');
+  const password = formData.get('password');
 
-    const response = await fetch(`${API_URL}/auth`, {
-      method: 'POST',
-      body: JSON.stringify({ email, password }),
-      headers: { 'Content-Type': 'application/json' },
-    });
+  const response = await fetch(`${API_URL}/auth`, {
+    method: 'POST',
+    body: JSON.stringify({ email, password }),
+    headers: { 'Content-Type': 'application/json' },
+  });
 
-    if (!response.ok) {
-      const message = await response.text();
-      throw Error(message)
-    }
-      
-    const data = await response.json();
-    return data.token;
-    
-  } catch (error) {
-    console.log(error)
-    throw Error('Something went wrongg')
+  if (response.status === 401) {
+    return null;
   }
+
+  if (!response.ok) {
+    const message = await response.text();
+    throw new Error(message);
+  }
+
+  const json = await response.json();
+
+  return json.token;
 }
