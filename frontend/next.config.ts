5136c94ee46e484d92c799688e83dab5a87b63c8
DavidSheltonSF <david.faria012@gmail.com>
2025-12-11 10:37:05 -0300

    chore(next): remove rewrite configs

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,14 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   // output: 'export'
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:3080/api/:path*',
-      },
-    ];
-  },
 };
 
 export default nextConfig;
