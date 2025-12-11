3acba2e7684cca75ee1d69aa68b144457df0f8ca
DavidSheltonSF <david.faria012@gmail.com>
2025-12-11 13:12:40 -0300

    chore(clientPage): use logo-loading.gif int the loading.tsx file

--- frontend/app/clientPage/loading.tsx
+++ frontend/app/clientPage/loading.tsx
@@ -2,7 +2,7 @@ export default function loading() {
   return (
     <div className="bg-color-primary-light h-[100vh] flex justify-center items-center">
       <div className="size-[90px] overflow-hidden rounded-full bg-white">
-        <video autoPlay muted loop playsInline className="size-full object-cover" src="logo-loading.mp4"></video>
+        <img src="logo-loading.gif" alt="" />
       </div>
     </div>
   );
