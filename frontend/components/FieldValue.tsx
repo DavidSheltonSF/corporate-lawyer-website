b4ce11efc676e7604995cbbfc580cb078f02ce4c
DavidSheltonSF <david.faria012@gmail.com>
2025-12-13 19:43:28 -0300

    feat(components): add FieldValue with props field, value and gap.
    Displays a pair of field: value

---
+++ frontend/components/FieldValue.tsx
@@ -0,0 +1,19 @@
+interface Props {
+  field:  string;
+  value: string;
+  gap?: string;
+}
+
+export function FieldValue({ field, value, gap="8px" }: Props) {
+  return (
+    <span
+      style={{
+        display: 'flex',
+        gap,
+      }}
+    >
+      <p className="font-bold">{field}</p>
+      <p>{value}</p>
+    </span>
+  );
+}
