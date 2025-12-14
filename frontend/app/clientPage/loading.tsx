f066a83b140faa689e6a187424801e5479596871
DavidSheltonSF <david.faria012@gmail.com>
2025-12-13 23:11:30 -0300

    fix(clientPage/loading.tsx): use absolute path to get the icons to avoid route conflicts

--- frontend/app/clientPage/loading.tsx
+++ frontend/app/clientPage/loading.tsx
@@ -2,7 +2,7 @@ export default function loading() {
   return (
     <div className="bg-color-primary-light h-[100vh] flex justify-center items-center">
       <div className="size-[90px] overflow-hidden rounded-full bg-white">
-        <img src="logo-loading.gif" alt="" />
+        <img src="/logo-loading.gif" alt="" />
       </div>
     </div>
   );
