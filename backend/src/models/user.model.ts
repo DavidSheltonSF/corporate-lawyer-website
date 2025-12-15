57f458d53e0fb818c72caa1432b2fb2bd48712b3
DavidSheltonSF <david.faria012@gmail.com>
2025-12-14 23:26:32 -0300

    refactor(UserSchema): enhance data consistency
    - Set trim: true in firstName, lastName, email and cpf
    - Set unique:  in email and cpf
    - Set lowercase in email

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,14 +1,15 @@
 import { Schema, model, Document } from 'mongoose';
 import { User } from '../types/User';
+import bcrypt from 'bcrypt';
 
 interface IUserModel extends User, Document {}
 
 const UserSchema = new Schema<IUserModel>(
   {
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    email: { type: String, required: true },
-    cpf: { type: String, required: true },
+    firstName: { type: String, required: true, trim: true },
+    lastName: { type: String, required: true, trim: true },
+    email: { type: String, required: true, unique: true, lowercase: true, trim: true },
+    cpf: { type: String, required: true, unique: true, trim: true },
     password: { type: String, required: true },
     role: {
       type: String,
