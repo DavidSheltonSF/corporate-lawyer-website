d2b5c03a14d4c212baae50053363857f3ad20355
DavidSheltonSF <david.faria012@gmail.com>
2025-12-15 20:43:36 -0300

    feat(components): add a black transparent background behind PrimaryModalWindow tho smothly make the elements behind it darker  and reduce the user distraction

--- frontend/components/PrimaryModalWindow.tsx
+++ frontend/components/PrimaryModalWindow.tsx
@@ -9,15 +9,17 @@ export function PrimaryModalWindow({ children }: { children: React.ReactNode }) 
   }
 
   return (
-    <div className="flex pt-[80px] fixed z-99999 left-1/2 translate-x-[-50%] top-[80px] bg-color-primary w-[960px] h-[80vh] rounded-xl overflow-hidden">
-      <div className="bg-color-white size-full">{children}</div>
+    <div className="fixed top-0 h-full w-full bg-black/20">
+      <div className="flex pt-[80px] fixed z-99999 left-1/2 translate-x-[-50%] top-[80px] bg-color-primary w-[960px] h-[80vh] rounded-xl overflow-hidden">
+        <div className="bg-color-white size-full">{children}</div>
 
-      <button
-        className="absolute right-[16px] top-[40px] translate-y-[-50%] size-[40px] cursor-pointer hover:bg-white/20 transition-[background-color] duration-300 rounded-lg"
-        onClick={closeModal}
-      >
-        <img className="size-full" src="/icons/close.svg" alt="" />
-      </button>
+        <button
+          className="absolute right-[16px] top-[40px] translate-y-[-50%] size-[40px] cursor-pointer hover:bg-white/20 transition-[background-color] duration-300 rounded-lg"
+          onClick={closeModal}
+        >
+          <img className="size-full" src="/icons/close.svg" alt="" />
+        </button>
+      </div>
     </div>
   );
 }
