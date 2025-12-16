f4808ddd5a97cbaf2ec7675945058ed64b5a33f0
DavidSheltonSF <david.faria012@gmail.com>
2025-12-16 17:06:50 -0300

    refactor(backend/services): make UserService methods return UserResponse

--- backend/src/services/user.service.ts
+++ backend/src/services/user.service.ts
@@ -1,17 +1,21 @@
 import { UserModel } from '../models/user.model';
 import { User } from '../types/User';
+import { UserResponse } from '../types/UserResponse';
 
 export class UserService {
-  async create(data: User): Promise<Omit<User, 'password'>> {
+  async create(data: User): Promise<UserResponse> {
     try {
       const createdUser = await UserModel.create(data);
 
       return {
+        id: createdUser._id.toString(),
         firstName: createdUser.firstName,
         lastName: createdUser.lastName,
         cpf: createdUser.cpf,
         email: createdUser.email,
         role: createdUser.role,
+        createdAt: createdUser.createdAt,
+        updatedAt: createdUser.updatedAt,
       };
     } catch (error: any) {
       if (error.code === 11000) {
@@ -22,32 +26,58 @@ export class UserService {
     }
   }
 
-  async findAll(): Promise<Omit<User, 'password'>[]> {
-    return await UserModel.find(
-      {},
-      {
-        password: 0,
-      }
-    ).lean();
+  async findAll(): Promise<UserResponse[]> {
+    const users = await UserModel.find({});
+
+    return users.map((user) => {
+      return {
+        id: user._id.toString(),
+        firstName: user.firstName,
+        lastName: user.lastName,
+        cpf: user.cpf,
+        email: user.email,
+        role: user.role,
+        createdAt: user.createdAt,
+        updatedAt: user.updatedAt,
+      };
+    });
   }
 
-  async findById(id: string): Promise<Omit<User, 'password'>> {
+  async findById(id: string): Promise<UserResponse> {
     const foundUser = await UserModel.findById(id, { password: 0 }).lean();
 
     if (!foundUser) {
       throw Error('User not found');
     }
 
-    return foundUser;
+    return {
+      id: foundUser._id.toString(),
+      firstName: foundUser.firstName,
+      lastName: foundUser.lastName,
+      cpf: foundUser.cpf,
+      email: foundUser.email,
+      role: foundUser.role,
+      createdAt: foundUser.createdAt,
+      updatedAt: foundUser.updatedAt,
+    };
   }
 
-  async findByEmail(email: string): Promise<Omit<User, 'password'>> {
+  async findByEmail(email: string): Promise<UserResponse> {
     const foundUser = await UserModel.findOne({ email }, { password: 0 }).lean();
 
     if (!foundUser) {
       throw Error('User not found');
     }
 
-    return foundUser;
+    return {
+      id: foundUser._id.toString(),
+      firstName: foundUser.firstName,
+      lastName: foundUser.lastName,
+      cpf: foundUser.cpf,
+      email: foundUser.email,
+      role: foundUser.role,
+      createdAt: foundUser.createdAt,
+      updatedAt: foundUser.updatedAt,
+    };
   }
 }
