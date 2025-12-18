3a7ec452d9dd04709a0bce69d2a59221a3c361f8
DavidSheltonSF <david.faria012@gmail.com>
2025-12-18 20:39:30 -0300

    feat(backend/services): add CaseService with create, findAll, findById and findByClientId methods

---
+++ backend/src/services/case.service.test.ts
@@ -0,0 +1,186 @@
+import { Types } from 'mongoose';
+import { DatabaseConnector } from '../config/database';
+import { CaseModel } from '../models/case.model';
+import { Case } from '../types/Case';
+import { CaseService } from './case.service';
+import { config } from 'dotenv';
+import { CaseStatusEnum } from '../types/CaseStatusEnum';
+
+config();
+
+jest.setTimeout(999999);
+
+describe('Test CaseService', () => {
+  beforeAll(async () => {
+    await DatabaseConnector.connect();
+  });
+
+  beforeEach(async () => {
+    await CaseModel.deleteMany({});
+  });
+
+  afterAll(async () => {
+    await DatabaseConnector.disconnect();
+  });
+
+  function makeSut() {
+    const caseService = new CaseService();
+
+    return {
+      caseService,
+    };
+  }
+
+  test('should create a new case', async () => {
+    const { caseService } = makeSut();
+
+    const newCase: Case = {
+      client: Types.ObjectId.createFromTime(511),
+      lawyers: [Types.ObjectId.createFromTime(55555)],
+      processNumber: '261514514584615648',
+      title: 'Process Title',
+      court: 'STJ',
+      courtDivision: 'Vara Cívil',
+      description: 'Case description',
+      status: CaseStatusEnum.em_andamento,
+    };
+
+    const createdCase = await caseService.create(newCase);
+    const { createdAt, updatedAt, ...casesWithoutTimeStamps } = createdCase;
+
+    expect(newCase).toMatchObject(casesWithoutTimeStamps);
+  });
+
+  test('should find all cases', async () => {
+    const { caseService } = makeSut();
+
+    const newCases: Case[] = [
+      {
+        client: Types.ObjectId.createFromTime(511),
+        lawyers: [Types.ObjectId.createFromTime(55555)],
+        processNumber: '261514514584615648',
+        title: 'Process Title',
+        court: 'STJ',
+        courtDivision: 'Vara Cívil',
+        description: 'Case description',
+        status: CaseStatusEnum.em_andamento,
+      },
+      {
+        client: Types.ObjectId.createFromTime(511),
+        lawyers: [Types.ObjectId.createFromTime(8801555)],
+        processNumber: '11111451458777648',
+        title: 'Process Title',
+        court: 'STJ',
+        courtDivision: 'Vara Cívil',
+        description: 'Case description',
+        status: CaseStatusEnum.encerrado,
+      },
+    ];
+
+    await CaseModel.create(newCases);
+
+    const foundCases = await caseService.findAll();
+    const casesWithoutTimeStamps = foundCases.map((cas) => {
+      const { createdAt, updatedAt, ...casWithoutTime } = cas;
+      return casWithoutTime;
+    });
+
+    expect(newCases).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining(casesWithoutTimeStamps[0]),
+        expect.objectContaining(casesWithoutTimeStamps[1]),
+      ])
+    );
+  });
+
+  test('should find case by id', async () => {
+    const { caseService } = makeSut();
+
+    const newCase1 = {
+      client: Types.ObjectId.createFromTime(511),
+      lawyers: [Types.ObjectId.createFromTime(55555)],
+      processNumber: '261514514584615648',
+      title: 'Process Title',
+      court: 'STJ',
+      courtDivision: 'Vara Cívil',
+      description: 'Case description',
+      status: CaseStatusEnum.em_andamento,
+    };
+
+    const newCase2 = {
+      client: Types.ObjectId.createFromTime(511),
+      lawyers: [Types.ObjectId.createFromTime(8801555)],
+      processNumber: '11111451458777648',
+      title: 'Process Title',
+      court: 'STJ',
+      courtDivision: 'Vara Cívil',
+      description: 'Case description',
+      status: CaseStatusEnum.encerrado,
+    };
+
+    const createdCase1 = await CaseModel.create(newCase1);
+    const createdCase2 = await CaseModel.create(newCase2);
+
+    const foundCase = await caseService.findById(createdCase1._id.toString());
+    if (!foundCase) {
+      throw Error('No case was found in FindById test');
+    }
+    const { createdAt, updatedAt, ...caseWithoutTimeStamps } = foundCase;
+
+    expect(newCase1).toMatchObject(caseWithoutTimeStamps);
+  });
+
+  test('should find case by client id', async () => {
+    const { caseService } = makeSut();
+
+    const clientId = Types.ObjectId.createFromTime(511);
+    const newCases: Case[] = [
+      {
+        client: clientId,
+        lawyers: [Types.ObjectId.createFromTime(55555)],
+        processNumber: '261514514584615648',
+        title: 'Process1',
+        court: 'STJ',
+        courtDivision: 'Vara Cívil',
+        description: 'Case description',
+        status: CaseStatusEnum.em_andamento,
+      },
+      {
+        client: clientId,
+        lawyers: [Types.ObjectId.createFromTime(8801555)],
+        processNumber: '11111451458777648',
+        title: 'Process2',
+        court: 'STJ',
+        courtDivision: 'Vara Cívil',
+        description: 'Case description',
+        status: CaseStatusEnum.encerrado,
+      },
+      {
+        client: Types.ObjectId.createFromTime(8555),
+        lawyers: [Types.ObjectId.createFromTime(8801555)],
+        processNumber: '11111451478899648',
+        title: 'Process3',
+        court: 'STJ',
+        courtDivision: 'Vara Cívil',
+        description: 'Case description',
+        status: CaseStatusEnum.encerrado,
+      },
+    ];
+
+    await CaseModel.create(newCases);
+
+    const foundCases = await caseService.findByClientId(clientId.toString());
+
+    const casesWithoutTimeStamps = foundCases.map((cas) => {
+      const { createdAt, updatedAt, ...casWithoutTime } = cas;
+      return casWithoutTime;
+    });
+
+    expect(newCases).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining(casesWithoutTimeStamps[0]),
+        expect.objectContaining(casesWithoutTimeStamps[1]),
+      ])
+    );
+  });
+});
---
+++ backend/src/services/case.service.ts
@@ -0,0 +1,96 @@
+import { CaseModel } from '../models/case.model';
+import { Case } from '../types/Case';
+import { CaseResponse } from '../types/CaseResponse';
+
+export class CaseService {
+  async create(data: Case): Promise<CaseResponse> {
+    try {
+      const createdCase = await CaseModel.create(data);
+
+      return {
+        client: createdCase.client,
+        lawyers: createdCase.lawyers,
+        processNumber: createdCase.processNumber,
+        title: createdCase.title,
+        description: createdCase.description,
+        court: createdCase.court,
+        courtDivision: createdCase.courtDivision,
+        status: createdCase.status,
+        createdAt: createdCase.createdAt,
+        updatedAt: createdCase.updatedAt,
+      };
+    } catch (error: any) {
+      if (error.code === 11000) {
+        throw Error(`A case with processNumber ${data.processNumber} already exists`);
+      }
+      throw error;
+    }
+  }
+
+  async findAll(): Promise<CaseResponse[]> {
+    const foundCases = await CaseModel.find({}).lean();
+
+    return foundCases.map((cas) => {
+      return {
+        client: cas.client,
+        lawyers: cas.lawyers,
+        title: cas.title,
+        processNumber: cas.processNumber,
+        court: cas.court,
+        courtDivision: cas.courtDivision,
+        description: cas.description,
+        status: cas.status,
+        createdAt: cas.createdAt,
+        updatedAt: cas.updatedAt,
+      };
+    });
+  }
+
+  async findById(id: string): Promise<CaseResponse | null> {
+    try {
+      const foundCase = await CaseModel.findById(id);
+
+      if (!foundCase) {
+        throw Error('Case not found');
+      }
+
+      return {
+        client: foundCase.client,
+        lawyers: foundCase.lawyers,
+        title: foundCase.title,
+        processNumber: foundCase.processNumber,
+        court: foundCase.court,
+        courtDivision: foundCase.courtDivision,
+        description: foundCase.description,
+        status: foundCase.status,
+        createdAt: foundCase.createdAt,
+        updatedAt: foundCase.updatedAt,
+      };
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async findByClientId(id: string): Promise<CaseResponse[]> {
+    try {
+      const foundCases = await CaseModel.find({ client: id });
+
+      return foundCases.map((cas) => {
+        return {
+          client: cas.client,
+          lawyers: cas.lawyers,
+          title: cas.title,
+          processNumber: cas.processNumber,
+          court: cas.court,
+          courtDivision: cas.courtDivision,
+          description: cas.description,
+          status: cas.status,
+          createdAt: cas.createdAt,
+          updatedAt: cas.updatedAt,
+        };
+      });
+    } catch (error) {
+      throw error;
+    }
+  }
+}
