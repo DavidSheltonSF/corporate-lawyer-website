a3fa681b0e312f88cf19db1518cd0b2bd17f05c2
DavidSheltonSF <david.faria012@gmail.com>
2025-12-19 16:07:32 -0300

    feat(backend/types): add CaseQuery with limit, page, title, processNumber, status and client properties

---
+++ backend/src/types/CaseQuery.ts
@@ -0,0 +1,8 @@
+export interface CaseQuery {
+  limit?: number | undefined;
+  page?: number | undefined;
+  title?: string | undefined;
+  processNumber?: string | undefined;
+  status?: string | undefined;
+  client?: string | undefined;
+}
