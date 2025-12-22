dca06f1c60732f42293cc1894e76c04ecb9eca3c
DavidSheltonSF <david.faria012@gmail.com>
2025-12-22 15:13:20 -0300

    feat(frontend/component): add fade-out animation when closing the modal in PrimaryModalWindow

--- frontend/components/PrimaryModalWindow.tsx
+++ frontend/components/PrimaryModalWindow.tsx
@@ -1,15 +1,19 @@
 'use client';
 import { useRouter } from 'next/navigation';
+import { useRef } from 'react';
 
 export function PrimaryModalWindow({ children }: { children: React.ReactNode }) {
   const router = useRouter();
+  const windowRef = useRef<HTMLDivElement | null>(null);
 
   function closeModal() {
-    router.back();
+    const modalWindow = document.querySelector('.modalWindow');
+    modalWindow?.classList.add('fade-out-animation-fast');
+    setTimeout(() => router.back(), 200);
   }
 
   return (
-    <div className="fixed top-0 h-full w-full bg-black/20">
+    <div className="modalWindow fixed top-0 h-full w-full bg-black/20 fade-in-animation-fast">
       <div className="flex pt-[80px] fixed z-99999 left-1/2 translate-x-[-50%] top-[80px] bg-color-primary w-[960px] h-[80vh] rounded-xl overflow-hidden">
         <div className="bg-color-white size-full">{children}</div>
 
