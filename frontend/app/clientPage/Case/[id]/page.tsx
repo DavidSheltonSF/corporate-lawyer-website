7a12267a24396d58d8c26144536ed80180036d7e
DavidSheltonSF <david.faria012@gmail.com>
2025-12-22 09:46:02 -0300

    refactor(case modal): add conditional rendering to not render anything if the values come undefined

--- frontend/app/clientPage/Case/[id]/page.tsx
+++ frontend/app/clientPage/Case/[id]/page.tsx
@@ -18,16 +18,19 @@ export default async function CasePage({ params }: { params: Promise<{ id: strin
       </header>
       <main className="text-xl">
         <div className="flex flex-col gap-[8px] pl-[24px] py-[24px]">
-          <FieldValue field="nº:" value={processNumber} />
-          <FieldValue field="cliente:" value={`${client?.firstName} ${client?.lastName}`} />
+          <FieldValue field="nº:" value={processNumber || ''} />
+          <FieldValue
+            field="cliente:"
+            value={`${client?.firstName || ''} ${client?.lastName || ''}`}
+          />
           <FieldValue field="advogados:" value={formatStringList(lawyersNames)} />
-          <FieldValue field="status:" value={status} />
-          <FieldValue field="Tribunal:" value={court} />
-          <FieldValue field="Vara:" value={courtDivision} />
+          <FieldValue field="status:" value={status || ''} />
+          <FieldValue field="Tribunal:" value={court || ''} />
+          <FieldValue field="Vara:" value={courtDivision || ''} />
         </div>
         <div className="flex flex-col gap-[8px] px-[24px] py-[24px] border-t border-black/30">
           <h1 className="font-bold text-3xl">Resumo</h1>
-          <p>{description}</p>
+          <p>{description || ''}</p>
         </div>
         <div className="flex items-center h-[56px] bg-color-primary border-t pl-[24px]">
           <h1 className=" text-color-white text-3xl">Documentação</h1>
