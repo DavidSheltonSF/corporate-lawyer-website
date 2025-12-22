00f88e549bc6eed49a962a7ac43bf99d35a936d1
DavidSheltonSF <david.faria012@gmail.com>
2025-12-22 14:53:10 -0300

    refactor(frontend/services): make fetchCaseById call notFound if the response status is 404

--- frontend/services/fetchCaseById.ts
+++ frontend/services/fetchCaseById.ts
@@ -1,14 +1,20 @@
 import { API_URL } from '@/config/api';
 import { CaseWithRelations } from '@/types/CaseWithRelations';
 import { WithId } from '@/types/WithId';
+import { notFound } from 'next/navigation';
 
 export async function fetchCaseById(
   id: string,
   populateFields?: string[]
 ): Promise<WithId<CaseWithRelations>> {
   const response = await fetch(
-    `${API_URL}/cases/${id}?populate=${populateFields ? populateFields?.join(',') : ''}`
+    `${API_URL}/cases/507f1f77bcf86cd799439011
+?populate=${populateFields ? populateFields?.join(',') : ''}`
   );
+
+  if(response.status === 404) {
+    return notFound()
+  }
 
   if (!response.ok) {
     const message = await response.text();
