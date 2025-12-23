c14511356b16c5eb3ec6c32c1d61e73350c64ea7
DavidSheltonSF <david.faria012@gmail.com>
2025-12-22 21:05:51 -0300

    feat(frontend/componets): add DocumentTable

---
+++ frontend/components/DocumentTable.tsx
@@ -0,0 +1,36 @@
+import { CaseDocumentProps } from '@/types/CaseDocumentProps';
+import Link from 'next/link';
+
+interface Props {
+  documents: CaseDocumentProps[];
+}
+
+export function DocumentTable({ documents }: Props) {
+  return (
+    <div className="table w-[88%] h-fit">
+      <div className="header">
+        <div>Name</div>
+        <div>Upload</div>
+        <div></div>
+      </div>
+
+      {documents.map((document, index) => {
+        return (
+          <div key={index} className="row">
+            <div className="overflow-wrap">{document.name}</div>
+            <div>{`${document.uploadedAt.toUTCString()} - ${document.uploadedBy.firstName} ${
+              document.uploadedBy.lastName
+            }`}</div>
+            <div className="flex justify-center items-center">
+              <Link href={document.url} target='_blank'>
+                <button className="bg-color-primary text-color-white px-[16px] py-[4px] rounded-sm hover:brightness-130 cursor-pointer">
+                  Download
+                </button>
+              </Link>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
