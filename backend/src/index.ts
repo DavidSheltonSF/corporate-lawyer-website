1331d7615815faf37d73501df05a35a4c0f997d9
DavidSheltonSF <david.faria012@gmail.com>
2025-12-24 17:08:50 -0300

    chore(backend): allow multiple origins for tests

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,9 +15,18 @@ const app = express();
 
 app.use(express.json());
 
+const allowedOrigins = [process.env.FRONTEND, 'http://192.168.0.118:3001'];
 app.use(
   cors({
-    origin: process.env.FRONTEND,
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error('Not allowed by cores'));
+    },
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   })
