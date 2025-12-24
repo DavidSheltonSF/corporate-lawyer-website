9d3a8395c6cd3f6c86373f8081ec9965de5eea71
DavidSheltonSF <david.faria012@gmail.com>
2025-12-23 22:27:33 -0300

    feat(backend/types) add HttpCode and HttpResponse

---
+++ backend/src/types/HttpCode.ts
@@ -0,0 +1,7 @@
+export enum HttpCode {
+  ok = 'OK',
+  not_found = 'NOT_FOUND',
+  unouthorized = 'UNAUTHORIZED',
+  bad_request = 'BAD_REQUEST',
+  server_error = 'SERVER_ERROR',
+}
---
+++ backend/src/types/HttpResponse.ts
@@ -0,0 +1,7 @@
+import { HttpCode } from "./HttpCode"
+
+export interface HttpResponse {
+  data: any
+  message?: string
+  code: HttpCode
+}
