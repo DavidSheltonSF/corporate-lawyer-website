75910691bac1185346515e4e4bb1fea1094e09e7
DavidSheltonSF <david.faria012@gmail.com>
2025-12-24 00:02:58 -0300

    perf(clentPage/loading): use logo-loading-webp, and Image next component for better UI performance

--- frontend/app/clientPage/loading.tsx
+++ frontend/app/clientPage/loading.tsx
@@ -1,8 +1,10 @@
+import Image from "next/image";
+
 export default function loading() {
   return (
     <div className="bg-color-primary-light h-[100vh] flex justify-center items-center">
       <div className="size-[90px] overflow-hidden rounded-full bg-white">
-        <img src="/logo-loading.gif" alt="" />
+        <Image src="/logo-loading.webp" priority alt="Icone de carregamento" />
       </div>
     </div>
   );
