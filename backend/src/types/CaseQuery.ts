df535de052402367a11ed5388e634d385af7d732
DavidSheltonSF <david.faria012@gmail.com>
2025-12-26 18:36:11 -0300

    refactor(backend/types): remove title and processNumber from query and add query for simplicity

--- backend/src/types/CaseQuery.ts
+++ backend/src/types/CaseQuery.ts
@@ -1,8 +1,7 @@
 export interface CaseQuery {
   limit?: number | undefined;
   page?: number | undefined;
-  title?: string | undefined;
-  processNumber?: string | undefined;
+  query?: string | undefined;
   status?: string | undefined;
   client?: string | undefined;
 }
