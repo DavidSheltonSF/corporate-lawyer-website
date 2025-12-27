86a2aa9caa1018654bcabbbed6dc4195a2dfce23
DavidSheltonSF <david.faria012@gmail.com>
2025-12-27 19:30:24 -0300

    refactor(frontend/components): enhance PrimaryModalWindow responsiveness
    - Make the close button a non-absolute element and wrap it with a flex div
    
    - Make the modal main div width responsive

--- frontend/components/PrimaryModalWindow.tsx
+++ frontend/components/PrimaryModalWindow.tsx
@@ -27,16 +27,17 @@ export function PrimaryModalWindow({ children }: { children: React.ReactNode }) 
     <div className="fixed top-0 h-full w-full bg-black/20 fade-in-animation-fast">
       <div
         ref={windowRef}
-        className="modalWindow flex pt-[80px] fixed z-99999 left-1/2 translate-x-[-50%] top-[80px] bg-color-primary w-[960px] h-[85vh] rounded-xl overflow-hidden"
+        className="modalWindow flex flex-col  min-lg:pt-[80px] fixed z-99999 left-1/2 translate-x-[-50%] top-[8px]  min-lg:top-[80px] bg-color-primary w-[95%] min-lg:w-[960px] h-[85vh] rounded-xl overflow-hideden"
       >
-        <div className="bg-color-white size-full">{children}</div>
-
-        <button
-          className="absolute right-[16px] top-[40px] translate-y-[-50%] size-[40px] cursor-pointer hover:bg-white/20 transition-[background-color] duration-300 rounded-lg"
-          onClick={closeModal}
-        >
-          <img className="size-full" src="/icons/close.svg" alt="" />
-        </button>
+        <div className="flex justify-end items-center h-[56px] pr-[8px]">
+          <button
+            className="size-[40px] cursor-pointer hover:bg-white/20 transition-[background-color] duration-300 rounded-lg"
+            onClick={closeModal}
+          >
+            <img className="size-full" src="/icons/close.svg" alt="" />
+          </button>
+        </div>
+        <div className="bg-color-white size-full overflow-auto">{children}</div>
       </div>
     </div>
   );
