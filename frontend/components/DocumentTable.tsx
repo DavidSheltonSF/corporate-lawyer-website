5eb3f6b5de45c581b79adfe827451d81adac950f
DavidSheltonSF <david.faria012@gmail.com>
2025-12-28 17:04:53 -0300

    chore(frontend/components): enhance DocumentTable responsiveness
    - Made  Upload column to be hidden on small screens
    
    - Added a mobile download button to be displayed only on small screens

--- frontend/components/DocumentTable.tsx
+++ frontend/components/DocumentTable.tsx
@@ -1,4 +1,5 @@
 import { CaseDocumentPopulated } from '@/types/CaseDocumentPopulated';
+import Image from 'next/image';
 import Link from 'next/link';
 
 interface Props {
@@ -10,7 +11,7 @@ export function DocumentTable({ documents }: Props) {
     <div className="table w-[88%] h-fit">
       <div className="header">
         <div>Name</div>
-        <div>Upload</div>
+        <div className='hidden min-lg:blok'>Upload</div>
         <div></div>
       </div>
 
@@ -18,13 +19,16 @@ export function DocumentTable({ documents }: Props) {
         return (
           <div key={index} className="row">
             <div className="overflow-wrap">{document.name}</div>
-            <div>{`${document.uploadedAt.toUTCString()} - ${document.uploadedBy.firstName} ${
+            <div className='hidden min-lg:block'>{`${document.uploadedAt.toUTCString()} - ${document.uploadedBy.firstName} ${
               document.uploadedBy.lastName
             }`}</div>
             <div className="flex justify-center items-center">
               <Link href={document.url} target='_blank'>
-                <button className="bg-color-primary text-color-white px-[16px] py-[4px] rounded-sm hover:brightness-130 cursor-pointer">
+                <button className="hidden min-lg:block bg-color-primary text-color-white px-[16px] py-[4px] rounded-sm hover:brightness-130 cursor-pointer">
                   Download
+                </button>
+                <button className='flex relative justify-center items-center min-lg:hidden bg-color-primary rounded-sm hover:brightness-130 cursor-pointer size-[40px]'>
+                  <Image src="/icons/download.svg" fill sizes='32px' alt=''></Image>
                 </button>
               </Link>
             </div>
