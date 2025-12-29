7d71d3fc86a35c900ac245953e497cd94fedd330
DavidSheltonSF <david.faria012@gmail.com>
2025-12-29 16:48:54 -0300

    refactor(frontend/component): make DashBoardCard a client component and allow it selects a section defined by sectionIndex
    - Added sectionIndex property
    - Added onclick event with a condition to set the selected section if sectionIndex is provided
    - Added a condition to make DashboardCard a cursor-pointer element if sectionIndex is provided

--- frontend/components/DashboardCard.tsx
+++ frontend/components/DashboardCard.tsx
@@ -1,13 +1,30 @@
-import { ReactNode } from 'react';
+'use client';
+
+import { DynamicSectionContext, DynamicSectionContextType } from '@/contexts/DynamicSectionContext';
+import { ReactNode, useContext } from 'react';
 
 interface Props {
   title: string;
   children: ReactNode;
+  sectionIndex?: number;
 }
 
-export function DashboardCard({ title, children }: Props) {
+export function DashboardCard({ title, sectionIndex, children }: Props) {
+  const context = useContext<DynamicSectionContextType | undefined>(DynamicSectionContext);
+
+  if (!context) {
+    throw Error('DynamicSection context was not provided');
+  }
+
+  const { setSelectedSection } = context;
+
   return (
-    <div className="flex flex-col bg-color-primary-light h-[160px] rounded-xl py-[16px] flex-[0_1_300px]">
+    <div
+      className={`flex flex-col bg-color-primary-light h-[160px] rounded-xl py-[16px] flex-[0_1_300px] ${
+        sectionIndex !== undefined && 'cursor-pointer'
+      }`}
+      onClick={() => sectionIndex !== undefined && setSelectedSection(sectionIndex)}
+    >
       <h1 className="ml-[24px] font-bold text-[1.5rem]">{title}</h1>
       <div className="flex flex-col gap-[16px] bg-color-primary h-[90%] px-[24px] py-[8px]">
         {children}
