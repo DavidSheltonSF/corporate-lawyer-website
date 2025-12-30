3c68ccbd7846c4841cefd6cdf27e088ba3b02ab3
DavidSheltonSF <david.faria012@gmail.com>
2025-12-30 16:59:24 -0300

    refactor(frontend/actions): simplify login by removing its error handling and making it return nothing, since errors will be handled by the component that calls it

--- frontend/actions/login.ts
+++ frontend/actions/login.ts
@@ -1,23 +1,9 @@
 'use server';
-
 import { authenticateUser } from '@/services/authenticateUser';
-import { isString } from '@/lib/isString';
 import { cookies } from 'next/headers';
 
-export async function login(formData: FormData): Promise<{ error: boolean; message: string }> {
-  try {
-    const token = await authenticateUser(formData);
-
-    if (!token || !isString(token)) {
-      return { error: true, message: 'Email or password is incorrect' };
-    }
-
-    const cookiesStore = await cookies();
-    cookiesStore.set('authentication', token, { httpOnly: true });
-
-    return { error: false, message: '' };
-  } catch (error) {
-    console.log(error);
-    return { error: true, message: 'Something went wrong in the server' };
-  }
+export async function login(formData: FormData): Promise<void> {
+  const token = await authenticateUser(formData);
+  const cookiesStore = await cookies();
+  cookiesStore.set('authentication', token, { httpOnly: true });
 }
