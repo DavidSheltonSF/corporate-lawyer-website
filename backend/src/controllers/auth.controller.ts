04650d8e60e2fa5cd25d7648f1495db44dbb65d2
DavidSheltonSF <david.faria012@gmail.com>
2025-12-30 13:38:06 -0300

    feat(backend/controllers): add AuthController

---
+++ backend/src/controllers/auth.controller.ts
@@ -0,0 +1,76 @@
+import { type Request, type Response } from 'express';
+import { ok, unauthorized } from '../helpers/http-helpers';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import { UserService } from '../services/user.service';
+import { AuthService } from '../services/auth.service';
+
+export class AuthController {
+  constructor(private userService = new UserService(), private authService = new AuthService()) {}
+
+  getMe = async (req: Request, res: Response) => {
+    const token = req.headers.authorization;
+
+    if (!token) {
+      return res.send(400).send(unauthorized('Token missing'));
+    }
+
+    const payload = jwt.decode(token) as JwtPayload;
+
+    const email = payload.email;
+    if (!email) {
+      return res.send(400).send(unauthorized('Token provided is invalid'));
+    }
+
+    const user = await this.userService.findByEmail(email);
+
+    return res.status(200).json(ok(user));
+  };
+
+  auth = async (req: Request, res: Response) => {
+    try {
+      const authService = new AuthService();
+      const body = req.body;
+
+      if (!body) {
+        return res.status(400).send({
+          code: 'BAD_REQUEST',
+          message: 'Body request is missing',
+        });
+      }
+
+      const { email, password } = body;
+
+      if (!email) {
+        return res.status(400).send({
+          code: 'BAD_REQUEST',
+          message: 'Missing email in the body request',
+        });
+      }
+
+      if (!password) {
+        return res.status(400).send({
+          code: 'BAD_REQUEST',
+          message: 'Missing password in the body request',
+        });
+      }
+
+      const auth = await this.authService.authenticate(email, password);
+
+      return res.status(200).send(auth);
+    } catch (error: any) {
+      console.log(error);
+
+      // Check if it is Unauthorized error
+      if (error.statusCode === 401) {
+        return res.status(error.statusCode).send({
+          code: error.code,
+          message: error.message,
+        });
+      }
+
+      return res.status(500).send({
+        message: 'Something went wron in the server side',
+      });
+    }
+  };
+}
