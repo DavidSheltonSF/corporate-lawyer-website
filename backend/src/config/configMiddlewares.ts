20090bd4d74ec8ac30a9c65504676e5b1b575386
DavidSheltonSF <david.faria012@gmail.com>
2025-12-30 13:48:12 -0300

    feat(config): add configMiddlewares

---
+++ backend/src/config/configMiddlewares.ts
@@ -0,0 +1,26 @@
+import { Application, json } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export function configMiddlewares(app: Application) {
+  app.use(json());
+
+  const allowedOrigins = [process.env.FRONTEND, 'http://192.168.0.118:3001'];
+  app.use(
+    cors({
+      origin: (origin, callback) => {
+        if (!origin) return callback(null, true);
+
+        if (allowedOrigins.includes(origin)) {
+          return callback(null, true);
+        }
+
+        return callback(new Error('Not allowed by cores'));
+      },
+      credentials: true,
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    })
+  );
+}
