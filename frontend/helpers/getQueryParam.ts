3e830ff8d45e06b663e1c4688ea2724e2ade4021
DavidSheltonSF <david.faria012@gmail.com>
2025-12-30 22:45:45 -0300

    chore(frontend/helpers): remove  getQueryParam and paginate, since they are not used anymore

--- frontend/helpers/getQueryParam.ts
+++
@@ -1,5 +0,0 @@
-export function getQueryParam(req: Request, key: string): string | null {
-  const { searchParams } = new URL(req.url);
-
-  return searchParams.get(key);
-}
--- frontend/helpers/paginate.ts
+++
@@ -1,9 +0,0 @@
-export function paginate(items: any[], page: number, limit: number) {
-  // If the items quantity does not overflow the limit, the pagination is not necessary
-  if (items.length <= limit) {
-    return items;
-  }
-  const start = (page - 1) * limit;
-  const end = start + limit;
-  return items.slice(start, end);
-}
