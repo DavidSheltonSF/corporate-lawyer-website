4d82889e23904e956a7f24f5ac8fafda47fcd21c
DavidSheltonSF <david.faria012@gmail.com>
2026-01-01 16:54:12 -0300

    refactor(frontend/services): make fetchUserByToken throw an error if the response is not ok instead of returning null for simplicity

--- frontend/services/fetchUserByToken.ts
+++ frontend/services/fetchUserByToken.ts
@@ -2,18 +2,18 @@ import { API_URL } from '@/config/api';
 import { User } from '@/types/User';
 import { WithId } from '@/types/WithId';
 
-export async function fetchUserByToken(token: string): Promise<WithId<User> | null> {
+export async function fetchUserByToken(token: string): Promise<WithId<User>> {
   const response = await fetch(`${API_URL}/me`, {
     headers: {
       Authorization: token,
     },
   });
 
-  const json = await response.json();
-
-  if (json.status > 300) {
-    return null;
+  if (!response.ok) {
+    throw Error(await response.text());
   }
+
+  const json = await response.json();
 
   return json.data;
 }
