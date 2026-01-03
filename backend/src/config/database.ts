cfea1318602663e621f00d066c1011c9ada2c5c5
DavidSheltonSF <david.faria012@gmail.com>
2026-01-03 16:43:13 -0300

    fix(backend/database): check if the database is connected before trying to connect or disconnect

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,9 +1,12 @@
 import mongoose from 'mongoose';
-
 export class DatabaseConnector {
+  static connected = false;
+
   static async connect() {
     try {
+      if (this.connected) return;
       await mongoose.connect(process.env.MONGODB_URI as string);
+      this.connected = true;
       console.log('Database connected');
     } catch (error) {
       console.log('Database connection error ' + error);
@@ -13,7 +16,9 @@ export class DatabaseConnector {
 
   static async disconnect() {
     try {
+      if (!this.connected) return;
       await mongoose.disconnect();
+      this.connected = false;
     } catch (error) {
       console.log(error);
     }
