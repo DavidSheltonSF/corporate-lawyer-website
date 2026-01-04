07e58cd76fe89d3198b69ad9998f8164e3588c17
DavidSheltonSF <david.faria012@gmail.com>
2026-01-03 22:39:59 -0300

    refactor(backend/infra): make MongodbUserRepository methods map data before returning it.
    - Methods return user data with Id

--- backend/src/infra/mongodb/repositories/MongodbUserRepository.ts
+++ backend/src/infra/mongodb/repositories/MongodbUserRepository.ts
@@ -1,18 +1,61 @@
 import { UserRepository } from '../../../domain/repositories/UserRepository';
 import { User } from '../../../types/User';
+import { WithId } from '../../../types/WithId';
 import { UserModel } from '../models/user.model';
 
-export class MongodbUserRepository implements UserRepository{
-  async findById(id: string): Promise<User | null> {
-    return await UserModel.findById(id).lean();
+export class MongodbUserRepository implements UserRepository {
+  async findById(id: string): Promise<WithId<User> | null> {
+    const user = await UserModel.findById(id).lean();
+
+    if (!user) {
+      return null;
+    }
+    return {
+      id: user._id.toString(),
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      cpf: user.cpf,
+      password: user.password,
+      role: user.role,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt,
+    };
   }
 
-  async findByEmail(email: string): Promise<User | null> {
-    return await UserModel.findOne({ email }).lean();
+  async findByEmail(email: string): Promise<WithId<User> | null> {
+    const user = await UserModel.findOne({ email }).lean();
+
+    if (!user) {
+      return null;
+    }
+
+    return {
+      id: user._id.toString(),
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      cpf: user.cpf,
+      password: user.password,
+      role: user.role,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt,
+    };
   }
 
-  async create(user: User): Promise<User> {
-    const createdUser = await UserModel.create(user);
-    return createdUser.toObject();
+  async create(data: User): Promise<WithId<User>> {
+    const user = await UserModel.create(data);
+
+    return {
+      id: user._id.toString(),
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      cpf: user.cpf,
+      password: user.password,
+      role: user.role,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt,
+    };
   }
 }
