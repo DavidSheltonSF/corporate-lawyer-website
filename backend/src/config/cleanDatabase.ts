63f5f8c69bdb0f3e058b789fb085c1cff93d75bf
DavidSheltonSF <david.faria012@gmail.com>
2026-01-05 19:43:53 -0300

    chore(backend): move /fakeDatabases , cleanDatabase and populateDatabase to /tests

--- backend/src/config/cleanDatabase.ts
+++
@@ -1,7 +0,0 @@
-import { CaseModel } from '../infra/mongodb/models/case.model';
-import { UserModel } from '../infra/mongodb/models/user.model';
-
-export async function cleanDatabase() {
-  await UserModel.deleteMany({});
-  await CaseModel.deleteMany({});
-}
--- backend/src/tests/populateDatabase.ts
+++ backend/src/tests/populateDatabase.ts
@@ -1,7 +1,7 @@
 import { fakeCases } from '../fakeDatabase/cases';
 import { fakeUserDatabase } from '../fakeDatabase/users';
-import { CaseModel } from '../infra/mongodb/models/case.model';
-import { UserModel } from '../infra/mongodb/models/user.model';
+import { CaseModel } from '../models/case.model';
+import { UserModel } from '../models/user.model';
 
 export async function populateDatabase() {
   await UserModel.create(fakeUserDatabase);
