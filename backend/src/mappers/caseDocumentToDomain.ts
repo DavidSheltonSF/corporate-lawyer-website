87781f1811812a46db94d47f3d7e2352dc4f0774
DavidSheltonSF <david.faria012@gmail.com>
2026-01-05 15:39:59 -0300

    feat(backend/mappers): add caseDocumentToDomain and userDocumentToDomain

---
+++ backend/src/mappers/caseDocumentToDomain.ts
@@ -0,0 +1,34 @@
+import { Case } from '../entities/Case';
+import { CaseDocument } from '../models/case.model';
+import { WithId } from '../types/WithId';
+
+export function caseDocumentToDomain(cas: CaseDocument): WithId<Case> {
+  const client = cas.client.toString();
+  const lawyers = cas.lawyers.map((lawyer) => lawyer.toString());
+  let documents;
+  let hearings;
+
+  if (cas.documents) {
+    documents = cas.documents.map((document) => document.toString());
+  }
+
+  if (cas.hearings) {
+    hearings = cas.hearings.map((hearing) => hearing.toString());
+  }
+
+  return {
+    id: cas._id.toString(),
+    title: cas.title,
+    processNumber: cas.processNumber,
+    court: cas.court,
+    courtDivision: cas.courtDivision,
+    status: cas.status,
+    description: cas.description,
+    client,
+    lawyers,
+    documents,
+    hearings,
+    createdAt: cas.createdAt,
+    updatedAt: cas.updatedAt,
+  };
+}
---
+++ backend/src/mappers/userDocumentToDomain.ts
@@ -0,0 +1,17 @@
+import { User } from '../entities/User';
+import { UserDocument } from '../models/user.model';
+import { WithId } from '../types/WithId';
+
+export function userDocumentToDomain(user: UserDocument): WithId<User> {
+  return {
+    id: user._id.toString(),
+    firstName: user.firstName,
+    lastName: user.firstName,
+    email: user.email,
+    cpf: user.cpf,
+    password: user.password,
+    role: user.role,
+    createdAt: user.createdAt,
+    updatedAt: user.updatedAt,
+  };
+}
