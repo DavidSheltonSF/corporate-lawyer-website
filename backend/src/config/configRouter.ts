b6b21879858e8cbe0e383869736077824cfad866
DavidSheltonSF <david.faria012@gmail.com>
2026-01-05 19:39:32 -0300

    chore(backend/config): make configRouter use factories to build controllers

--- backend/src/config/configRouter.ts
+++ backend/src/config/configRouter.ts
@@ -1,17 +1,17 @@
 import { Application, Router } from 'express';
 import { requireAuth } from '../middlewares/requireAuth';
-import { AuthController } from '../controllers/auth.controller';
-import { CaseController } from '../controllers/case.controller';
+import { makeAuthController } from '../factories/makeAuthController';
+import { makeCaseController } from '../factories/makeCaseController';
 
 export function configRouter(app: Application) {
-  const authController = new AuthController();
-  const caseController = new CaseController();
+  const authController = makeAuthController();
+  const caseController = makeCaseController();
 
   const router = Router();
   router.get('/api/me', requireAuth, authController.getMe);
   router.post('/api/auth', authController.auth);
   router.get('/api/cases/:id', caseController.findById);
-  router.get('/api/client/:id/cases/stats', caseController.getStatsByClient)
+  router.get('/api/client/:id/cases/stats', caseController.getStatsByClient);
   router.get('/api/client/:id/cases', caseController.findByClientId);
 
   app.use(router);
