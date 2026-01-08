1bd0edc7b75c70679b94c65c02c8cf9679661345
DavidSheltonSF <david.faria012@gmail.com>
2026-01-07 23:22:53 -0300

    refactor(backend/controllers): update findByClientId to use caseService.findCaseCards, for maching recente changes in the service layer

--- backend/src/controllers/CaseController.ts
+++ backend/src/controllers/CaseController.ts
@@ -3,6 +3,7 @@ import { badRequest, notFound, ok, serverError } from '../helpers/http-helpers';
 import { ICaseService } from '../services/ICaseService';
 import { IUserService } from '../services/IUserService';
 import { ICaseController } from './ICaseController';
+import { CasePopulateOptions } from '../types/CasePopulateOptions';
 
 export class CaseController implements ICaseController {
   constructor(private caseService: ICaseService, private userService: IUserService) {}
@@ -38,6 +39,12 @@ export class CaseController implements ICaseController {
     const { status, query, populate } = req.query;
 
     const populateFields = String(populate).split(',');
+    const casePopulateFields: CasePopulateOptions = {};
+
+    casePopulateFields.client = populateFields.includes('client') ? true : false;
+    casePopulateFields.lawyers = populateFields.includes('lawyers') ? true : false;
+    casePopulateFields.documents = populateFields.includes('documents') ? true : false;
+    casePopulateFields.hearings = populateFields.includes('hearings') ? true : false;
 
     const page = req.query.page || 1;
     const limit = req.query.limit || 4;
@@ -46,7 +53,7 @@ export class CaseController implements ICaseController {
       return res.status(400).send(badRequest('Missing id param'));
     }
 
-    const casesPaginated = await this.caseService.findAll(
+    const casesPaginated = await this.caseService.findCaseCards(
       {
         query: query ? String(query) : undefined,
         status: status ? String(status) : undefined,
@@ -54,7 +61,7 @@ export class CaseController implements ICaseController {
         page: page ? Number(page) : undefined,
         client: id ? String(id) : undefined,
       },
-      populateFields
+      casePopulateFields
     );
 
     const pagination = {
