e0dd4f8c9a63d540ed011f4e3f699114fae889f1
DavidSheltonSF <david.faria012@gmail.com>
2026-01-10 19:04:44 -0300

    feat(backend/mocks): add creteMockUserRepository for tests

---
+++ backend/src/tests/mocks/repositories/createMockUserRepository.ts
@@ -0,0 +1,11 @@
+import { UserRepository } from '../../../repositories/UserRepository';
+import { mockUserMongoDocs } from '../mockUserMongoDocs';
+
+export const createMockUserRepository = (): UserRepository => {
+  return {
+    findById: jest.fn().mockResolvedValue(mockUserMongoDocs[0]!),
+    findAll: jest.fn().mockResolvedValue(mockUserMongoDocs),
+    findByEmail: jest.fn().mockResolvedValue(mockUserMongoDocs[0]!),
+    create: jest.fn().mockResolvedValue(mockUserMongoDocs[0]!),
+  };
+};
