11f5815dec33d7cd59108c2d377c784820cc082a
DavidSheltonSF <david.faria012@gmail.com>
2026-01-10 16:42:20 -0300

    test(mocks): add mockCaseMongoDocs for tests

---
+++ backend/src/tests/mocks/mockCaseMongoDocs.ts
@@ -0,0 +1,16 @@
+import { Types } from 'mongoose';
+import { CaseStatusEnum } from '../../types/CaseStatusEnum';
+
+export const mockCaseMongoDocs = [
+  {
+    id: Types.ObjectId.createFromTime(485855),
+    client: Types.ObjectId.createFromTime(45855),
+    court: 'STJ',
+    courtDivision: 'Família',
+    description: 'Fake description',
+    lawyers: [Types.ObjectId.createFromTime(488888)],
+    processNumber: '2254787-55.5877.1.55.4787', //NNNNNNN-DD.AAAA.J.TR.OOOO,
+    status: CaseStatusEnum.aberto,
+    title: 'Case Title',
+  },
+];
