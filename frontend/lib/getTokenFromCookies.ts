5be14bc4264ce2368503d09c1b5d77062d0b3795
DavidSheltonSF <david.faria012@gmail.com>
2026-01-14 16:41:44 -0300

    chore(frontend/getTokenFromCookies): renamt constant auth to authToken and update the key to access the token for matching recent changes in the login action

--- frontend/lib/getTokenFromCookies.ts
+++ frontend/lib/getTokenFromCookies.ts
@@ -4,8 +4,8 @@ import { cookies } from 'next/headers';
 
 export async function getTokenFromCookies(): Promise<string> {
   const cookieStore = await cookies();
-  const auth = cookieStore.get('authentication');
-  const token = auth?.value;
+  const authToken = cookieStore.get('authToken');
+  const token = authToken?.value;
 
   if (!token) {
     throw Error('Token from cookies was not found');
