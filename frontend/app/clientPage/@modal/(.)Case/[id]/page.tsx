bf145714b0989b09a1f738fcf847a838353169ac
DavidSheltonSF <david.faria012@gmail.com>
2026-01-19 17:01:46 -0300

    refactor(frontend/@modal) make CaseModal use RouteModal instead of  PrimaryModalWindow, since RouteModal specializes PrimaryModalWindow for intercepting-routes

--- frontend/app/clientPage/@modal/(.)Case/[id]/page.tsx
+++ frontend/app/clientPage/@modal/(.)Case/[id]/page.tsx
@@ -1,10 +1,10 @@
 import CasePage from '@/app/clientPage/Case/[id]/page';
-import { PrimaryModalWindow } from '@/components/PrimaryModalWindow';
+import { RouteModal } from '@/components/RouteModal';
 
 export default async function CaseModal({ params }: { params: Promise<{ id: string }> }) {
   return (
-    <PrimaryModalWindow>
+    <RouteModal>
       <CasePage params={params} />
-    </PrimaryModalWindow>
+    </RouteModal>
   );
 }
