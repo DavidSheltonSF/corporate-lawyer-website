304e8f8ebc05b182d41736bad932129e617302ef
DavidSheltonSF <david.faria012@gmail.com>
2026-01-19 17:44:07 -0300

    chore(frontend/components): remove its unecessary wrapping div

--- frontend/components/PrimaryModalWindow.tsx
+++ frontend/components/PrimaryModalWindow.tsx
@@ -29,21 +29,19 @@ export function PrimaryModalWindow(props: Props) {
   const { closeModal, children } = props;
 
   return (
-    <div className="h-full w-full bg-black/20 fade-in-animation-fast">
-      <div
-        ref={windowRef}
-        className="modalWindow flex flex-col relative  bg-color-primary size-full"
-      >
-        <div className="flex justify-end items-center h-[56px] pr-[8px]">
-          <button
-            className="size-[40px] cursor-pointer hover:bg-white/20 transition-[background-color] duration-300 rounded-lg"
-            onClick={() => closeModal()}
-          >
-            <img className="size-full" src="/icons/close.svg" alt="" />
-          </button>
-        </div>
-        <div className="bg-color-white size-full overflow-auto">{children}</div>
+    <div
+      ref={windowRef}
+      className="modalWindow flex flex-col relative  bg-color-primary size-full fade-in-animation-fast"
+    >
+      <div className="flex justify-end items-center h-[56px] pr-[8px]">
+        <button
+          className="size-[40px] cursor-pointer hover:bg-white/20 transition-[background-color] duration-300 rounded-lg"
+          onClick={() => closeModal()}
+        >
+          <img className="size-full" src="/icons/close.svg" alt="" />
+        </button>
       </div>
+      <div className="bg-color-white size-full overflow-auto">{children}</div>
     </div>
   );
 }
