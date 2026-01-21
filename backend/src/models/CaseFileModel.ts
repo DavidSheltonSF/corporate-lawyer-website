b6e5c894c42021322ad976d19aeaf42c86744dee
DavidSheltonSF <david.faria012@gmail.com>
2026-01-21 13:42:52 -0300

    refactor(CaseFileModel): format CaseFileSchema for improved readability and make case  an index

--- backend/src/models/CaseFileModel.ts
+++ backend/src/models/CaseFileModel.ts
@@ -9,11 +9,14 @@ export interface ICaseFileModel {
 
 interface CaseFileMongoDocument extends ICaseFileModel, Document {}
 
-const CaseFileSchema = new Schema<CaseFileMongoDocument>({
-  name: { type: String, required: true },
-  url: { type: String, required: true, unique: true },
-  uploadedBy: { type: Types.ObjectId, required: true, ref: 'Users' },
-  case: { type: Types.ObjectId, required: true, ref: 'Cases' },
-}, {timestamps: true});
+const CaseFileSchema = new Schema<CaseFileMongoDocument>(
+  {
+    name: { type: String, required: true },
+    url: { type: String, required: true, unique: true },
+    uploadedBy: { type: Types.ObjectId, required: true, ref: 'Users' },
+    case: { type: Types.ObjectId, required: true, ref: 'Cases', index: true },
+  },
+  { timestamps: true }
+);
 
 export const CaseFileModel = model<CaseFileMongoDocument>('CaseFiles', CaseFileSchema);
