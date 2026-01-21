6a67791ff3072d5296dd27287ce7e8852824d370
DavidSheltonSF <david.faria012@gmail.com>
2026-01-21 13:18:26 -0300

    chore(dtos): delete CaseFileResponseDTO, since CaseFileDTO already holds the CaseFile data for responses

--- backend/src/dtos/caseFile/CaseFileResponseDTO.ts
+++
@@ -1,9 +0,0 @@
-import { UserName } from '../../types/UserName';
-import { WithId } from '../../types/WithId';
-
-export interface CaseFileResponseDTO {
-  name: string;
-  url: string;
-  uploadedBy: WithId<UserName>;
-  uploadedAt: string;
-}
