da31a37fbdf79bf661e7599708c648a5e626c6ed
DavidSheltonSF <david.faria012@gmail.com>
2026-01-21 11:58:35 -0300

    feat(CaseMapper): add persistenceToDomain

--- backend/src/mappers/CaseMapper.ts
+++ backend/src/mappers/CaseMapper.ts
@@ -1,5 +1,7 @@
 import { CaseCardDTO } from '../dtos/case/CaseCardDTO';
+import { Case } from '../entities/Case';
 import { WithId } from '../types/WithId';
+import { CaseCardPersistence } from './Case/CasePersistence';
 import { CaseCardPersistencePopulated } from './Case/CasePersistencePopulated';
 import { CaseFileMapper } from './CaseFile/CaseFileMapper';
 import { HearingMapper } from './Hearing/HearingMapper';
@@ -27,6 +29,30 @@ export class CaseMapper {
       hearings,
       createdAt: caseCardPersistence.createdAt.toISOString(),
       updatedAt: caseCardPersistence.updatedAt.toISOString(),
+    };
+  }
+
+  static persistenceToDomain(cas: unknown): WithId<Case> {
+    const casePersistence = cas as CaseCardPersistence;
+
+    const lawyers = casePersistence.lawyers.map((lawyer) => lawyer.toString());
+    const documents = casePersistence.documents.map((document) => document.toString());
+    const hearings = casePersistence.hearings.map((hearing) => hearing.toString());
+
+    return {
+      id: casePersistence._id.toString(),
+      title: casePersistence.title,
+      processNumber: casePersistence.processNumber,
+      court: casePersistence.court,
+      courtDivision: casePersistence.courtDivision,
+      status: casePersistence.status,
+      description: casePersistence.description,
+      client: casePersistence.client.toString(),
+      lawyers,
+      documents,
+      hearings,
+      createdAt: casePersistence.createdAt,
+      updatedAt: casePersistence.updatedAt,
     };
   }
 }
