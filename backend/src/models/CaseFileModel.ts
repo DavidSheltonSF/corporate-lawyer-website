e52ade328f57aed785bef3daae0a739020239905
DavidSheltonSF <david.faria012@gmail.com>
2026-01-22 13:30:21 -0300

    refactor(CaseFileModel): Prepare it to be a subdocument of Case
    - Added uploadedAt to CaseFileSchema with default: Date.now and remove timestamps
    
    - Added _id with default () => new Types.objectId()
    
    - Export the CaseFileSchema

--- backend/src/models/CaseFileModel.ts
+++ backend/src/models/CaseFileModel.ts
@@ -4,19 +4,22 @@ export interface ICaseFileModel {
   name: string;
   url: string;
   uploadedBy: Types.ObjectId;
+  uploadedAt: Date;
   case: Types.ObjectId;
 }
 
 interface CaseFileMongoDocument extends ICaseFileModel, Document {}
 
-const CaseFileSchema = new Schema<CaseFileMongoDocument>(
+export const CaseFileSchema = new Schema<CaseFileMongoDocument>(
   {
+    _id: { type: Schema.Types.ObjectId, default: () => new Types.ObjectId() },
     name: { type: String, required: true },
     url: { type: String, required: true, unique: true },
     uploadedBy: { type: Types.ObjectId, required: true, ref: 'Users' },
     case: { type: Types.ObjectId, required: true, ref: 'Cases', index: true },
+    uploadedAt: { type: Date, default: Date.now },
   },
-  { timestamps: true }
+  { _id: false }
 );
 
 export const CaseFileModel = model<CaseFileMongoDocument>('CaseFiles', CaseFileSchema);
