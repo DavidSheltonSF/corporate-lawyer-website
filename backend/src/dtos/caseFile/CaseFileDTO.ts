176eb3d2edcb406a94f07c3ec343e1205cd9a7c3
DavidSheltonSF <david.faria012@gmail.com>
2026-01-22 17:08:13 -0300

    refactor(CaseFileDTO): add size and mimeType, rename createdAt to uploadedAt and remove case

--- backend/src/dtos/caseFile/CaseFileDTO.ts
+++ backend/src/dtos/caseFile/CaseFileDTO.ts
@@ -4,7 +4,8 @@ import { WithId } from '../../types/WithId';
 export interface CaseFileDTO {
   name: string;
   url: string;
+  size: number;
+  mimeType: string;
   uploadedBy: WithId<UserIdentity>;
-  case: string;
-  createdAt: string;
+  uploadedAt: string;
 }
