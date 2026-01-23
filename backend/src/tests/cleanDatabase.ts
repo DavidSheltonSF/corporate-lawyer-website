e9013167841ebfec6122d687157f9c1b0ab1267a
DavidSheltonSF <david.faria012@gmail.com>
2026-01-22 22:07:03 -0300

    feat(cleanDatabase):  add line to clean CaseFile collection

--- backend/src/tests/cleanDatabase.ts
+++ backend/src/tests/cleanDatabase.ts
@@ -1,7 +1,9 @@
+import { CaseFileModel } from '../models/CaseFileModel';
 import { CaseModel } from '../models/CaseModel';
 import { UserModel } from '../models/UserModel';
 
 export async function cleanDatabase() {
   await UserModel.deleteMany({});
   await CaseModel.deleteMany({});
+  await CaseFileModel.deleteMany({});
 }
