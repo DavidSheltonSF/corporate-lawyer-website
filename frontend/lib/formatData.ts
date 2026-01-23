79b49fc09a82e020fdec44552ba96e79d934ff43
DavidSheltonSF <david.faria012@gmail.com>
2026-01-22 23:46:00 -0300

    feat(frontend/lib): add formatData

---
+++ frontend/lib/formatData.ts
@@ -0,0 +1,7 @@
+export function formatData(date: string | Date): string {
+  return new Date(date).toLocaleDateString('pt-BR', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+  });
+}
