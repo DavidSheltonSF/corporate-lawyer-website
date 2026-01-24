2289d7117515388c6f6518eb325bbf3709d1f4ee
DavidSheltonSF <david.faria012@gmail.com>
2026-01-24 16:24:09 -0300

    refactor(SecondaryModalWindow): move modal open/close responsability and size/position to parent

--- frontend/components/modals/SecondaryModalWindow.tsx
+++ frontend/components/modals/SecondaryModalWindow.tsx
@@ -1,42 +1,21 @@
-import { ReactNode, useEffect } from 'react';
+'use client';
+import { ReactNode } from 'react';
 
 interface Props {
+  closeModal: Function;
   children: ReactNode;
-  modalIsOpen: boolean;
-  setModalIsOpen: any;
 }
 
-export function SecondaryModalWindow({ modalIsOpen, setModalIsOpen, children }: Props) {
-  useEffect(() => {
-    const modalWindow: HTMLElement | null = document.querySelector('.modal-window');
-
-    function openModal() {
-      modalWindow?.classList.remove('hidden');
-      modalWindow?.classList.add('fade-in-animation-fast');
-      modalWindow?.classList.remove('fade-out-animation');
-    }
-
-    function closeModal() {
-      modalWindow?.classList.add('fade-out-animation');
-      modalWindow?.classList.remove('fade-in-animation-fast');
-    }
-
-    modalIsOpen ? openModal() : closeModal();
-  }, [modalIsOpen]);
-
-  function handleClick() {
-    setModalIsOpen(false);
-  }
-
+export function SecondaryModalWindow({ closeModal, children }: Props) {
   return (
     <div
-      className={`flex flex-col hidden modal-window z-9999 flex-col fixed left-[50%] translate-x-[-50%] top-[10vh] lg:top-[15vh]  w-[80%] md:w-[50%] lg:w-[30rem] min-h-[25rem] rounded-xl p-[16px] text-color-white font-bold bg-color-black-dark border border-color-secondary`}
+      className={`flex relative flex-col size-[100%] text-color-white font-bold bg-color-black-dark p-[16px]`}
     >
       <div>{children}</div>
 
       <button
-        className="self-end w-full rounded-full py-[8px] lg:absolute lg:w-[88px] lg:rounded-md right-[16px] bottom-[16px] bg-color-secondary text-color-black font-bold cursor-pointer hover:brightness-150"
-        onClick={handleClick}
+        className="w-full mt-auto rounded-full py-[8px] lg:absolute  lg:w-[88px] lg:rounded-md lg:right-[16px] bottom-[16px] bg-color-secondary text-color-black font-bold cursor-pointer hover:brightness-150"
+        onClick={() => closeModal()}
       >
         Ok
       </button>
