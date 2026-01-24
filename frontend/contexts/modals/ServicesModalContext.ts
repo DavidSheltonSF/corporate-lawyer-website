776bdc4c7ba86bd76eb5ea1f445c6599511569c4
DavidSheltonSF <david.faria012@gmail.com>
2026-01-24 15:34:20 -0300

    refactor(ServicesModalContext): remove null type from setServiceAreaId

--- frontend/contexts/modals/ServicesModalContext.ts
+++ frontend/contexts/modals/ServicesModalContext.ts
@@ -4,7 +4,7 @@ import { ModalContextType } from './ModalContextType';
 export const ServicesModalContext = createContext<
   | (ModalContextType & {
       serviceAreaId: string;
-      setServiceAreaId: Dispatch<SetStateAction<string>> | null;
+      setServiceAreaId: Dispatch<SetStateAction<string>>;
     })
   | undefined
 >(undefined);
