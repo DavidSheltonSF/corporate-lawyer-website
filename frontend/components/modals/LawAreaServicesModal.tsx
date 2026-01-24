f85c06c89cb8db724f8bd67a36f982122f3993fb
DavidSheltonSF <david.faria012@gmail.com>
2026-01-24 16:28:46 -0300

    feat(components/modal): add LawAreaServicesModal to manage SecondaryModalWindow and show The services provided by the law firm by law areas

---
+++ frontend/components/modals/LawAreaServicesModal.tsx
@@ -0,0 +1,28 @@
+'use client';
+import { useContext } from 'react';
+import { SecondaryModalWindow } from './SecondaryModalWindow';
+import { ServiceDetailsModal } from './ServiceDetailsModal';
+import { ServicesModalContext } from '@/contexts/modals/ServicesModalContext';
+import { MissingContextError } from '@/errors/MissingContextError';
+
+export function LawAreaServicesModal() {
+  const context = useContext(ServicesModalContext);
+
+  if (!context) {
+    throw new MissingContextError('ServicesModalContext');
+  }
+
+  const { isOpen, setIsOpen, serviceAreaId } = context;
+
+  return (
+    isOpen && (
+      <div
+        className={`flex z-9999 fixed left-[50%] translate-x-[-50%] top-[10vh] w-[30rem] h-[25rem] rounded-xl border border-color-secondary overflow-hidden bg-green-200`}
+      >
+        <SecondaryModalWindow closeModal={() => setIsOpen(false)}>
+          <ServiceDetailsModal serviceAreaId={serviceAreaId}></ServiceDetailsModal>
+        </SecondaryModalWindow>
+      </div>
+    )
+  );
+}
