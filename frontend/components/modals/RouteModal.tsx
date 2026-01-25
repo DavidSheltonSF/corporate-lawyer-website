3193dc055fed4ce682e7bff3002f9aecf7eebc67
DavidSheltonSF <david.faria012@gmail.com>
2026-01-25 15:06:27 -0300

    chore(frontend/modals/RouteModal): show RouteModalSkeleton while RouteModal is loading

--- frontend/components/modals/RouteModal.tsx
+++ frontend/components/modals/RouteModal.tsx
@@ -1,20 +1,24 @@
 'use client';
 import { useRouter } from 'next/navigation';
 import { PrimaryModalWindow } from './PrimaryModalWindow';
+import { Suspense } from 'react';
+import { RouteModalSkeleton } from './RouteModalSkeleton';
 
 export function RouteModal({ children }: { children: React.ReactNode }) {
   const router = useRouter();
   return (
-    <div className="fixed z-99999 left-1/2 translate-x-[-50%] top-[8px] min-lg:top-[80px] w-[95%] min-lg:w-[960px] h-[85vh] rounded-xl overflow-hidden">
-      <PrimaryModalWindow
-        closeModal={() => {
-          const modalWindow = document.querySelector('.modalWindow');
-          modalWindow?.classList.add('fade-out-animation-fast');
-          setTimeout(() => router.back(), 200);
-        }}
-      >
-        {children}
-      </PrimaryModalWindow>
-    </div>
+    <Suspense fallback={<RouteModalSkeleton />}>
+      <div className="fixed z-99999 left-1/2 translate-x-[-50%] top-[8px] min-lg:top-[80px] w-[95%] min-lg:w-[960px] h-[85vh] rounded-xl overflow-hidden">
+        <PrimaryModalWindow
+          closeModal={() => {
+            const modalWindow = document.querySelector('.modalWindow');
+            modalWindow?.classList.add('fade-out-animation-fast');
+            setTimeout(() => router.back(), 200);
+          }}
+        >
+          {children}
+        </PrimaryModalWindow>
+      </div>
+    </Suspense>
   );
 }
