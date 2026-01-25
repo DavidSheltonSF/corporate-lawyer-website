7b0f2870adac4f932285bdba76c5d42e6b210365
DavidSheltonSF <david.faria012@gmail.com>
2026-01-25 15:25:29 -0300

    chore(RouteModal): add shadow design and reduce the timeout for closing the modal

--- frontend/components/modals/RouteModal.tsx
+++ frontend/components/modals/RouteModal.tsx
@@ -8,12 +8,12 @@ export function RouteModal({ children }: { children: React.ReactNode }) {
   const router = useRouter();
   return (
     <Suspense fallback={<RouteModalSkeleton />}>
-      <div className="fixed z-99999 left-1/2 translate-x-[-50%] top-[8px] min-lg:top-[80px] w-[95%] min-lg:w-[960px] h-[85vh] rounded-xl overflow-hidden">
+      <div className="fixed z-99999 left-1/2 translate-x-[-50%] top-[8px] min-lg:top-[80px] w-[95%] min-lg:w-[960px] h-[85vh] rounded-xl overflow-hidden shadow-[0px_0px__4px_black]">
         <PrimaryModalWindow
           closeModal={() => {
             const modalWindow = document.querySelector('.modalWindow');
             modalWindow?.classList.add('fade-out-animation-fast');
-            setTimeout(() => router.back(), 200);
+            setTimeout(() => router.back(), 100);
           }}
         >
           {children}
