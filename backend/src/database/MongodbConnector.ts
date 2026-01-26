852a3660a3421f5c2e8878532b127218fe738837
DavidSheltonSF <david.faria012@gmail.com>
2026-01-26 19:15:33 -0300

    refactor(MongodbConnector): remove connectToTestDatabaseAndReturn and deleteDatabase for simplicity and security

--- backend/src/database/MongodbConnector.ts
+++ backend/src/database/MongodbConnector.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+
 export class MongodbConnector {
   static instance: MongodbConnector | null = null;
 
@@ -13,29 +14,6 @@ export class MongodbConnector {
     } catch (error) {
       console.log('Database connection error ' + error);
       process.exit(1);
-    }
-  }
-
-  static async connectToTestDatabaseAndReturn(name: string) {
-    try {
-      if (this.instance) return this.instance;
-      await mongoose.connect(
-        `mongodb+srv://davidAdmin:davidAdmin@cluster0.zhgudt8.mongodb.net/${name}?appName=Cluster0`
-      );
-      console.log('Database connected');
-      return new MongodbConnector(mongoose.connection);
-    } catch (error) {
-      console.log('Database connection error ' + error);
-      process.exit(1);
-    }
-  }
-
-  async deleteDatabase() {
-    try {
-      if (!this.connection) return;
-      await this.connection.dropDatabase();
-    } catch (error) {
-      console.log(error);
     }
   }
 
