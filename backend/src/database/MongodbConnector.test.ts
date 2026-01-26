9f2a2507f3179809f1299c1e89b1b9a1f6e5b88f
DavidSheltonSF <david.faria012@gmail.com>
2026-01-26 19:40:31 -0300

    test(MongodbConnector): add test file

---
+++ backend/src/database/MongodbConnector.test.ts
@@ -0,0 +1,13 @@
+import { MongodbConnector } from "./MongodbConnector"
+
+describe('Testing MongodbConnector', () => {
+test('should be returned the same MongodbConnector instance', async () => {
+  const connection1 = await MongodbConnector.connectAndReturn();
+  const connection2 = await MongodbConnector.connectAndReturn();
+
+  // Disconnect both in case of they are different
+  connection1.disconnect()
+  connection2.disconnect();
+  expect(connection1).toBe(connection2)
+});
+})
