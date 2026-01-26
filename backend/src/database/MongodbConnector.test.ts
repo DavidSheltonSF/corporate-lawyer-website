a7b7ff5c12fe53683740232581ff6de801232bf6
DavidSheltonSF <david.faria012@gmail.com>
2026-01-26 19:55:55 -0300

    refactor(MogodbConnector - test): set jest timeout to 99999

--- backend/src/database/MongodbConnector.test.ts
+++ backend/src/database/MongodbConnector.test.ts
@@ -1,13 +1,14 @@
-import { MongodbConnector } from "./MongodbConnector"
+import { MongodbConnector } from './MongodbConnector';
+jest.setTimeout(99999);
 
 describe('Testing MongodbConnector', () => {
-test('should be returned the same MongodbConnector instance', async () => {
-  const connection1 = await MongodbConnector.connectAndReturn();
-  const connection2 = await MongodbConnector.connectAndReturn();
+  test('should be returned the same MongodbConnector instance', async () => {
+    const connection1 = await MongodbConnector.connectAndReturn();
+    const connection2 = await MongodbConnector.connectAndReturn();
 
-  // Disconnect both in case of they are different
-  connection1.disconnect()
-  connection2.disconnect();
-  expect(connection1).toBe(connection2)
+    // Disconnect both in case of they are different
+    connection1.disconnect();
+    connection2.disconnect();
+    expect(connection1).toBe(connection2);
+  });
 });
-})
