2c9d67ec15bedc8afb26e9c3522164a98dc78289
DavidSheltonSF <david.faria012@gmail.com>
2026-01-26 12:18:53 -0300

    chore(services): remove unused CaseFileService

--- backend/src/services/caseFile/ICaseFileService.ts
+++
@@ -1,8 +0,0 @@
-import { CaseFileDTO } from '../../dtos/caseFile/CaseFileDTO';
-import { CreateCaseFileDTO } from '../../dtos/caseFile/CreateCaseFileDTO';
-import { WithId } from '../../types/WithId';
-
-export interface ICaseFileService {
-  findByCaseId(id: string): Promise<WithId<CaseFileDTO>[]>;
-  create(data: CreateCaseFileDTO): Promise<WithId<CaseFileDTO>>;
-}
