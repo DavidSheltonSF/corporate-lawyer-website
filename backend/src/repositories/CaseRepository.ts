3a396c9cd667364a2cb4d9442d5bcd9a93777a55
DavidSheltonSF <david.faria012@gmail.com>
2026-01-26 12:40:18 -0300

    refactor(repositories): remove casePopulateOptions from CaseRepository.findCaseCards
    The method will already  return cases with the necessary fields populated

--- backend/src/repositories/CaseRepository.ts
+++ backend/src/repositories/CaseRepository.ts
@@ -2,17 +2,13 @@ import { CaseCardDTO } from '../dtos/case/CaseCardDTO';
 import { CreateCaseDTO } from '../dtos/user/CreateCaseDTO';
 import { Case } from '../entities/Case';
 import { CaseFile } from '../entities/CaseFile';
-import { CasePopulateOptions } from '../types/CasePopulateOptions';
 import { CaseQuery } from '../types/CaseQuery';
 import { CaseStats } from '../types/CaseStats';
 import { Page } from '../types/Page';
 import { WithId } from '../types/WithId';
 
 export interface CaseRepository {
-  findCaseCards(
-    queryParams: CaseQuery,
-    casePopulateOptions: CasePopulateOptions
-  ): Promise<Page<WithId<CaseCardDTO>>>;
+  findCaseCards(queryParams: CaseQuery): Promise<Page<WithId<CaseCardDTO>>>;
   findById(id: string): Promise<WithId<CaseCardDTO> | null>;
   create(user: CreateCaseDTO): Promise<WithId<Case>>;
   getStats(client?: string): Promise<CaseStats | null>;
