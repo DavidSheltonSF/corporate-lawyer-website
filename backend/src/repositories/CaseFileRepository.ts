1e862919e9fa1266609dad52ca0f708490985118
DavidSheltonSF <david.faria012@gmail.com>
2026-01-26 12:19:51 -0300

    chore(repositories): remove unused CaseFileRepository and its implementation

--- backend/src/repositories/CaseFileRepository.ts
+++
@@ -1,8 +0,0 @@
-import { CaseFileDTO } from '../dtos/caseFile/CaseFileDTO';
-import { CaseFile } from '../entities/CaseFile';
-import { WithId } from '../types/WithId';
-
-export interface CaseFileRepository {
-  findByCaseId(id: string): Promise<WithId<CaseFileDTO>[]>;
-  create(data: CaseFile): Promise<WithId<CaseFileDTO>>;
-}
