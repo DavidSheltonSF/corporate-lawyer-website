af4a8818ae113133d5a0d62a36b809570fa4f5f6
DavidSheltonSF <david.faria012@gmail.com>
2026-01-26 18:55:06 -0300

    feat(database): add MongodbConnector
    Created a singleton class to handle the Mongodb connection

---
+++ backend/src/database/MongodbConnector.ts
@@ -0,0 +1,51 @@
+import mongoose from 'mongoose';
+export class MongodbConnector {
+  static instance: MongodbConnector | null = null;
+
+  private constructor(private connection: mongoose.Connection | null = null) {}
+
+  static async connectAndReturn() {
+    try {
+      if (this.instance) return this.instance;
+      await mongoose.connect(process.env.MONGODB_URI as string);
+      console.log('Database connected');
+      return new MongodbConnector(mongoose.connection);
+    } catch (error) {
+      console.log('Database connection error ' + error);
+      process.exit(1);
+    }
+  }
+
+  static async connectToTestDatabaseAndReturn(name: string) {
+    try {
+      if (this.instance) return this.instance;
+      await mongoose.connect(
+        `mongodb+srv://davidAdmin:davidAdmin@cluster0.zhgudt8.mongodb.net/${name}?appName=Cluster0`
+      );
+      console.log('Database connected');
+      return new MongodbConnector(mongoose.connection);
+    } catch (error) {
+      console.log('Database connection error ' + error);
+      process.exit(1);
+    }
+  }
+
+  async deleteDatabase() {
+    try {
+      if (!this.connection) return;
+      await this.connection.dropDatabase();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async disconnect() {
+    try {
+      if (!this.connection) return;
+      await this.connection.close();
+      this.connection = null;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
