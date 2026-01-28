8d21bffb712337058af4844e0ba8785780737a7c
DavidSheltonSF <david.faria012@gmail.com>
2026-01-28 18:10:00 -0300

    feat(UploadModal): add drag and drop feature

--- frontend/components/modals/UploadModal.tsx
+++ frontend/components/modals/UploadModal.tsx
@@ -16,6 +16,29 @@ export function UploadModal({
   const { isOpen, setIsOpen } = useContext<any>(UploadModalContext);
   const [uploadState, setUploadState] = useState<null | RequestState>(null);
 
+  useEffect(() => {
+    const handleGlobalDragOver = (e: any) => {
+      const files = [...e.dataTransfer.items].filter((item) => item.kind === 'file');
+
+      if (files.length > 0) {
+        const dropArea = document.querySelector('.dropArea');
+        e.preventDefault();
+
+        if (!dropArea?.contains(e.target)) {
+          e.dataTransfer.dropEffect = 'none';
+        }
+      }
+    };
+    const handleGlobalDrop = (e: any) => {
+      if ([...e.dataTransfer.items].some((item) => item.kind === 'file')) {
+        e.preventDefault();
+      }
+    };
+
+    window.addEventListener('dragover', handleGlobalDragOver);
+    window.addEventListener('drop', handleGlobalDrop);
+  });
+
   function closeModal() {
     if (uploadState?.status === 'loading') return;
     setIsOpen(false);
@@ -45,6 +68,50 @@ export function UploadModal({
     }
   }
 
+  function handleDragOver(e: React.DragEvent) {
+    const fileItems = [...e.dataTransfer.items].filter((item) => item.kind === 'file');
+
+    if (fileItems.length > 0) {
+      e.preventDefault();
+      const dropArea = e.currentTarget;
+      dropArea.classList.add('border-[2px]', 'border-blue-400');
+
+      if (fileItems.some((item) => item.type === 'application/pdf')) {
+        e.dataTransfer.dropEffect === 'copy';
+      } else {
+        e.dataTransfer.dropEffect === 'none';
+      }
+    }
+  }
+
+  function handleDragLeave(e: React.DragEvent<HTMLLabelElement>) {
+    e.preventDefault();
+    const dropArea = e.currentTarget;
+    dropArea.classList.remove('border-[2px]', 'border-blue-400');
+  }
+
+  async function handleDrop(e: React.DragEvent<HTMLLabelElement>) {
+    try {
+      e.preventDefault();
+      const fileItems = [...e.dataTransfer.items].map((item) => item.getAsFile());
+
+      if (!fileItems[0]) return;
+
+      setUploadState({ status: 'loading' });
+
+      const formData = new FormData();
+      formData.append('file', fileItems[0]);
+
+      await fetchUploadCaseFile(formData, caseId);
+
+      setUploadState({ status: 'ok', message: 'Arquivo adicionado com sucesso!' });
+      setUpdateFiles(true);
+    } catch (error) {
+      console.log(error);
+      setUploadState({ status: 'error', message: 'Arquivo não adicionado' });
+    }
+  }
+
   return (
     isOpen && (
       <div className="absolute z-99999999999 top-[15%] left-1/2 translate-x-[-50%] w-[350px] h-[300px] rounded-lg overflow-hidden shadow-[0px_0px__3px_black]">
@@ -63,23 +130,30 @@ export function UploadModal({
                 {uploadState.message}
               </p>
             )}
-            <div
-              className={`flex flex-col items-center justify-around bg-gray-200 h-[80%] w-[90%] rounded-md p-[8px] border border-dashed ${
+            <label
+              htmlFor="input-file"
+              className={`dropArea flex flex-col items-center justify-around bg-gray-200 h-[80%] w-[90%] rounded-md p-[8px] border border-dashed ${
                 uploadState?.status === 'loading' && 'animate-pulse border-blue-400 border-[2px]'
               }`}
+              onDragOver={handleDragOver}
+              onDragLeave={handleDragLeave}
+              onDrop={handleDrop}
             >
               <h1 className="text-2xl ">Arraste e Largue o arquivo</h1>
 
               <UploadCloudIcon width="56px" height="56px" color="#2c2c2c85" />
 
-              <label
-                className="py-[8px] px-[16px] bg-color-primary hover:brightness-130 text-color-white rounded-md cursor-pointer"
-                htmlFor="input-file"
-              >
+              <div className="py-[8px] px-[16px] bg-color-primary hover:brightness-130 text-color-white rounded-md cursor-pointer">
                 Escolher Arquivo
-                <input className="hidden" id="input-file" type="file" onChange={handleChange} accept='application/pdf'/>
-              </label>
-            </div>
+                <input
+                  className="hidden"
+                  id="input-file"
+                  type="file"
+                  onChange={handleChange}
+                  accept="application/pdf"
+                />
+              </div>
+            </label>
           </div>
         </PrimaryModalWindow>
       </div>
