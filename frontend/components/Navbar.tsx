65fa401342b4c886b41273f25f0a02b959a4fc9d
DavidSheltonSF <david.faria012@gmail.com>
2026-02-06 18:55:23 -0300

    refactor(Navbar): make the mobile menu close when touching outside it

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { NavbarItem } from './NavbarItem';
 import { NavbarMobileMenuButton } from './NavbarMobileMenuButton';
 import { NavbarList } from './NavbarList';
@@ -8,9 +8,25 @@ import Link from 'next/link';
 
 export function Navbar() {
   const [menuIsOpen, setMenuIsOpen] = useState(false);
+  const navbarRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    function handleClickOutside(e: TouchEvent) {
+      if (navbarRef.current && !navbarRef.current.contains(e.target as Node)) {
+        setMenuIsOpen(false);
+      }
+    }
+
+    document.addEventListener('touchstart', handleClickOutside);
+
+    return () => document.removeEventListener('touchstart', handleClickOutside);
+  });
 
   return (
-    <nav className="absolute top-0 flex justify-between items-center bg-[var(--black-color)]/48 w-full h-auto py-[4px] px-[24px] lg:px-[40px]">
+    <nav
+      ref={navbarRef}
+      className="absolute top-0 flex justify-between items-center bg-[var(--black-color)]/48 w-full h-auto py-[4px] px-[24px] lg:px-[40px]"
+    >
       <div>
         <Link href="/">
           <img
