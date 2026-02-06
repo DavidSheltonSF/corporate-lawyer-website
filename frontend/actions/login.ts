24775539a481bb142fc1ea35cbe2f9db2d2c1d39
DavidSheltonSF <david.faria012@gmail.com>
2026-02-06 18:35:43 -0300

    chore(login): remove secure and sameSite options in the cookies's sessings for tests

--- frontend/actions/login.ts
+++ frontend/actions/login.ts
@@ -5,5 +5,6 @@ import { cookies } from 'next/headers';
 export async function login(formData: FormData): Promise<void> {
   const token = await authenticateUser(formData);
   const cookiesStore = await cookies();
-  cookiesStore.set('authToken', token, { httpOnly: true, secure: true, sameSite: 'none' });
+  //cookiesStore.set('authToken', token, { httpOnly: true, secure: true, sameSite: 'none' });
+  cookiesStore.set('authToken', token, { httpOnly: true });
 }
