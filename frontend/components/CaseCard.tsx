f9417b4938c8361f75af13248fb41179d28b3159
DavidSheltonSF <david.faria012@gmail.com>
2026-02-08 16:12:58 -0300

    refactor(CaseCard): open state-based CaseModal on clicking a card instead of opening a route-based modal

--- frontend/components/CaseCard.tsx
+++ frontend/components/CaseCard.tsx
@@ -6,9 +6,10 @@ import { CaseStatusEnum } from '@/types/CaseStatusEnum';
 import { reduceString } from '@/lib/reduceString';
 import { TooltipContainer } from './TooltipContainer';
 import { WithId } from '@/types/WithId';
-import Link from 'next/link';
 import { UserIdentity } from '@/types/UserIdentity';
 import { CaseStatusLabel } from '@/lib/CaseStatusLabel';
+import { MissingContextError } from '@/errors/MissingContextError';
+import { useCaseModalContext } from '@/hooks/useCaseModalContext';
 
 interface Props {
   caseData: WithId<Case> & {
@@ -49,60 +50,65 @@ export function CaseCard({ caseData }: Props) {
     return `${lawyer.firstName} ${lawyer.lastName}`;
   });
 
+  const caseModalContext = useCaseModalContext();
+
+  if (!caseModalContext) {
+    throw new MissingContextError('CaseModalContext');
+  }
+
+  const { setIsOpen, setCaseId } = caseModalContext;
+
   return (
-    <article className="flex flex-col fade-in-animation  bg-color-primary w-full min-md:w-[80%] min-lg:w-[640px] min-h-[280px] h-max rounded-xl">
-      <Link
-        className="flex flex-col flex-1"
-        href={`clientPage/Case/${id}`}
-        scroll={false}
+    <article
+      onClick={() => {
+        setCaseId(id);
+        setIsOpen(true);
+      }}
+      className="flex flex-col fade-in-animation  bg-color-primary w-full min-md:w-[80%] min-lg:w-[640px] min-h-[280px] h-max rounded-xl"
+    >
+      <header className="flex items-center justify-center min-md:justify-start w-full p-[16px] min-md:p-[24px]">
+        <TooltipContainer
+          label={title}
+          tooltipLabelProps={{
+            color: '#ffd000ff',
+            backgroundColor: '#000',
+            position: {
+              bottom: '115%',
+              left: '50%',
+              translateX: '-50%',
+            },
+          }}
+        >
+          <h1 className="h-fit font-bold text-center min-md:text-start text-xl min-md:text-3xl">
+            {reduceString(title, 35)}
+          </h1>
+        </TooltipContainer>
+      </header>
+      <main
+        className="flex flex-1 flex-col gap-[16px] px-[24px] py-[16px] bg-color-white text-color-black text-lg"
         style={{
           borderRadius: 'inherit',
         }}
       >
-        <header className="flex items-center justify-center min-md:justify-start w-full p-[16px] min-md:p-[24px]">
-          <TooltipContainer
-            label={title}
-            tooltipLabelProps={{
-              color: '#ffd000ff',
-              backgroundColor: '#000',
-              position: {
-                bottom: '115%',
-                left: '50%',
-                translateX: '-50%',
-              },
-            }}
-          >
-            <h1 className="h-fit font-bold text-center min-md:text-start text-xl min-md:text-3xl">
-              {reduceString(title, 35)}
-            </h1>
-          </TooltipContainer>
-        </header>
-        <main
-          className="flex flex-1 flex-col gap-[16px] px-[24px] py-[16px] bg-color-white text-color-black text-lg"
-          style={{
-            borderRadius: 'inherit',
-          }}
-        >
-          <span className="flex gap-[8px]">
-            <p className="font-bold">nº:</p>
-            <p>{processNumber}</p>
-          </span>
-          <span className="flex gap-[8px]">
-            <p className="font-bold">cliente:</p>
-            <p>
-              {userData?.firstName} {userData?.lastName}
-            </p>
-          </span>
-          <span className="flex gap-[8px]">
-            <p className="font-bold">advogados:</p>
-            <p>{formatStringList(renderLawyers)}</p>
-          </span>
-          <span className="flex gap-[8px]">
-            <p className="font-bold">status:</p>
-            <p className={`font-bold ${statusColor}`}>{CaseStatusLabel[status]}</p>
-          </span>
-        </main>
-      </Link>
+        <span className="flex gap-[8px]">
+          <p className="font-bold">nº:</p>
+          <p>{processNumber}</p>
+        </span>
+        <span className="flex gap-[8px]">
+          <p className="font-bold">cliente:</p>
+          <p>
+            {userData?.firstName} {userData?.lastName}
+          </p>
+        </span>
+        <span className="flex gap-[8px]">
+          <p className="font-bold">advogados:</p>
+          <p>{formatStringList(renderLawyers)}</p>
+        </span>
+        <span className="flex gap-[8px]">
+          <p className="font-bold">status:</p>
+          <p className={`font-bold ${statusColor}`}>{CaseStatusLabel[status]}</p>
+        </span>
+      </main>
     </article>
   );
 }
