192c7d8ae71143e54ff232bc2a0b52d52c7c73f2
DavidSheltonSF <david.faria012@gmail.com>
2026-02-08 14:21:40 -0300

    refactor(ModalsProvider): provide CaseModal states (caseModalIsOpen, caseId) and its set state actions through context

--- frontend/contexts/modals/ModalsProvider.tsx
+++ frontend/contexts/modals/ModalsProvider.tsx
@@ -4,12 +4,15 @@ import { useState } from 'react';
 import { UploadModalContext } from './UploadModalContext';
 import { ServicesModalContext } from './ServicesModalContext';
 import { ServiceDetailsModal } from '@/components/modals/ServiceDetailsModal';
+import { CaseModalContext } from './CaseModalContext';
 
 interface Props {
   children: React.ReactNode;
 }
 
 export function ModalsProvider({ children }: Props) {
+  const [caseModalIsOpen, setCaseModalIsOpen] = useState(false);
+  const [caseId, setCaseId] = useState(null);
   const [uploadModalIsOpen, setUploadModalIsOpen] = useState(false);
   const [servicesModalIsOpen, setServicesModalIsOpen] = useState(false);
   const [serviceAreaId, setServiceAreaId] = useState('');
@@ -27,7 +30,9 @@ export function ModalsProvider({ children }: Props) {
         <UploadModalContext.Provider
           value={{ isOpen: uploadModalIsOpen, setIsOpen: setUploadModalIsOpen }}
         >
-          {children}
+          <CaseModalContext value={{ isOpen: caseModalIsOpen, setIsOpen: setCaseModalIsOpen, caseId, setCaseId }}>
+            {children}
+          </CaseModalContext>
         </UploadModalContext.Provider>
       </ServicesModalContext.Provider>
     </div>
