226e31b2df24a3093a6cd2daa16607cd6710379a
DavidSheltonSF <david.faria012@gmail.com>
2026-02-10 16:57:25 -0300

    chore(CaseModal): enhance design, responsiveness  and code readability
    - Set top to 2%
    - Set height to 82vh in mobile screens and 95vh on desktop and larger screens
    - Remove unnecesssary classes and HTML tags
    - Set the height of the div wrapping CaseFilesSection to 224px on mobile screens and to 316px on desktop and larger screens

--- frontend/components/modals/CaseModal.tsx
+++ frontend/components/modals/CaseModal.tsx
@@ -71,7 +71,7 @@ export function CaseModal() {
     isOpen && (
       <div
         ref={modalRef}
-        className="fixed z-99999999999 top-[4%] left-1/2 translate-x-[-50%] w-[90%] min-lg:w-[880px] h-[872px] rounded-lg overflow-hidden shadow-[0px_0px__3px_black] text-color-black"
+        className="fixed z-99999999999 top-[2%] left-1/2 translate-x-[-50%] w-[90%] min-lg:w-[880px] h-[82vh] min-lg:h-[95vh] rounded-lg overflow-hidden shadow-[0px_0px__3px_black] text-color-black"
       >
         <PrimaryModalWindow
           closeModal={() => {
@@ -81,40 +81,38 @@ export function CaseModal() {
           {loading || !caseData ? (
             <CaseModalSkeleton />
           ) : (
-            <div className="flex flex-col size-full">
-              <div className="size-full bg-color-white">
-                <header className="flex items-center bg-color-primary min-h-[72px] border-t border-white/50 pl-[24px] py-[16px]">
-                  <h1 className=" text-color-white text-4xl">{caseData?.title}</h1>
-                </header>
-                <main className="flex flex-col h-full text-xl">
-                  <div className="flex min-h-[30%] flex-col gap-[8px] pl-[24px] py-[24px]">
-                    <FieldValue field="nº:" value={caseData?.processNumber || ''} />
-                    <FieldValue
-                      field="cliente:"
-                      value={`${caseData?.client?.firstName || ''} ${
-                        caseData?.client?.lastName || ''
-                      }`}
-                    />
-                    <FieldValue field="lawyers" value={formatStringList(lawyersNames)} />
-                    <FieldValue field="status:" value={caseData?.status || ''} />
-                    <FieldValue field="Tribunal:" value={caseData?.court || ''} />
-                    <FieldValue field="Vara:" value={caseData?.courtDivision || ''} />
+            <div className="flex flex-col size-full bg-color-white">
+              <header className="w-full bg-color-primary p-[16px] border-t border-white/50">
+                <h1 className="text-3xl text-color-white font-bold ">{caseData.title}</h1>
+              </header>
+              <main className="flex flex-col w-full text-lg min-lg:text-xl">
+                <div className="flex flex-col gap-[8px] border-b border-black/50 p-[16px]">
+                  <FieldValue field="nº:" value={caseData.processNumber} />
+                  <FieldValue
+                    field="cliente:"
+                    value={`
+                    ${caseData.client.firstName} ${caseData.client.lastName}
+                    `}
+                  />
+                  <FieldValue field="advogados:" value={formatStringList(lawyersNames)} />
+                  <FieldValue field="status:" value={caseData.status} />
+                  <FieldValue field="tribunal:" value={caseData.court} />
+                  <FieldValue field="vara:" value={caseData.courtDivision} />
+                </div>
+                <div className="flex flex-col gap-[8px] border-b border-black/50 p-[16px]">
+                  <h1 className="text-2xl font-bold">Resumo</h1>
+                  <p>{caseData.description}</p>
+                </div>
+                <div className="flex flex-col gap-[8px] border-b border-black/50">
+                  <div className="w-full bg-color-primary p-[16px]">
+                    <h1 className="text-2xl font-bold text-color-white">Arquivos</h1>
                   </div>
-                  <div className="flex flex-col min-h-[20%] gap-[8px] px-[24px] py-[24px] border-t border-black/30">
-                    <h1 className="font-bold text-3xl">Resumo</h1>
-                    <p>{caseData?.description || ''}</p>
-                  </div>
-                  <div className="flex relative items-center h-[56px] bg-color-primary border-t pl-[24px]">
-                    <h1 className=" text-color-white text-3xl">Documentação</h1>
-                    <div className="absolute top-1/2 translate-y-[-50%] right-[16px]">
-                      <OpenUploadModalButton />
-                    </div>
-                  </div>
-                  <div className="flex w-full h-[240px]">
+
+                  <div className="h-[224px] min-lg:h-[316px] overflow-auto">
                     <CaseFilesSection id={caseId || ''} files={caseData.files} />
                   </div>
-                </main>
-              </div>
+                </div>
+              </main>
             </div>
           )}
         </PrimaryModalWindow>
