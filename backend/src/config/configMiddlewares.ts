62987c765dd7165cfb3ce9a52deb9ee31f196540
DavidSheltonSF <david.faria012@gmail.com>
2026-02-23 17:55:59 -0300

    chore(configMiddlewares): check if FRONTEND env variable is defined and simplify cors config

--- backend/src/config/configMiddlewares.ts
+++ backend/src/config/configMiddlewares.ts
@@ -7,19 +7,23 @@ dotenv.config();
 
 export function configMiddlewares(app: Application) {
   app.use(json());
-  app.use(cookieParser())
+  app.use(cookieParser());
 
-  const allowedOrigins = [process.env.FRONTEND, 'http://192.168.0.118:3001'];
+  const allowedOrigin = process.env.FRONTEND;
+  if (!allowedOrigin) {
+    throw Error('FRONTEND environment variable is not defined');
+  }
+
   app.use(
     cors({
       origin: (origin, callback) => {
         if (!origin) return callback(null, true);
 
-        if (allowedOrigins.includes(origin)) {
+        if (allowedOrigin === origin) {
           return callback(null, true);
         }
 
-        return callback(new Error('Not allowed by cores'));
+        return callback(new Error(`Origin ${origin} not allowed by cors`));
       },
       credentials: true,
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
