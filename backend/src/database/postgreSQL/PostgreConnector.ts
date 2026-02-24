40a9841b201d2e49dde5a94aa48ffa46574670cf
DavidSheltonSF <david.faria012@gmail.com>
2026-02-24 18:19:19 -0300

    refactor(PostgreConnector):
    - Add isConnected attribute
    - Check if the database is connected before connect, disconnect or execute queries
    - Add cleanTable
    - Add dropTableIfExists
    - Add createTableUsers
    - Add createTableLawsuits
    - Add createTableLawsuitsLawyersRelation
    - Add createTableLawsuitFiles

--- backend/src/database/postgreSQL/PostgreConnector.ts
+++ backend/src/database/postgreSQL/PostgreConnector.ts
@@ -6,6 +6,7 @@ config();
 export class PostgreConnector {
   static instance: PostgreConnector | null = null;
   private client: Client;
+  isConnected: boolean = false;
 
   private constructor(config: ClientConfig) {
     this.client = new Client(config);
@@ -30,23 +31,102 @@ export class PostgreConnector {
   }
   async connect() {
     try {
+      if (this.isConnected) return;
       await this.client.connect();
+      this.isConnected = true;
+      console.log('PostgreSQL Connected');
     } catch (error) {
       console.log(error);
       process.exit(1);
     }
   }
+
   async disconnect() {
+    if (!this.isConnected) return;
+    this.isConnected = false;
     await this.client.end();
   }
+
   async query(queryStream: any): Promise<QueryResult> {
+    if (!this.isConnected) {
+      throw Error('Could not execute query. PostgreSQL is not connected');
+    }
     return await this.client.query(queryStream);
   }
-  async resetTables() {
-    await this.query(`
-      DROP TABLE IF EXISTS cases;
-      
 
-      `);
+  async cleanTable(tableName: string) {
+    await this.query(`
+      DELETE FROM ${tableName};
+    `);
+  }
+
+  async dropTableIfExists(tableName: string) {
+    try {
+      await this.query(`DROP TABLE IF EXISTS ${tableName}`);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async createTableUsers() {
+    await this.query(` 
+      CREATE TABLE users (
+        id SERIAL PRIMARY KEY,
+        first_name TEXT NOT NULL,
+        last_name TEXT NOT NULL,
+        email TEXT UNIQUE NOT NULL,
+        cpf TEXT UNIQUE NOT NULL,
+        role TEXT CHECK (role IN ('client', 'lawyer', 'admin')) NOT NULL,
+        password TEXT NOT NULL,
+        created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+      );`);
+  }
+
+  async createTableLawsuits() {
+    await this.query(`
+      CREATE TABLE lawsuits (
+        id SERIAL PRIMARY KEY,
+        client_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+        process_number TEXT UNIQUE NOT NULL,
+        title TEXT NOT NULL,
+        description TEXT NOT NULL,
+        court TEXT NOT NULL,
+        court_division TEXT NOT NULL,
+        status  TEXT CHECK (status IN ('open', 'pending', 'closed')) NOT NULL,
+        created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+      );
+
+      CREATE INDEX idx_lawsuit s_client_id
+      ON lawsuit s(client_id);
+    `);
+  }
+
+  async createTableLawsuitsLawyersRelation() {
+    await this.query(`
+      CREATE TABLE lawsuits_lawyers_relation (
+        lawsuit_id INTEGER NOT NULL REFERENCES lawsuit s(id) ON DELETE CASCADE,
+        lawyer_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+        PRIMARY KEY (lawsuit_id, lawyer_id)
+      );
+
+      CREATE INDEX idx_lawsuits_lawyers_relation_lawyer_id
+      ON lawsuits_lawyers_relation(lawyer_id);
+    `);
+  }
+
+  async createTableLawsuitFiles() {
+    await this.query(`
+      CREATE TABLE lawsuit s_files (
+        id SERIAL PRIMARY KEY,
+        name TEXT NOT NULL,
+        url TEXT NOT NULL,
+        mime_type TEXT NOT NULL,
+        uploaded_by INTEGER REFERENCES users(id) ON DELETE SET NULL,
+        lawsuit _id INTEGER NOT NULL REFERENCES lawsuit s(id) ON DELETE CASCADE,
+        UNIQUE(url, lawsuit _id)
+      );
+    `);
   }
 }
