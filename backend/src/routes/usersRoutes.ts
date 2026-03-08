42804b19c8dcc513135ee741680484d103ef7ecd
DavidSheltonSF <david.faria012@gmail.com>
2026-03-08 20:45:26 -0300

    feat(routes): add userRoutes

---
+++ backend/src/routes/usersRoutes.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+import { IUserController } from '../controllers/user/IUserController';
+import { expressHttpAdapter } from './adapters/expressHttpAdapter';
+
+export function usersRoutes(router: Router, userController: IUserController) {
+  router.get('/api/users', expressHttpAdapter(userController.findAll));
+  router.get('/api/users/:id', expressHttpAdapter(userController.findById));
+  router.post('/api/users', expressHttpAdapter(userController.create));
+}
