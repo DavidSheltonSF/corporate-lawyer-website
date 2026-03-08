200dc6a8451c8793b75aacc70764b2a010fd56b4
DavidSheltonSF <david.faria012@gmail.com>
2026-03-08 20:46:04 -0300

    chore(configRouter): include users routes

--- backend/src/config/configRouter.ts
+++ backend/src/config/configRouter.ts
@@ -3,15 +3,19 @@ import { requireAuth } from '../middlewares/requireAuth';
 import { makeAuthController } from '../factories/controllers/makeAuthController';
 import { makeCaseController } from '../factories/controllers/makeCaseController';
 import { casesRoutes } from '../routes/casesRoutes';
+import { usersRoutes } from '../routes/usersRoutes';
+import { makeUserController } from '../factories/controllers/makeUserController';
 
 export function configRouter(app: Application) {
   const authController = makeAuthController();
   const caseController = makeCaseController();
+  const userController = makeUserController();
 
   const router = Router();
   router.get('/api/me', requireAuth, authController.getMe);
   router.post('/api/auth', authController.auth);
   casesRoutes(router, caseController);
+  usersRoutes(router, userController);
 
   app.use(router);
 }
