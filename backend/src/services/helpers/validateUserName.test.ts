bb427bd09c83a5da1eeb45482d93d0e3370d0d30
DavidSheltonSF <david.faria012@gmail.com>
2026-03-08 15:19:27 -0300

    feat(services/helpers): add validateUserName with tests

---
+++ backend/src/services/helpers/validateUserName.test.ts
@@ -0,0 +1,42 @@
+import { InvalidNameError } from '../../errors/domain/InvalidNameError';
+import { validateUserName } from './validateUserName';
+
+describe(`Testing ${validateUserName.name}`, () => {
+  function getThrownError(callBack: Function) {
+    try {
+      callBack();
+      return null;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  test('should not throw error when name is valid', () => {
+    const thrownError = getThrownError(() => validateUserName('Gustavo'));
+    expect(thrownError).toBeNull();
+  });
+
+  test('should throw error when name is less than 3 characters', () => {
+    const thrownError = getThrownError(() => validateUserName('U'));
+    expect(thrownError).toBeInstanceOf(InvalidNameError);
+  });
+
+  test('should throw error when name is more than 100 characters', () => {
+    let bigString = ``;
+    for (let i = 0; i < 102; i++) {
+      bigString += 'W';
+    }
+    const thrownError = getThrownError(() => validateUserName(bigString));
+    expect(thrownError).toBeInstanceOf(InvalidNameError);
+  });
+
+  test('should throw error when name has any numeric characters', () => {
+    const thrownError = getThrownError(() => validateUserName('Davi4'));
+    expect(thrownError).toBeInstanceOf(InvalidNameError);
+  });
+
+  test('should throw error when name has any special characters', () => {
+    const thrownError = getThrownError(() => validateUserName('Davi#'));
+    expect(thrownError).toBeInstanceOf(InvalidNameError);
+  });
+});
---
+++ backend/src/services/helpers/validateUserName.ts
@@ -0,0 +1,9 @@
+import { InvalidNameError } from '../../errors/domain/InvalidNameError';
+
+export function validateUserName(name: string) {
+  const regex = /^[A-Za-zÀ-ÿ\s']{2,100}$/;
+
+  if (!name.match(regex)) {
+    throw new InvalidNameError(name);
+  }
+}
