62f6138d30b1cfb3c8d384de9a3cf3c9e73516ef
DavidSheltonSF <david.faria012@gmail.com>
2026-03-08 19:38:44 -0300

    chore(backend): delete unnecessary type HttpCode and HttpResponse

--- backend/src/factories/HttpResponse/HttpCode.ts
+++
@@ -1,9 +0,0 @@
-export enum HttpCode {
-  ok = 'OK',
-  created = 'CREATED',
-  unprocessable_entity = 'UNPROCESSABLE_ENTITY',
-  not_found = 'NOT_FOUND',
-  unouthorized = 'UNAUTHORIZED',
-  bad_request = 'BAD_REQUEST',
-  server_error = 'SERVER_ERROR',
-}
--- backend/src/factories/HttpResponse/HttpResponse.ts
+++
@@ -1,7 +0,0 @@
-import { HttpCode } from './HttpCode';
-
-export interface HttpResponse<T> {
-  data?: T;
-  message?: string;
-  code: HttpCode;
-}
