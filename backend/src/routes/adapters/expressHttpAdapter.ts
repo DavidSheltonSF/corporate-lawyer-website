750dbb513eba5553e1c248b279d6a7ad6dc46e69
DavidSheltonSF <david.faria012@gmail.com>
2026-03-08 20:32:50 -0300

    fix(expressHttpAdapter): change return type to Promise<HttpResponse<T>> and add await before assigning 'result'

--- backend/src/routes/adapters/expressHttpAdapter.ts
+++ backend/src/routes/adapters/expressHttpAdapter.ts
@@ -1,18 +1,18 @@
-import { Request, Response, type RequestHandler } from 'express';
+import { Request, Response } from 'express';
 import { HttpRequest } from '../../controllers/types/HttpRequest';
 import { HttpResponse } from '../../controllers/types/HttpResponse';
 
 export function expressHttpAdapter<T>(
-  controllerHandler: (httpRequest: HttpRequest) => HttpResponse<T>
-): RequestHandler {
-  return (req: Request, res: Response) => {
+  controllerHandler: (httpRequest: HttpRequest) => Promise<HttpResponse<T>>
+) {
+  return async (req: Request, res: Response) => {
     const httpRequest: HttpRequest = {
       body: req.body,
       params: req.params,
       query: req.query,
     };
 
-    const result = controllerHandler(httpRequest);
+    const result = await controllerHandler(httpRequest);
     return res.status(result.status).json(result);
   };
 }
