f599f81a9c23c1780f280cda73ce33704d422568
DavidSheltonSF <david.faria012@gmail.com>
2026-03-09 14:52:43 -0300

    refactor(HttpRequest): add file and user optional properties

--- backend/src/controllers/types/HttpRequest.ts
+++ backend/src/controllers/types/HttpRequest.ts
@@ -1,5 +1,7 @@
-export interface HttpRequest{
-  body?: any,
-  params?: any
-  query?: any
+export interface HttpRequest {
+  body?: any;
+  params?: any;
+  query?: any;
+  file?: any;
+  user?: { id: string; email: string };
 }
