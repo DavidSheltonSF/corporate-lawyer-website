652e8f423fb0f49518a5cc5a303685edfa3c827b
DavidSheltonSF <david.faria012@gmail.com>
2026-03-09 14:58:34 -0300

    refactor(expressHttpAdapter): adapt possible req.user field

--- backend/src/routes/adapters/expressHttpAdapter.ts
+++ backend/src/routes/adapters/expressHttpAdapter.ts
@@ -1,15 +1,19 @@
 import { Request, Response } from 'express';
 import { HttpRequest } from '../../controllers/types/HttpRequest';
 import { HttpResponse } from '../../controllers/types/HttpResponse';
+import { AuthenticatedUser } from '../../types/AuthenticatedUser';
 
 export function expressHttpAdapter<T>(
   controllerHandler: (httpRequest: HttpRequest) => Promise<HttpResponse<T>>
 ) {
   return async (req: Request, res: Response) => {
+    const authReq = req as Request & AuthenticatedUser;
     const httpRequest: HttpRequest = {
-      body: req.body,
-      params: req.params,
-      query: req.query,
+      body: authReq.body,
+      params: authReq.params,
+      query: authReq.query,
+      file: authReq.file,
+      user: authReq.user,
     };
 
     const result = await controllerHandler(httpRequest);
