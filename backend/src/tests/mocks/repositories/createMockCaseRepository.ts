71221974e2549a0c05f99f6d08fa14df4ec29f77
DavidSheltonSF <david.faria012@gmail.com>
2026-03-09 15:57:48 -0300

    chore(createMockCaseRepository): rename findCaseCards to findCases for matching recent changes in the repository

--- backend/src/tests/mocks/repositories/createMockCaseRepository.ts
+++ backend/src/tests/mocks/repositories/createMockCaseRepository.ts
@@ -4,7 +4,7 @@ import { mockCaseMongoDocs } from '../mockCaseMongoDocs';
 export const createMockCaseRepository = (): CaseRepository => {
   return {
     findById: jest.fn().mockResolvedValue(mockCaseMongoDocs[0]!),
-    findCaseCards: jest.fn().mockResolvedValue(mockCaseMongoDocs),
+    findCases: jest.fn().mockResolvedValue(mockCaseMongoDocs),
     create: jest.fn().mockResolvedValue(mockCaseMongoDocs[0]!),
     getStats: jest.fn().mockResolvedValue({ inProgress: 2, closed: 3 }),
     exists: jest.fn().mockResolvedValue(true),
