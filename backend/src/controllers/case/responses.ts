57150d765e52ab94648e443a319a537a5bb8ba2d
DavidSheltonSF <david.faria012@gmail.com>
2026-03-10 18:09:30 -0300

    feat(controllers/case): add FindByClientResponse, FindByIdResponse, CreateResponse, GetMyStatsResponse, FindFilesByCaseIdResponse

---
+++ backend/src/controllers/case/responses.ts
@@ -0,0 +1,14 @@
+import { CaseCardDTO } from '../../dtos/case/CaseCardDTO';
+import { CaseFileDTO } from '../../dtos/caseFile/CaseFileDTO';
+import { CaseResponseDTO } from '../../dtos/user/CaseResponseDTO';
+import { CaseStats } from '../../types/CaseStats';
+import { Page } from '../../types/Page';
+import { WithId } from '../../types/WithId';
+import { HttpResponse } from '../types/HttpResponse';
+
+export type FindByClientResponse = HttpResponse<Page<WithId<CaseCardDTO>> | null>;
+export type FindByIdResponse = HttpResponse<WithId<CaseCardDTO> | null>;
+export type CreateResponse = HttpResponse<WithId<CaseResponseDTO> | null>;
+export type GetMyStatsResponse = HttpResponse<CaseStats | null>;
+export type AddFileResponse = HttpResponse<void | null>;
+export type FindFilesByCaseId = HttpResponse<WithId<CaseFileDTO>[] | null>;
