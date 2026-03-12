97403726443a1a1bb6cdce86047d03a7610e1804
DavidSheltonSF <david.faria012@gmail.com>
2026-03-12 15:08:24 -0300

    feat(backend/errors): add InvalidEmailError

---
+++ backend/src/errors/domain/InvalidEmailError.ts.ts
@@ -0,0 +1,6 @@
+export class InvalidEmailError extends Error {
+  constructor(email: string) {
+    super(`Email '${email} is invalid. Expected format: example@email.com`);
+    this.name = InvalidEmailError.name;
+  }
+}
