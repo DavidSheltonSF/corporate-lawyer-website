d644c0ca9ef5212792948da291ed78069423af6f
DavidSheltonSF <david.faria012@gmail.com>
2026-03-12 17:26:02 -0300

    chore(backend/validateCPF): rename email param to cpf for consistency

--- backend/src/services/validators/validateCPF.ts
+++ backend/src/services/validators/validateCPF.ts
@@ -1,8 +1,8 @@
 import { InvalidCPFError } from '../../errors/domain/InvalidCPFError';
 
-export function validateCPF(email: string) {
+export function validateCPF(cpf: string) {
   const regex = /^\d{3}\.?\d{3}\.?\d{3}\-?\d{2}$/;
-  if (!regex.test(email)) {
-    throw new InvalidCPFError(email);
+  if (!regex.test(cpf)) {
+    throw new InvalidCPFError(cpf);
   }
 }
