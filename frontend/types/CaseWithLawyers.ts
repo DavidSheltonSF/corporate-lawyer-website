a40dc20d97c49609f7abda3556af880192b16011
DavidSheltonSF <david.faria012@gmail.com>
2026-03-12 13:42:10 -0300

    chore(frontend/types): remove unused CaseWithLawyers

--- frontend/types/CaseWithLawyers.ts
+++
@@ -1,7 +0,0 @@
-import { Case } from './Case';
-import { UserIdentity } from './UserIdentity';
-import { WithId } from './WithId';
-
-export type CaseWithLawyers = Case & {
-  lawyers: WithId<UserIdentity>[];
-};
