23d728499a921117e6a422965606140d34ae241c
DavidSheltonSF <david.faria012@gmail.com>
2026-03-12 16:05:38 -0300

    feat(backend/services/validators): add validatePassword

---
+++ backend/src/services/validators/validatePassword.test.ts
@@ -0,0 +1,33 @@
+import { InvalidPasswordError } from '../../errors/domain/InvalidPasswordError';
+import { validatePassword } from './validatePassword';
+
+describe(`Testing ${validatePassword.name}`, () => {
+  function getThrownError(callBack: Function) {
+    try {
+      callBack();
+      return null;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  test('should not throw error when password is valid', () => {
+    const thrownError1 = getThrownError(() => validatePassword('Mauro#123'));
+    const thrownError2 = getThrownError(() => validatePassword('Vigo@123'));
+    const thrownError3 = getThrownError(() => validatePassword('Tamiriocho123!'));
+    expect(thrownError1).toBeNull();
+    expect(thrownError2).toBeNull();
+    expect(thrownError3).toBeNull();
+  });
+
+  test('should throw InvalidPasswordError if role provided is invalid', () => {
+    const thrownError1 = getThrownError(() => validatePassword('mario010203'));
+    const thrownError2 = getThrownError(() => validatePassword('Tamiriocho123515160'));
+    const thrownError3 = getThrownError(() => validatePassword('##Gdagnuenskalgafga'));
+    const thrownError4 = getThrownError(() => validatePassword(''));
+    expect(thrownError1).toBeInstanceOf(InvalidPasswordError);
+    expect(thrownError2).toBeInstanceOf(InvalidPasswordError);
+    expect(thrownError3).toBeInstanceOf(InvalidPasswordError);
+    expect(thrownError4).toBeInstanceOf(InvalidPasswordError);
+  });
+});
---
+++ backend/src/services/validators/validatePassword.ts
@@ -0,0 +1,8 @@
+import { InvalidPasswordError } from '../../errors/domain/InvalidPasswordError';
+
+export function validatePassword(password: string) {
+  const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
+  if (!regex.test(password)) {
+    throw new InvalidPasswordError(password);
+  }
+}
