def6f0e341ae501c69e295f25083a2b70ef0470f
DavidSheltonSF <david.faria012@gmail.com>
2026-03-13 19:11:44 -0300

    feat(frontend/components): add InputWithLabel

---
+++ frontend/components/form/InputWithLabel.tsx
@@ -0,0 +1,21 @@
+interface Props {
+  id: string;
+  name: string;
+  label: string;
+}
+
+export function InputWithLabel({ id, name, label }: Props) {
+  return (
+    <div>
+      <label className="text-[1.3rem]" htmlFor="first-name-input">
+        {label}
+      </label>
+      <input
+        id={id}
+        name={name}
+        className="border h-[40px] w-full rounded-sm px-[8px]"
+        type="text"
+      />
+    </div>
+  );
+}
