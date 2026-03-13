55ab591f2eff8dd7d169f073933273098f6be0f1
DavidSheltonSF <david.faria012@gmail.com>
2026-03-13 18:42:47 -0300

    feat(frontend/components): add RequestFeedback

---
+++ frontend/components/form/RequestFeedback.tsx
@@ -0,0 +1,39 @@
+import { RequestState } from '@/types/RequestState';
+
+interface Props {
+  requestState: RequestState | null;
+}
+
+export function RequestFeedback({ requestState }: Props) {
+  if (!requestState) {
+    return null;
+  }
+
+  const { status, message } = requestState;
+
+  let color = '';
+  switch (status) {
+    case 'ok':
+      color = 'var(--color-ok)';
+      break;
+
+    case 'error':
+      color = 'var(--color-error)';
+      break;
+
+    default:
+      color = 'black';
+      break;
+  }
+
+  return (
+    <span
+      className="text-[1.2rem] font-bold"
+      style={{
+        color,
+      }}
+    >
+      {status === 'loading' ? 'Loading...' : message}
+    </span>
+  );
+}
