a7be7960cd6c8b27463d576e9cd6c66f484c52da
DavidSheltonSF <david.faria012@gmail.com>
2026-03-12 21:15:40 -0300

    feat(backend/usersRoutes): add endpoint to get all clients

--- backend/src/routes/usersRoutes.ts
+++ backend/src/routes/usersRoutes.ts
@@ -4,6 +4,7 @@ import { expressHttpAdapter } from './adapters/expressHttpAdapter';
 
 export function usersRoutes(router: Router, userController: IUserController) {
   router.get('/api/users', expressHttpAdapter(userController.findAll));
+  router.get('/api/clients', expressHttpAdapter(userController.findClients));
   router.get('/api/users/:id', expressHttpAdapter(userController.findById));
   router.post('/api/users', expressHttpAdapter(userController.create));
 }
