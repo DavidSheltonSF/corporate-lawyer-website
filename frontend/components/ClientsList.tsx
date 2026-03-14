c5ddf3605883032bee9ad179d747d4c29040be78
DavidSheltonSF <david.faria012@gmail.com>
2026-03-14 18:29:37 -0300

    refactor(frontend/components):  add ClientsCardOptionsModal to ClientsList

--- frontend/components/ClientsList.tsx
+++ frontend/components/ClientsList.tsx
@@ -1,9 +1,10 @@
 'use client';
 import { WithId } from '@/types/WithId';
-import { Activity } from 'react';
+import { Activity, useState } from 'react';
 import { CaseCardSkeleton } from './CaseCardSkeleton';
 import { ClientCard } from './ClientCard';
 import { SafeUser } from '@/types/SafeUser';
+import { ClientCardOptionsModal } from './modals/ClientCardOptionsModal';
 
 interface Props {
   clients: WithId<SafeUser>[];
@@ -11,8 +12,17 @@ interface Props {
 }
 
 export function ClientsList({ clients, loading }: Props) {
+  const [optionsModalIsOpen, setOptionsModalIsOpen] = useState(false);
+
   const renderCases = clients?.map((client, index) => {
-    return <ClientCard key={index} clientData={client} />;
+    return (
+      <ClientCard
+        optionsModalIsOpen={optionsModalIsOpen}
+        setOptionsModalIsOpen={setOptionsModalIsOpen}
+        key={index}
+        clientData={client}
+      />
+    );
   });
 
   const renderCaseSkeletons = Array.from({ length: 4 }).map((page, index) => {
@@ -20,6 +30,7 @@ export function ClientsList({ clients, loading }: Props) {
   });
   return (
     <div className="flex flex-col gap-[32px] mt-[88px] w-full">
+      <ClientCardOptionsModal isOpen={optionsModalIsOpen} setIsOpen={setOptionsModalIsOpen} />
       <Activity mode={!loading && (!clients || clients.length === 0) ? 'visible' : 'hidden'}>
         <h1 className="text-3xl">Nenhum cliente encontrado</h1>
       </Activity>
