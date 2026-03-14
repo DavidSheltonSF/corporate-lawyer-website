784aa7217ec44c72428a185d966172293c382316
DavidSheltonSF <david.faria012@gmail.com>
2026-03-14 18:29:03 -0300

    refactor(frontend/components):  add button to open modal in ClientCard
    - Added optionsModalIsOpen prop
    - Added setOptionsModalIsOpen props
    - Added button with VerticalMoreIcon

--- frontend/components/ClientCard.tsx
+++ frontend/components/ClientCard.tsx
@@ -1,12 +1,16 @@
 import { WithId } from '@/types/WithId';
 import { FieldValue } from './FieldValue';
 import { SafeUser } from '@/types/SafeUser';
+import { VerticalMoreIcon } from './icons/VerticalMoreIcon';
+import { Dispatch, SetStateAction } from 'react';
 
 interface Props {
   clientData: WithId<SafeUser>;
+  optionsModalIsOpen: boolean;
+  setOptionsModalIsOpen: Dispatch<SetStateAction<boolean>>;
 }
 
-export function ClientCard({ clientData }: Props) {
+export function ClientCard({ clientData, optionsModalIsOpen, setOptionsModalIsOpen }: Props) {
   const { id, firstName, lastName, email, cpf, role } = clientData;
 
   // const userModalContext = useCaseModalContext();
@@ -25,10 +29,13 @@ export function ClientCard({ clientData }: Props) {
       }}
       className="flex flex-col fade-in-animation  bg-color-primary w-full min-md:w-[80%] min-lg:w-[640px] min-h-[280px] h-max rounded-xl cursor-pointer"
     >
-      <header className="flex items-center justify-center min-md:justify-start w-full p-[16px] min-md:p-[24px]">
+      <header className="flex items-center justify-between w-full p-[16px] min-md:p-[24px]">
         <h1 className="h-fit font-bold text-center min-md:text-start text-xl min-md:text-3xl">
           {`${firstName} ${lastName}`}
         </h1>
+        <button onClick={() => setOptionsModalIsOpen(true)}>
+          <VerticalMoreIcon color="var(--white-color)" height="32px" width="32px" />
+        </button>
       </header>
       <main
         className="flex flex-1 flex-col gap-[16px] px-[24px] py-[16px] bg-color-white text-color-black text-lg"
