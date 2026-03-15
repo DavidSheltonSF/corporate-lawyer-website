8bdb8108331d528296b0f585bb4a62290ecb3d27
DavidSheltonSF <david.faria012@gmail.com>
2026-03-15 17:48:49 -0300

    feat(frontend/services): add deleteClients

---
+++ frontend/services/deleteClient.ts
@@ -0,0 +1,22 @@
+import { API_URL } from '@/config/api';
+import { getTokenFromCookies } from '@/lib/getTokenFromCookies';
+import { SafeUser } from '@/types/SafeUser';
+import { WithId } from '@/types/WithId';
+
+export async function deleteClient(id: string): Promise<WithId<SafeUser>> {
+  const token = await getTokenFromCookies();
+  const response = await fetch(`${API_URL}/clients/${id}`, {
+    headers: {
+      Authorization: token,
+    },
+    method: 'DELETE',
+  });
+
+  const json = await response.json();
+
+  if (!response.ok) {
+    throw Error(json.message);
+  }
+
+  return json.data;
+}
