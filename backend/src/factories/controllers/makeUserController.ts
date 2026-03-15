0f5067c7cb6d3c9e4c6e22036a93d35e8259f0c4
DavidSheltonSF <david.faria012@gmail.com>
2026-03-15 19:57:53 -0300

    feat(backend/factories/controllers/makeUserController): include case repository in user service initialization

--- backend/src/factories/controllers/makeUserController.ts
+++ backend/src/factories/controllers/makeUserController.ts
@@ -2,9 +2,11 @@ import { UserController } from '../../controllers/user/UserController';
 import { IUserController } from '../../controllers/user/IUserController';
 import { MongodbUserRepository } from '../../database/mongoDB/repositories/MongodbUserRepository';
 import { UserService } from '../../services/user/UserService';
+import { MongodbCaseRepository } from '../../database/mongoDB/repositories/MongodbCaseRepository';
 
 export function makeUserController(): IUserController {
   const userRepository = new MongodbUserRepository();
-  const userService = new UserService(userRepository);
+  const caseRepository = new MongodbCaseRepository();
+  const userService = new UserService(userRepository, caseRepository);
   return new UserController(userService);
 }
