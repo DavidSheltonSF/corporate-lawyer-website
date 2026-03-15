d19ed78befe4a75e3b51750384442bcdc70fd200
DavidSheltonSF <david.faria012@gmail.com>
2026-03-15 17:49:51 -0300

    refactor(frontend/modals/ClientCardOptionsModal): add client deletion feature
    - Added selectedUserId prop
    - Added onDeleteClick function that calls deleteClient

--- frontend/components/modals/ClientCardOptionsModal.tsx
+++ frontend/components/modals/ClientCardOptionsModal.tsx
@@ -1,26 +1,55 @@
 'use client';
 import { PrimaryModalWindow } from './PrimaryModalWindow';
 import { Button } from '../Button';
+import { deleteClient } from '@/services/deleteClient';
+import { useState } from 'react';
+import { RequestState } from '@/types/RequestState';
+import { RequestFeedback } from '../form/RequestFeedback';
 
 interface Props {
   isOpen: boolean;
   closeModal: Function;
+  selectedUserId: string | null;
 }
 
-export function ClientCardOptionsModal({ isOpen, closeModal }: Props) {
+export function ClientCardOptionsModal({ isOpen, closeModal, selectedUserId }: Props) {
+  const [requestState, setRequestState] = useState<RequestState | null>(null);
+
+  async function onDeleteClick() {
+    try {
+      setRequestState({ status: 'loading' });
+      const result = await deleteClient(selectedUserId || '');
+      setRequestState({
+        status: 'ok',
+        message: `${result.firstName} ${result.lastName} was deleted successfully`,
+      });
+    } catch (error: any) {
+      console.log(error);
+      setRequestState({ status: 'error', message: error.message });
+    }
+  }
+
   return (
     isOpen && (
       <PrimaryModalWindow
         additionalStyles="fixed z-99999999999 top-[15%] left-1/2 translate-x-[-50%] w-[360px] h-fit rounded-lg overflow-hidden shadow-[0px_0px__3px_black]"
         closeModal={() => {
+          setRequestState(null);
           closeModal();
         }}
       >
         <div className="size-full flex flex-col text-center items-center justify-center gap-[8px] p-[8px]">
+          <div className="my-[24px]">
+            <RequestFeedback requestState={requestState} />
+          </div>
           <Button backgroundColor="var(--primary-color)" textColor="var(--white-color)">
             Alterar Dados
           </Button>
-          <Button backgroundColor="var(--primary-color)" textColor="var(--white-color)">
+          <Button
+            onclick={onDeleteClick}
+            backgroundColor="var(--primary-color)"
+            textColor="var(--white-color)"
+          >
             Deletar Cliente
           </Button>
         </div>
