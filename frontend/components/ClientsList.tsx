10dd3412cd347446626ac3c2455b1dc775da9a46
DavidSheltonSF <david.faria012@gmail.com>
2026-03-15 17:52:08 -0300

    feat(frontend/components/ClientsList): enhance client options modal handling
    - Add openOptionsModal function
    - Add closeOptionsModal function
    - Pass openOptionsModal to ClientCard
    - Pass closeOptionsModal and selectedUser TO  ClientCardOptionsModal

--- frontend/components/ClientsList.tsx
+++ frontend/components/ClientsList.tsx
@@ -13,16 +13,20 @@ interface Props {
 
 export function ClientsList({ clients, loading }: Props) {
   const [optionsModalIsOpen, setOptionsModalIsOpen] = useState(false);
+  const [selectedUserId, setSelectedUserId] = useState<string | null>(null);
+
+  const openOptionsModal = (id: string) => {
+    setSelectedUserId(id);
+    setOptionsModalIsOpen(true);
+  };
+
+  const closeOptionsModal = () => {
+    setSelectedUserId(null);
+    setOptionsModalIsOpen(false);
+  };
 
   const renderCases = clients?.map((client, index) => {
-    return (
-      <ClientCard
-        optionsModalIsOpen={optionsModalIsOpen}
-        setOptionsModalIsOpen={setOptionsModalIsOpen}
-        key={index}
-        clientData={client}
-      />
-    );
+    return <ClientCard openOptionsModal={openOptionsModal} key={index} clientData={client} />;
   });
 
   const renderCaseSkeletons = Array.from({ length: 4 }).map((page, index) => {
@@ -30,7 +34,11 @@ export function ClientsList({ clients, loading }: Props) {
   });
   return (
     <div className="flex flex-col gap-[32px] mt-[88px] w-full">
-      <ClientCardOptionsModal isOpen={optionsModalIsOpen} setIsOpen={setOptionsModalIsOpen} />
+      <ClientCardOptionsModal
+        isOpen={optionsModalIsOpen}
+        closeModal={closeOptionsModal}
+        selectedUserId={selectedUserId}
+      />
       <Activity mode={!loading && (!clients || clients.length === 0) ? 'visible' : 'hidden'}>
         <h1 className="text-3xl">Nenhum cliente encontrado</h1>
       </Activity>
