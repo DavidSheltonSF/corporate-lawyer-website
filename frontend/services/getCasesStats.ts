0c07828d408638b5825c8a6efbc351ed850eee82
DavidSheltonSF <david.faria012@gmail.com>
2026-03-16 01:18:06 -0300

    feat(frontend/services): add getCasesStats

---
+++ frontend/services/getCasesStats.ts
@@ -0,0 +1,21 @@
+import { API_URL } from '@/config/api';
+import { getTokenFromCookies } from '@/lib/getTokenFromCookies';
+import { CasesStats } from '@/types/CasesStats';
+
+export async function getCasesStats(): Promise<CasesStats> {
+  const token = await getTokenFromCookies();
+
+  const response = await fetch(`${API_URL}/cases/stats`, {
+    headers: {
+      Authorization: token,
+    },
+  });
+
+  if (!response.ok) {
+    throw Error(await response.text());
+  }
+
+  const json = await response.json();
+
+  return json.data;
+}
