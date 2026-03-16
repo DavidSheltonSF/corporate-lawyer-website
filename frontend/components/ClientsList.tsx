5e1d3b15bd6e095da7928a4a7c7694023f2828e9
DavidSheltonSF <david.faria012@gmail.com>
2026-03-16 13:42:00 -0300

    refactor(components/ClientsList): simplify modal handling by integrating ClientCardModalsProvider

--- frontend/components/ClientsList.tsx
+++ frontend/components/ClientsList.tsx
@@ -1,11 +1,13 @@
 'use client';
 import { WithId } from '@/types/WithId';
-import { Activity, Dispatch, SetStateAction, useState } from 'react';
+import { Activity, useState } from 'react';
 import { CaseCardSkeleton } from './CaseCardSkeleton';
 import { ClientCard } from './ClientCard';
 import { SafeUser } from '@/types/SafeUser';
 import { ClientCardOptionsModal } from './modals/ClientCardOptionsModal';
 import { RequestState } from '@/types/RequestState';
+import { DeleteClientModal } from './modals/DeleteClientModal';
+import { ClientCardModalsProvider } from '@/contexts/modals/ClientCardModalsProvider';
 
 interface Props {
   clients: WithId<SafeUser>[];
@@ -14,21 +16,8 @@ interface Props {
 }
 
 export function ClientsList({ clients, requestState, loadClients }: Props) {
-  const [optionsModalIsOpen, setOptionsModalIsOpen] = useState(false);
-  const [selectedUserId, setSelectedUserId] = useState<string | null>(null);
-
-  const openOptionsModal = (id: string) => {
-    setSelectedUserId(id);
-    setOptionsModalIsOpen(true);
-  };
-
-  const closeOptionsModal = () => {
-    setSelectedUserId(null);
-    setOptionsModalIsOpen(false);
-  };
-
   const renderCases = clients?.map((client, index) => {
-    return <ClientCard openOptionsModal={openOptionsModal} key={index} clientData={client} />;
+    return <ClientCard key={index} clientData={client} />;
   });
 
   const renderCaseSkeletons = Array.from({ length: 4 }).map((page, index) => {
@@ -51,16 +40,14 @@ export function ClientsList({ clients, requestState, loadClients }: Props) {
 
   return (
     <div className="flex flex-col gap-[32px] mt-[88px] w-full">
-      <ClientCardOptionsModal
-        isOpen={optionsModalIsOpen}
-        closeModal={closeOptionsModal}
-        selectedUserId={selectedUserId}
-        loadClients={loadClients}
-      />
-      <Activity mode={!isLoading ? 'visible' : 'hidden'}>
-        <h1 className="text-3xl">{message}</h1>
-      </Activity>
-      {isLoading ? renderCaseSkeletons : renderCases}
+      <ClientCardModalsProvider>
+        <ClientCardOptionsModal />
+        <DeleteClientModal loadClients={loadClients} />
+        <Activity mode={!isLoading ? 'visible' : 'hidden'}>
+          <h1 className="text-3xl">{message}</h1>
+        </Activity>
+        {isLoading ? renderCaseSkeletons : renderCases}
+      </ClientCardModalsProvider>
     </div>
   );
 }
