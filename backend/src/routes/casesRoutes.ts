5c1a7addaa396239bc31bdb0275cfdea13c82f41
DavidSheltonSF <david.faria012@gmail.com>
2026-03-16 01:17:41 -0300

    feat(caseRoutes): add route to get global case statistics

--- backend/src/routes/casesRoutes.ts
+++ backend/src/routes/casesRoutes.ts
@@ -5,8 +5,10 @@ import { upload } from '../middlewares/uploadFile';
 import { expressHttpAdapter } from './adapters/expressHttpAdapter';
 
 export function casesRoutes(router: Router, caseController: ICaseController) {
-  router.get('/api/cases/:id', expressHttpAdapter(caseController.findById));
+  
   router.get('/api/my/cases/stats', requireAuth, expressHttpAdapter(caseController.getMyStats));
+  router.get('/api/cases/stats', requireAuth, expressHttpAdapter(caseController.getStats));
+  router.get('/api/cases/:id', expressHttpAdapter(caseController.findById));
   router.get('/api/my/cases', requireAuth, expressHttpAdapter(caseController.findMyCases));
   router.get(
     '/api/my/cases/:id/caseFiles',
