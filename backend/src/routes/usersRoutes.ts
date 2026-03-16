6bb61baeb56da903043d1c74ffc84fc892a77800
DavidSheltonSF <david.faria012@gmail.com>
2026-03-16 01:26:29 -0300

    feat(backend/routes): add POST route for creating clients in usersRoutes

--- backend/src/routes/usersRoutes.ts
+++ backend/src/routes/usersRoutes.ts
@@ -6,6 +6,7 @@ import { requireAuth } from '../middlewares/requireAuth';
 export function usersRoutes(router: Router, userController: IUserController) {
   router.get('/api/users', expressHttpAdapter(userController.findAll));
   router.get('/api/clients', expressHttpAdapter(userController.findClients));
+  router.post('/api/clients', expressHttpAdapter(userController.createClient));
   router.get('/api/users/:id', expressHttpAdapter(userController.findById));
   router.delete('/api/clients/:id', requireAuth, expressHttpAdapter(userController.deleteById));
 }
