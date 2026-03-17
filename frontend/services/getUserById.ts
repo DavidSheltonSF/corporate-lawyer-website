4be0039114b328402302ecd6cbc856ee55987695
DavidSheltonSF <david.faria012@gmail.com>
2026-03-17 15:28:54 -0300

    refactor(frontend/services/getUserById): remove error handling
    Services shouldn't handle erros, they should thrown them

--- frontend/services/getUserById.ts
+++ frontend/services/getUserById.ts
@@ -5,24 +5,19 @@ import { User } from '@/types/User';
 import { WithId } from '@/types/WithId';
 
 export async function getUserById(id: string): Promise<WithId<User>> {
-  try {
-    if (!id) {
-      throw new MissingRequiredArgumentError('getUserById', 'id');
-    }
-
-    const response = await fetch(`${API_URL}/users/${id}`);
-
-    if (!response.ok) {
-      throw new Error(await response.text().catch(() => 'Unknown Error'));
-    }
-    const json = await response.json();
-
-    if (!json?.data) {
-      throw new InvalidAPIResponseError('Missing data', json);
-    }
-    return json.data;
-  } catch (error) {
-    console.log(error);
-    throw Error('Could not get user by id', { cause: error });
+  if (!id) {
+    throw new MissingRequiredArgumentError('getUserById', 'id');
   }
+
+  const response = await fetch(`${API_URL}/users/${id}`);
+
+  if (!response.ok) {
+    throw new Error(await response.text().catch(() => 'Unknown Error'));
+  }
+  const json = await response.json();
+
+  if (!json?.data) {
+    throw new InvalidAPIResponseError('Missing data', json);
+  }
+  return json.data;
 }
