135317710c2cd75867e1afcb23261045bfd49a7e
DavidSheltonSF <david.faria012@gmail.com>
2026-03-28 12:54:38 -0300

    chore(backend/userRoutes): remove unused /api/users endpoint

--- backend/src/routes/usersRoutes.ts
+++ backend/src/routes/usersRoutes.ts
@@ -4,7 +4,6 @@ import { expressHttpAdapter } from './adapters/expressHttpAdapter';
 import { requireAuth } from '../middlewares/requireAuth';
 
 export function usersRoutes(router: Router, userController: IUserController) {
-  router.get('/api/users', expressHttpAdapter(userController.findAll)); //remove
   router.get('/api/clients', expressHttpAdapter(userController.findClients));
   router.post('/api/clients', expressHttpAdapter(userController.createClient));
   router.get('/api/users/:id', expressHttpAdapter(userController.findById));
