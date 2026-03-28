06759487b6053b57688be7fa7a45d71824e930b6
DavidSheltonSF <david.faria012@gmail.com>
2026-03-28 12:46:42 -0300

    chore(backend/userRoutes): rename PUT /api/clients/:id to /api/users/:id and DELETE /api/clients/:id to /api/users/:id for clarity

--- backend/src/routes/usersRoutes.ts
+++ backend/src/routes/usersRoutes.ts
@@ -4,11 +4,11 @@ import { expressHttpAdapter } from './adapters/expressHttpAdapter';
 import { requireAuth } from '../middlewares/requireAuth';
 
 export function usersRoutes(router: Router, userController: IUserController) {
-  router.get('/api/users', expressHttpAdapter(userController.findAll));
+  router.get('/api/users', expressHttpAdapter(userController.findAll)); //remove
   router.get('/api/clients', expressHttpAdapter(userController.findClients));
   router.post('/api/clients', expressHttpAdapter(userController.createClient));
   router.get('/api/users/:id', expressHttpAdapter(userController.findById));
   router.get('/api/clients/:id', requireAuth, expressHttpAdapter(userController.findClientById));
-  router.put('/api/clients/:id', requireAuth, expressHttpAdapter(userController.updateById));
-  router.delete('/api/clients/:id', requireAuth, expressHttpAdapter(userController.deleteById));
+  router.put('/api/users/:id', requireAuth, expressHttpAdapter(userController.updateById));
+  router.delete('/api/users/:id', requireAuth, expressHttpAdapter(userController.deleteById));
 }
