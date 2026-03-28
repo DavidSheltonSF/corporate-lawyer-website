ef9c9785679018cbc5bccc4372ac734ca2b3b7a5
DavidSheltonSF <david.faria012@gmail.com>
2026-03-28 19:07:44 -0300

    feat(casesRoutes): add POST /api/cases endpoint

--- backend/src/routes/casesRoutes.ts
+++ backend/src/routes/casesRoutes.ts
@@ -5,7 +5,7 @@ import { upload } from '../middlewares/uploadFile';
 import { expressHttpAdapter } from './adapters/expressHttpAdapter';
 
 export function casesRoutes(router: Router, caseController: ICaseController) {
-  
+  router.post('/api/cases', requireAuth, expressHttpAdapter(caseController.create));
   router.get('/api/my/cases/stats', requireAuth, expressHttpAdapter(caseController.getMyStats));
   router.get('/api/cases/stats', requireAuth, expressHttpAdapter(caseController.getStats));
   router.get('/api/cases/:id', expressHttpAdapter(caseController.findById));
