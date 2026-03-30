989317a5a5a6c4c19b6a57d25f279348f2816340
DavidSheltonSF <david.faria012@gmail.com>
2026-03-30 16:54:07 -0300

    chore(frontend/CasesList):  Centralize CaseModal related states to avoid using context
    - Add selectedCaseId and caseModalIsOpen states
    
    - Pass setSelectedCaseId to  CaseCard by props
    
    - Pass a function that opnes the CaseModel to CaseCard's openCaseModal props
    
    - Pass selectedCaseId, caseModalIsOpen and setCaseModalIsOPen to CaseModal by props

--- frontend/components/CasesList.tsx
+++ frontend/components/CasesList.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { CaseCard } from './CaseCard';
 import { WithId } from '@/types/WithId';
-import { Activity } from 'react';
+import { Activity, useState } from 'react';
 import { CaseCardSkeleton } from './CaseCardSkeleton';
 import { CaseWithRelations } from '@/types/CaseWithRelations';
 import { CaseModal } from './modals/CaseModal';
@@ -12,8 +12,17 @@ interface Props {
 }
 
 export function CasesList({ cases, loading }: Props) {
+  const [selectedCaseId, setSelectedCaseId] = useState<string | null>(null);
+  const [caseModalIsOpen, setCaseModalIsOpen] = useState(false);
   const renderCases = cases?.map((cas, index) => {
-    return <CaseCard key={index} caseData={cas} />;
+    return (
+      <CaseCard
+        setSelectedCaseId={setSelectedCaseId}
+        openCaseModal={() => setCaseModalIsOpen(true)}
+        key={index}
+        caseData={cas}
+      />
+    );
   });
 
   const renderCaseSkeletons = Array.from({ length: 4 }).map((page, index) => {
@@ -21,7 +30,11 @@ export function CasesList({ cases, loading }: Props) {
   });
   return (
     <div className="flex flex-col gap-[32px] mt-[88px] w-full">
-      <CaseModal />
+      <CaseModal
+        selectedCaseId={selectedCaseId}
+        isOpen={caseModalIsOpen}
+        setIsOpen={setCaseModalIsOpen}
+      />
       <Activity mode={!loading && (!cases || cases.length === 0) ? 'visible' : 'hidden'}>
         <h1 className="text-3xl">Nenhum caso encontrado</h1>
       </Activity>
