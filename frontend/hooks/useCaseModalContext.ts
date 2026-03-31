ae626acdf448b188c532d99255e4a549c36cfefa
DavidSheltonSF <david.faria012@gmail.com>
2026-03-31 16:56:13 -0300

    chore(frontend/hooks): remove unused useCaseModalContext

--- frontend/hooks/useCaseModalContext.ts
+++
@@ -1,4 +0,0 @@
-import { CaseModalContext } from '@/contexts/modals/CaseModalContext';
-import { useContext } from 'react';
-
-export const useCaseModalContext = (): CaseModalContext | undefined => useContext(CaseModalContext);
