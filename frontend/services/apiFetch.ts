bbb3b002e217d42d4955231568960605bdde640d
DavidSheltonSF <david.faria012@gmail.com>
2026-04-01 20:16:13 -0300

    feat(frontend/services): add apiFetch to centralize API error throwing  and feching stuff

---
+++ frontend/services/apiFetch.ts
@@ -0,0 +1,34 @@
+import { getTokenFromCookies } from '@/lib/getTokenFromCookies';
+import { handleLogout } from '@/lib/handleLogout';
+
+interface RequestInit {
+  headers?: {
+    'Content-type'?: string;
+    Authorization?: string;
+  };
+  method?: 'PUT' | 'POST' | 'GET' | 'DELETE';
+}
+
+export async function apiFetch(url: string, options: RequestInit): Promise<Response> {
+  const token = await getTokenFromCookies();
+
+  const response = await fetch(url, {
+    ...options,
+    headers: {
+      ...(options.headers || {}),
+      Authorization: token,
+    },
+  });
+
+  if (response.status === 401) {
+    handleLogout();
+    throw Error('Unouthorized');
+  }
+
+  if(!response.ok){
+    const json = await response.json();
+    throw Error(json.message)
+  }
+
+  return response;
+}
