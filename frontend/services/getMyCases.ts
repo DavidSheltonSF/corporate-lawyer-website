0808667f74b9be62ea77f697db23c8a3103b3212
DavidSheltonSF <david.faria012@gmail.com>
2026-04-01 20:19:13 -0300

    chore(frontend/services/getMyCases): use apiFetch to centralize fetching stuff

--- frontend/services/getMyCases.ts
+++ frontend/services/getMyCases.ts
@@ -4,6 +4,7 @@ import { getTokenFromCookies } from '@/lib/getTokenFromCookies';
 import { CaseWithRelations } from '@/types/CaseWithRelations';
 import { Page } from '@/types/Page';
 import { WithId } from '@/types/WithId';
+import { apiFetch } from './apiFetch';
 
 export async function getMyCases(
   queryParams: {
@@ -14,37 +15,27 @@ export async function getMyCases(
   },
   populate?: string[]
 ): Promise<Page<WithId<CaseWithRelations>>> {
-  try {
-    if (!queryParams) {
-      throw new MissingRequiredArgumentError(getMyCases.name, 'queryParams');
-    }
-
-    const { page, limit, query, status } = queryParams;
-
-    const baseRoute = `${API_URL}/my/cases`;
-    console.log(queryParams);
-
-    const queryString = `?page=${page}&limit=${limit || ''}&query=${query || ''}&status=${
-      status || ''
-    }&populate=${populate || ''}`;
-
-    const token = await getTokenFromCookies();
-
-    const response = await fetch(`${baseRoute}/${queryString}`, {
-      headers: {
-        Authorization: token,
-      },
-    });
-
-    if (!response.ok) {
-      throw Error(await response.text());
-    }
-
-    const responseJson = await response.json();
-
-    return responseJson.data;
-  } catch (error: any) {
-    console.log(error);
-    throw new Error(error);
+  if (!queryParams) {
+    throw new MissingRequiredArgumentError(getMyCases.name, 'queryParams');
   }
+
+  const { page, limit, query, status } = queryParams;
+
+  const baseRoute = `${API_URL}/my/cases`;
+
+  const queryString = `?page=${page}&limit=${limit || ''}&query=${query || ''}&status=${
+    status || ''
+  }&populate=${populate || ''}`;
+
+  const token = await getTokenFromCookies();
+
+  const response = await apiFetch(`${baseRoute}/${queryString}`, {
+    headers: {
+      Authorization: token,
+    },
+  });
+
+  const responseJson = await response.json();
+
+  return responseJson.data;
 }
