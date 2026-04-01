56b9d650eabef5f30c9175c596bd5044b6a59f0b
DavidSheltonSF <david.faria012@gmail.com>
2026-04-01 19:18:45 -0300

    chore(frontend/DropdownInputWithLabel): enhance UX
    - Make dropdown close when clicking outside
    
    - Add open-close transition
    
    - Add a ArrowDropUp icon

--- frontend/components/form/DropdownInputWithLabel.tsx
+++ frontend/components/form/DropdownInputWithLabel.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { ArrowDropUp } from '../icons/ArrowDropUp';
 
 interface Props {
   id: string;
@@ -11,6 +12,21 @@ interface Props {
 export function DropdownInputWithLabel({ id, name, label, items, itemLabel }: Props) {
   const [selectedValue, setSelectedValue] = useState(0);
   const [listIsOpen, setListIsOpen] = useState(false);
+  const dropRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    function handleClickOutside(e: MouseEvent) {
+      if (dropRef.current && !dropRef.current.contains(e.target as Node)) {
+        setListIsOpen(false);
+      }
+    }
+
+    document.addEventListener('click', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  });
 
   const renderItems = items.map((item, index) => {
     return (
@@ -19,7 +35,7 @@ export function DropdownInputWithLabel({ id, name, label, items, itemLabel }: Pr
           setSelectedValue(index);
           setListIsOpen(false);
         }}
-        className="bg-white hover:brightness-80 cursor-pointer"
+        className="bg-white hover:brightness-80 cursor-pointer px-[8px]"
         key={index}
       >
         {itemLabel(item)}
@@ -32,20 +48,28 @@ export function DropdownInputWithLabel({ id, name, label, items, itemLabel }: Pr
       <label className="text-[1.3rem]" htmlFor={id}>
         {label}
       </label>
-      <div className="flex flex-col relative">
-        <input
-          onClick={() => setListIsOpen(true)}
-          id={id}
-          name={name}
-          className="border h-[40px] w-full rounded-sm px-[8px]"
-          type="text"
-          value={itemLabel(items[selectedValue])}
-          readOnly
-        />
+      <div ref={dropRef} className="flex flex-col relative">
+        <div className="flex jusfity-center items-center border h-[40px] w-full rounded-sm px-[8px]">
+          <input
+            onClick={() => setListIsOpen(true)}
+            id={id}
+            name={name}
+            className="w-full h-full"
+            type="text"
+            value={itemLabel(items[selectedValue])}
+            readOnly
+          />
+          <span className={`transition-[rotate] duration-300 ${!listIsOpen && 'rotate-180'}`}>
+            <ArrowDropUp height="24px" width="24px" />
+          </span>
+        </div>
         <div
-          className={`absolute z-1 w-full bg-white overflow-hidden w-full top-[100%] h-0 ${
-            listIsOpen && 'h-fit border p-[8px]'
+          className={`transition-[height] duration-300 absolute z-1 w-full bg-white overflow-hidden w-full top-[100%]  ${
+            listIsOpen && 'border py-[8px]'
           }`}
+          style={{
+            height: listIsOpen ? items.length * 36 : 0,
+          }}
         >
           <ul>{renderItems}</ul>
         </div>
