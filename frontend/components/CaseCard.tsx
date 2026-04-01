ad5f914ac17f0b9284f55c3a2640243df6005d6d
DavidSheltonSF <david.faria012@gmail.com>
2026-04-01 18:50:40 -0300

    refactor(frontend/components/CaseCard): add options button to open the Options Modal
    -  Add openOptionsModal prop
    -  Make case modal to be open when user clicks on the main part  of the card

--- frontend/components/CaseCard.tsx
+++ frontend/components/CaseCard.tsx
@@ -6,14 +6,16 @@ import { WithId } from '@/types/WithId';
 import { CaseStatusLabel } from '@/lib/CaseStatusLabel';
 import { CaseWithRelations } from '@/types/CaseWithRelations';
 import { Dispatch, SetStateAction } from 'react';
+import { VerticalMoreIcon } from './icons/VerticalMoreIcon';
 
 interface Props {
   setSelectedCaseId: Dispatch<SetStateAction<string | null>>;
   openCaseModal: Function;
+  openOptionsModal: Function;
   caseData: WithId<CaseWithRelations>;
 }
 
-export function CaseCard({ caseData, setSelectedCaseId, openCaseModal }: Props) {
+export function CaseCard({ caseData, setSelectedCaseId, openCaseModal, openOptionsModal }: Props) {
   const clientData = caseData.client;
 
   const { id, title, processNumber, status } = caseData;
@@ -39,14 +41,8 @@ export function CaseCard({ caseData, setSelectedCaseId, openCaseModal }: Props) 
   });
 
   return (
-    <article
-      onClick={() => {
-        setSelectedCaseId(id);
-        openCaseModal();
-      }}
-      className="flex flex-col fade-in-animation  bg-color-primary w-full min-md:w-[80%] min-lg:w-[640px] min-h-[280px] h-max rounded-xl cursor-pointer"
-    >
-      <header className="flex items-center justify-center min-md:justify-start w-full p-[16px] min-md:p-[24px]">
+    <article className="flex flex-col fade-in-animation  bg-color-primary w-full min-md:w-[80%] min-lg:w-[656px] min-h-[280px] h-max rounded-xl cursor-pointer">
+      <header className="flex items-center justify-center min-md:justify-between w-full p-[16px] min-md:p-[24px]">
         <TooltipContainer
           label={title}
           tooltipLabelProps={{
@@ -63,8 +59,21 @@ export function CaseCard({ caseData, setSelectedCaseId, openCaseModal }: Props) 
             {reduceString(title, 35)}
           </h1>
         </TooltipContainer>
+        <button
+          className="cursor-pointer"
+          onClick={() => {
+            openOptionsModal();
+            setSelectedCaseId(id);
+          }}
+        >
+          <VerticalMoreIcon color="var(--white-color)" height="32px" width="32px" />
+        </button>
       </header>
       <main
+        onClick={() => {
+          setSelectedCaseId(id);
+          openCaseModal();
+        }}
         className="flex flex-1 flex-col gap-[16px] px-[24px] py-[16px] bg-color-white text-color-black text-lg"
         style={{
           borderRadius: 'inherit',
