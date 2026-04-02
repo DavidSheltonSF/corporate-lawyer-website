b2cc19d92a82c89c9b1ea342b0400f32e5041c15
DavidSheltonSF <david.faria012@gmail.com>
2026-04-02 19:32:24 -0300

    refactor(routes/usersRoutes): add requireAuth middleware to findClient, createClient and findById routes

--- backend/src/routes/usersRoutes.ts
+++ backend/src/routes/usersRoutes.ts
@@ -4,9 +4,9 @@ import { expressHttpAdapter } from './adapters/expressHttpAdapter';
 import { requireAuth } from '../middlewares/requireAuth';
 
 export function usersRoutes(router: Router, userController: IUserController) {
-  router.get('/api/clients', expressHttpAdapter(userController.findClients));
-  router.post('/api/clients', expressHttpAdapter(userController.createClient));
-  router.get('/api/users/:id', expressHttpAdapter(userController.findById));
+  router.get('/api/clients', requireAuth, expressHttpAdapter(userController.findClients));
+  router.post('/api/clients', requireAuth, expressHttpAdapter(userController.createClient));
+  router.get('/api/users/:id', requireAuth, expressHttpAdapter(userController.findById));
   router.get('/api/clients/:id', requireAuth, expressHttpAdapter(userController.findClientById));
   router.put('/api/users/:id', requireAuth, expressHttpAdapter(userController.updateById));
   router.delete('/api/users/:id', requireAuth, expressHttpAdapter(userController.deleteById));
