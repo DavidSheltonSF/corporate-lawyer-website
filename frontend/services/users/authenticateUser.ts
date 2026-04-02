1dfa4d7e9dc3d8e62d405914eb7b436d3481b475
DavidSheltonSF <david.faria012@gmail.com>
2026-04-02 16:11:45 -0300

    chore(frontend/services/authenticateUser): standardize data fetching using apiFetch

--- frontend/services/users/authenticateUser.ts
+++ frontend/services/users/authenticateUser.ts
@@ -1,25 +1,17 @@
 import { API_URL } from '@/config/api';
-import { ServerError } from '@/errors/ServerError';
+import { apiFetch } from '../apiFetch';
 
 export async function authenticateUser(formData: FormData): Promise<string> {
   const email = formData.get('email');
   const password = formData.get('password');
 
-  const response = await fetch(`${API_URL}/auth`, {
+  const response = await apiFetch(`${API_URL}/auth`, {
     method: 'POST',
     body: JSON.stringify({ email, password }),
     headers: { 'Content-Type': 'application/json' },
   });
 
   const json = await response.json();
-
-  if (response.status === 500) {
-    throw new ServerError();
-  }
-
-  if (!response.ok) {
-    throw new Error(json.message);
-  }
 
   return json.data.token;
 }
