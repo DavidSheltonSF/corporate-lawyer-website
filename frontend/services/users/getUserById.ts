619105581b9807ed45a496488a26c4f2a080c1bb
DavidSheltonSF <david.faria012@gmail.com>
2026-04-02 16:19:17 -0300

    chore(frontend/services/getUserById): standardize data fetching using apiFetch

--- frontend/services/users/getUserById.ts
+++ frontend/services/users/getUserById.ts
@@ -3,17 +3,14 @@ import { InvalidAPIResponseError } from '@/errors/InvalidAPIResponseError';
 import { MissingRequiredArgumentError } from '@/errors/MissingRequiredArgumentError';
 import { User } from '@/types/User';
 import { WithId } from '@/types/WithId';
+import { apiFetch } from '../apiFetch';
 
 export async function getUserById(id: string): Promise<WithId<User>> {
   if (!id) {
     throw new MissingRequiredArgumentError('getUserById', 'id');
   }
 
-  const response = await fetch(`${API_URL}/users/${id}`);
-
-  if (!response.ok) {
-    throw new Error(await response.text().catch(() => 'Unknown Error'));
-  }
+  const response = await apiFetch(`${API_URL}/users/${id}`);
   const json = await response.json();
 
   if (!json?.data) {
