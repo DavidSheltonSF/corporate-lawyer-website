c78a58e8fc9865d5384ac2202f6104f3f69c4878
DavidSheltonSF <david.faria012@gmail.com>
2026-04-03 18:05:10 -0300

    refactor(frontend/lib/getTokenFromCookies): update return type to allow null and remove error throw

--- frontend/lib/getTokenFromCookies.ts
+++ frontend/lib/getTokenFromCookies.ts
@@ -2,14 +2,10 @@
 
 import { cookies } from 'next/headers';
 
-export async function getTokenFromCookies(): Promise<string> {
+export async function getTokenFromCookies(): Promise<string | null> {
   const cookieStore = await cookies();
   const authToken = cookieStore.get('authToken');
   const token = authToken?.value;
 
-  if (!token) {
-    throw Error('Token from cookies was not found');
-  }
-
-  return token;
+  return token || null;
 }
