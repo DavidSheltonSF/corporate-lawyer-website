45d593cc5952a353d3f31308ae336c58e975be64
DavidSheltonSF <david.faria012@gmail.com>
2026-04-03 20:15:37 -0300

    refactor(frontend/actions/login): add error handling for better debugging

--- frontend/actions/login.ts
+++ frontend/actions/login.ts
@@ -3,8 +3,13 @@ import { authenticateUser } from '@/services/users/authenticateUser';
 import { cookies } from 'next/headers';
 
 export async function login(formData: FormData): Promise<void> {
-  const token = await authenticateUser(formData);
-  const cookiesStore = await cookies();
-  //cookiesStore.set('authToken', token, { httpOnly: true, secure: true, sameSite: 'none' });
-  cookiesStore.set('authToken', token, { httpOnly: true });
+  try {
+    const token = await authenticateUser(formData);
+    const cookiesStore = await cookies();
+    //cookiesStore.set('authToken', token, { httpOnly: true, secure: true, sameSite: 'none' });
+    cookiesStore.set('authToken', token, { httpOnly: true });
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 }
