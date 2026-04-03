771724aaad014630d41a89c9572b2d44bc9cbc1d
DavidSheltonSF <david.faria012@gmail.com>
2026-04-03 17:42:05 -0300

    refactor(backend/services/validators/validateProcessNumber): make the regex used to validate the process number stricter

--- backend/src/services/validators/cases/validateProcessNumber.ts
+++ backend/src/services/validators/cases/validateProcessNumber.ts
@@ -1,7 +1,8 @@
 import { InvalidProcessNumberError } from '../../../errors/domain/InvalidProcessNumberError';
 
 export function validateProcessNumber(processNumber: string) {
-  const regex = /^\d{20}$/;
+  //NNNNNNN-DD.AAAA.J.TR.OOOO
+  const regex = /^\d{7}-\d{2}\.\d{4}\.\d{1}\.\d{2}\.\d{4}$/;
   if (!regex.test(processNumber)) {
     throw new InvalidProcessNumberError(processNumber);
   }
