205971acc7872a60b23e7aec86d5a3b6538b627e
DavidSheltonSF <david.faria012@gmail.com>
2026-04-04 19:00:40 -0300

    chore(frontend/services/getCaseById): update to return WithId<Case>

--- frontend/services/cases/getCaseById.ts
+++ frontend/services/cases/getCaseById.ts
@@ -1,11 +1,11 @@
 import { API_URL } from '@/config/api';
 import { InvalidAPIResponseError } from '@/errors/InvalidAPIResponseError';
 import { MissingRequiredArgumentError } from '@/errors/MissingRequiredArgumentError';
-import { CaseWithRelations } from '@/types/CaseWithRelations';
 import { WithId } from '@/types/WithId';
 import { apiFetch } from '../apiFetch';
+import { Case } from '@/types/Case';
 
-export async function getCaseById(id: string): Promise<WithId<CaseWithRelations>> {
+export async function getCaseById(id: string): Promise<WithId<Case>> {
   if (!id) {
     throw new MissingRequiredArgumentError(getCaseById.name, 'id');
   }
