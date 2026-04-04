9dd6e64fc1a5f12a721427023fd86cb1ff563c52
DavidSheltonSF <david.faria012@gmail.com>
2026-04-04 14:23:18 -0300

    test(backend/CaseService): add test for InvalidProcessNumberError, InvalidCaseTitleError and InvalidCaseStatusError in create method

--- backend/src/services/case/CaseService.test.ts
+++ backend/src/services/case/CaseService.test.ts
@@ -1,6 +1,9 @@
 import { CaseService } from './CaseService';
 import { CasesStatus } from '../../types/CasesStatus';
 import { createMockCaseRepository } from '../../tests/mocks/repositories/createMockCaseRepository';
+import { InvalidProcessNumberError } from '../../errors/domain/InvalidProcessNumberError';
+import { InvalidCaseTitleError } from '../../errors/domain/InvalidCaseTitleError';
+import { InvalidCaseStatusError } from '../../errors/domain/InvalidCaseStatusError';
 
 describe('Test CaseService', () => {
   function makeSut() {
@@ -29,6 +32,78 @@ describe('Test CaseService', () => {
 
     await caseService.create(newCase);
     expect(caseRepository.create).toHaveBeenLastCalledWith(newCase);
+  });
+
+  test('should throw InvalidProcessNumberError and not call CaseRepository.create if the process number provided is invalid', async () => {
+    const { caseService, caseRepository } = makeSut();
+
+    const newCase = {
+      client: 'xfafdsfafsfasfffff',
+      lawyers: ['hhtshhhhhthtfsj'],
+      processNumber: '2158748',
+      title: 'Ação de Usucapião Urbano',
+      court: 'STJ',
+      courtDivision: 'Vara Cívil',
+      description: 'Case description',
+      status: CasesStatus.open,
+    };
+
+    await expect(caseService.create(newCase)).rejects.toThrow(InvalidProcessNumberError);
+    expect(caseRepository.create).toHaveBeenCalledTimes(0);
+  });
+
+  test('should throw InvalidCaseTitleError and not call CaseRepository.create if the case title provided is invalid', async () => {
+    const { caseService, caseRepository } = makeSut();
+
+    const newCase = {
+      client: 'xfafdsfafsfasfffff',
+      lawyers: ['hhtshhhhhthtfsj'],
+      processNumber: '2158748-55.5558.5.87.8858',
+      title: 'Ação d',
+      court: 'STJ',
+      courtDivision: 'Vara Cívil',
+      description: 'Case description',
+      status: CasesStatus.open,
+    };
+
+    await expect(caseService.create(newCase)).rejects.toThrow(InvalidCaseTitleError);
+    expect(caseRepository.create).toHaveBeenCalledTimes(0);
+  });
+
+  test('should throw InvalidProcessNumberError if the process number provided is invalid', async () => {
+    const { caseService, caseRepository } = makeSut();
+
+    const newCase = {
+      client: 'xfafdsfafsfasfffff',
+      lawyers: ['hhtshhhhhthtfsj'],
+      processNumber: '2158748',
+      title: 'Ação de Usucapião Urbano',
+      court: 'STJ',
+      courtDivision: 'Vara Cívil',
+      description: 'Case description',
+      status: CasesStatus.open,
+    };
+
+    await expect(caseService.create(newCase)).rejects.toThrow(InvalidProcessNumberError);
+    expect(caseRepository.create).toHaveBeenCalledTimes(0);
+  });
+
+  test('should throw InvalidCaseStatusError and not call CaseRepository.create if the case status provided is invalid', async () => {
+    const { caseService, caseRepository } = makeSut();
+
+    const newCase = {
+      client: 'xfafdsfafsfasfffff',
+      lawyers: ['hhtshhhhhthtfsj'],
+      processNumber: '2158748-55.5558.5.87.8858',
+      title: 'Ação de Usucapião Urbano',
+      court: 'STJ',
+      courtDivision: 'Vara Cívil',
+      description: 'Case description',
+      status: 'banana',
+    };
+
+    await expect(caseService.create(newCase)).rejects.toThrow(InvalidCaseStatusError);
+    expect(caseRepository.create).toHaveBeenCalledTimes(0);
   });
 
   test('should call caseRepository.updateById with the provided id and data', async () => {
