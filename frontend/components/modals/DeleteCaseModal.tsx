7fea835fe52a94f7340468fdf20777279177b349
DavidSheltonSF <david.faria012@gmail.com>
2026-04-04 21:40:46 -0300

    feat(frontend/modals): add DeleteCaseModal

---
+++ frontend/components/modals/DeleteCaseModal.tsx
@@ -0,0 +1,50 @@
+'use case';
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+import { PrimaryModalWindow } from './PrimaryModalWindow';
+import { Button } from '../Button';
+
+import { deleteCaseById } from '@/services/users/deleteCaseById';
+import { RequestState } from '@/types/RequestState';
+import { RequestFeedback } from '../form/RequestFeedback';
+import { ConfirmModal } from './ConfirmModal';
+
+interface Props {
+  selectedCaseId: string | null;
+  isOpen: Boolean;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+  loadCases: Function;
+}
+
+export function DeleteCaseModal({ selectedCaseId, isOpen, setIsOpen, loadCases }: Props) {
+  const [requestState, setRequestState] = useState<RequestState | null>(null);
+
+  useEffect(() => {
+    return () => {
+      setRequestState(null);
+    };
+  }, []);
+
+  async function deleteCase() {
+    try {
+      setRequestState({ status: 'loading' });
+      await deleteCaseById(selectedCaseId || '');
+      setRequestState({ status: 'ok', message: 'Case deleted successfuly' });
+      loadCases();
+    } catch (error: any) {
+      setRequestState({ status: 'error', message: error.message });
+      console.log(error);
+    }
+  }
+
+  return (
+    isOpen && (
+      <ConfirmModal
+        requestState={requestState}
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+        onConfirm={() => deleteCase()}
+        text=" Deseja excluir este processo? Esssa ação não pode ser revertida."
+      />
+    )
+  );
+}
