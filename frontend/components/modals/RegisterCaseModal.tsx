0f57787af97244a87b88eec43ff6dc69b9792b0a
DavidSheltonSF <david.faria012@gmail.com>
2026-04-05 18:55:09 -0300

    chore(frontend/RegisterCaseModal): remove invalid paddingY props from Button component and enhance button responsiveness

--- frontend/components/modals/RegisterCaseModal.tsx
+++ frontend/components/modals/RegisterCaseModal.tsx
@@ -65,9 +65,9 @@ export function RegisterCaseModal({ isOpen, setIsOpen, selectedClientId }: Props
               <InputWithLabel id="description-input" name="description" label="Descrição" />
             </div>
 
-            <div className="w-full min-lg:w-[200px] min-lg:ml-auto min:lg:mt-auto">
+            <div className="flex justify-end w-full bg-black min-md:w-[200px]  min-md:ml-auto">
               <Button
-                paddingY="8px"
+                width="100%"
                 backgroundColor="var(--primary-color)"
                 textColor="var(--white-color)"
                 fontSize="1.2rem"
