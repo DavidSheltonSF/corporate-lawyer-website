bfdad95658594590dbbb6b49c0e636ff5919034c
DavidSheltonSF <david.faria012@gmail.com>
2026-04-06 18:01:50 -0300

    chore(backend/errors): remove unused CaseNotFoundError and UserNotFoundError

--- backend/src/errors/application/CaseNotFoundError.ts
+++
@@ -1,6 +0,0 @@
-export class CaseNotFoundError extends Error {
-  constructor(caseId: string) {
-    super(`Case with id '${caseId}' was not found`);
-    this.name = CaseNotFoundError.name;
-  }
-}
