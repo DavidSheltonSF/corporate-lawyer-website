159d904247888054b92af15e3074f1f5d72e5067
DavidSheltonSF <david.faria012@gmail.com>
2026-04-06 17:07:46 -0300

    feat(bakend/config): add configErrorHandler

---
+++ backend/src/config/configErrorHandler.ts
@@ -0,0 +1,19 @@
+import { Application, NextFunction, Request, Response } from 'express';
+import { DomainError } from '../errors/domain/DomainError';
+import { BaseHttpError } from '../errors/presentation/BaseHttpError';
+
+export function configErrorHandler(app: Application) {
+  app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+    console.error(err);
+
+    if (err instanceof BaseHttpError) {
+      return res.status(err.statusCode).json({ message: err.message });
+    }
+
+    if (err instanceof DomainError) {
+      return res.status(422).json({ message: err.message });
+    }
+
+    return res.status(500).json({ message: 'Internal server error' });
+  });
+}
