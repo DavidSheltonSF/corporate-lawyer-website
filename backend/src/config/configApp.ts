a8176ee849e740a4b1c3d3f16d6376b42f1e7298
DavidSheltonSF <david.faria012@gmail.com>
2026-04-06 17:09:18 -0300

    refactor(backend/configApp): use configErrorHandler

--- backend/src/config/configApp.ts
+++ backend/src/config/configApp.ts
@@ -1,8 +1,10 @@
 import { type Application } from 'express';
 import { configMiddlewares } from './configMiddlewares';
 import { configRouter } from './configRouter';
+import { configErrorHandler } from './configErrorHandler';
 
 export function configApp(app: Application) {
   configMiddlewares(app);
   configRouter(app);
+  configErrorHandler(app);
 }
