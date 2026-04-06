edad729f1343c3516feae02a00b2faca64feb84a
DavidSheltonSF <david.faria012@gmail.com>
2026-04-06 16:04:54 -0300

    chore(backend/errors): remove unused BaseHttpError and UnauthorizedError

--- backend/src/errors/UnauthorizedError.ts
+++
@@ -1,9 +0,0 @@
-import { BaseHttpError } from './BaseHttpError';
-
-export class UnauthorizedError extends BaseHttpError {
-  statusCode = 401;
-  code = 'UNAUTHORIZED';
-  constructor(message: string) {
-    super(message);
-  }
-}
