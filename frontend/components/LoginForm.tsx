57a239ce2a37d6c8c3c986e6bbe68e202669db60
DavidSheltonSF <david.faria012@gmail.com>
2026-04-08 23:01:57 -0300

    refactor(frontend/components/LoginForm):  improve request state management

--- frontend/components/LoginForm.tsx
+++ frontend/components/LoginForm.tsx
@@ -1,42 +1,44 @@
 'use client';
-import { Activity, ReactNode, useState } from 'react';
+import { Activity, ReactNode, useEffect, useState } from 'react';
 import { InputForm } from './InputForm';
 import { LoadingMessage } from './LoadingMessage';
 
 import { login } from '@/actions/login';
 import { redirect } from 'next/navigation';
+import { RequestState } from '@/types/RequestState';
 
 export function LoginForm() {
-  const [errorMessage, setErrorMessage] = useState('');
-  const [loading, setLoading] = useState(false);
+  const [requestState, setRequestState] = useState<RequestState | null>(null);
 
   async function handleSubmit(formData: FormData) {
-    let authError = false;
     try {
+      setRequestState({ status: 'loading' });
       await login(formData);
+      setRequestState({ status: 'ok' });
     } catch (error: any) {
       console.log(error);
-      setErrorMessage(error.message);
-      authError = true;
-    } finally {
-      setLoading(false);
-      if (!authError) {
-        redirect('/clientPage');
-      }
+      setRequestState({ status: 'error', message: error.message });
     }
   }
+
+  useEffect(() => {
+    if (requestState?.status === 'ok') {
+      redirect('/clientPage');
+    }
+  }, [requestState]);
+
   return (
     <form
       className="flex flex-col w-[90%] lg:w-[480px] bg-color-primary border-[2px] border-color-primary-light rounded-[16px] px-[24px] py-[40px]  gap-[32px] text-color-white text-xl"
       action={handleSubmit}
     >
-      <Activity mode={errorMessage !== '' ? 'visible' : 'hidden'}>
+      <Activity mode={requestState?.status === 'error' ? 'visible' : 'hidden'}>
         <div className="text-red-400 text-center font-bold">
-          <p>{errorMessage}</p>
+          <p>{requestState?.message}</p>
         </div>
       </Activity>
       <div className="text-center font-bold">
-        <LoadingMessage message="Loading" loading={loading} />
+        <LoadingMessage message="Loading" loading={requestState?.status === 'loading'} />
       </div>
 
       <InputForm
@@ -58,7 +60,6 @@ export function LoginForm() {
       <button
         type="submit"
         className="w-full bg-[var(--primary-color-light)] rounded-full font-bold h-[40px] hover:brightness-124 transition-all duration-[300ms] cursor-pointer"
-        onClick={() => setLoading(true)}
       >
         Entrar
       </button>
