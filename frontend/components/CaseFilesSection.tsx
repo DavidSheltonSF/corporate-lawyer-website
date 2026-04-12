dd6ab9142da4e7a83d7faab7f879caee1db9b1dc
DavidSheltonSF <david.faria012@gmail.com>
2026-04-12 19:31:15 -0300

    chore(frontend/CaseFilesSection): remove unnecessary updateFiles state

--- frontend/components/CaseFilesSection.tsx
+++ frontend/components/CaseFilesSection.tsx
@@ -6,20 +6,14 @@ import { getMyCaseFilesByCaseId } from '@/services/cases/getMyCaseFilesByCaseId'
 
 export function CaseFilesSection({ id, files }: { id: string; files: CaseFile[] }) {
   const [currentFiles, setCurrentFiles] = useState(files);
-  const [updateFiles, setUpdateFiles] = useState(false);
 
   useEffect(() => {
     async function fetchCases() {
       const caseFiles = await getMyCaseFilesByCaseId(id);
       setCurrentFiles(caseFiles);
     }
-
-    if (updateFiles) {
-      fetchCases();
-    }
-
-    setUpdateFiles(false);
-  }, [updateFiles]);
+    fetchCases();
+  }, []);
 
   return (
     <div className="w-full h-full pl-[24px] pb-[16px] overflow-y-scroll">
