914eccecdf8ac180d366c9909d0a113e5118aadb
DavidSheltonSF <david.faria012@gmail.com>
2026-04-12 19:09:14 -0300

    refactor(frontend/OpenUploadModalButton): use the function provided by handleClick prop to open the modal instead of using context

--- frontend/components/OpenUploadModalButton.tsx
+++ frontend/components/OpenUploadModalButton.tsx
@@ -1,29 +1,14 @@
 'use client';
-
 import { Button } from './Button';
-import { useContext } from 'react';
-import { UploadModalContext } from '@/contexts/modals/UploadModalContext';
-import { ModalContextType } from '@/contexts/modals/ModalContextType';
-import { MissingContextError } from '@/errors/MissingContextError';
 
 interface Props {
+  handleClick: Function;
   disabled?: boolean;
 }
-export function OpenUploadModalButton({ disabled }: Props) {
-  const context = useContext<ModalContextType | undefined>(UploadModalContext);
-
-  if (!context) {
-    throw new MissingContextError('UploadModalContext');
-  }
-  const { setIsOpen } = context;
-
-  function handleClick() {
-    setIsOpen!(true);
-  }
-
+export function OpenUploadModalButton({ handleClick, disabled }: Props) {
   return (
     <div className="size-[48px] min-md:h-[48px] min-md:w-[200px]">
-      <Button height="100%" width="100%" onclick={handleClick} disabled={disabled}>
+      <Button height="100%" width="100%" onclick={() => handleClick()} disabled={disabled}>
         <span className="flex justify-center items-center size-full md:hidden">
           <img src="/icons/upload.svg" alt="" />
         </span>
