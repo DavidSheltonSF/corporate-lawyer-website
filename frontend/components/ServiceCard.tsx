e8efe184935b882b4d0f0d3d39f6dae09f79f82a
DavidSheltonSF <david.faria012@gmail.com>
2026-04-16 18:59:57 -0300

    fix(frontend/ServiceCard): correct typo in class name and remove unnecessary text size class

--- frontend/components/ServiceCard.tsx
+++ frontend/components/ServiceCard.tsx
@@ -29,8 +29,8 @@ export function ServiceCard({ serviceDetails }: Props) {
         </TooltipContainer>
       </header>
       <main className="flex flex-col items-center text-center gap-[16px]">
-        <h1 className="text-3xl lfont-bold mt-[16px]">{title}</h1>
-        <p className="w-[92%] lg:w-auto text-xl">{description}</p>
+        <h1 className="text-3xl font-bold mt-[16px]">{title}</h1>
+        <p className="w-[92%] lg:w-auto ">{description}</p>
       </main>
     </article>
   );
