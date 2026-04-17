6334d4f3b767a2fb8ed2c75eb2a9c8ce20dc9435
DavidSheltonSF <david.faria012@gmail.com>
2026-04-17 13:36:26 -0300

    chore(frontend/ServiceDetailsModal): remove unnecessary font size classes and warp title with h2 instead of h2

--- frontend/components/modals/ServiceDetailsModal.tsx
+++ frontend/components/modals/ServiceDetailsModal.tsx
@@ -8,9 +8,9 @@ export function ServiceDetailsModal({ serviceAreaId }: Props) {
   const area = servicesDetails[serviceAreaId];
   return (
     <div>
-      <h3 className="font-bold text-2xl self-center">{area?.title}</h3>
+      <h2 className="font-bold self-center">{area?.title}</h2>
       <div className="flex flex-col gap-[8px] my-[16px] size-full scroll-hidden p-[8px] rounded-xl">
-        <p className="text-xl">Prestamos os seguintes serviços:</p>
+        <p>Prestamos os seguintes serviços:</p>
         <ul className="flex list-disc list-inside flex-col gap-[8px]">
           {area?.services.map((service: any, index: number) => {
             return <li key={index}>{service}</li>;
