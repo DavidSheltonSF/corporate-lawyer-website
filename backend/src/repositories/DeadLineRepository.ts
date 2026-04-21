273e80e8cdca136096b15e2856d2221b9346ca28
DavidSheltonSF <david.faria012@gmail.com>
2026-04-21 19:24:40 -0300

    fix(repositories): update create and findByCaseId methods to use DeadlineDTO and make all methods return Deadline

--- backend/src/repositories/DeadLineRepository.ts
+++ backend/src/repositories/DeadLineRepository.ts
@@ -1,14 +1,14 @@
 import { WithId } from '../types/WithId';
 import { DeadlineDTO } from '../dtos/deadLine/DeadlineDTO';
-import { Deadline } from '../entities/Deadline';
 import { UpdateDeadlineDTO } from '../dtos/deadLine/UpdateDeadlineDTO';
+import { Deadline } from '../entities/Deadline';
 
 export interface DeadLineRepository {
-  create(data: Deadline): Promise<WithId<Deadline>>;
+  create(data: DeadlineDTO): Promise<WithId<Deadline>>;
   findAll(): Promise<WithId<Deadline>[]>;
   findById(id: string): Promise<WithId<Deadline> | null>;
-  findByCaseId(id: string): Promise<WithId<DeadlineDTO>[]>;
-  deleteById(id: string): Promise<WithId<DeadlineDTO> | null>;
-  updateById(id: string, data: Partial<UpdateDeadlineDTO>): Promise<WithId<DeadlineDTO> | null>;
+  findByCaseId(id: string): Promise<WithId<Deadline>[]>;
+  deleteById(id: string): Promise<WithId<Deadline> | null>;
+  updateById(id: string, data: Partial<UpdateDeadlineDTO>): Promise<WithId<Deadline> | null>;
   existsById(id: string): Promise<boolean>;
 }
