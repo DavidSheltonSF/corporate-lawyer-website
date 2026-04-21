af06b352b554e06a402b1ec229a6f5eefb629371
DavidSheltonSF <david.faria012@gmail.com>
2026-04-20 22:31:48 -0300

    feat(backend/mappers): add DeadlineMapper

---
+++ backend/src/mappers/Deadline/DeadlineMapper.ts
@@ -0,0 +1,34 @@
+import { WithMongoId } from '../../database/mongoDB/types/WithMongoId';
+import { DeadlineDTO } from '../../dtos/deadLine/DeadlineDTO';
+import { Deadline } from '../../entities/Deadline';
+import { WithId } from '../../types/WithId';
+
+export class DeadlineMapper {
+  static persistenceToDomain(data: WithMongoId<any>): WithId<Deadline> {
+    const { _id, processId, clientId, priority, type, status, startDate, dueDate } = data;
+    return {
+      id: _id.toString(),
+      processId,
+      clientId,
+      startDate,
+      dueDate,
+      type,
+      status,
+      priority,
+    };
+  }
+
+  static persistenceToPresentation(data: WithMongoId<any>): WithId<DeadlineDTO> {
+    const { _id, processId, clientId, priority, type, status, startDate, dueDate } = data;
+    return {
+      id: _id.toString(),
+      processId,
+      clientId,
+      startDate: startDate.toString(),
+      dueDate: dueDate.toString(),
+      type,
+      status,
+      priority,
+    };
+  }
+}
