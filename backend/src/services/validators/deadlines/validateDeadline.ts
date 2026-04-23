b0da8816e0a576a62d54ffd138fb507102d3c71d
DavidSheltonSF <david.faria012@gmail.com>
2026-04-23 13:22:53 -0300

    refactor(backend/validators/validateDeadline): use validateDate to validate the startDate and the dueDate, and validateDeadlineDateRange

--- backend/src/services/validators/deadlines/validateDeadline.ts
+++ backend/src/services/validators/deadlines/validateDeadline.ts
@@ -1,10 +1,12 @@
 import { DeadlineDTO } from '../../../dtos/deadLine/DeadlineDTO';
+import { validateDate } from '../validateDate';
+import { validateDeadlineDateRange } from '../validateDeadlineDateRange';
 import { validateDeadlinePriority } from './validateDeadlinePriority';
 import { validateDeadlineStatus } from './validateDeadlineStatus';
 import { validateDeadlineType } from './validateDeadlineType';
 
 export function validateDeadline(data: DeadlineDTO) {
-  const { type, priority, status } = data;
+  const { type, priority, status, startDate, dueDate } = data;
 
   if (status) {
     validateDeadlineStatus(status);
@@ -12,4 +14,11 @@ export function validateDeadline(data: DeadlineDTO) {
 
   validateDeadlineType(type);
   validateDeadlinePriority(priority);
+  validateDate(startDate);
+  validateDate(dueDate);
+
+  const parsedStartDate = new Date(startDate);
+  const parsedDueDate = new Date(dueDate);
+
+  validateDeadlineDateRange(parsedStartDate, parsedDueDate);
 }
