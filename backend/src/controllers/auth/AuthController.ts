255553917a015f2a64e815a719ffa712e5fca6d2
DavidSheltonSF <david.faria012@gmail.com>
2026-04-25 15:43:25 -0300

    refactor(AuthController): replace error response handling with error throwing for better error management

--- backend/src/controllers/auth/AuthController.ts
+++ backend/src/controllers/auth/AuthController.ts
@@ -5,6 +5,8 @@ import { IAuthController } from './IAuthController';
 import { HttpResponseFactory } from '../../factories/HttpResponse/HttpResponseFactory';
 import { getMissingFields } from '../../helpers/getMissingFields';
 import { HttpRequest } from '../types/HttpRequest';
+import { UnauthorizedError } from '../../errors/presentation/UnauthorizedError';
+import { BadRequestError } from '../../errors/presentation/BadRequestError';
 
 export class AuthController implements IAuthController {
   constructor(private authService: IAuthService, private userService: IUserService) {}
@@ -13,14 +15,14 @@ export class AuthController implements IAuthController {
     const token = httpRequest.headers.authorization;
 
     if (!token) {
-      return HttpResponseFactory.makeUnauthorized('Token missing');
+      throw new UnauthorizedError('Token missing');
     }
 
     const payload = jwt.decode(token) as JwtPayload;
 
     const email = payload.email;
     if (!email) {
-      return HttpResponseFactory.makeUnauthorized('Token provided is invalid');
+      throw new UnauthorizedError('Token provided is invalid');
     }
 
     const user = await this.userService.findByEmail(email);
@@ -29,39 +31,26 @@ export class AuthController implements IAuthController {
   };
 
   auth = async (httpRequest: HttpRequest) => {
-    try {
-      const body = httpRequest.body;
+    const body = httpRequest.body;
 
-      if (!body) {
-        return HttpResponseFactory.makeBadRequest('Body request is missing');
-      }
-
-      const missingFields = getMissingFields(body, ['email', 'password']);
-
-      if (missingFields.length > 0) {
-        return HttpResponseFactory.makeBadRequest(
-          `Missing required ${missingFields} fields in request body`
-        );
-      }
-
-      const { email, password } = body;
-
-      const response = await this.authService.authenticate(email, password);
-
-      if (response.message) {
-        return HttpResponseFactory.makeUnauthorized(response.message);
-      }
-
-      return HttpResponseFactory.makeOk(response.token);
-    } catch (error: any) {
-      console.log(error);
-
-      // Check if it is Unauthorized error
-      if (error.statusCode === 401) {
-        return HttpResponseFactory.makeUnauthorized(error.message);
-      }
-
-      return HttpResponseFactory.makeServerError(error.message);
+    if (!body) {
+      throw new BadRequestError('Body request is missing');
     }
+
+    const missingFields = getMissingFields(body, ['email', 'password']);
+
+    if (missingFields.length > 0) {
+      throw new BadRequestError(`Missing required ${missingFields} fields in request body`);
+    }
+
+    const { email, password } = body;
+
+    const response = await this.authService.authenticate(email, password);
+
+    if (response.message) {
+      throw new UnauthorizedError(response.message);
+    }
+
+    return HttpResponseFactory.makeOk(response.token);
   };
 }
