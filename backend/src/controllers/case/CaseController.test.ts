6ff6293065c06d9b870f1d0011b40defdbe276c3
DavidSheltonSF <david.faria012@gmail.com>
2026-04-25 16:09:19 -0300

    refactor(CaseController.test): streamline test setup and improve readability by consolidating httpRequest initialization

--- backend/src/controllers/case/CaseController.test.ts
+++ backend/src/controllers/case/CaseController.test.ts
@@ -14,74 +14,75 @@ describe(`Test ${CaseController.name}`, () => {
   function makeSut() {
     const caseRepository = createMockCaseRepository();
     const userRepository = createMockUserRepository();
+    userRepository.findById = jest.fn().mockResolvedValue({
+      _id: 'dfsdfsa',
+      firstName: 'José',
+      lastName: 'Almeida',
+      email: 'jo@email.com',
+      cpf: '15588787855',
+      password: 'jose123',
+      role: UserRole.lawyer,
+    });
+
     const userService = new UserService(userRepository, caseRepository);
     const caseService = new CaseService(caseRepository);
+
     const caseController = new CaseController(caseService, userService);
+
+    const httpRequest: HttpRequest = {
+      params: {
+        id: 'fakeId',
+      },
+      query: {},
+      body: {},
+      headers: {},
+      user: {
+        id: 'fakeid',
+        email: 'fake@email.com',
+      },
+    };
 
     return {
       caseRepository,
       userRepository,
       caseService,
       caseController,
+      httpRequest,
     };
   }
 
   test('should call CaseRepository.create and return 201', async () => {
-    const { caseController, caseRepository, userRepository } = makeSut();
+    const { caseController, caseRepository, httpRequest } = makeSut();
 
     const newCase: CreateCaseDTO = {
-      title: 'Case Title',
+      title: 'Ação de Usucapião Urbano',
       client: 'fakeid',
       court: 'fakecourt',
       courtDivision: 'fakecourtdivision',
       description: 'description bla bla',
       lawyers: ['fakeidiii'],
-      processNumber: '21454651554',
+      processNumber: '8585874-77.5855.8.11.1258', // NNNNNNN-DD.AAAA.J.TR.OOOO
       status: CasesStatus.open,
     };
 
-    const httpRequest = {
-      user: { id: 'dskfsadf', email: 'user@email.com' },
-      body: newCase,
-    };
-    userRepository.findById = jest.fn().mockResolvedValue({
-      _id: 'dfsdfsa',
-      firstName: 'José',
-      lastName: 'Almeida',
-      email: 'jo@email.com',
-      cpf: '15588787855',
-      password: 'jose123',
-      role: UserRole.lawyer,
-    });
-    const response = await caseController.create(httpRequest);
-    console.log(response);
+    httpRequest.body = newCase;
 
+    const response = await caseController.create(httpRequest);
     expect(caseRepository.create).toHaveBeenCalledWith(newCase);
     expect(response.status).toBe(HttpStatusCode.created);
   });
 
   test('should call CaseRepository.updateById and return 200', async () => {
-    const { caseController, caseRepository, userRepository } = makeSut();
+    const { caseController, caseRepository, httpRequest } = makeSut();
 
     const updatedData: UpdateCaseDTO = {
-      title: 'Case Title',
+      title: 'Pedido de penção para menores',
       court: 'fakecourt',
     };
 
-    const httpRequest = {
-      user: { id: 'dskfsadf', email: 'user@email.com' },
-      params: { id: 'fdsfafdfaffa' },
-      body: updatedData,
-    };
-    userRepository.findById = jest.fn().mockResolvedValue({
-      _id: 'dfsdfsa',
-      firstName: 'José',
-      lastName: 'Almeida',
-      email: 'jo@email.com',
-      cpf: '15588787855',
-      password: 'jose123',
-      role: UserRole.lawyer,
-    });
+    httpRequest.body = updatedData;
+    httpRequest.params = { id: 'fdsfafdfaffa' };
+
     const response = await caseController.updateById(httpRequest);
 
     expect(caseRepository.updateById).toHaveBeenCalledWith(httpRequest.params.id, httpRequest.body);
@@ -89,13 +90,7 @@ describe(`Test ${CaseController.name}`, () => {
   });
 
   test('should call caseRepository.findById with the provided id and return OK (200)', async () => {
-    const { caseController, caseRepository } = makeSut();
-
-    const httpRequest = {
-      params: {
-        id: 'fakeId',
-      },
-    };
+    const { caseController, caseRepository, httpRequest } = makeSut();
 
     const response = await caseController.findById(httpRequest);
 
@@ -103,32 +98,35 @@ describe(`Test ${CaseController.name}`, () => {
     expect(response.status).toBe(HttpStatusCode.ok);
   });
 
+  test('should call caseRepository.findPopulatedById with the provided id and return OK (200)', async () => {
+    const { caseController, caseRepository, httpRequest } = makeSut();
+
+    httpRequest.query.populate = 'true';
+
+    const response = await caseController.findById(httpRequest);
+
+    expect(caseRepository.findPopulatedById).toHaveBeenCalledWith(httpRequest.params.id);
+    expect(response.status).toBe(HttpStatusCode.ok);
+  });
+
   test('should call caseRepository.findCases with provided data and return OK (200)', async () => {
-    const { caseController, caseRepository } = makeSut();
+    const { caseController, caseRepository, httpRequest } = makeSut();
 
     const status = 'open';
     const limit = 4;
     const page = 1;
     const query = 'Query string,';
 
-    const httpRequest: HttpRequest = {
-      user: {
-        id: 'fakeId',
-        email: 'user@email.com',
-      },
-
-      query: {
-        status,
-        query,
-        page,
-        limit,
-      },
+    httpRequest.query = {
+      status,
+      query,
+      page,
+      limit,
     };
 
     const response = await caseController.findMyCases(httpRequest);
 
-    expect(caseRepository.findCases).toHaveBeenCalledWith({
-      client: httpRequest.user?.id,
+    expect(caseRepository.findPopulatedByClientId).toHaveBeenCalledWith(httpRequest.user?.id, {
       status,
       limit,
       page,
@@ -138,14 +136,7 @@ describe(`Test ${CaseController.name}`, () => {
   });
 
   test('should call caseRepository.getStatsByClientId with provided data and return OK (200) ', async () => {
-    const { caseController, caseRepository } = makeSut();
-
-    const httpRequest: HttpRequest = {
-      user: {
-        id: 'fakeId',
-        email: 'user@email.com',
-      },
-    };
+    const { caseController, caseRepository, httpRequest } = makeSut();
 
     const response = await caseController.getMyStats(httpRequest);
 
@@ -154,7 +145,7 @@ describe(`Test ${CaseController.name}`, () => {
   });
 
   test('should call caseRepository.addFile with provided data and return OK (200) ', async () => {
-    const { caseController, caseRepository } = makeSut();
+    const { caseController, caseRepository, httpRequest } = makeSut();
 
     const file = {
       originalname: 'Document x',
@@ -162,18 +153,7 @@ describe(`Test ${CaseController.name}`, () => {
       mimeType: 'pdf',
     };
 
-    const httpRequest: HttpRequest = {
-      user: {
-        id: 'fakeId',
-        email: 'user@email.com',
-      },
-
-      params: {
-        id: 'fakeCaseId',
-      },
-
-      file,
-    };
+    httpRequest.file = file;
 
     const response = await caseController.uploadMyFile(httpRequest);
 
@@ -182,12 +162,7 @@ describe(`Test ${CaseController.name}`, () => {
   });
 
   test('should call caseRepository.findById with provided id and return OK (200) ', async () => {
-    const { caseController, caseRepository } = makeSut();
-    const httpRequest: HttpRequest = {
-      params: {
-        id: 'fakeCaseId',
-      },
-    };
+    const { caseController, caseRepository, httpRequest } = makeSut();
 
     const response = await caseController.findFilesByCaseId(httpRequest);
 
@@ -196,12 +171,7 @@ describe(`Test ${CaseController.name}`, () => {
   });
 
   test('should call caseRepository.deleteById with provided id and return OK (200) ', async () => {
-    const { caseController, caseRepository } = makeSut();
-    const httpRequest: HttpRequest = {
-      params: {
-        id: 'fakeCaseId',
-      },
-    };
+    const { caseController, caseRepository, httpRequest } = makeSut();
 
     const response = await caseController.findFilesByCaseId(httpRequest);
 
