0adfc841855de728b7255361386fc32bc25202ea
DavidSheltonSF <david.faria012@gmail.com>
2026-04-25 15:31:52 -0300

    chore(backend/mappers/DeadlineMapper): remove unused persistenceToDomain method

--- backend/src/mappers/Deadline/DeadlineMapper.ts
+++ backend/src/mappers/Deadline/DeadlineMapper.ts
@@ -1,23 +1,8 @@
 import { WithMongoId } from '../../database/mongoDB/types/WithMongoId';
 import { DeadlineDTO } from '../../dtos/deadLine/DeadlineDTO';
-import { Deadline } from '../../entities/Deadline';
 import { WithId } from '../../types/WithId';
 
 export class DeadlineMapper {
-  static persistenceToDomain(data: WithMongoId<any>): WithId<Deadline> {
-    const { _id, caseId, clientId, priority, type, status, startDate, dueDate } = data;
-    return {
-      id: _id.toString(),
-      caseId,
-      clientId,
-      startDate,
-      dueDate,
-      type,
-      status,
-      priority,
-    };
-  }
-
   static persistenceToPresentation(data: WithMongoId<any>): WithId<DeadlineDTO> {
     const { _id, caseId, clientId, priority, type, status, startDate, dueDate } = data;
     return {
