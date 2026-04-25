e8f4d4b39a25cf7ac55ef57a2ad849ac07a9ee28
DavidSheltonSF <david.faria012@gmail.com>
2026-04-24 23:26:42 -0300

    feat(backend/controllers/helpers): add requireAuthenticatedLawyer

---
+++ backend/src/controllers/helpers/requireAutheticatedLawyer.ts
@@ -0,0 +1,24 @@
+import { ForbiddenError } from '../../errors/presentation/ForbiddenError';
+import { IUserService } from '../../services/user/IUserService';
+import { UserRole } from '../../types/UserRole';
+import { HttpRequest } from '../types/HttpRequest';
+import { getAuthenticatedUser } from './getAuthenticatedUser';
+
+export async function requireAutheticatedLawyer(
+  httpRequest: HttpRequest,
+  userService: IUserService
+) {
+  const authUser = getAuthenticatedUser(httpRequest);
+  const { id } = authUser;
+
+  const authUserData = await userService.findById(id);
+  if (!authUserData) {
+    throw new ForbiddenError(`Could not execute operation. User with id '${id}' was not found`);
+  }
+
+  if (authUserData.role !== UserRole.lawyer) {
+    throw new ForbiddenError(
+      `Could not execute operation. User with id '${authUserData.id}' is not a lawyer`
+    );
+  }
+}
