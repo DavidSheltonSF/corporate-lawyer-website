9fafef0136ae7646887557c757b93dad8fb1fefa
DavidSheltonSF <david.faria012@gmail.com>
2026-04-26 17:31:26 -0300

    refactor(backend/DeadlineMapper): make persistenceToPresentation return DeadlineResponseDTO instead of DeadlineDTO

--- backend/src/mappers/Deadline/DeadlineMapper.ts
+++ backend/src/mappers/Deadline/DeadlineMapper.ts
@@ -1,18 +1,33 @@
 import { WithMongoId } from '../../database/mongoDB/types/WithMongoId';
 import { DeadlineDTO } from '../../dtos/deadLine/DeadlineDTO';
+import { DeadlineResponseDTO } from '../../dtos/deadLine/DeadlineResponseDTO';
 import { WithId } from '../../types/WithId';
 
 export class DeadlineMapper {
-  static persistenceToPresentation(data: WithMongoId<any>): WithId<DeadlineDTO> {
-    const { _id, caseId, lawyerId, priority, type, startDate, dueDate } = data;
+  static persistenceToPresentation(data: WithMongoId<any>): WithId<DeadlineResponseDTO> {
+    const {
+      _id,
+      caseId,
+      lawyerId,
+      priority,
+      type,
+      intimationDate,
+      days,
+      startDate,
+      dueDate,
+      status,
+    } = data;
     return {
       id: _id.toString(),
       caseId: caseId.toString(),
       lawyerId: lawyerId.toString(),
-      startDate: startDate.toString(),
-      dueDate: dueDate.toString(),
+      intimationDate: intimationDate.toISOString(),
+      days,
+      startDate: startDate.toISOString(),
+      dueDate: dueDate.toISOString(),
       type,
       priority,
+      status,
     };
   }
 }
