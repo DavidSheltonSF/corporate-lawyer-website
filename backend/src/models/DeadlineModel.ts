f6522bc76b90d552996904dee60e420c9d4014f6
DavidSheltonSF <david.faria012@gmail.com>
2026-04-26 17:24:28 -0300

    fix(backend/DeadlineModel): add days field in IDeadlineModel and startDate and dueDate to DeadlineSchema

--- backend/src/models/DeadlineModel.ts
+++ backend/src/models/DeadlineModel.ts
@@ -8,29 +8,32 @@ export interface IDeadlineModel {
   lawyerId: Types.ObjectId;
   type: DeadlineType;
   intimationDate: Date;
+  days: number;
   startDate: Date;
   dueDate: Date;
-  days: number;
   priority: DeadlinePriority;
 }
 
 interface DeadlineMongoDocument extends IDeadlineModel, Document {}
 
-const DeadlineSchema = new Schema<DeadlineMongoDocument>({
-  caseId: { type: Types.ObjectId, ref: 'Cases', index: true, required: true },
-  lawyerId: { type: Types.ObjectId, ref: 'Users', index: true, required: true },
-  type: {
-    type: String,
-    enum: Object.values(DeadlineType),
-    required: true,
-  },
-  intimationDate: { type: Date, required: true },
-  days: { type: Number, required: true },
-  priority: {
-    type: String,
-    enum: Object.values(DeadlinePriority),
-    required: true,
-  },
-});
+const DeadlineSchema = new Schema<DeadlineMongoDocument>(
+  {
+    caseId: { type: Types.ObjectId, ref: 'Cases', index: true, required: true },
+    lawyerId: { type: Types.ObjectId, ref: 'Users', index: true, required: true },
+    type: {
+      type: String,
+      enum: Object.values(DeadlineType),
+      required: true,
+    },
+    intimationDate: { type: Date, required: true },
+    days: { type: Number, required: true },
+    startDate: { type: Date, required: true },
+    dueDate: { type: Date, required: true },
+    priority: {
+      type: String,
+      enum: Object.values(DeadlinePriority),
+      required: true,
+    },
+  });
 
 export const DeadlineModel = model<DeadlineMongoDocument>('Deadlines', DeadlineSchema);
