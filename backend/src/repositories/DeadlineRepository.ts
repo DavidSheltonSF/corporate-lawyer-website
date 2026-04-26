199ae123b4c728520c6e44dd621e273175ae6b48
DavidSheltonSF <david.faria012@gmail.com>
2026-04-26 17:32:44 -0300

    refactor(backend/repositories/DeadlineRepository): make all methods return DeadlineResponseDTO

--- backend/src/repositories/DeadlineRepository.ts
+++ backend/src/repositories/DeadlineRepository.ts
@@ -1,13 +1,14 @@
 import { WithId } from '../types/WithId';
 import { DeadlineDTO } from '../dtos/deadLine/DeadlineDTO';
 import { UpdateDeadlineDTO } from '../dtos/deadLine/UpdateDeadlineDTO';
+import { DeadlineResponseDTO } from '../dtos/deadLine/DeadlineResponseDTO';
 
 export interface DeadlineRepository {
-  create(data: DeadlineDTO): Promise<WithId<DeadlineDTO>>;
-  findAll(): Promise<WithId<DeadlineDTO>[]>;
-  findById(id: string): Promise<WithId<DeadlineDTO> | null>;
-  findByCaseId(id: string): Promise<WithId<DeadlineDTO>[]>;
-  deleteById(id: string): Promise<WithId<DeadlineDTO> | null>;
-  updateById(id: string, data: UpdateDeadlineDTO): Promise<WithId<DeadlineDTO> | null>;
+  create(data: DeadlineDTO): Promise<WithId<DeadlineResponseDTO>>;
+  findAll(): Promise<WithId<DeadlineResponseDTO>[]>;
+  findById(id: string): Promise<WithId<DeadlineResponseDTO> | null>;
+  findByCaseId(id: string): Promise<WithId<DeadlineResponseDTO>[]>;
+  deleteById(id: string): Promise<WithId<DeadlineResponseDTO> | null>;
+  updateById(id: string, data: UpdateDeadlineDTO): Promise<WithId<DeadlineResponseDTO> | null>;
   existsById(id: string): Promise<boolean>;
 }
