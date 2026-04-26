12c0dcde61d36098ff9035539b95431736bf4b58
DavidSheltonSF <david.faria012@gmail.com>
2026-04-26 17:35:34 -0300

    refactor(backend/MongodbDeadlineRepository.test): update deadline tests to include status checks and refactor deadlineDTO usage

--- backend/src/database/mongoDB/repositories/MongodbDeadlineRepository.test.ts
+++ backend/src/database/mongoDB/repositories/MongodbDeadlineRepository.test.ts
@@ -36,43 +36,109 @@ describe('Test DeadlineRepository', () => {
   test('should create a new deadline', async () => {
     const { deadlineRepository } = makeSut();
 
+    const today = new Date();
+    const yesterday = new Date(today);
+    yesterday.setDate(yesterday.getDate() - 1);
+    const tomorrow = new Date(today);
+    tomorrow.setDate(tomorrow.getDate() + 1);
+
     const deadlineDTO = {
-      caseId: Types.ObjectId.createFromTime(848484).toString(),
-      lawyerId: Types.ObjectId.createFromTime(8484).toString(),
+      caseId: Types.ObjectId.createFromTime(823775684).toString(),
+      lawyerId: Types.ObjectId.createFromTime(872576365).toString(),
       type: DeadlineType.PAGAMENTO,
-      startDate: '2050-02-02',
-      dueDate: '2050-03-02',
-      status: DeadlineStatus.EM_ANDAMENTO,
+      intimationDate: today.toISOString(),
+      days: 5,
+      startDate: tomorrow.toISOString(),
+      dueDate: new Date(tomorrow.getDate() + 5).toISOString(),
       priority: DeadlinePriority.ALTA,
     };
 
     const deadline = await deadlineRepository.create(deadlineDTO);
-    expect(deadline.caseId.toString()).toBe(deadlineDTO.caseId.toString());
-    expect(deadline.lawyerId.toString()).toBe(deadlineDTO.lawyerId.toString());
-    expect(deadline.type).toBe(deadlineDTO.type);
-    expect(deadline.status).toBe(deadlineDTO.status);
-    expect(deadline.priority).toBe(deadlineDTO.priority);
+    const createdDeadline = await DeadlineModel.findById(deadline.id);
+
+    expect(deadline).toEqual(expect.objectContaining(deadlineDTO));
+    expect(createdDeadline?.caseId.toString()).toEqual(deadlineDTO.caseId);
+    expect(createdDeadline?.lawyerId.toString()).toEqual(deadlineDTO.lawyerId);
+    expect(createdDeadline?.intimationDate.toISOString()).toEqual(deadlineDTO.intimationDate);
+    expect(createdDeadline?.days).toEqual(deadlineDTO.days);
+    expect(createdDeadline?.startDate.toISOString()).toEqual(deadlineDTO.startDate);
+    expect(createdDeadline?.dueDate.toISOString()).toEqual(deadlineDTO.dueDate);
+    expect(createdDeadline?.type).toEqual(deadlineDTO.type);
+    expect(createdDeadline?.priority).toEqual(deadlineDTO.priority);
+  });
+
+  test('should return deadlines with proper status', async () => {
+    const { deadlineRepository } = makeSut();
+
+    const today = new Date();
+    const yesterday = new Date(today);
+    yesterday.setDate(yesterday.getDate() - 1);
+    const tomorrow = new Date(today);
+    tomorrow.setDate(tomorrow.getDate() + 1);
+
+    const pendingDeadlineDTO = {
+      caseId: Types.ObjectId.createFromTime(823775684).toString(),
+      lawyerId: Types.ObjectId.createFromTime(872576365).toString(),
+      type: DeadlineType.PAGAMENTO,
+      intimationDate: today.toISOString(),
+      startDate: tomorrow.toISOString(),
+      dueDate: new Date(tomorrow.getDate() + 5).toISOString(),
+      days: 5,
+      priority: DeadlinePriority.ALTA,
+    };
+
+    const openDeadlineDTO = {
+      caseId: Types.ObjectId.createFromTime(877273333484).toString(),
+      lawyerId: Types.ObjectId.createFromTime(555584).toString(),
+      type: DeadlineType.PAGAMENTO,
+      intimationDate: new Date(yesterday.getDate() - 1).toISOString(), // 1 days before yesterday
+      startDate: yesterday.toISOString(), //yesterday
+      dueDate: tomorrow.toISOString(), // tomorrow
+      days: 2,
+      priority: DeadlinePriority.ALTA,
+    };
+
+    const expiredDeadlineDTO = {
+      caseId: Types.ObjectId.createFromTime(848484).toString(),
+      lawyerId: Types.ObjectId.createFromTime(8484).toString(),
+      type: DeadlineType.PAGAMENTO,
+      intimationDate: new Date('2026-04-01').toISOString(),
+      startDate: new Date('2026-04-4').toISOString(),
+      dueDate: new Date('2026-04-9').toISOString(),
+      days: 5,
+      priority: DeadlinePriority.ALTA,
+    };
+
+    const pendingDeadline = await deadlineRepository.create(pendingDeadlineDTO);
+    const openDeadline = await deadlineRepository.create(openDeadlineDTO);
+    const expiredDeadline = await deadlineRepository.create(expiredDeadlineDTO);
+
+    expect(pendingDeadline.status).toBe(DeadlineStatus.PENDENTE);
+    expect(openDeadline.status).toBe(DeadlineStatus.EM_ANDAMENTO);
+    expect(expiredDeadline.status).toBe(DeadlineStatus.VENCIDO);
   });
 
   test('should find deadline all deadlines', async () => {
     const { deadlineRepository } = makeSut();
     const newDeadlines = [
       {
-        caseId: Types.ObjectId.createFromTime(848484).toString(),
-        lawyerId: Types.ObjectId.createFromTime(8484).toString(),
+        caseId: Types.ObjectId.createFromTime(87484).toString(),
+        lawyerId: Types.ObjectId.createFromTime(8884).toString(),
         type: DeadlineType.PAGAMENTO,
-        startDate: new Date('2050-02-02').toString(),
-        dueDate: new Date('2050-03-02').toString(),
-        status: DeadlineStatus.EM_ANDAMENTO,
-        priority: DeadlinePriority.ALTA,
+        intimationDate: new Date('2026-03-01').toISOString(),
+        startDate: new Date('2026-03-08').toISOString(),
+        dueDate: new Date('2026-03-28').toISOString(),
+        days: 5,
+        priority: DeadlinePriority.BAIXA,
       },
       {
-        caseId: Types.ObjectId.createFromTime(8484).toString(),
-        lawyerId: Types.ObjectId.createFromTime(847774).toString(),
-        type: DeadlineType.CONTESTACAO,
-        startDate: new Date('2050-02-02').toString(),
-        dueDate: new Date('2050-03-02').toString(),
-        status: DeadlineStatus.CONCLUIDO,
+        caseId: Types.ObjectId.createFromTime(822584).toString(),
+        lawyerId: Types.ObjectId.createFromTime(28557).toString(),
+        type: DeadlineType.OUTRO,
+        intimationDate: new Date('2026-02-01').toISOString(),
+        startDate: new Date('2026-02-27').toISOString(),
+        dueDate: new Date('2026-02-28').toISOString(),
+        days: 5,
         priority: DeadlinePriority.ALTA,
       },
     ];
@@ -88,77 +154,80 @@ describe('Test DeadlineRepository', () => {
   test('should find deadline by id', async () => {
     const { deadlineRepository } = makeSut();
 
-    const newDeadline = {
+    const deadlineDTO = {
       caseId: Types.ObjectId.createFromTime(848484).toString(),
       lawyerId: Types.ObjectId.createFromTime(8484).toString(),
       type: DeadlineType.PAGAMENTO,
-      startDate: new Date('2050-02-02').toString(),
-      dueDate: new Date('2050-03-02').toString(),
-      status: DeadlineStatus.EM_ANDAMENTO,
+      intimationDate: new Date('2026-04-01').toISOString(),
+      startDate: new Date('2026-04-27').toISOString(),
+      dueDate: new Date('2026-04-28').toISOString(),
+      days: 5,
       priority: DeadlinePriority.ALTA,
     };
 
-    const newId = (await DeadlineModel.create(newDeadline))._id;
+    const newId = (await DeadlineModel.create(deadlineDTO))._id;
 
     const deadline = await deadlineRepository.findById(newId.toString());
 
     if (!deadline) {
       throw Error('Deadline not found');
     }
-    expect(deadline).toEqual(expect.objectContaining(newDeadline));
-
+    expect(deadline).toEqual(expect.objectContaining(deadlineDTO));
   });
 
   test('should find deadlines by case id', async () => {
     const { deadlineRepository } = makeSut();
 
-    const newDeadline = {
+    const deadlineDTO = {
       caseId: Types.ObjectId.createFromTime(848484).toString(),
       lawyerId: Types.ObjectId.createFromTime(8484).toString(),
       type: DeadlineType.PAGAMENTO,
-      startDate: new Date('2050-02-02').toString(),
-      dueDate: new Date('2050-03-02').toString(),
-      status: DeadlineStatus.EM_ANDAMENTO,
+      intimationDate: new Date('2026-04-01').toISOString(),
+      startDate: new Date('2026-04-27').toISOString(),
+      dueDate: new Date('2026-04-28').toISOString(),
+      days: 5,
       priority: DeadlinePriority.ALTA,
     };
 
     const otherCaseDeadline = {
-      caseId: Types.ObjectId.createFromTime(8484884613614).toString(),
-      lawyerId: Types.ObjectId.createFromTime(8484).toString(),
+      caseId: Types.ObjectId.createFromTime(847777).toString(),
+      lawyerId: Types.ObjectId.createFromTime(88777).toString(),
       type: DeadlineType.PAGAMENTO,
-      startDate: new Date('2050-02-02').toString(),
-      dueDate: new Date('2050-03-02').toString(),
-      status: DeadlineStatus.EM_ANDAMENTO,
+      intimationDate: new Date('2026-04-01').toISOString(),
+      startDate: new Date('2026-04-27').toISOString(),
+      dueDate: new Date('2026-04-28').toISOString(),
+      days: 5,
       priority: DeadlinePriority.ALTA,
     };
 
-    await DeadlineModel.create(newDeadline);
+    await DeadlineModel.create(deadlineDTO);
     await DeadlineModel.create(otherCaseDeadline);
 
-    const deadlines = await deadlineRepository.findByCaseId(newDeadline.caseId.toString());
+    const deadlines = await deadlineRepository.findByCaseId(deadlineDTO.caseId.toString());
 
-    expect(deadlines).toContainEqual(expect.objectContaining(newDeadline));
+    expect(deadlines).toContainEqual(expect.objectContaining(deadlineDTO));
     expect(deadlines).not.toContainEqual(expect.objectContaining(otherCaseDeadline));
   });
 
   test('should delete a deadline', async () => {
     const { deadlineRepository } = makeSut();
 
-    const newDeadline = {
+    const deadlineDTO = {
       caseId: Types.ObjectId.createFromTime(848484).toString(),
       lawyerId: Types.ObjectId.createFromTime(8484).toString(),
       type: DeadlineType.PAGAMENTO,
-      startDate: new Date('2050-02-02').toString(),
-      dueDate: new Date('2050-03-02').toString(),
-      status: DeadlineStatus.EM_ANDAMENTO,
+      intimationDate: new Date('2026-04-01').toISOString(),
+      startDate: new Date('2026-04-27').toISOString(),
+      dueDate: new Date('2026-04-28').toISOString(),
+      days: 5,
       priority: DeadlinePriority.ALTA,
     };
 
-    const deadlineId = (await DeadlineModel.create(newDeadline))._id;
+    const deadlineId = (await DeadlineModel.create(deadlineDTO))._id;
 
     const result = await deadlineRepository.deleteById(deadlineId.toString());
 
-    expect(result).toEqual(expect.objectContaining(newDeadline));
+    expect(result).toEqual(expect.objectContaining(deadlineDTO));
 
     // Ensure deadline is actually deleted
     const deletedDeadline = await DeadlineModel.findById(deadlineId);
@@ -168,17 +237,18 @@ describe('Test DeadlineRepository', () => {
   test('should update a deadline', async () => {
     const { deadlineRepository } = makeSut();
 
-    const newDeadline = {
+    const deadlineDTO = {
       caseId: Types.ObjectId.createFromTime(848484).toString(),
       lawyerId: Types.ObjectId.createFromTime(8484).toString(),
       type: DeadlineType.PAGAMENTO,
-      startDate: new Date('2050-02-02').toString(),
-      dueDate: new Date('2050-03-02').toString(),
-      status: DeadlineStatus.EM_ANDAMENTO,
+      intimationDate: new Date('2026-04-01').toISOString(),
+      startDate: new Date('2026-04-27').toISOString(),
+      dueDate: new Date('2026-04-28').toISOString(),
+      days: 5,
       priority: DeadlinePriority.ALTA,
     };
 
-    const deadlineId = (await DeadlineModel.create(newDeadline))._id;
+    const deadlineId = (await DeadlineModel.create(deadlineDTO))._id;
 
     const updatedData = {
       type: DeadlineType.REPLICA,
@@ -196,17 +266,18 @@ describe('Test DeadlineRepository', () => {
   test('should return true if deadline exists, but false if deadline does not exist', async () => {
     const { deadlineRepository } = makeSut();
 
-    const newDeadline = {
+    const deadlineDTO = {
       caseId: Types.ObjectId.createFromTime(848484).toString(),
       lawyerId: Types.ObjectId.createFromTime(8484).toString(),
       type: DeadlineType.PAGAMENTO,
-      startDate: new Date('2050-02-02').toString(),
-      dueDate: new Date('2050-03-02').toString(),
-      status: DeadlineStatus.EM_ANDAMENTO,
+      intimationDate: new Date('2026-04-01').toISOString(),
+      startDate: new Date('2026-04-27').toISOString(),
+      dueDate: new Date('2026-04-28').toISOString(),
+      days: 5,
       priority: DeadlinePriority.ALTA,
     };
 
-    const newId = (await DeadlineModel.create(newDeadline))._id;
+    const newId = (await DeadlineModel.create(deadlineDTO))._id;
 
     const existingDeadline = await deadlineRepository.existsById(newId.toString());
     const nonExistingDeadline = await deadlineRepository.existsById(
