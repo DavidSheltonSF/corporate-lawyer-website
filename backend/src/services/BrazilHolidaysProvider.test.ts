a3522e229790c3c89c81d8823b77849beee615ae
DavidSheltonSF <david.faria012@gmail.com>
2026-04-27 13:18:29 -0300

    style(tests): format BrazilHolidaysProvider test for consistency

--- backend/src/services/BrazilHolidaysProvider.test.ts
+++ backend/src/services/BrazilHolidaysProvider.test.ts
@@ -2,15 +2,18 @@ import { BrazilianState } from '../types/BrazilianState';
 import { City } from '../types/City';
 import { BrazilHolidaysProvider } from './BrazilHolidaysProvider';
 
-describe(`Testing ${BrazilHolidaysProvider}`, () => {
+describe(`Testing ${BrazilHolidaysProvider.name}`, () => {
   function makeSut() {
     const brazilHolidaysProvider = new BrazilHolidaysProvider();
-    return {brazilHolidaysProvider}
+    return { brazilHolidaysProvider };
   }
   test(`should return Belford Roxo's holidays, Rio de Janeiro's (STATE) Holidays and Brazil's holidays`, () => {
-    const {brazilHolidaysProvider} = makeSut();
+    const { brazilHolidaysProvider } = makeSut();
 
-    const holidays = brazilHolidaysProvider.getLocalHolidays(BrazilianState.RIO_DE_JANEIRO, City.RIO_DE_JANEIRO);
-    console.log(holidays)
+    const holidays = brazilHolidaysProvider.getLocalHolidays(
+      BrazilianState.RIO_DE_JANEIRO,
+      City.RIO_DE_JANEIRO
+    );
+    console.log(holidays);
   });
 });
