6761124802ed043f84660e44cd0a17687979fcf7
DavidSheltonSF <david.faria012@gmail.com>
2026-04-27 20:14:46 -0300

    refactor(backend/mock/mockCaseMongoDocs): make it use type WithMongoId<IcaseModel>[]

--- backend/src/tests/mocks/mockCaseMongoDocs.ts
+++ backend/src/tests/mocks/mockCaseMongoDocs.ts
@@ -1,9 +1,13 @@
 import { Types } from 'mongoose';
 import { CasesStatus } from '../../types/CasesStatus';
+import { ICaseModel } from '../../models/CaseModel';
+import { City } from '../../types/City';
+import { BrazilianState } from '../../types/BrazilianState';
+import { WithMongoId } from '../../database/mongoDB/types/WithMongoId';
 
-export const mockCaseMongoDocs = [
+export const mockCaseMongoDocs: WithMongoId<ICaseModel>[] = [
   {
-    id: Types.ObjectId.createFromTime(485855),
+    _id: Types.ObjectId.createFromTime(485855),
     client: Types.ObjectId.createFromTime(45855),
     court: 'STJ',
     courtDivision: 'Família',
@@ -12,5 +16,11 @@ export const mockCaseMongoDocs = [
     processNumber: '2254787-55.5877.1.55.4787', //NNNNNNN-DD.AAAA.J.TR.OOOO,
     status: CasesStatus.open,
     title: 'Case Title',
+    files: [],
+    hearings: [],
+    localization: {
+      state: BrazilianState.RIO_DE_JANEIRO,
+      city: City.BELFORD_ROXO,
+    },
   },
 ];
