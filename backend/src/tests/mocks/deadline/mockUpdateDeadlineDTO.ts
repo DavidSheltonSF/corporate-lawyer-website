f3c71d4a01abfb9c9c19d2a3873abcc1a45bccac
DavidSheltonSF <david.faria012@gmail.com>
2026-04-28 19:25:00 -0300

    chore(backend/mocks/mockUpdateDeadlineDTO): use  Date object with toISOString() to create the intimation date for consistency

--- backend/src/tests/mocks/deadline/mockUpdateDeadlineDTO.ts
+++ backend/src/tests/mocks/deadline/mockUpdateDeadlineDTO.ts
@@ -5,7 +5,7 @@ import { DeadlineType } from '../../../types/DeadLineType';
 export function mockUpateDeadlineDTO(): UpdateDeadlineDTO {
   return {
     type: DeadlineType.CONTESTACAO,
-    intimationDate: '2026-02-25',
+    intimationDate: new Date().toISOString(),
     days: 5,
     priority: DeadlinePriority.ALTA,
   };
