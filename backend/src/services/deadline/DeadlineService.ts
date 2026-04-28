62478aeea94911ee01d682078874cc6adfeb627b
DavidSheltonSF <david.faria012@gmail.com>
2026-04-27 23:17:59 -0300

    refactor(backend/services): update DeadlineService and IDeadlineService to use DeadlineResponseDTO for improved response handling

--- backend/src/services/deadline/DeadlineService.ts
+++ backend/src/services/deadline/DeadlineService.ts
@@ -1,9 +1,16 @@
 import { DeadlineDTO } from '../../dtos/deadLine/DeadlineDTO';
+import { DeadlineResponseDTO } from '../../dtos/deadLine/DeadlineResponseDTO';
 import { UpdateDeadlineDTO } from '../../dtos/deadLine/UpdateDeadlineDTO';
+import { CaseNotFoundError } from '../../errors/domain/CaseNotFoundError';
+import { DomainError } from '../../errors/domain/DomainError';
 import { CaseRepository } from '../../repositories/CaseRepository';
 import { DeadlineRepository } from '../../repositories/DeadlineRepository';
+import { BrazilState } from '../../types/BrazilState';
 import { WithId } from '../../types/WithId';
-import { getDeadlineStatus } from '../helpers/getDeadlineStatus';
+import { DeadlineCalculator } from '../helpers/DeadlineCalculator';
+import { getBrazilState } from '../helpers/getBrazilState';
+import { getCity } from '../helpers/getCity';
+import { HolidaysProvider } from '../HolidaysProvider';
 import { validateDeadline } from '../validators/deadlines/validateDeadline';
 import { validateDeadlinePartial } from '../validators/deadlines/validateDeadlinePartial';
 import { IDeadlineService } from './IDeadlineService';
@@ -11,36 +18,60 @@ import { IDeadlineService } from './IDeadlineService';
 export class DeadlineService implements Partial<IDeadlineService> {
   constructor(
     private readonly deadlineRepository: DeadlineRepository,
-    private readonly caseRepository: CaseRepository
+    private readonly caseRepository: CaseRepository,
+    private readonly holidaysProvider: HolidaysProvider
   ) {}
 
-  async create(data: DeadlineDTO): Promise<WithId<DeadlineDTO>> {
+  async create(data: DeadlineDTO): Promise<WithId<DeadlineResponseDTO>> {
     validateDeadline(data);
-    const status = getDeadlineStatus(data);
-    return await this.deadlineRepository.create({ ...data, status });
+
+    const { caseId, intimationDate, days } = data;
+
+    const deadlineCase = await this.caseRepository.findById(caseId);
+    if (!deadlineCase) {
+      throw new CaseNotFoundError(caseId);
+    }
+
+    const { state, city } = deadlineCase.location;
+
+    const validState = getBrazilState(state);
+    const validCity = getCity(city);
+
+    const deadlineCalculator = new DeadlineCalculator(this.holidaysProvider, {
+      state: validState,
+      city: validCity,
+    });
+
+    const startDate = deadlineCalculator.getNextBusinessDay(new Date(intimationDate));
+    const duedate = deadlineCalculator.getDueDate(startDate, days);
+
+    return await this.deadlineRepository.create(data, startDate, duedate);
   }
 
   async findAll(): Promise<WithId<DeadlineDTO>[]> {
     return await this.deadlineRepository.findAll();
   }
 
-  async findById(id: string): Promise<WithId<DeadlineDTO> | null> {
+  async findById(id: string): Promise<WithId<DeadlineResponseDTO> | null> {
     return await this.deadlineRepository.findById(id);
   }
 
-  async findByCaseId(id: string): Promise<WithId<DeadlineDTO>[] | null> {
+  async findByCaseId(id: string): Promise<WithId<DeadlineResponseDTO>[] | null> {
     const caseExists = await this.caseRepository.exists(id);
     if (!caseExists) return null;
 
     return await this.deadlineRepository.findByCaseId(id);
   }
 
-  async updateById(id: string, data: UpdateDeadlineDTO): Promise<WithId<DeadlineDTO> | null> {
+  async updateById(
+    id: string,
+    data: UpdateDeadlineDTO
+  ): Promise<WithId<DeadlineResponseDTO> | null> {
     validateDeadlinePartial(data);
     return await this.deadlineRepository.updateById(id, data);
   }
 
-  async deleteById(id: string): Promise<WithId<DeadlineDTO> | null> {
+  async deleteById(id: string): Promise<WithId<DeadlineResponseDTO> | null> {
     return await this.deadlineRepository.deleteById(id);
   }
 }
--- backend/src/services/deadline/IDeadlineService.ts
+++ backend/src/services/deadline/IDeadlineService.ts
@@ -1,12 +1,13 @@
 import { DeadlineDTO } from '../../dtos/deadLine/DeadlineDTO';
+import { DeadlineResponseDTO } from '../../dtos/deadLine/DeadlineResponseDTO';
 import { UpdateDeadlineDTO } from '../../dtos/deadLine/UpdateDeadlineDTO';
 import { WithId } from '../../types/WithId';
 
 export interface IDeadlineService {
-  create(data: DeadlineDTO): Promise<WithId<DeadlineDTO>>;
+  create(data: DeadlineDTO): Promise<WithId<DeadlineResponseDTO>>;
   findAll(): Promise<WithId<DeadlineDTO>[]>;
-  findById(id: string): Promise<WithId<DeadlineDTO> | null>;
-  findByCaseId(id: string): Promise<WithId<DeadlineDTO>[] | null>;
-  updateById(id: string, data: UpdateDeadlineDTO): Promise<WithId<DeadlineDTO> | null>;
-  deleteById(id: string): Promise<WithId<DeadlineDTO> | null>;
+  findById(id: string): Promise<WithId<DeadlineResponseDTO> | null>;
+  findByCaseId(id: string): Promise<WithId<DeadlineResponseDTO>[] | null>;
+  updateById(id: string, data: UpdateDeadlineDTO): Promise<WithId<DeadlineResponseDTO> | null>;
+  deleteById(id: string): Promise<WithId<DeadlineResponseDTO> | null>;
 }
