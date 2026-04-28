b2b8c9f57b388cd39252cdfdc8280e3ca89299b3
DavidSheltonSF <david.faria012@gmail.com>
2026-04-28 16:03:10 -0300

    refactor(dtos): simplify CreateClientResponseDTO by extending UserDTO and removing redundant fields

--- backend/src/dtos/user/CreateClientResponseDTO.ts
+++ backend/src/dtos/user/CreateClientResponseDTO.ts
@@ -1,8 +1,3 @@
-export interface CreateClientResponseDTO {
-  firstName: string;
-  lastName: string;
-  email: string;
-  cpf: string;
-  role: string;
-  password: string;
-}
+import { UserDTO } from './UserDTO';
+
+export type CreateClientResponseDTO = UserDTO & { password: string };
