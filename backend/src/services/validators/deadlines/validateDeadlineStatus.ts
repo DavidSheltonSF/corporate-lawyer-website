32c9b67e50d16d7ffec0a77f707f4dd18b84cdf1
DavidSheltonSF <david.faria012@gmail.com>
2026-04-27 22:59:44 -0300

    chore(backend/validators): remove unused validateDeadlineStatus and validateDeadlineDateRange functions

--- backend/src/services/validators/deadlines/validateDeadlineStatus.ts
+++
@@ -1,9 +0,0 @@
-import { InvalidDeadlineStatusError } from '../../../errors/domain/InvalidDeadlineStatusError';
-import { DeadlineStatus } from '../../../types/DeadLineStatus';
-
-export function validateDeadlineStatus(status: string) {
-  const validStatuses = Object.values(DeadlineStatus) as string[];
-  if (!validStatuses.includes(status)) {
-    throw new InvalidDeadlineStatusError(status);
-  }
-}
