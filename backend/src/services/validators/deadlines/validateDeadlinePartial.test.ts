8c69d1c26b6022f1bceb26c4791046f2b9010bd4
DavidSheltonSF <david.faria012@gmail.com>
2026-04-28 17:14:21 -0300

    refactor(tests): simplify validateDeadlinePartial tests by using mockUpdateDeadlineDTO and importing getThrownError from other file

--- backend/src/services/validators/deadlines/validateDeadlinePartial.test.ts
+++ backend/src/services/validators/deadlines/validateDeadlinePartial.test.ts
@@ -1,67 +1,34 @@
-import { DeadlineStatus } from '../../../types/DeadLineStatus';
 import { validateDeadlinePartial } from './validateDeadlinePartial';
-import { DeadlineType } from '../../../types/DeadLineType';
-import { DeadlinePriority } from '../../../types/DeadLinePriority';
 import { InvalidDeadlineTypeError } from '../../../errors/domain/InvalidDeadlineTypeError';
 import { InvalidDeadlinePriorityError } from '../../../errors/domain/InvalidDeadlinePriorityError';
 import { InvalidDateError } from '../../../errors/domain/InvalidDateError';
+import { getThrownError } from '../../../tests/helpers/getThrownError';
+import { mockUpateDeadlineDTO } from '../../../tests/mocks/deadline/mockUpdateDeadlineDTO';
 
 describe(`Testing ${validateDeadlinePartial.name}`, () => {
-  function getThrownError(callBack: Function) {
-    try {
-      callBack();
-      return null;
-    } catch (error) {
-      return error;
-    }
-  }
-
   test('should not throw error if all deadline fields are  valid', () => {
-    const deadlineDTO = {
-      type: DeadlineType.PAGAMENTO,
-      dateRange: {
-        startDate: '2050-02-15',
-        dueDate: '2050-02-20',
-      },
-      status: DeadlineStatus.EM_ANDAMENTO,
-      priority: DeadlinePriority.ALTA,
-    };
-
+    const deadlineDTO = mockUpateDeadlineDTO();
     const thrownError = getThrownError(() => validateDeadlinePartial(deadlineDTO));
     expect(thrownError).toBeNull();
   });
 
   test('should throw InvalidDeadlineTypeError if type provided is invalid', () => {
-    const deadlineDTO = {
-      type: 'banana',
-      startDate: '2050-02-15',
-    };
+    const deadlineDTO = mockUpateDeadlineDTO();
+    deadlineDTO.type = 'banana';
     const thrownError = getThrownError(() => validateDeadlinePartial(deadlineDTO));
     expect(thrownError).toBeInstanceOf(InvalidDeadlineTypeError);
   });
 
   test('should throw InvalidDeadlinePriorityError if type provided is invalid', () => {
-    const deadlineDTO = {
-      type: DeadlineType.AUDIENCIA,
-      dateRange: {
-        startDate: '2050-02-15',
-        dueDate: '2050-02-20',
-      },
-      status: DeadlineStatus.EM_ANDAMENTO,
-      priority: 'banana',
-    };
+    const deadlineDTO = mockUpateDeadlineDTO();
+    deadlineDTO.priority = 'banana';
     const thrownError = getThrownError(() => validateDeadlinePartial(deadlineDTO));
     expect(thrownError).toBeInstanceOf(InvalidDeadlinePriorityError);
   });
 
-  test('should throw InvalidDateError if the start date or the due date provided is invalid', () => {
-    const deadlineDTO = {
-      type: DeadlineType.PAGAMENTO,
-      dateRange: { startDate: '2050-02-55', dueDate: '2050-02-20' },
-      status: DeadlineStatus.EM_ANDAMENTO,
-      priority: DeadlinePriority.ALTA,
-    };
-
+  test('should throw InvalidDateError if the start intimationDate provided is invalid', () => {
+    const deadlineDTO = mockUpateDeadlineDTO();
+    deadlineDTO.intimationDate = 'banana';
     const thrownError = getThrownError(() => validateDeadlinePartial(deadlineDTO));
     expect(thrownError).toBeInstanceOf(InvalidDateError);
   });
