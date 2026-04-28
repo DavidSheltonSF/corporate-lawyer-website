e56f74714aab496d193551244ac5ea9308735638
DavidSheltonSF <david.faria012@gmail.com>
2026-04-27 23:17:43 -0300

    refactor(tests): enhance DeadlineService tests by adding holidays provider and updating deadline creation logic

--- backend/src/services/deadline/DeadlineService.test.ts
+++ backend/src/services/deadline/DeadlineService.test.ts
@@ -1,47 +1,59 @@
 import { Types } from 'mongoose';
-import { EntityAlreadyExistsError } from '../../errors/domain/EntityAlreadyExistsError';
-import { InvalidEmailError } from '../../errors/domain/InvalidEmailError';
-import { InvalidNameError } from '../../errors/domain/InvalidNameError';
 import { createMockCaseRepository } from '../../tests/mocks/repositories/createMockCaseRepository';
 import { createMockDeadlineRepository } from '../../tests/mocks/repositories/createMockDeadlineRepository';
 import { DeadlineService } from './DeadlineService';
 import { DeadlineType } from '../../types/DeadLineType';
-import { DeadlineStatus } from '../../types/DeadLineStatus';
 import { DeadlinePriority } from '../../types/DeadLinePriority';
 import { InvalidDeadlineTypeError } from '../../errors/domain/InvalidDeadlineTypeError';
 import { InvalidDeadlinePriorityError } from '../../errors/domain/InvalidDeadlinePriorityError';
-import { InvalidDeadlineStatusError } from '../../errors/domain/InvalidDeadlineStatusError';
 import { InvalidDateError } from '../../errors/domain/InvalidDateError';
-import { InvalidDateRangeError } from '../../errors/domain/InvalidDateRangeError';
+import { BrazilState } from '../../types/BrazilState';
+import { City } from '../../types/City';
+import { BrazilHolidaysProvider } from '../BrazilHolidaysProvider';
+import { createDate } from '../../utils/createDate';
+import { DeadlineCalculator } from '../helpers/DeadlineCalculator';
 
 describe(`Test ${DeadlineService.name}`, () => {
   function makeSut() {
     const caseRepository = createMockCaseRepository();
     const deadlineRepository = createMockDeadlineRepository();
-    const deadlineService = new DeadlineService(deadlineRepository, caseRepository);
+    const holidaysProvider = new BrazilHolidaysProvider();
+    const deadlineService = new DeadlineService(
+      deadlineRepository,
+      caseRepository,
+      holidaysProvider
+    );
 
     return {
       deadlineRepository,
       deadlineService,
+      holidaysProvider,
     };
   }
 
   test('should call DeadlineRepository.create', async () => {
-    const { deadlineService, deadlineRepository } = makeSut();
-
+    const { deadlineService, deadlineRepository, holidaysProvider } = makeSut();
     const newDeadline = {
       caseId: Types.ObjectId.createFromTime(848484).toString(),
       lawyerId: Types.ObjectId.createFromTime(8484).toString(),
       type: DeadlineType.PAGAMENTO,
-      startDate: '2050-02-02',
-      dueDate: '2050-03-02',
-      status: DeadlineStatus.EM_ANDAMENTO,
+      intimationDate: createDate(2026, 4, 27).toISOString(),
+      days: 3,
       priority: DeadlinePriority.ALTA,
     };
 
+    const deadlineCalculator = new DeadlineCalculator(holidaysProvider, {
+      state: BrazilState.RIO_DE_JANEIRO,
+      city: City.BELFORD_ROXO,
+    });
+
+    const startDate = deadlineCalculator.getNextBusinessDay(new Date(newDeadline.intimationDate));
+    const dueDate = deadlineCalculator.getDueDate(new Date(startDate), newDeadline.days);
+
+
     await deadlineService.create(newDeadline);
 
-    expect(deadlineRepository.create).toHaveBeenCalled();
+    expect(deadlineRepository.create).toHaveBeenCalledWith(newDeadline, startDate, dueDate );
   });
 
   test('should thow InvalidDeadlineTypeError if the type provided is invalid', async () => {
@@ -51,29 +63,12 @@ describe(`Test ${DeadlineService.name}`, () => {
       caseId: Types.ObjectId.createFromTime(848484).toString(),
       lawyerId: Types.ObjectId.createFromTime(8484).toString(),
       type: 'banana',
-      startDate: '2050-02-02',
-      dueDate: '2050-03-02',
-      status: DeadlineStatus.EM_ANDAMENTO,
+      intimationDate: '2050-02-02',
+      days: 5,
       priority: DeadlinePriority.ALTA,
     };
 
     await expect(deadlineService.create(newDeadline)).rejects.toThrow(InvalidDeadlineTypeError);
-  });
-
-  test('should thow InvalidDeadlineStatusError if the status provided is invalid', async () => {
-    const { deadlineService } = makeSut();
-
-    const newDeadline = {
-      caseId: Types.ObjectId.createFromTime(848484).toString(),
-      lawyerId: Types.ObjectId.createFromTime(8484).toString(),
-      type: DeadlineType.PAGAMENTO,
-      startDate: '2050-02-02',
-      dueDate: '2050-03-02',
-      status: 'banana',
-      priority: DeadlinePriority.ALTA,
-    };
-
-    await expect(deadlineService.create(newDeadline)).rejects.toThrow(InvalidDeadlineStatusError);
   });
 
   test('should thow InvalidDeadlinePriorityError if the priority provided is invalid', async () => {
@@ -83,45 +78,27 @@ describe(`Test ${DeadlineService.name}`, () => {
       caseId: Types.ObjectId.createFromTime(848484).toString(),
       lawyerId: Types.ObjectId.createFromTime(8484).toString(),
       type: DeadlineType.PAGAMENTO,
-      startDate: '2050-02-02',
-      dueDate: '2050-03-02',
-      status: DeadlineStatus.EM_ANDAMENTO,
+      intimationDate: '2050-02-02',
+      days: 5,
       priority: 'banana',
     };
 
     await expect(deadlineService.create(newDeadline)).rejects.toThrow(InvalidDeadlinePriorityError);
   });
 
-  test('should thow InvalidDateError if one of the dates provided is invalid', async () => {
+  test('should thow InvalidDateError if the intimationDate provided is invalid', async () => {
     const { deadlineService } = makeSut();
 
     const newDeadline = {
       caseId: Types.ObjectId.createFromTime(848484).toString(),
       lawyerId: Types.ObjectId.createFromTime(8484).toString(),
       type: DeadlineType.PAGAMENTO,
-      startDate: '2050-02-31',
-      dueDate: '2050-03-02',
-      status: DeadlineStatus.EM_ANDAMENTO,
+      intimationDate: '2050-02-50',
+      days: 5,
       priority: DeadlinePriority.ALTA,
     };
 
     await expect(deadlineService.create(newDeadline)).rejects.toThrow(InvalidDateError);
-  });
-
-  test('should thow InvalidDateError if the start date provided is greater than the due date', async () => {
-    const { deadlineService } = makeSut();
-
-    const newDeadline = {
-      caseId: Types.ObjectId.createFromTime(848484).toString(),
-      lawyerId: Types.ObjectId.createFromTime(8484).toString(),
-      type: DeadlineType.PAGAMENTO,
-      startDate: '2026-03-15',
-      dueDate: '2026-03-02',
-      status: DeadlineStatus.EM_ANDAMENTO,
-      priority: DeadlinePriority.ALTA,
-    };
-
-    await expect(deadlineService.create(newDeadline)).rejects.toThrow(InvalidDateRangeError);
   });
 
   test('should find all deadlines', async () => {
