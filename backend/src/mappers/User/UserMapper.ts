cf9a6521e12509daf72d438a597609780348149a
DavidSheltonSF <david.faria012@gmail.com>
2026-04-28 15:16:51 -0300

    chore(backend/mappers/UserMapper): rename persistenceToDomain to persistenceToPresentation and update return type to UserDTO

--- backend/src/mappers/User/UserMapper.ts
+++ backend/src/mappers/User/UserMapper.ts
@@ -1,8 +1,8 @@
-import { User } from '../../entities/User';
+import { UserDTO } from '../../dtos/user/UserDTO';
 import { WithId } from '../../types/WithId';
 
 export class UserMapper {
-  static persistenceToDomain(data: any): WithId<User> {
+  static persistenceToPresentation(data: any): WithId<UserDTO> {
     const { firstName, lastName, email, cpf, role, password } = data;
     return { id: data._id.toString(), firstName, lastName, email, cpf, role, password };
   }
