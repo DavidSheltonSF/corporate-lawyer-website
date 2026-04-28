784a60acacf106ff36fbd2e8d1cb7912d8dfbd1d
DavidSheltonSF <david.faria012@gmail.com>
2026-04-28 13:49:07 -0300

    refactor(backend/mappers): simplify HearingMapper by removing generic typing and HearingPersistence type

--- backend/src/mappers/Hearing/HearingMapper.ts
+++ backend/src/mappers/Hearing/HearingMapper.ts
@@ -1,9 +1,8 @@
 import { HearingDTO } from '../../dtos/hearing/HearingDTO';
 import { WithId } from '../../types/WithId';
-import { HearingPersistence } from './HearingPersistence';
 
 export class HearingMapper {
-  static persistenceToPresentation<T extends HearingPersistence>(hearing: T): WithId<HearingDTO> {
+  static persistenceToPresentation(hearing: any): WithId<HearingDTO> {
     return {
       id: hearing._id.toString(),
       date: hearing.date.toISOString(),
