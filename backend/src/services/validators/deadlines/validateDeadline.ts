412a4b31c8b684d9dfa77f55344c67f86df17cf2
DavidSheltonSF <david.faria012@gmail.com>
2026-04-27 23:00:09 -0300

    refactor(backend/validators): simplify validateDeadline function by removing unused parameters and validations

--- backend/src/services/validators/deadlines/validateDeadline.ts
+++ backend/src/services/validators/deadlines/validateDeadline.ts
@@ -1,24 +1,11 @@
 import { DeadlineDTO } from '../../../dtos/deadLine/DeadlineDTO';
 import { validateDate } from '../validateDate';
-import { validateDeadlineDateRange } from '../validateDeadlineDateRange';
 import { validateDeadlinePriority } from './validateDeadlinePriority';
-import { validateDeadlineStatus } from './validateDeadlineStatus';
 import { validateDeadlineType } from './validateDeadlineType';
 
 export function validateDeadline(data: DeadlineDTO) {
-  const { type, priority, status, startDate, dueDate } = data;
-
-  if (status) {
-    validateDeadlineStatus(status);
-  }
-
+  const { type, priority, intimationDate } = data;
   validateDeadlineType(type);
   validateDeadlinePriority(priority);
-  validateDate(startDate);
-  validateDate(dueDate);
-
-  const parsedStartDate = new Date(startDate);
-  const parsedDueDate = new Date(dueDate);
-
-  validateDeadlineDateRange(parsedStartDate, parsedDueDate);
+  validateDate(intimationDate);
 }
