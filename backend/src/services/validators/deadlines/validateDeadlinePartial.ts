b094805feb7d37d439a12603029ba016b411fae4
DavidSheltonSF <david.faria012@gmail.com>
2026-04-27 23:19:22 -0300

    refactor(backend/validators): simplify validateDeadlinePartial by removing unused status and dateRange validations

--- backend/src/services/validators/deadlines/validateDeadlinePartial.ts
+++ backend/src/services/validators/deadlines/validateDeadlinePartial.ts
@@ -1,16 +1,10 @@
 import { UpdateDeadlineDTO } from '../../../dtos/deadLine/UpdateDeadlineDTO';
 import { validateDate } from '../validateDate';
-import { validateDeadlineDateRange } from '../validateDeadlineDateRange';
 import { validateDeadlinePriority } from './validateDeadlinePriority';
-import { validateDeadlineStatus } from './validateDeadlineStatus';
 import { validateDeadlineType } from './validateDeadlineType';
 
 export function validateDeadlinePartial(data: UpdateDeadlineDTO) {
-  const { type, priority, status, dateRange } = data;
-
-  if (status) {
-    validateDeadlineStatus(status);
-  }
+  const { type, priority, intimationDate } = data;
 
   if (type) {
     validateDeadlineType(type);
@@ -20,13 +14,7 @@ export function validateDeadlinePartial(data: UpdateDeadlineDTO) {
     validateDeadlinePriority(priority);
   }
 
-  if (dateRange) {
-    const { startDate, dueDate } = dateRange;
-    validateDate(startDate);
-    validateDate(dueDate);
-
-    const parsedStartDate = new Date(startDate);
-    const parsedDueDate = new Date(dueDate);
-    validateDeadlineDateRange(parsedStartDate, parsedDueDate);
+  if (intimationDate) {
+    validateDate(intimationDate);
   }
 }
