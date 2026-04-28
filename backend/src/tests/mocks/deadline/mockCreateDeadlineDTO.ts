214ee5a9fc7d762b91d52ff7df3b9719609ea21b
DavidSheltonSF <david.faria012@gmail.com>
2026-04-28 19:24:30 -0300

    chore(backend/mocks/mockCreateDeadlineDTO): use Types.ObjectId of mongoose lib to create the caseId and the lawyerId, and use Date object with toISOString() to create the intimation date for consistency

--- backend/src/tests/mocks/deadline/mockCreateDeadlineDTO.ts
+++ backend/src/tests/mocks/deadline/mockCreateDeadlineDTO.ts
@@ -1,13 +1,14 @@
+import { Types } from 'mongoose';
 import { CreateDeadlineDTO } from '../../../dtos/deadLine/CreateDeadlineDTO';
 import { DeadlinePriority } from '../../../types/DeadLinePriority';
 import { DeadlineType } from '../../../types/DeadLineType';
 
 export function mockCreateDeadlineDTO(): CreateDeadlineDTO {
   return {
-    caseId: 'fafdfgdgagagadga',
-    lawyerId: 'fsojnfosngishgomgg56',
+    caseId: Types.ObjectId.createFromTime(48585555).toString(),
+    lawyerId: Types.ObjectId.createFromTime(4822525).toString(),
     type: DeadlineType.CONTESTACAO,
-    intimationDate: '2026-02-25',
+    intimationDate: new Date().toISOString(),
     days: 5,
     priority: DeadlinePriority.ALTA,
   };
