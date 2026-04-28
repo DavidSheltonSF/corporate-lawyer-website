46ae0cfe2e7041a3c6c1c585ef9a20f5b6cf4880
DavidSheltonSF <david.faria012@gmail.com>
2026-04-27 22:58:57 -0300

    chore(backend/errors): remove unnecessary InvalidDateRangeError and InvalidDeadlineStatusError

--- backend/src/errors/domain/InvalidDateRangeError.ts
+++
@@ -1,8 +0,0 @@
-import { DomainError } from './DomainError';
-
-export class InvalidDateRangeError extends DomainError {
-  constructor(message: string) {
-    super(message);
-    this.name = InvalidDateRangeError.name;
-  }
-}
