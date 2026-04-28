e169a2b15364a70a003ea58c4ece06d8678b50c0
DavidSheltonSF <david.faria012@gmail.com>
2026-04-28 16:52:02 -0300

    chore(tests/validateDeadlinePartial): remove redundant tests and unused imports

--- backend/src/services/validators/deadlines/validateDeadlinePartial.test.ts
+++ backend/src/services/validators/deadlines/validateDeadlinePartial.test.ts
@@ -1,5 +1,3 @@
-import { Types } from 'mongoose';
-import { InvalidDeadlineStatusError } from '../../../errors/domain/InvalidDeadlineStatusError';
 import { DeadlineStatus } from '../../../types/DeadLineStatus';
 import { validateDeadlinePartial } from './validateDeadlinePartial';
 import { DeadlineType } from '../../../types/DeadLineType';
@@ -7,7 +5,6 @@ import { DeadlinePriority } from '../../../types/DeadLinePriority';
 import { InvalidDeadlineTypeError } from '../../../errors/domain/InvalidDeadlineTypeError';
 import { InvalidDeadlinePriorityError } from '../../../errors/domain/InvalidDeadlinePriorityError';
 import { InvalidDateError } from '../../../errors/domain/InvalidDateError';
-import { InvalidDateRangeError } from '../../../errors/domain/InvalidDateRangeError';
 
 describe(`Testing ${validateDeadlinePartial.name}`, () => {
   function getThrownError(callBack: Function) {
@@ -43,20 +40,6 @@ describe(`Testing ${validateDeadlinePartial.name}`, () => {
     expect(thrownError).toBeInstanceOf(InvalidDeadlineTypeError);
   });
 
-  test('should throw InvalidDeadlineTypeError if type provided is invalid', () => {
-    const deadlineDTO = {
-      type: DeadlineType.AUDIENCIA,
-      dateRange: {
-        startDate: '2050-02-15',
-        dueDate: '2050-02-20',
-      },
-      status: 'banana',
-      priority: DeadlinePriority.ALTA,
-    };
-    const thrownError = getThrownError(() => validateDeadlinePartial(deadlineDTO));
-    expect(thrownError).toBeInstanceOf(InvalidDeadlineStatusError);
-  });
-
   test('should throw InvalidDeadlinePriorityError if type provided is invalid', () => {
     const deadlineDTO = {
       type: DeadlineType.AUDIENCIA,
@@ -81,17 +64,5 @@ describe(`Testing ${validateDeadlinePartial.name}`, () => {
 
     const thrownError = getThrownError(() => validateDeadlinePartial(deadlineDTO));
     expect(thrownError).toBeInstanceOf(InvalidDateError);
-  });
-
-  test('should throw InvalidDateRangeError if the start date provided is greater thant the due date', () => {
-    const deadlineDTO = {
-      type: DeadlineType.PAGAMENTO,
-      dateRange: { startDate: '2050-02-20', dueDate: '2050-02-15' },
-      status: DeadlineStatus.EM_ANDAMENTO,
-      priority: DeadlinePriority.ALTA,
-    };
-
-    const thrownError = getThrownError(() => validateDeadlinePartial(deadlineDTO));
-    expect(thrownError).toBeInstanceOf(InvalidDateRangeError);
   });
 });
