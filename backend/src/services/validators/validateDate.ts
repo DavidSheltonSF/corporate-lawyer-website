b2116b5477f5732e2174b1e0b655e12564b1bc06
DavidSheltonSF <david.faria012@gmail.com>
2026-04-27 23:17:28 -0300

    refactor(backend/validators): update validateDate function to use formattedDate for error handling

--- backend/src/services/validators/validateDate.ts
+++ backend/src/services/validators/validateDate.ts
@@ -1,12 +1,14 @@
 import { InvalidDateError } from '../../errors/domain/InvalidDateError';
 
 export function validateDate(dateString: string) {
+  const formattedDate = dateString.split('T')[0] as string;
+
   const regex = /^\d{4}-\d{2}-\d{2}$/;
-  if (!regex.test(dateString)) {
-    throw new InvalidDateError(dateString);
+  if (!regex.test(formattedDate)) {
+    throw new InvalidDateError(formattedDate);
   }
 
-  const [year, month, day] = dateString.split('-').map(Number);
+  const [year, month, day] = formattedDate.split('-').map(Number);
   if (
     year === undefined ||
     month === undefined ||
@@ -15,11 +17,11 @@ export function validateDate(dateString: string) {
     isNaN(Number(month)) ||
     isNaN(Number(day))
   ) {
-    throw new InvalidDateError(dateString);
+    throw new InvalidDateError(formattedDate);
   }
 
   if (day < 1 || day > 31 || month < 1 || month > 12) {
-    throw new InvalidDateError(dateString);
+    throw new InvalidDateError(formattedDate);
   }
 
   // Decreace 1 from month for local-safe parsing
@@ -30,6 +32,6 @@ export function validateDate(dateString: string) {
   // will silently fix the data, so the data string and the Data object will not match
   // throwig the error bellow
   if (year !== date.getFullYear() || month !== date.getMonth() + 1 || day !== date.getDate()) {
-    throw new InvalidDateError(dateString);
+    throw new InvalidDateError(formattedDate);
   }
 }
