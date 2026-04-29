66dc3775ed74e32a736012602ac96900ee4b07b2
DavidSheltonSF <david.faria012@gmail.com>
2026-04-28 21:12:00 -0300

    chore(backend/mocks): make mockDeadlineMongoPersistence use casual instead of faker

--- backend/src/tests/mocks/mockDeadlineMongoPersistence.ts
+++ backend/src/tests/mocks/mockDeadlineMongoPersistence.ts
@@ -2,17 +2,19 @@ import { WithMongoId } from '../../database/mongoDB/types/WithMongoId';
 import { IDeadlineModel } from '../../models/DeadlineModel';
 import { DeadlineType } from '../../types/DeadLineType';
 import { DeadlinePriority } from '../../types/DeadLinePriority';
-import { simpleFaker } from '@faker-js/faker';
 import { mockMongoId } from '../helpers/mockMongoId';
+import casual from 'casual';
 
-export const mockDeadlineMongoPersistence: WithMongoId<IDeadlineModel> = {
-  _id: mockMongoId(),
-  caseId: mockMongoId(),
-  lawyerId: mockMongoId(),
-  type: simpleFaker.helpers.enumValue(DeadlineType),
-  intimationDate: new Date('2025-05-01'),
-  days: simpleFaker.number.int({ min: 1, max: 30 }),
-  startDate: new Date('2025-05-05'),
-  dueDate: new Date('2025-05-08'),
-  priority: DeadlinePriority.ALTA,
-};
+export function mockDeadlineMongoPersistence(): WithMongoId<IDeadlineModel> {
+  return {
+    _id: mockMongoId(),
+    caseId: mockMongoId(),
+    lawyerId: mockMongoId(),
+    type: casual.random_element(Object.keys(DeadlineType)),
+    intimationDate: new Date('2025-05-01'),
+    days: casual.integer(0, 10),
+    startDate: new Date('2025-05-05'),
+    dueDate: new Date('2025-05-08'),
+    priority: casual.random_element(Object.keys(DeadlinePriority)),
+  };
+}
