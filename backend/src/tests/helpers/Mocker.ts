9845edca0d0874f079020ec299fb5a025b348eb2
DavidSheltonSF <david.faria012@gmail.com>
2026-04-28 21:28:04 -0300

    fix(tests/helpers/Mocker): add mockMongoId

--- backend/src/tests/helpers/Mocker.ts
+++ backend/src/tests/helpers/Mocker.ts
@@ -1,15 +1,20 @@
 import casual from 'casual';
+import { Types, type ObjectId } from 'mongoose';
 
 export class Mocker {
   static mockName(): string {
     return casual.name;
   }
 
-  static mockInteger(from: number, to: number): number {
+  static mockInteger(from?: number, to?: number): number {
     return casual.integer(from, to);
   }
 
   static mockEnum<T>(enumerator: any): string {
     return casual.random_element(Object.keys(enumerator));
+  }
+
+  static mockMongoId(): ObjectId {
+    return Types.ObjectId.createFromTime(this.mockInteger()).
   }
 }
