8879169202ae64a62136d697b295ab07deebf125
DavidSheltonSF <david.faria012@gmail.com>
2026-04-28 21:12:54 -0300

    chore(backend/mocks): simplify createMockDeadlineRepostory by using mockDeadlinMongoPersistence instead of mockDeadlineMongoDocs

--- backend/src/tests/mocks/repositories/createMockDeadlineRepository.ts
+++ backend/src/tests/mocks/repositories/createMockDeadlineRepository.ts
@@ -1,14 +1,18 @@
 import { DeadlineRepository } from '../../../repositories/DeadlineRepository';
-import { mockDeadlineMongoDocs } from '../mockDeadlineMongoDocs';
+import { mockDeadlineMongoPersistence } from '../mockDeadlineMongoPersistence';
 
 export const createMockDeadlineRepository = (): DeadlineRepository => {
   return {
-    create: jest.fn().mockResolvedValue(mockDeadlineMongoDocs[0]!),
-    findById: jest.fn().mockResolvedValue(mockDeadlineMongoDocs[0]!),
-    findByCaseId: jest.fn().mockResolvedValue(mockDeadlineMongoDocs),
-    findAll: jest.fn().mockResolvedValue(mockDeadlineMongoDocs),
-    deleteById: jest.fn().mockResolvedValue(mockDeadlineMongoDocs[0]!),
-    updateById: jest.fn().mockResolvedValue(mockDeadlineMongoDocs[0]!),
+    create: jest.fn().mockResolvedValue(mockDeadlineMongoPersistence()),
+    findById: jest.fn().mockResolvedValue(mockDeadlineMongoPersistence()),
+    findByCaseId: jest
+      .fn()
+      .mockResolvedValue([mockDeadlineMongoPersistence(), mockDeadlineMongoPersistence()]),
+    findAll: jest
+      .fn()
+      .mockResolvedValue([mockDeadlineMongoPersistence(), mockDeadlineMongoPersistence()]),
+    deleteById: jest.fn().mockResolvedValue(mockDeadlineMongoPersistence()),
+    updateById: jest.fn().mockResolvedValue(mockDeadlineMongoPersistence()),
     existsById: jest.fn().mockResolvedValue(true),
   };
 };
