d2a5627658f9511f7d0d243071814b4c9b577252
DavidSheltonSF <david.faria012@gmail.com>
2026-04-28 21:29:11 -0300

    chore(backend/mocks): refactor mockCreateDeadlineDTO to use Moker

--- backend/src/tests/mocks/deadline/mockCreateDeadlineDTO.ts
+++ backend/src/tests/mocks/deadline/mockCreateDeadlineDTO.ts
@@ -1,15 +1,15 @@
-import { Types } from 'mongoose';
 import { CreateDeadlineDTO } from '../../../dtos/deadLine/CreateDeadlineDTO';
 import { DeadlinePriority } from '../../../types/DeadLinePriority';
 import { DeadlineType } from '../../../types/DeadLineType';
+import { Mocker } from '../../helpers/Mocker';
 
 export function mockCreateDeadlineDTO(): CreateDeadlineDTO {
   return {
-    caseId: Types.ObjectId.createFromTime(48585555).toString(),
-    lawyerId: Types.ObjectId.createFromTime(4822525).toString(),
-    type: DeadlineType.CONTESTACAO,
+    caseId: Mocker.mockMongoId().toString(),
+    lawyerId: Mocker.mockMongoId().toString(),
+    type: Mocker.mockEnum(DeadlineType),
     intimationDate: new Date().toISOString(),
-    days: 5,
-    priority: DeadlinePriority.ALTA,
+    days: Mocker.mockInteger(1, 30),
+    priority: Mocker.mockEnum(DeadlinePriority),
   };
 }
