2b69d52efdb8f46159ff034dbe74d1be796e7e8b
DavidSheltonSF <david.faria012@gmail.com>
2026-04-29 17:23:04 -0300

    feat(backend/tests/helpers/Mocker): mockEmail, mockCpf, mockPassword

--- backend/src/tests/helpers/Mocker.ts
+++ backend/src/tests/helpers/Mocker.ts
@@ -6,6 +6,18 @@ export class Mocker {
     return casual.name;
   }
 
+  static mockEmail(): string {
+    return casual.email;
+  }
+
+  static mockCpf(): string {
+    return casual.random_element(['15855855577', '225.558.115-55']);
+  }
+
+  static mockPassword(): string {
+    return casual.random_element(['Ja#32588', 'vRUIA@24949446S4466SSSFAGA']);
+  }
+
   static mockInteger(from?: number, to?: number): number {
     return casual.integer(from, to);
   }
