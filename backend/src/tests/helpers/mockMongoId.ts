fdf184545c029b805e0d67abf3f01e4a4bb0a0e0
DavidSheltonSF <david.faria012@gmail.com>
2026-04-28 21:11:35 -0300

    chore(backend/mocks): make mockMongoId use casual instead of faker

--- backend/src/tests/helpers/mockMongoId.ts
+++ backend/src/tests/helpers/mockMongoId.ts
@@ -1,6 +1,5 @@
-import { simpleFaker } from '@faker-js/faker';
+import casual from 'casual';
 import { Types } from 'mongoose';
-
 export function mockMongoId(): Types.ObjectId {
-  return Types.ObjectId.createFromTime(simpleFaker.number.int());
+  return Types.ObjectId.createFromTime(casual.integer(1, 30));
 }
