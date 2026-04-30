113893ac5652f1236138e34c0bcf8a4a1374ba79
DavidSheltonSF <david.faria012@gmail.com>
2026-04-30 18:45:56 -0300

    fix(validateProcessNumber): update regex to allow optional delimiters

--- backend/src/services/validators/cases/validateProcessNumber.ts
+++ backend/src/services/validators/cases/validateProcessNumber.ts
@@ -2,7 +2,7 @@ import { InvalidProcessNumberError } from '../../../errors/domain/InvalidProcess
 
 export function validateProcessNumber(processNumber: string) {
   //NNNNNNN-DD.AAAA.J.TR.OOOO
-  const regex = /^\d{7}-\d{2}\.\d{4}\.\d{1}\.\d{2}\.\d{4}$/;
+  const regex = /^\d{7}-?\d{2}\.?\d{4}\.?\d{1}\.?\d{2}\.?\d{4}$/;
   if (!regex.test(processNumber)) {
     throw new InvalidProcessNumberError(processNumber);
   }
