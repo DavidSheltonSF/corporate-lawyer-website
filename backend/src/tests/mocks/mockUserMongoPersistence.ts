f5fbc839068a5bd2f28f33d1c6c4456d7f76d282
DavidSheltonSF <david.faria012@gmail.com>
2026-04-30 18:25:58 -0300

    refactor(mockUserMongoPersistence): replace Mocker methods with UserFieldsMocker for consistency

--- backend/src/tests/mocks/mockUserMongoPersistence.ts
+++ backend/src/tests/mocks/mockUserMongoPersistence.ts
@@ -2,15 +2,16 @@ import { WithMongoId } from '../../database/mongoDB/types/WithMongoId';
 import { Mocker } from '../helpers/Mocker';
 import { IUserModel } from '../../models/UserModel';
 import { UserRole } from '../../types/UserRole';
+import { UserFieldsMocker } from '../helpers/UserFieldsMocker';
 
 export function mockUserMongoPersistence(): WithMongoId<IUserModel> {
   return {
     _id: Mocker.mockMongoId(),
-    firstName: Mocker.mockName(),
-    lastName: Mocker.mockName(),
-    email: Mocker.mockEmail(),
-    cpf: Mocker.mockCpf(),
-    password: Mocker.mockPassword(),
+    firstName: UserFieldsMocker.mockName(),
+    lastName: UserFieldsMocker.mockName(),
+    email: UserFieldsMocker.mockEmail(),
+    cpf: UserFieldsMocker.mockCpf(),
+    password: UserFieldsMocker.mockPassword(),
     role: Mocker.mockEnum(UserRole),
     createdAt: new Date(),
     updatedAt: new Date(),
