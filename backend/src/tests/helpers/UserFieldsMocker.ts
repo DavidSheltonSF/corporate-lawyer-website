d1d13bb69ab07180e0d52f8efff614b75e86ba93
DavidSheltonSF <david.faria012@gmail.com>
2026-04-30 18:22:08 -0300

    feat(backend/tests/helpers): add UserFieldsMocker

---
+++ backend/src/tests/helpers/UserFieldsMocker.ts
@@ -0,0 +1,19 @@
+import casual from 'casual';
+
+export class UserFieldsMocker {
+  static mockName(): string {
+    return casual.first_name;
+  }
+
+  static mockEmail(): string {
+    return casual.email.toLowerCase();
+  }
+
+  static mockCpf(): string {
+    return casual.random_element(['15855855577', '225.558.115-55']);
+  }
+
+  static mockPassword(): string {
+    return casual.random_element(['Ja#32588', 'vRUIA@24949446S4466SSSFAGA']);
+  }
+}
