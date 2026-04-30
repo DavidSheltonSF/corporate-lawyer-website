157fbf27da0146267215dea41ee0e34b1d7bbd8e
DavidSheltonSF <david.faria012@gmail.com>
2026-04-30 18:25:22 -0300

    refactor(mocks): remove unused mock methods from Mocker class

--- backend/src/tests/helpers/Mocker.ts
+++ backend/src/tests/helpers/Mocker.ts
@@ -2,22 +2,6 @@ import casual from 'casual';
 import { Types } from 'mongoose';
 
 export class Mocker {
-  static mockName(): string {
-    return casual.first_name;
-  }
-
-  static mockEmail(): string {
-    return casual.email.toLowerCase();
-  }
-
-  static mockCpf(): string {
-    return casual.random_element(['15855855577', '225.558.115-55']);
-  }
-
-  static mockPassword(): string {
-    return casual.random_element(['Ja#32588', 'vRUIA@24949446S4466SSSFAGA']);
-  }
-
   static mockInteger(from?: number, to?: number): number {
     return casual.integer(from, to);
   }
