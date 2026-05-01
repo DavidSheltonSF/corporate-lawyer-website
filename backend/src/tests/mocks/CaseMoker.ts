733262553283c16973685f840e98c9954be0ada7
DavidSheltonSF <david.faria012@gmail.com>
2026-05-01 14:29:06 -0300

    feat(backend/CaseMocker): add mockCaseDTOWithId

--- backend/src/tests/mocks/CaseMoker.ts
+++ backend/src/tests/mocks/CaseMoker.ts
@@ -1,9 +1,9 @@
-
 import { Mocker } from '../helpers/Mocker';
 import { CaseFieldsMocker } from '../helpers/CaseFieldsMocker';
 import { CasesStatus } from '../../types/CasesStatus';
 import { CreateCaseDTO } from '../../dtos/case/CreateCaseDTO';
 import { CaseDTO } from '../../dtos/case/CaseDTO';
+import { WithId } from '../../types/WithId';
 
 export class CaseMocker {
   static mockCreateCaseDTO(): CreateCaseDTO {
@@ -22,6 +22,23 @@ export class CaseMocker {
 
   static mockCaseDTO(): CaseDTO {
     return {
+      client: Mocker.mockMongoId().toString(),
+      lawyers: [Mocker.mockMongoId().toString()],
+      processNumber: CaseFieldsMocker.mockProcessNumber(),
+      title: CaseFieldsMocker.mockCaseTitle(),
+      description: CaseFieldsMocker.mockDescription(),
+      court: CaseFieldsMocker.mockCaseTitle(),
+      courtDivision: CaseFieldsMocker.mockCaseTitle(),
+      files: [],
+      hearings: [],
+      status: Mocker.mockEnum(CasesStatus),
+      location: CaseFieldsMocker.mockLocation(),
+    };
+  }
+
+  static mockCaseDTOWithId(): WithId<CaseDTO> {
+    return {
+      id: Mocker.mockMongoId().toString(),
       client: Mocker.mockMongoId().toString(),
       lawyers: [Mocker.mockMongoId().toString()],
       processNumber: CaseFieldsMocker.mockProcessNumber(),
