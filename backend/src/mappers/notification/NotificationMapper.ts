40367e9fcd8bf1837113746418c36d7e38cd71e8
DavidSheltonSF <david.faria012@gmail.com>
2026-05-02 19:04:35 -0300

    feat(backend/mappers): add NotificationMapper

---
+++ backend/src/mappers/notification/NotificationMapper.ts
@@ -0,0 +1,20 @@
+import { NotificationDTO } from '../../dtos/notification/NotificationDTO';
+import { WithId } from '../../types/WithId';
+
+export class NotificationeMapper {
+  static persistenceToPresentation(notification: any): WithId<NotificationDTO> {
+    const { _id, userId, title, message, type, channel, isRead, createdAt, readAt } = notification;
+
+    return {
+      id: _id.toString(),
+      userId: userId.toString(),
+      title,
+      message,
+      type,
+      channel,
+      isRead,
+      createdAt: createdAt.toISOString(),
+      readAt: readAt.toISOString(),
+    };
+  }
+}
