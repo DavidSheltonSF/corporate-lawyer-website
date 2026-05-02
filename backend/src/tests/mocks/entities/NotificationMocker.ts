fa862c6c4db4bd8f818c84b91ff5bfcc1344ba86
DavidSheltonSF <david.faria012@gmail.com>
2026-05-02 18:40:16 -0300

    feat(backnd/mocks): add NotificationMoker

---
+++ backend/src/tests/mocks/entities/NotificationMocker.ts
@@ -0,0 +1,41 @@
+import { CreateNotificationDTO } from '../../../dtos/notification/CreateNotificationDTO';
+import { NotificationDTO } from '../../../dtos/notification/NotificationDTO';
+import { NotificationChannel } from '../../../types/NotificationChannel';
+import { NotificationType } from '../../../types/NotificationType';
+import { WithId } from '../../../types/WithId';
+import { GenericMocker } from '../fields/GenericMocker';
+import { NotificationFieldsMocker } from '../fields/NotificationFieldsMoker';
+
+export class NotificationMoker {
+  static mockCreateNotificationDTO(): CreateNotificationDTO {
+    return {
+      userId: GenericMocker.mockMongoId().toString(),
+      type: GenericMocker.mockEnum(NotificationType),
+      channel: [
+        GenericMocker.mockEnum(NotificationChannel),
+        GenericMocker.mockEnum(NotificationChannel),
+      ],
+      title: NotificationFieldsMocker.mockTitle(),
+      message: NotificationFieldsMocker.mockMessage(),
+      isRead: GenericMocker.mockBoolean(),
+      readAt: new Date().toISOString(),
+    };
+  }
+
+  static mockNotificationDTOWithId(): WithId<NotificationDTO> {
+    return {
+      id: GenericMocker.mockMongoId().toString(),
+      userId: GenericMocker.mockMongoId().toString(),
+      type: GenericMocker.mockEnum(NotificationType),
+      channel: [
+        GenericMocker.mockEnum(NotificationChannel),
+        GenericMocker.mockEnum(NotificationChannel),
+      ],
+      title: NotificationFieldsMocker.mockTitle(),
+      message: NotificationFieldsMocker.mockMessage(),
+      isRead: GenericMocker.mockBoolean(),
+      createdAt: new Date().toISOString(),
+      readAt: new Date().toISOString(),
+    };
+  }
+}
