1c1c031f945828a750e7ccf745ed0a3dd3cf6a97
DavidSheltonSF <david.faria012@gmail.com>
2026-05-03 15:54:53 -0300

    feat(backend/events): add CaseCreateHandler

---
+++ backend/src/events/case/CaseCreateHandler.ts
@@ -0,0 +1,41 @@
+import { INotificationService } from '../../services/notification/INotificationService';
+import { NotificationChannel } from '../../types/NotificationChannel';
+import { NotificationType } from '../../types/NotificationType';
+import { EventBus } from '../EventBust';
+import { CASE_CREATED, CaseCreatedEvent } from './CaseEvents';
+
+export class CaseCreateHandler {
+  constructor(private readonly notificationService: INotificationService) {}
+
+  register(eventBus: EventBus) {
+    eventBus.on<CaseCreatedEvent>(CASE_CREATED, async (event) => {
+      const { clientId, lawyerId, caseId, caseTitle } = event;
+
+      const promises = [
+        this.notificationService.create({
+          userId: clientId,
+          channels: [NotificationChannel.IN_APP],
+          type: NotificationType.CASE_CREATED,
+          title: 'Novo processo cadastrado',
+          message: `Processo "${caseTitle}" cadastrado com sucesso`,
+          metadata: {
+            caseId,
+          },
+        }),
+
+        this.notificationService.create({
+          userId: lawyerId,
+          channels: [NotificationChannel.IN_APP],
+          type: NotificationType.CASE_CREATED,
+          title: 'Novo processo cadastrado',
+          message: `Processo "${caseTitle}" cadastrado com sucesso`,
+          metadata: {
+            caseId,
+          },
+        }),
+      ];
+
+      await Promise.allSettled(promises);
+    });
+  }
+}
