e1de27096400c3a2c5345f0d0b0e61239661fa9f
DavidSheltonSF <david.faria012@gmail.com>
2026-05-04 12:18:52 -0300

    feat(frontend/lib): add BrazilStateLabel and CityLabel

---
+++ frontend/lib/BrazilStateLabel.ts
@@ -0,0 +1,5 @@
+import { BrazilState } from '@/types/BrazilState';
+
+export const BrazilStateLabel: Record<BrazilState, string> = {
+  [BrazilState.RIO_DE_JANEIRO]: 'Rio de Janeiro',
+};
---
+++ frontend/lib/CityLabel.ts
@@ -0,0 +1,7 @@
+import { City } from "@/types/City";
+
+export const CityLabel: Record<City, string> = {
+  [City.RIO_DE_JANEIRO]: 'Rio de Janeiro',
+  [City.BELFORD_ROXO]: 'Belford Roxo',
+  [City.DUQUE_DE_CAXIAS]: 'Duque de Caxias',
+};
