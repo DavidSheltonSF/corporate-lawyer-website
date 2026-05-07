d188499ad14a52924e4265a09f3bb4d7f1c3e284
DavidSheltonSF <david.faria012@gmail.com>
2026-05-07 17:45:35 -0300

    feat(backend/controllers): add MarkAsReadResponse type for notification responses

--- backend/src/controllers/notification/responses.ts
+++ backend/src/controllers/notification/responses.ts
@@ -4,3 +4,5 @@ import { HttpResponse } from '../types/HttpResponse';
 
 export type FindByIdResponse = HttpResponse<WithId<NotificationDTO> | null>;
 export type FindMyResponse = HttpResponse<WithId<NotificationDTO>[] | null>;
+export type MarkAsReadResponse = HttpResponse<WithId<NotificationDTO>[] | null>;
+
