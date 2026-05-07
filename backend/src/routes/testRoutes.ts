e3d9e599935047dbc4e0b1c466f0174e976891db
DavidSheltonSF <david.faria012@gmail.com>
2026-05-07 17:58:39 -0300

    feat(backend/routes/testRoutes): add  to create fake notifications

--- backend/src/routes/testRoutes.ts
+++ backend/src/routes/testRoutes.ts
@@ -1,6 +1,7 @@
 import { Request, Response, Router } from 'express';
 import { UserModel } from '../models/UserModel';
 import { NotificationModel } from '../models/NotificationModel';
+import { Types } from 'mongoose';
 
 export function testRoutes(router: Router) {
   router.get('/api/users/', async (req: Request, res: Response) => {
@@ -9,6 +10,48 @@ export function testRoutes(router: Router) {
   });
   router.get('/api/notifications/', async (req: Request, res: Response) => {
     const response = await NotificationModel.find();
+    res.status(200).json({ data: response });
+  });
+
+  router.post('/api/notifications/', async (req: Request, res: Response) => {
+    const response = await NotificationModel.create([
+      {
+        userId: 'a3f9c1e70d4b8a2fff12ac90',
+        type: 'CASE_CREATED',
+        channel: [],
+        title: 'Novo processo cadastrado',
+        message: 'Processo "Pedido de Abescorpus" cadastrado com sucesso',
+        isRead: false,
+        metadata: {
+          caseId: '69fcce6790233e78aff43b71',
+        },
+        createdAt: '2026-05-07T17:39:51.393Z',
+      },
+      {
+        userId: 'a3f9c1e70d4b8a2fff12ac90',
+        type: 'CASE_CREATED',
+        channel: [],
+        title: 'Processo atualizado',
+        message: 'Processo "Pedido de Abescorpus" foi atualizado',
+        isRead: true,
+        metadata: {
+          caseId: '69fcce6790233e78aff43b71',
+        },
+        createdAt: '2026-05-08T10:20:30.123Z',
+      },
+      {
+        userId: 'a3f9c1e70d4b8a2fff12ac90',
+        type: 'CASE_CREATED',
+        channel: [],
+        title: 'Processo encerrado',
+        message: 'Processo "Pedido de Abescorpus" foi encerrado com sucesso',
+        isRead: false,
+        metadata: {
+          caseId: '69fcce6790233e78aff43b71',
+        },
+        createdAt: '2026-05-09T14:15:45.789Z',
+      },
+    ]);
     res.status(200).json({ data: response });
   });
   router.delete('/api/notifications/clean', async (req: Request, res: Response) => {
