6c4741797b5e5fa5f956b680cb77737a5e49bd74
DavidSheltonSF <david.faria012@gmail.com>
2026-05-07 16:40:30 -0300

    feat(frontend/components): refactor NotificationsModal to use NotificationCard for rendering notifications

--- frontend/components/notifications/NotificationsModal.tsx
+++ frontend/components/notifications/NotificationsModal.tsx
@@ -9,6 +9,7 @@ import { WithId } from '@/types/WithId';
 import { NotificationIcon } from '../icons/NotificationIcon';
 import { Notification } from '@/types/Notification';
 import { formatRelativeTime } from '@/lib/formatRelativeTime';
+import { NotificationCard } from './NotificationCard';
 
 interface Props {
   isOpen: boolean;
@@ -39,22 +40,7 @@ export function NotificationsModal({ isOpen, setIsOpen }: Props) {
   }, []);
 
   const renderNotifications = notifications.map((notification, index) => {
-    return (
-      <article className="flex border w-[90%] min-h-fit rounded-md p-[16px]" key={index}>
-        <div className="flex size-full items-center gap-[16px]">
-          <div className="flex justify-center items-center size-[56px] border rounded-md">
-            <NotificationIcon width="60%" height="60%" color="var(--primary-color-light)" />
-          </div>
-          <div className="flex flex-col h-full w-full">
-            <h3 className="font-bold">{notification.title}</h3>
-            <p>{notification.message}</p>
-            <p className="text-gray-500 small-text">
-              {formatRelativeTime(new Date(notification.createdAt))}
-            </p>
-          </div>
-        </div>
-      </article>
-    );
+    return <NotificationCard key={index} notification={notification} />;
   });
 
   return (
