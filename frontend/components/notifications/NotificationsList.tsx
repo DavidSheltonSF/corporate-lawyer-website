6debe5e7260cfdc257d34992989c55ab07dcc7cf
DavidSheltonSF <david.faria012@gmail.com>
2026-05-07 19:48:46 -0300

    feat(frontend/components): add NotificationsList

---
+++ frontend/components/notifications/NotificationsList.tsx
@@ -0,0 +1,23 @@
+import { Notification } from '@/types/Notification';
+import { WithId } from '@/types/WithId';
+import { NotificationCard } from './NotificationCard';
+import { Dispatch, SetStateAction } from 'react';
+
+interface Props {
+  notifications: WithId<Notification>[];
+  unreadCount: number;
+  setUnreadCount: Dispatch<SetStateAction<number>>;
+}
+export function NotificationsList({ notifications, unreadCount, setUnreadCount }: Props) {
+  const renderNotifications = notifications.map((notification, index) => {
+    return (
+      <NotificationCard
+        key={index}
+        notificationData={notification}
+        decreaceUnreadCount={() => setUnreadCount(unreadCount - 1)}
+      />
+    );
+  });
+
+  return <div className="flex flex-col gap-[24px]">{renderNotifications}</div>;
+}
