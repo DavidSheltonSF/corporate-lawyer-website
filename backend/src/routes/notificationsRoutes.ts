59ceed861fb69c72a28c672025d5b0252f29da97
DavidSheltonSF <david.faria012@gmail.com>
2026-05-07 17:45:49 -0300

    feat(backend/routes): add route to mark notifications as read

--- backend/src/routes/notificationsRoutes.ts
+++ backend/src/routes/notificationsRoutes.ts
@@ -3,11 +3,19 @@ import { expressHttpAdapter } from './adapters/expressHttpAdapter';
 import { requireAuth } from '../middlewares/requireAuth';
 import { INotificationsController } from '../controllers/notification/INotificationController';
 
-export function notificationsRoutes(router: Router, notificationController: INotificationsController) {
-  router.get('/api/notifications/:id', requireAuth, expressHttpAdapter(notificationController.findById));
+export function notificationsRoutes(
+  router: Router,
+  notificationController: INotificationsController
+) {
+  router.get(
+    '/api/notifications/:id',
+    requireAuth,
+    expressHttpAdapter(notificationController.findById)
+  );
   router.get(
     '/api/my/notifications/',
     requireAuth,
     expressHttpAdapter(notificationController.findMy)
   );
+  router.put('/api/notifications/:id/read', notificationController.markAsRead);
 }
