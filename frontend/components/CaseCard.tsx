9dfa45fdce53e21510a7118b947b8dea30428a00
DavidSheltonSF <david.faria012@gmail.com>
2026-05-08 19:44:23 -0300

    chore(frontend/components):  update to improve code structure
    - Use Card component;
    - Use css variables as possible values for statusColor instead of Tailwind
    - Pass statusColor by props to the FieldValue rendering the status field

--- frontend/components/CaseCard.tsx
+++ frontend/components/CaseCard.tsx
@@ -1,35 +1,31 @@
 import { formatStringList } from '@/lib/formatStringList';
 import { CaseStatusEnum } from '@/types/CaseStatusEnum';
-import { reduceString } from '@/lib/reduceString';
-import { TooltipContainer } from './TooltipContainer';
 import { WithId } from '@/types/WithId';
 import { CaseStatusLabel } from '@/lib/CaseStatusLabel';
 import { CaseWithRelations } from '@/types/CaseWithRelations';
-import { Dispatch, SetStateAction } from 'react';
-import { VerticalMoreIcon } from './icons/VerticalMoreIcon';
+import { FieldValue } from './FieldValue';
+import { Card } from './ui/Card/Card';
 
 interface Props {
-  setSelectedCaseId: Dispatch<SetStateAction<string | null>>;
   openCaseModal: Function;
   openOptionsModal: Function;
   caseData: WithId<CaseWithRelations>;
 }
 
-export function CaseCard({ caseData, setSelectedCaseId, openCaseModal, openOptionsModal }: Props) {
+export function CaseCard({ caseData, openCaseModal, openOptionsModal }: Props) {
   const clientData = caseData.client;
 
-  const { id, title, processNumber, status } = caseData;
+  const { title, processNumber, status } = caseData;
 
   let statusColor = '';
 
   switch (status) {
     case CaseStatusEnum.open:
-      statusColor = 'text-green-400';
+      statusColor = 'var(--color-green)';
       break;
 
-      break;
     case CaseStatusEnum.closed:
-      statusColor = 'text-red-500';
+      statusColor = 'var(--color-red)';
       break;
 
     default:
@@ -41,63 +37,11 @@ export function CaseCard({ caseData, setSelectedCaseId, openCaseModal, openOptio
   });
 
   return (
-    <article className="flex flex-col fade-in-animation  bg-color-primary w-full min-md:w-[80%] min-lg:w-[656px] min-h-[280px] h-max rounded-xl cursor-pointer">
-      <header className="flex items-center justify-center min-md:justify-between w-full p-[16px] min-md:p-[24px]">
-        <TooltipContainer
-          label={title}
-          tooltipLabelProps={{
-            color: '#ffd000ff',
-            backgroundColor: '#000',
-            position: {
-              bottom: '115%',
-              left: '50%',
-              translateX: '-50%',
-            },
-          }}
-        >
-          <h1 className="h-fit font-bold text-center min-md:text-start text-xl min-md:text-3xl">
-            {reduceString(title, 35)}
-          </h1>
-        </TooltipContainer>
-        <button
-          className="cursor-pointer"
-          onClick={() => {
-            openOptionsModal();
-            setSelectedCaseId(id);
-          }}
-        >
-          <VerticalMoreIcon color="var(--white-color)" height="32px" width="32px" />
-        </button>
-      </header>
-      <main
-        onClick={() => {
-          setSelectedCaseId(id);
-          openCaseModal();
-        }}
-        className="flex flex-1 flex-col gap-[16px] px-[24px] py-[16px] bg-color-white text-color-black text-lg"
-        style={{
-          borderRadius: 'inherit',
-        }}
-      >
-        <span className="flex gap-[8px]">
-          <p className="font-bold">nº:</p>
-          <p>{processNumber}</p>
-        </span>
-        <span className="flex gap-[8px]">
-          <p className="font-bold">cliente:</p>
-          <p>
-            {clientData?.firstName} {clientData?.lastName}
-          </p>
-        </span>
-        <span className="flex gap-[8px]">
-          <p className="font-bold">advogados:</p>
-          <p>{formatStringList(lawyersNames)}</p>
-        </span>
-        <span className="flex gap-[8px]">
-          <p className="font-bold">status:</p>
-          <p className={`font-bold ${statusColor}`}>{CaseStatusLabel[status]}</p>
-        </span>
-      </main>
-    </article>
+    <Card title={title} openModal={openCaseModal} openOptionsModal={openOptionsModal}>
+      <FieldValue field="nº" value={processNumber} />
+      <FieldValue field="cliente:" value={`${clientData?.firstName} ${clientData?.lastName}`} />
+      <FieldValue field="advogados:" value={formatStringList(lawyersNames)} />
+      <FieldValue valueTextColor={statusColor} field="status:" value={CaseStatusLabel[status]} />
+    </Card>
   );
 }
