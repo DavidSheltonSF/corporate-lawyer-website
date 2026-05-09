ca0fd75bf8735feca093d7f5631a4eb0b8a90808
DavidSheltonSF <david.faria012@gmail.com>
2026-05-09 15:41:13 -0300

    refactor(frontend/components/ui):  simplify the search logic by removing text state and adding query and action, so the search bar can execute the search (action) by itself

--- frontend/components/ui/Search/SearchBar.tsx
+++ frontend/components/ui/Search/SearchBar.tsx
@@ -11,34 +11,35 @@ import { SearchInput } from './SearchInput';
 import { SearchButton } from './SearchButton';
 
 interface Props {
+  query: string
   setQuery: Dispatch<SetStateAction<string>>;
+  action: () => void
 }
 
 SearchBar.Input = SearchInput;
 SearchBar.Button = SearchButton;
 
-export function SearchBar({ setQuery }: Props) {
-  const [text, setText] = useState('');
+export function SearchBar({ query, setQuery, action }: Props) {
   function handleChange(e: ChangeEvent<HTMLInputElement>) {
     const searchBar = e.target;
-    setText(searchBar.value);
+    setQuery(searchBar.value);
   }
 
   function handleKeyDown(e: KeyboardEvent) {
     if (e.key === 'Enter') {
-      setQuery(text);
+      action()
     }
   }
 
   function handleClick(e: MouseEvent) {
-    setQuery(text);
+    action()
   }
 
   return (
     <div className="flex gap-[16px] bg-color-white w-full min-md:w-[70%] min-lg:w-[520px] h-[48px] rounded-full p-[2px]">
       <div className="flex gap-[16px] flex-1 rounded-full overflow-hidden">
         <SearchBar.Input
-          value={text}
+          value={query}
           placeholder="Pesquisar processo..."
           onChange={handleChange}
           onKeyDown={handleKeyDown}
