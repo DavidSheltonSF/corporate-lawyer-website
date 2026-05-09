5f97d0c795f78eb4dc21c5dd2e1468d505acbc40
DavidSheltonSF <david.faria012@gmail.com>
2026-05-09 11:49:52 -0300

    chore(frontend/components): update ClientModal to use ClientModalHeader, ClientModaInfo and ClientModalCases for applying a Composed Component pattern

--- frontend/components/modals/ClientModal.tsx
+++ frontend/components/modals/ClientModal.tsx
@@ -4,19 +4,24 @@ import { PrimaryModal } from '../ui/Modal/PrimaryModal';
 import { FieldValue } from '../FieldValue';
 import { CaseModalSkeleton } from './CaseModalSkeleton';
 import { SafeUser } from '@/types/SafeUser';
-import { Button } from '../ui/Button/Button';
 import { getClientWithCases } from '@/services/users/getClientWithCases';
 import { Case } from '@/types/Case';
-import { reduceString } from '@/lib/reduceString';
 import { handleLogout } from '@/lib/handleLogout';
 import { UnauthorizedError } from '@/errors/UnauthorizedError';
+import { ClientModalHeader } from './ClientModalHeader';
+import { ClientModalInfo } from './ClientModalInfo';
+import { ClientModalCases } from './ClientModalCases';
 
 interface Props {
   clientId: string | null;
   isOpen: boolean;
   setIsOpen: Dispatch<SetStateAction<boolean>>;
-  openRegisterCaseModal: Function;
+  openRegisterCaseModal: () => void;
 }
+
+ClientModal.Header = ClientModalHeader;
+ClientModal.Info = ClientModalInfo;
+ClientModal.Cases = ClientModalCases;
 
 export function ClientModal({ isOpen, setIsOpen, clientId, openRegisterCaseModal }: Props) {
   const [clientData, setClientData] = useState<(SafeUser & { cases: Case[] }) | null>(null);
@@ -56,18 +61,6 @@ export function ClientModal({ isOpen, setIsOpen, clientId, openRegisterCaseModal
     };
   }, [isOpen]);
 
-  const renderClientCases = clientData?.cases.map((cas, index) => {
-    return (
-      <div
-        key={index}
-        className="flex flex-col gap-[8px] w-full border-[1px] p-[8px] rounded-[8px]"
-      >
-        <h2 className="font-bold ">{reduceString(cas.title, 24)}</h2>
-        <div className="flex flex-col">{reduceString(cas.description || '', 70)}</div>
-      </div>
-    );
-  });
-
   return (
     isOpen && (
       <PrimaryModal
@@ -82,30 +75,12 @@ export function ClientModal({ isOpen, setIsOpen, clientId, openRegisterCaseModal
           <CaseModalSkeleton />
         ) : (
           <div className="flex flex-col size-full bg-color-white">
-            <header className="w-full bg-color-primary p-[16px] border-t border-white/50">
-              <h1 className="text-3xl text-color-white font-bold ">
-                {clientData.firstName} {clientData.lastName}
-              </h1>
-            </header>
-            <main className="flex flex-col w-full text-lg min-lg:text-xl">
-              <div className="flex flex-col gap-[8px] border-b border-black/50 p-[16px]">
-                <FieldValue field="Nomeº:" value={clientData.firstName} />
-                <FieldValue field="Sobrenome:" value={clientData.lastName} />
-                <FieldValue field="Email:" value={clientData.email} />
-                <FieldValue field="CPF:" value={clientData.cpf} />
-              </div>
-              <div className="flex flex-col gap-[8px] border-b border-black/50">
-                <div className="relative w-full bg-color-primary p-[16px]">
-                  <h1 className="text-2xl font-bold text-color-white">Processos</h1>
-                  <div className="absolute right-[16px] top-[50%] translate-y-[-50%]">
-                    <Button onclick={() => openRegisterCaseModal()}>Adicionar Processo</Button>
-                  </div>
-                </div>
-                <div className="flex flex-col gap-[24px] p-[24px] h-[224px] min-lg:h-[316px] overflow-auto">
-                  {renderClientCases}
-                </div>
-              </div>
-            </main>
+            <ClientModal.Header firstName={clientData.firstName} lastName={clientData.lastName} />
+            <ClientModal.Info clientData={clientData} />
+            <ClientModal.Cases
+              cases={clientData.cases}
+              openRegisterCaseModal={openRegisterCaseModal}
+            />
           </div>
         )}
       </PrimaryModal>
