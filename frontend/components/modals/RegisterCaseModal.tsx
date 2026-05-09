e8e75a1a0ffb8ce3013a6690dc5c85d420006b6e
DavidSheltonSF <david.faria012@gmail.com>
2026-05-09 13:49:08 -0300

    refactor(frontend/RegisterCaseModal): replace setIsOpen with close for simplicity

--- frontend/components/modals/RegisterCaseModal.tsx
+++ frontend/components/modals/RegisterCaseModal.tsx
@@ -17,12 +17,13 @@ import { BrazilState } from '@/types/BrazilState';
 import { BrazilStateLabel } from '@/lib/BrazilStateLabel';
 import { City } from '@/types/City';
 import { CityLabel } from '@/lib/CityLabel';
+
 interface Props {
   selectedClientId: string | null;
   isOpen: boolean;
-  setIsOpen: Dispatch<SetStateAction<boolean>>;
+  close: () => void;
 }
-export function RegisterCaseModal({ isOpen, setIsOpen, selectedClientId }: Props) {
+export function RegisterCaseModal({ isOpen, close, selectedClientId }: Props) {
   const [requestState, setRequestState] = useState<RequestState | null>(null);
 
   const context = useAuthenticatedUserContext();
@@ -58,7 +59,7 @@ export function RegisterCaseModal({ isOpen, setIsOpen, selectedClientId }: Props
           'fixed z-99999999999 top-[2%] min-lg:top-[10%] left-1/2 translate-x-[-50%] w-[90%] min-lg:w-[678px] h-[90%] min-lg:h-fit rounded-lg overflow-hidden shadow-[0px_0px__3px_black] text-color-black'
         }
         closeModal={() => {
-          setIsOpen(false);
+          close();
           setRequestState(null);
         }}
       >
