24f9cc399dd65d38c76372df93446e8e30cd4565
DavidSheltonSF <david.faria012@gmail.com>
2026-05-10 17:56:24 -0300

    feat(frontend/app/clientPage): add CaseModal and CaseFilesUploadModal to ClientPage

--- frontend/app/clientPage/page.tsx
+++ frontend/app/clientPage/page.tsx
@@ -1,3 +1,4 @@
+'use';
 import { HeroSection } from '@/components/HeroSection';
 import { ClientView } from '@/components/views/ClientView';
 import { LawyerView } from '@/components/views/LawyerView';
@@ -7,6 +8,8 @@ import { User } from '@/types/User';
 import { WithId } from '@/types/WithId';
 import { redirect } from 'next/navigation';
 import { Notifications } from '@/components/features/notifications/Notifications';
+import { CaseModal } from '@/components/modals/CaseModal';
+import { CaseFilesUploadModal } from '@/components/modals/CaseFilesUploadModal';
 
 export default async function ClientPage() {
   let user: WithId<User> | null = null;
@@ -19,13 +22,15 @@ export default async function ClientPage() {
   }
 
   return (
-    <div className="bg-color-black min-h-[100vh]">
+    <div className="relative bg-color-black min-h-[100vh]">
       <HeroSection
         background="var(--blue-gradient)"
         title={`Bem vindo(a) ${user?.role === 'lawyer' ? 'Dra' : ''} ${user?.firstName}`}
         additionalStyles="h-[280px]"
       />
       <AuthenticatedUserProvider userData={user}>
+        <CaseModal />
+        <CaseFilesUploadModal/>
         {user.role === 'lawyer' ? <LawyerView /> : <ClientView />}
       </AuthenticatedUserProvider>
       <Notifications />
