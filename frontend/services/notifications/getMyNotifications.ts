14ad316ac0d3de2409e58a93849334f3bbe3789b
DavidSheltonSF <david.faria012@gmail.com>
2026-05-10 00:01:35 -0300

    refactor(frontend/services): update getMyNotifications to handle and return pagination

--- frontend/services/notifications/getMyNotifications.ts
+++ frontend/services/notifications/getMyNotifications.ts
@@ -2,10 +2,11 @@ import { API_URL } from '@/config/api';
 import { WithId } from '@/types/WithId';
 import { apiFetch } from '../apiFetch';
 import { Notification } from '@/types/Notification';
+import { Page } from '@/types/Page';
 
-export async function getMyNotifications(): Promise<WithId<Notification>[]> {
+export async function getMyNotifications(page: number, limit: number): Promise<Page<WithId<Notification>>> {
   const baseRoute = `${API_URL}/my/notifications`;
-  const response = await apiFetch(`${baseRoute}`);
+  const response = await apiFetch(`${baseRoute}?page=${page}&limit=${limit}`);
   const responseJson = await response.json();
   return responseJson.data;
 }
