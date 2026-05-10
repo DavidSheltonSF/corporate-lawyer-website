11700c7a29ea681320806fade1b6504450c70593
DavidSheltonSF <david.faria012@gmail.com>
2026-05-10 18:22:38 -0300

    refactor(frontend/components): simplify Notifications component by using NotificationsModalProvider

--- frontend/components/features/notifications/Notifications.tsx
+++ frontend/components/features/notifications/Notifications.tsx
@@ -1,21 +1,15 @@
 'use client';
-
-import { useState } from 'react';
 import { NotificationButton } from './NotificationButton';
 import { NotificationsModal } from './NotificationsModal';
+import { NotificationsModalProvider } from '@/contexts/modals/NotificationsModalProvider';
 
 export function Notifications() {
-  const [isOpen, setIsOpen] = useState(false);
-  const [unreadCount, setUnreadCount] = useState(0);
-
-  function openModal() {
-    setIsOpen(true);
-  }
-
   return (
     <>
-      <NotificationsModal isOpen={isOpen} setIsOpen={setIsOpen} setUnreadCount={setUnreadCount} unreadCount={unreadCount}  />
-      <NotificationButton openModal={openModal} unreadCount={unreadCount} />
+      <NotificationsModalProvider>
+        <NotificationsModal/>
+        <NotificationButton />
+      </NotificationsModalProvider>
     </>
   );
 }
