3239e90520fd114b3e2977ee30e68f63307a2f18
DavidSheltonSF <david.faria012@gmail.com>
2026-05-10 15:14:29 -0300

    chore(frontend/NotificationCard): add fade-in-animation class

--- frontend/components/features/notifications/NotificationCard.tsx
+++ frontend/components/features/notifications/NotificationCard.tsx
@@ -31,7 +31,7 @@ export function NotificationCard({ notificationData, decreaceUnreadCount }: Prop
   return (
     <article
       onClick={() => handleNotificationClick(notification.id)}
-      className={`flex border w-full min-h-fit rounded-md p-[16px] shadow-lg ${
+      className={`flex border w-full min-h-fit rounded-md p-[16px] shadow-lg fade-in-animation ${
         !isRead
           ? 'cursor-pointer border-color-primary-light inner-shadow-soft-primary hover:-translate-y-0.5 transition-all duration-300'
           : ''
