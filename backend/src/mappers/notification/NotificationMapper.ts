046702f8ff7185090b97136a73f6106859114b7e
DavidSheltonSF <david.faria012@gmail.com>
2026-05-10 17:52:29 -0300

    fix(backend/NotificationMapper): add metadata field  to mapping in persistenceToPresentation

--- backend/src/mappers/notification/NotificationMapper.ts
+++ backend/src/mappers/notification/NotificationMapper.ts
@@ -3,7 +3,8 @@ import { WithId } from '../../types/WithId';
 
 export class NotificationeMapper {
   static persistenceToPresentation(notification: any): WithId<NotificationDTO> {
-    const { _id, userId, title, message, type, channels, isRead, createdAt, readAt } = notification;
+    const { _id, userId, title, message, type, channels, isRead, createdAt, readAt, metadata } =
+      notification;
 
     return {
       id: _id.toString(),
@@ -15,6 +16,7 @@ export class NotificationeMapper {
       isRead,
       createdAt: createdAt.toISOString(),
       readAt: readAt?.toISOString() || null,
+      metadata,
     };
   }
 }
