5cbb9f516d40e011a02c3651cdda8bfe2aeb2bff
DavidSheltonSF <david.faria012@gmail.com>
2026-05-11 19:55:58 -0300

    chore(frontend/components/features): refine CardCaseCard component design with a more minimalist layout

--- frontend/components/features/cases/CaseCard.tsx
+++ frontend/components/features/cases/CaseCard.tsx
@@ -3,9 +3,10 @@ import { CaseStatusEnum } from '@/types/CaseStatusEnum';
 import { WithId } from '@/types/WithId';
 import { CaseStatusLabel } from '@/lib/CaseStatusLabel';
 import { CaseWithRelations } from '@/types/CaseWithRelations';
-import { FieldValue } from '../../FieldValue';
 import { Card } from '../../ui/Card/Card';
-import { reduceString } from '@/lib/reduceString';
+import { UserIcon } from '@/components/icons/UserIcon';
+import { BalanceIcon } from '@/components/icons/BalanceIcon';
+import { FolderIcon } from '@/components/icons/FolderIcon';
 
 interface Props {
   openCaseModal: Function;
@@ -38,12 +39,35 @@ export function CaseCard({ caseData, openCaseModal, openOptionsModal }: Props) {
   });
 
   return (
-    <Card openModal={openCaseModal} openOptionsModal={openOptionsModal}>
-      <h1>{reduceString(title, 40)}</h1>
-      <FieldValue field="nº" value={processNumber} />
-      <FieldValue field="cliente:" value={`${clientData?.firstName} ${clientData?.lastName}`} />
-      <FieldValue field="advogados:" value={formatStringList(lawyersNames)} />
-      <FieldValue valueTextColor={statusColor} field="status:" value={CaseStatusLabel[status]} />
+    <Card openOptionsModal={() => openOptionsModal()} openModal={() => openCaseModal()}>
+      <div className="flex flex-col text-color-black p-[24px] gap-[32px]">
+        <div className="flex flex-col gap-[8px]">
+          <h3 className="font-bold">{title}</h3>
+          <span className="small-text opacity-70">nº {processNumber}</span>
+        </div>
+        <div className="flex flex-col min-md:flex-row min-md:w-[88%] gap-[80px]">
+          <div className="flex gap-2 flex-1">
+            <UserIcon width="24px" height="24px" />
+            <span>
+              {clientData?.firstName} {clientData?.lastName}
+            </span>
+          </div>
+          <div className="flex gap-2 flex-1">
+            <BalanceIcon width="24px" height="24px" />
+            <span>{formatStringList(lawyersNames)}</span>
+          </div>
+          <div className="flex gap-2 flex-1">
+            <FolderIcon width="24px" height="24px" />
+            <span
+              style={{
+                color: statusColor,
+              }}
+            >
+              {CaseStatusLabel[status]}
+            </span>
+          </div>
+        </div>
+      </div>
     </Card>
   );
 }
