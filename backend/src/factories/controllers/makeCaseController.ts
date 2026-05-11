f725cc81b7198060d12987a1cdc8d5d54cbe1012
DavidSheltonSF <david.faria012@gmail.com>
2026-05-10 21:01:57 -0300

    feat(backend/makeCaseController): integrate CaseUpdateHandler for events

--- backend/src/factories/controllers/makeCaseController.ts
+++ backend/src/factories/controllers/makeCaseController.ts
@@ -4,6 +4,7 @@ import { MongodbCaseRepository } from '../../database/mongoDB/repositories/Mongo
 import { MongodbNotificationRepository } from '../../database/mongoDB/repositories/MongodbNotificationRepository';
 import { MongodbUserRepository } from '../../database/mongoDB/repositories/MongodbUserRepository';
 import { CaseCreateHandler } from '../../events/case/CaseCreateHandler';
+import { CaseUpdateHandler } from '../../events/case/CaseUpdateHandler';
 import { EventBus } from '../../events/EventBust';
 import { CaseService } from '../../services/case/CaseService';
 import { NotificationService } from '../../services/notification/NotificationService';
@@ -11,10 +12,12 @@ import { UserService } from '../../services/user/UserService';
 
 export function makeCaseController(): ICaseController {
   const eventBus = new EventBus();
-  const notificationRepository = new MongodbNotificationRepository()
-  const notificationService = new NotificationService(notificationRepository)
-  const caseCreatedHandler = new CaseCreateHandler(notificationService)
-  caseCreatedHandler.register(eventBus)
+  const notificationRepository = new MongodbNotificationRepository();
+  const notificationService = new NotificationService(notificationRepository);
+  const caseCreatedHandler = new CaseCreateHandler(notificationService);
+  const caseUpdatedHandler = new CaseUpdateHandler(notificationService);
+  caseCreatedHandler.register(eventBus);
+  caseUpdatedHandler.register(eventBus);
   const caseRepository = new MongodbCaseRepository();
   const caseService = new CaseService(caseRepository, eventBus);
   const userRepository = new MongodbUserRepository();
