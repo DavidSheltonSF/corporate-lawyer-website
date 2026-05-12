d66b6a3d563fa49a378486bc22072cf54c963a0e
DavidSheltonSF <david.faria012@gmail.com>
2026-05-11 23:37:25 -0300

    chore(hooks): update useCaseFilesUploadModalContext to return ModalContextType instead of CaseModalContext

--- frontend/hooks/useCaseFilesUploadModalContext.ts
+++ frontend/hooks/useCaseFilesUploadModalContext.ts
@@ -1,8 +1,9 @@
 import { CaseFilesUploadModalContext } from '@/contexts/modals/CaseFilesUploadModalContext';
+import { ModalContextType } from '@/contexts/modals/ModalContextType';
 import { MissingContextError } from '@/errors/MissingContextError';
 import { useContext } from 'react';
 
-export const useCaseFilesUploadModalContext = (): CaseFilesUploadModalContext => {
+export const useCaseFilesUploadModalContext = (): ModalContextType => {
   const context = useContext(CaseFilesUploadModalContext);
   if (!context) {
     throw new MissingContextError(CaseFilesUploadModalContext.name);
