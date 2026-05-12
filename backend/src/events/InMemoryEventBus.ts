b1b3c66c8fc95f99071bd9bae153772bf3410fa8
DavidSheltonSF <david.faria012@gmail.com>
2026-05-12 20:38:39 -0300

    feat(backend/events): add InMemoryEventBus implementing IEventBus

---
+++ backend/src/events/InMemoryEventBus.ts
@@ -0,0 +1,20 @@
+import { EventListener } from './EventListener';
+import { IEventBus } from './IEventBus';
+
+export class InMemoryEventBus implements IEventBus {
+  private listeners = new Map();
+  subscribe = (eventName: string, listener: EventListener) => {
+    const listeners: EventListener[] = this.listeners.get(eventName) || [];
+
+    listeners.push(listener);
+
+    this.listeners.set(eventName, listeners);
+  };
+
+  publish = (eventName: string, payload: unknown) => {
+    const listeners = this.listeners.get(eventName) as EventListener[];
+    listeners.forEach((listener) => {
+      listener.handle(payload);
+    });
+  };
+}
