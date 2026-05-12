41a65b0fa2664b8a01ebb54f08560666e3547710
DavidSheltonSF <david.faria012@gmail.com>
2026-05-12 12:52:47 -0300

    fix(bakend/factories/makeAuthController): add caseRepository to the userService contrutctor

--- backend/src/factories/controllers/makeAuthController.ts
+++ backend/src/factories/controllers/makeAuthController.ts
@@ -1,12 +1,14 @@
 import { AuthController } from '../../controllers/auth/AuthController';
 import { IAuthController } from '../../controllers/auth/IAuthController';
+import { MongodbCaseRepository } from '../../database/mongoDB/repositories/MongodbCaseRepository';
 import { MongodbUserRepository } from '../../database/mongoDB/repositories/MongodbUserRepository';
 import { AuthService } from '../../services/auth/AuthService';
 import { UserService } from '../../services/user/UserService';
 
 export function makeAuthController(): IAuthController {
   const userRepository = new MongodbUserRepository();
-  const userService = new UserService(userRepository);
+  const caseRepository = new MongodbCaseRepository();
+  const userService = new UserService(userRepository, caseRepository);
   const authService = new AuthService(userRepository);
   return new AuthController(authService, userService);
 }
