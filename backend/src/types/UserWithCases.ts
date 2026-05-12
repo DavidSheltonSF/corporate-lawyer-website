68578c29e70a6b56888579da264d228deb4415e0
DavidSheltonSF <david.faria012@gmail.com>
2026-05-12 14:55:19 -0300

    chore(backend/types): remove UserWithCases

--- backend/src/types/UserWithCases.ts
+++
@@ -1,5 +0,0 @@
-import { WithId } from './WithId';
-import { User } from '../entities/User';
-import { CaseDTO } from '../dtos/case/CaseDTO';
-
-export type UserWithCases = User & { cases: WithId<CaseDTO>[] };
