f38bc582a10dfa6bc1b987fc036aedb5f8056d80
DavidSheltonSF <david.faria012@gmail.com>
2026-05-11 22:38:05 -0300

    chore(frontend/components/features/clients): refactor ClientCard layout and structure for matching recent changes in the Card component

--- frontend/components/features/clients/ClientCard.tsx
+++ frontend/components/features/clients/ClientCard.tsx
@@ -2,6 +2,9 @@ import { WithId } from '@/types/WithId';
 import { FieldValue } from '../../FieldValue';
 import { SafeUser } from '@/types/SafeUser';
 import { Card } from '../../ui/Card/Card';
+import { UserIcon } from '@/components/icons/UserIcon';
+import { EmailIcon } from '@/components/icons/EmailIcon';
+import { PhoneIcon } from '@/components/icons/PhoneIcon';
 
 interface Props {
   clientData: WithId<SafeUser>;
@@ -14,13 +17,31 @@ export function ClientCard({ clientData, openClientModal, openOptionsModal }: Pr
 
   return (
     <Card openModal={openClientModal} openOptionsModal={openOptionsModal}>
-      <h1>
-        {firstName} {lastName}
-      </h1>
-      <FieldValue field="Nome" value={firstName} />
-      <FieldValue field="Sobrenome" value={lastName} />
-      <FieldValue field="Email" value={email} />
-      <FieldValue field="CPF" value={cpf} />
+      <div className="flex flex-col text-color-black p-[24px] gap-[32px]">
+        <header className="flex flex-col  gap-[8px] ">
+          <div className="flex items-center gap-[32px]">
+            <div className="flex justify-center items-center size-[5rem] border rounded-[8px]">
+              <UserIcon width="40px" height="40px" />
+            </div>
+            <h2>
+              {firstName} {lastName}
+            </h2>
+          </div>
+          <div className="small-text opacity-70">
+            <span>CPF: {cpf}</span>
+          </div>
+        </header>
+        <div className="flex flex-col min-md:flex-row min-md:w-[88%] min-md gap-[8px] min-md:gap-[80px]">
+          <div className="flex gap-2 flex-1">
+            <EmailIcon width="24px" height="24px" />
+            <span>{email}</span>
+          </div>
+          <div className="flex gap-2 flex-1">
+            <PhoneIcon width="24px" height="24px" />
+            <span>21969585855 - fake</span>
+          </div>
+        </div>
+      </div>
     </Card>
   );
 }
