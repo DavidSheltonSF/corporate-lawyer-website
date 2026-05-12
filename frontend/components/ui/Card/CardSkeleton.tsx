171474ee1280d61d43831f82c7a40f6cdf4948fb
DavidSheltonSF <david.faria012@gmail.com>
2026-05-11 22:11:56 -0300

    chore(frontend/components/features): refine CardSkeleton component design with a more minimalist layout

--- frontend/components/ui/Card/CardSkeleton.tsx
+++ frontend/components/ui/Card/CardSkeleton.tsx
@@ -1,15 +1,18 @@
 export function CardSkeleton() {
   return (
-    <article className="flex flex-col animate-pulse bg-color-primary w-full min-md:w-[80%] min-lg:w-[640px] min-h-[280px] rounded-xl overflow-hidden">
-      <header className="flex items-center pl-[24px] h-[56px]">
-        <span className="bg-white/50 rounded-xl w-[75%] h-[24px] font-bold text-3xl "></span>
-      </header>
-      <main className="flex flex-col gap-[16px] flex-1 px-[24px] py-[16px] bg-color-white text-color-black text-lg">
-        <span className="animate-pulse bg-black/50 rounded-xl w-[75%] h-[16px] font-bold text-3xl "></span>
-        <span className="animate-pulse bg-black/50 rounded-xl w-[70%] h-[16px] font-bold text-3xl "></span>
-        <span className="animate-pulse bg-black/50 rounded-xl w-[50%] h-[16px] font-bold text-3xl "></span>
-        <span className="animate-pulse bg-black/50 rounded-xl w-[70%] h-[16px] font-bold text-3xl "></span>
-      </main>
+    <article className="relative flex flex-col bg-color-white rounded-[8px] w-full h-fit min-md:w-[720px]">
+      <div className="flex flex-col text-color-black p-[24px] gap-[32px]">
+        <header className="flex flex-col gap-[8px]">
+          <span className="animate-pulse bg-black/20 rounded-md w-[55%] h-[24px] font-bold text-3xl " />
+          <span className="animate-pulse bg-black/20 rounded-md w-[35%] h-[8px] font-bold text-3xl " />
+        </header>
+        <div className="flex flex-col gap-[32px]">
+          <span className="animate-pulse bg-black/20 rounded-md w-[30%] h-[16px] font-bold text-3xl " />
+          <span className="animate-pulse bg-black/20 rounded-md w-[25%] h-[16px] font-bold text-3xl " />
+        </div>
+      </div>
     </article>
   );
 }
+
+
