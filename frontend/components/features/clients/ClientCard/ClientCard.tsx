63bfe70b67406cb085d0933bbf20bcb7fa9b5b5a
DavidSheltonSF <david.faria012@gmail.com>
2026-05-12 16:16:47 -0300

    feat(ClientCard): include phone number in ClientCard component

--- frontend/components/features/clients/ClientCard/ClientCard.tsx
+++ frontend/components/features/clients/ClientCard/ClientCard.tsx
@@ -14,13 +14,13 @@ ClientCard.Header = ClientCardHeader;
 ClientCard.Footer = ClientCardFooter;
 
 export function ClientCard({ clientData, openClientModal, openOptionsModal }: Props) {
-  const { firstName, lastName, email, cpf } = clientData;
+  const { firstName, lastName, email, phone, cpf } = clientData;
 
   return (
     <Card openModal={openClientModal} openOptionsModal={openOptionsModal}>
       <div className="flex flex-col text-color-black p-[24px] gap-[32px]">
         <ClientCard.Header clientName={`${firstName} ${lastName}`} cpf={cpf} />
-        <ClientCard.Footer email={email} phone="21969585855 - fake" />
+        <ClientCard.Footer email={email} phone={phone} />
       </div>
     </Card>
   );
