71a74dcbb56a61f5a73add5814ad331029c3b216
DavidSheltonSF <david.faria012@gmail.com>
2026-05-13 11:23:58 -0300

    chore(frontend/components/features/clients): refactor ClientModal to improve readability

--- frontend/components/features/clients/ClientModal/ClientModal.tsx
+++ frontend/components/features/clients/ClientModal/ClientModal.tsx
@@ -12,6 +12,7 @@ import { ClientModalHeader } from './ClientModalHeader';
 import { ClientModalInfo } from './ClientModalInfo';
 import { ClientModalCases } from './ClientModalCases';
 import { RequestState } from '@/types/RequestState';
+import { CardSkeleton } from '@/components/ui/Card/CardSkeleton';
 
 interface Props {
   clientId: string | null;
@@ -66,39 +67,39 @@ export function ClientModal({ isOpen, close, clientId, openRegisterCaseModal }: 
     };
   }, [isOpen]);
 
+  if (!isOpen) return null;
+
+  function renderContent() {
+    if (isLoading) {
+      return <CaseModalSkeleton />;
+    }
+
+    if (error || !clientData) {
+      return (
+        <div className="flex flex-col items-center size-ful pt-[80px] px-[24px] text-center gap-[16px]">
+          <h1>Cliente não encontrado</h1>
+          <h3>O cliente procurado foi removido do sistema ou não existe</h3>
+        </div>
+      );
+    }
+
+    return (
+      <div className="flex flex-col size-full">
+        <ClientModal.Header firstName={clientData.firstName} lastName={clientData.lastName} />
+        <ClientModal.Info clientData={clientData} />
+        <ClientModal.Cases cases={clientData.cases} openRegisterCaseModal={openRegisterCaseModal} />
+      </div>
+    );
+  }
+
   return (
-    isOpen && (
-      <PrimaryModal
-        additionalStyles={
-          'fixed z-99999999999 top-[2%] left-1/2 translate-x-[-50%] w-[90%] min-lg:w-[880px] h-[90%] rounded-lg overflow-hidden shadow-[0px_0px__3px_black] text-color-black'
-        }
-        closeModal={close}
-      >
-        {isLoading ? (
-          <CaseModalSkeleton />
-        ) : (
-          <div className="flex size-full">
-            {error || !clientData ? (
-              <div className="flex flex-col items-center size-ful pt-[80px] px-[24px] text-center gap-[16px]">
-                <h1>Cliente não encontrado</h1>
-                <h3>O cliente procurado foi removido do sistema ou não existe</h3>
-              </div>
-            ) : (
-              <div className="flex flex-col size-full">
-                <ClientModal.Header
-                  firstName={clientData.firstName}
-                  lastName={clientData.lastName}
-                />
-                <ClientModal.Info clientData={clientData} />
-                <ClientModal.Cases
-                  cases={clientData.cases}
-                  openRegisterCaseModal={openRegisterCaseModal}
-                />
-              </div>
-            )}
-          </div>
-        )}
-      </PrimaryModal>
-    )
+    <PrimaryModal
+      additionalStyles={
+        'fixed z-99999999999 top-[2%] left-1/2 translate-x-[-50%] w-[90%] min-lg:w-[880px] h-[90%] rounded-lg overflow-hidden shadow-[0px_0px__3px_black] text-color-black'
+      }
+      closeModal={close}
+    >
+      {renderContent()}
+    </PrimaryModal>
   );
 }
