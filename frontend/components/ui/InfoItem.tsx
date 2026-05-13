756b3ecb4c197ef58e30d4b951f9a9d76be7f63f
DavidSheltonSF <david.faria012@gmail.com>
2026-05-13 15:04:54 -0300

    feat(frontend/components/ui): add InfoItem

---
+++ frontend/components/ui/InfoItem.tsx
@@ -0,0 +1,22 @@
+import { IconProps } from '../icons/Icon';
+
+interface Props {
+  Icon: React.ComponentType<IconProps>;
+  value: string;
+  valueColor?: string;
+}
+
+export function InfoItem({ Icon, value, valueColor }: Props) {
+  return (
+    <div className="flex items-center gap-2 flex-1">
+      <Icon width="24px" height="24px" />
+      <span
+        style={{
+          color: valueColor,
+        }}
+      >
+        {value}
+      </span>
+    </div>
+  );
+}
