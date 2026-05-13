59ca31da368ac4b767f375d6159431fb83cd0be0
DavidSheltonSF <david.faria012@gmail.com>
2026-05-13 12:36:51 -0300

    feat(frontend/components/icons): add ClosIcon

---
+++ frontend/components/icons/CloseIcon.tsx
@@ -0,0 +1,9 @@
+import { Icon, IconProps } from './Icon';
+
+export function CloseIcon(props: IconProps) {
+  return (
+    <Icon {...props}>
+      <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
+    </Icon>
+  );
+}
