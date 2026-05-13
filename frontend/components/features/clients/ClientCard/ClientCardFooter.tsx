ce23ad2dd895061be62c081535ad13ab5af08347
DavidSheltonSF <david.faria012@gmail.com>
2026-05-12 21:30:08 -0300

    chore(components): remove redundant gap class from CaseCardFooter and ClientCardFooter

--- frontend/components/features/clients/ClientCard/ClientCardFooter.tsx
+++ frontend/components/features/clients/ClientCard/ClientCardFooter.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 export function ClientCardFooter({ email, phone }: Props) {
   return (
-    <footer className="flex flex-col min-md:flex-row min-md:w-[88%] min-md gap-[8px] min-md:gap-[80px]">
+    <footer className="flex flex-col min-md:flex-row min-md:w-[88%] gap-[8px]">
       <div className="flex gap-2 flex-1">
         <EmailIcon width="24px" height="24px" />
         <span>{email}</span>
