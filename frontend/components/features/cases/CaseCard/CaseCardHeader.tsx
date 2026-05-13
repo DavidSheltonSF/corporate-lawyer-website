800f2956ef326726dc1f9db08381cfbcc455107f
DavidSheltonSF <david.faria012@gmail.com>
2026-05-13 14:58:53 -0300

    chore(frontend/components/features): change header tag from h3 to h2 in CaseCardHeader

--- frontend/components/features/cases/CaseCard/CaseCardHeader.tsx
+++ frontend/components/features/cases/CaseCard/CaseCardHeader.tsx
@@ -6,7 +6,7 @@ interface Props {
 export function CaseCardHeader({ title, processNumber }: Props) {
   return (
     <header className="flex flex-col gap-[8px]">
-      <h3 className="font-bold">{title}</h3>
+      <h2 className="font-bold">{title}</h2>
       <span className="small-text opacity-70">nº {processNumber}</span>
     </header>
   );
