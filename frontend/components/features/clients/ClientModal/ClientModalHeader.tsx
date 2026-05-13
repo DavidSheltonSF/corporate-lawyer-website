28c6aa49eb0dff2015ff50a1a40c44cf951d178a
DavidSheltonSF <david.faria012@gmail.com>
2026-05-13 12:57:02 -0300

    chore(frontend/components/features): adjust padding of ClientModalHeader for improved layout

--- frontend/components/features/clients/ClientModal/ClientModalHeader.tsx
+++ frontend/components/features/clients/ClientModal/ClientModalHeader.tsx
@@ -5,7 +5,7 @@ interface Props {
 
 export function ClientModalHeader({ firstName, lastName }: Props) {
   return (
-    <header className="w-full p-[16px] border-b border-black/50">
+    <header className="w-full p-[24px] border-b border-black/50">
       <h1 className="text-3xl font-bold ">
         {firstName} {lastName}
       </h1>
