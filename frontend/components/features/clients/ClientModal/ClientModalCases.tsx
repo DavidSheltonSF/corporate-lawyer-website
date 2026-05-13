e8596d0522e290c41ea7aad7a2c88720be60e033
DavidSheltonSF <david.faria012@gmail.com>
2026-05-13 17:18:22 -0300

    fix(frontend/components/features): refactor ClientModalCases to use the cases id as key when rendering them for better security

--- frontend/components/features/clients/ClientModal/ClientModalCases.tsx
+++ frontend/components/features/clients/ClientModal/ClientModalCases.tsx
@@ -2,9 +2,10 @@ import { reduceString } from '@/lib/reduceString';
 import { Case } from '@/types/Case';
 import { Button } from '../../../ui/Button/Button';
 import { AddIcon } from '@/components/icons/AddIcon';
+import { WithId } from '@/types/WithId';
 
 interface Props {
-  cases: Case[];
+  cases: WithId<Case>[];
   openRegisterCaseModal: () => void;
 }
 
@@ -12,7 +13,7 @@ export function ClientModalCases({ cases, openRegisterCaseModal }: Props) {
   const renderClientCases = cases.map((cas, index) => {
     return (
       <div
-        key={index}
+        key={cas.id}
         className="flex flex-col gap-[8px] w-full border-[1px] p-[8px] rounded-[8px]"
       >
         <h2 className="font-bold ">{reduceString(cas.title, 24)}</h2>
@@ -25,13 +26,13 @@ export function ClientModalCases({ cases, openRegisterCaseModal }: Props) {
       <div className="flex justify-between items-center w-full ">
         <h1 className="text-2xl font-bold">Processos</h1>
         <Button
-        darkHover
+          darkHover
           backgroundColor="var(--white-color)"
           textColor="var(--black-color)"
           size="fit-content"
           paddingX="16px"
           paddingY="8px"
-          border='1px solid var(--black-color)'
+          border="1px solid var(--black-color)"
           onclick={() => openRegisterCaseModal()}
         >
           <span className="flex justify-center items-center gap-[8px]">
