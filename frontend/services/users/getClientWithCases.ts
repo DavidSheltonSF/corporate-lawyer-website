704db398ccb1fe40c4d24cf8d423bf874d79fbb4
DavidSheltonSF <david.faria012@gmail.com>
2026-05-13 20:48:57 -0300

    fix(frontend/services): fix type return in getClientWithCases to include id

--- frontend/services/users/getClientWithCases.ts
+++ frontend/services/users/getClientWithCases.ts
@@ -2,8 +2,11 @@ import { API_URL } from '@/config/api';
 import { Case } from '@/types/Case';
 import { SafeUser } from '@/types/SafeUser';
 import { apiFetch } from '../apiFetch';
+import { WithId } from '@/types/WithId';
 
-export async function getClientWithCases(id: string): Promise<SafeUser & { cases: Case[] }> {
+export async function getClientWithCases(
+  id: string
+): Promise<SafeUser & { cases: WithId<Case>[] }> {
   const response = await apiFetch(`${API_URL}/clients/${id}?include=cases`);
   const json = await response.json();
   return json.data;
