a89ca1b86d238ce62e2ec42a583ab93f44329e02
DavidSheltonSF <david.faria012@gmail.com>
2026-05-13 15:15:17 -0300

    chore(frontend/components/ui): remove flex-1 style from InfoItem

--- frontend/components/ui/InfoItem.tsx
+++ frontend/components/ui/InfoItem.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 export function InfoItem({ Icon, value, valueColor }: Props) {
   return (
-    <div className="flex items-center gap-2 flex-1">
+    <div className="flex items-center gap-2">
       <Icon width="24px" height="24px" />
       <span
         style={{
