8391b78732f5e3bb467b02fbae965750647a8749
DavidSheltonSF <david.faria012@gmail.com>
2026-05-13 22:47:08 -0300

    chore(frontend/context): update AuthenticatedUserProvider to create the userData state internaly and provide it

--- frontend/contexts/AuthenticatedUserProvider.tsx
+++ frontend/contexts/AuthenticatedUserProvider.tsx
@@ -1,18 +1,18 @@
 'use client';
 
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
 import { AuthenticatedUserContext } from './AuthenticatedUserContext';
 import { User } from '@/types/User';
 import { WithId } from '@/types/WithId';
 
 interface Props {
-  userData: WithId<User>;
   children: ReactNode;
 }
 
-export function AuthenticatedUserProvider({ userData, children }: Props) {
+export function AuthenticatedUserProvider({ children }: Props) {
+  const [userData, setUserData] = useState<WithId<User> | null>(null)
   return (
-    <AuthenticatedUserContext.Provider value={{ userData }}>
+    <AuthenticatedUserContext.Provider value={{ userData, setUserData }}>
       {children}
     </AuthenticatedUserContext.Provider>
   );
