88cbc5a7b951314db60be3531586920d5a462da0
DavidSheltonSF <david.faria012@gmail.com>
2026-05-13 23:54:07 -0300

    chore(frontend/context): remove SelectedCaseContext and its provider

--- frontend/contexts/cases/SelectedCaseContext.ts
+++
@@ -1,8 +0,0 @@
-import { Dispatch, SetStateAction, createContext } from 'react';
-
-export interface SelectedCaseContext {
-  selectedCaseId: string;
-  setSelectedCaseId: Dispatch<SetStateAction<string>>;
-}
-
-export const SelectedCaseContext = createContext<SelectedCaseContext | undefined>(undefined);
