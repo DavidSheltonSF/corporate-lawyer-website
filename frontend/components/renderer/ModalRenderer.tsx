8b8a79a3de269b8c3ed1c53f74c491588af3fac6
DavidSheltonSF <david.faria012@gmail.com>
2026-05-13 23:31:29 -0300

    feat(frontend/components/renderer): integrate CaseModal dto ModalRenderer

--- frontend/components/renderer/ModalRenderer.tsx
+++ frontend/components/renderer/ModalRenderer.tsx
@@ -1,15 +1,19 @@
-'use client'
+'use client';
 
-import { useModal } from "@/hooks/useModal";
-import { ClientModal } from "../features/clients/ClientModal/ClientModal";
+import { useModal } from '@/hooks/useModal';
+import { ClientModal } from '../features/clients/ClientModal/ClientModal';
+import { CaseModal } from '../features/cases/CaseModal/CaseModal';
 
 export function ModalRenderer() {
-  const {currentModal, modalData, openModal, closeModal} = useModal()
+  const { currentModal, modalData, openModal, closeModal } = useModal();
 
   switch (currentModal) {
     case 'client':
-      return <ClientModal data={modalData} close={closeModal} />
-  
+      return <ClientModal data={modalData} close={closeModal} />;
+
+    case 'case':
+      return <CaseModal data={modalData} close={closeModal} />;
+
     default:
       return null;
   }
