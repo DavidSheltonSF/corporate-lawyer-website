41d70c4bd66c2386c254bf4bd398c3196c1d68c0
DavidSheltonSF <david.faria012@gmail.com>
2026-05-14 14:23:14 -0300

    chore(frontend/components/): simplify the use of PrimaryModal in CaseModal for matching recent changes made in PrimaryModal

--- frontend/components/features/cases/CaseModal/CaseModal.tsx
+++ frontend/components/features/cases/CaseModal/CaseModal.tsx
@@ -137,7 +137,7 @@ export function CaseModal({ data, close }: Props) {
   return (
     <PrimaryModal
       additionalStyles={
-        'fixed z-99999999999 top-[2%] left-1/2 translate-x-[-50%] w-[90%] min-lg:w-[880px] h-[82vh] min-lg:h-[95vh] rounded-lg overflow-hidden shadow-[0px_0px__3px_black] text-color-black'
+        'top-[2%] left-1/2 translate-x-[-50%] w-[90%] min-lg:w-[880px] h-[82vh] min-lg:h-[95vh]'
       }
       closeModal={() => {
         close();
