ed5c2da37c55a00e5093957d13e0ac3c64e79a64
DavidSheltonSF <david.faria012@gmail.com>
2026-05-13 23:24:57 -0300

    refactor(frontend/components/features): refactor ClientCard to pass the client id to openClientModal

--- frontend/components/features/clients/ClientCard/ClientCard.tsx
+++ frontend/components/features/clients/ClientCard/ClientCard.tsx
@@ -14,12 +14,14 @@ ClientCard.Header = ClientCardHeader;
 ClientCard.Footer = ClientCardFooter;
 
 export function ClientCard({ clientData, openClientModal, openOptionsModal }: Props) {
-  const { firstName, lastName, email, phone, cpf } = clientData;
+  const { id, firstName, lastName, email, phone, cpf } = clientData;
 
   return (
     <Card
       className="w-full h-fit min-md:w-[720px]"
-      openModal={openClientModal}
+      openModal={() => {
+        openClientModal(id);
+      }}
       openOptionsModal={openOptionsModal}
     >
       <div className="flex flex-col text-color-black p-[24px] gap-[32px]">
