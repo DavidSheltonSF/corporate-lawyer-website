24fa9f8b8b5c15bfa4701e771e6fd9778db22fff
DavidSheltonSF <david.faria012@gmail.com>
2026-05-14 13:06:13 -0300

    refactor(Button): replace all style props with className for simplicity

--- frontend/components/ui/Button/Button.tsx
+++ frontend/components/ui/Button/Button.tsx
@@ -2,59 +2,24 @@
 
 interface Props {
   type?: 'submit' | 'reset' | 'button';
-  backgroundColor?: string;
-  textColor?: string;
-  fontSize?: string;
-  width?: string;
-  height?: string;
-  size?: string;
-  paddingX?: string;
-  paddingY?: string;
   children: React.ReactNode;
   onclick?: React.MouseEventHandler<HTMLButtonElement>;
   disabled?: boolean;
-  darkHover?: boolean; // decrease brightness on hover
-  border?: string;
+  className?: string;
 }
 
 export function Button(props: Props) {
-  const {
-    type,
-    backgroundColor = 'inherit',
-    textColor = 'var(--black-color)',
-    fontSize = '1.2rem',
-    children,
-    size,
-    width = size ? size : '128px',
-    height = size ? size : '56px',
-    paddingX = 'auto',
-    paddingY = 'auto',
-    onclick,
-    disabled,
-    darkHover,
-    border,
-  } = props;
+  const { type, children, onclick, disabled, className } = props;
 
   const baseStyles = 'w-full rounded-sm  px-[8px]';
   const activeStyles = 'transition-[filter] duration-300 cursor-pointer';
   const disabledStyles = 'cursor-default';
-  const hoverStyles = darkHover ? 'hover:brightness-90' : 'hover:brightness-120';
 
   return (
     <button
       type={type}
       disabled={disabled}
-      className={`${baseStyles} ${disabled ? disabledStyles : activeStyles} ${hoverStyles}`}
-      style={{
-        backgroundColor,
-        color: textColor,
-        fontSize,
-        height,
-        width,
-        paddingInline: paddingX,
-        paddingBlock: paddingY,
-        border,
-      }}
+      className={`${baseStyles} ${disabled ? disabledStyles : activeStyles} ${className}`}
       onClick={onclick}
     >
       {children}
