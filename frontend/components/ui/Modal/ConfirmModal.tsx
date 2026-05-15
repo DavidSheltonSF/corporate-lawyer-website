d00daeef5451c1aaea73144a32001a726f207bac
DavidSheltonSF <david.faria012@gmail.com>
2026-05-15 18:39:19 -0300

    refactor(ConfirmModal): refactor to integrate with ModalRenderer for global rendering

--- frontend/components/ui/Modal/ConfirmModal.tsx
+++ frontend/components/ui/Modal/ConfirmModal.tsx
@@ -6,55 +6,40 @@ import { RequestState } from '@/types/RequestState';
 import { RequestFeedback } from '../Feedback/RequestFeedback';
 
 interface Props {
-  onConfirm: Function;
-  isOpen: Boolean;
-  setIsOpen: Dispatch<SetStateAction<boolean>>;
-  text: string;
-  requestState: RequestState | null;
+  data: { onConfirm: () => void };
+  close: () => void;
 }
 
-export function ConfirmModal({ isOpen, setIsOpen, onConfirm, requestState, text }: Props) {
+export function ConfirmModal({ data, close }: Props) {
+  const { onConfirm } = data;
   return (
-    isOpen && (
-      <PrimaryModal
-        additionalStyles={
-          'fixed z-99999999999 top-[2%] min-lg:top-[10%] left-1/2 translate-x-[-50%] w-[90%] min-lg:w-[400px] h-[240px] rounded-lg overflow-hidden shadow-[0px_0px__3px_black] text-color-black'
-        }
-        closeModal={() => {
-          setIsOpen(false);
-        }}
-      >
-        <div className="flex flex-col size-full justify-between items-center p-[24px]">
-          <div className="flex w-full h-[80px] justify-center items-center">
-            {requestState === null ? <h3 className="text-center text-lg">{text}</h3> : ''}
-            <RequestFeedback requestState={requestState} />
-          </div>
-          <div className="flex h-fit justify-between items-end size-full ">
-            <Button
-              width="144px"
-              height="56px"
-              backgroundColor="#949494"
-              textColor="black"
-              fontSize="1.2rem"
-              onclick={() => {
-                setIsOpen(false);
-              }}
-            >
-              Cancelar
-            </Button>
-            <Button
-              width="144px"
-              height="56px"
-              backgroundColor="var(--primary-color)"
-              textColor="var(--white-color)"
-              fontSize="1.2rem"
-              onclick={() => onConfirm()}
-            >
-              Confirmar
-            </Button>
-          </div>
+    <PrimaryModal
+      additionalStyles={
+        'top-[2%] min-lg:top-[10%] left-1/2 translate-x-[-50%] w-[90%] min-lg:w-[400px] h-[240px]'
+      }
+      closeModal={close}
+    >
+      <div className="flex flex-col size-full justify-between items-center p-[24px]">
+        <div className="flex h-fit justify-between items-end size-full ">
+          <Button
+            className="w-[144px] h-[56px] bg-color-white text-color-black"
+            onclick={() => {
+              close();
+            }}
+          >
+            Cancelar
+          </Button>
+          <Button
+            className="w-[144px] h-[56px] bg-color-white text-color-black"
+            onclick={() => {
+              onConfirm();
+              close();
+            }}
+          >
+            Confirmar
+          </Button>
         </div>
-      </PrimaryModal>
-    )
+      </div>
+    </PrimaryModal>
   );
 }
