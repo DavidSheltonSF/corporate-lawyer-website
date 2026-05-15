391b6f9a5baf69c917a58152ea0f473c410663ba
DavidSheltonSF <david.faria012@gmail.com>
2026-05-15 19:01:27 -0300

    feat(ConfirmModal): add message prop and update styles for better UI

--- frontend/components/ui/Modal/ConfirmModal.tsx
+++ frontend/components/ui/Modal/ConfirmModal.tsx
@@ -6,23 +6,26 @@ import { RequestState } from '@/types/RequestState';
 import { RequestFeedback } from '../Feedback/RequestFeedback';
 
 interface Props {
-  data: { onConfirm: () => void };
+  data: { message: string; onConfirm: () => void };
   close: () => void;
 }
 
 export function ConfirmModal({ data, close }: Props) {
-  const { onConfirm } = data;
+  const { message, onConfirm } = data;
   return (
     <PrimaryModal
       additionalStyles={
-        'top-[2%] min-lg:top-[10%] left-1/2 translate-x-[-50%] w-[90%] min-lg:w-[400px] h-[240px]'
+        'top-[2%] min-lg:top-[10%] left-1/2 translate-x-[-50%] w-[90%] min-lg:w-[400px] h-[280px]'
       }
       closeModal={close}
     >
-      <div className="flex flex-col size-full justify-between items-center p-[24px]">
+      <div className="flex flex-col size-full justify-between items-center px-[16px] pt-[72px] pb-[16px]">
+        <div className="text-center">
+          <h2>{message}</h2>
+        </div>
         <div className="flex h-fit justify-between items-end size-full ">
           <Button
-            className="w-[144px] h-[56px] bg-color-white text-color-black"
+            className="w-[144px] h-[56px] bg-color-white text-color-black font-bold border border-black hover:brightness-95"
             onclick={() => {
               close();
             }}
@@ -30,7 +33,7 @@ export function ConfirmModal({ data, close }: Props) {
             Cancelar
           </Button>
           <Button
-            className="w-[144px] h-[56px] bg-color-white text-color-black"
+            className="w-[144px] h-[56px] bg-color-red text-color-white font-bold"
             onclick={() => {
               onConfirm();
               close();
