73fd2067ee06b60f209cf96b946bf26f307bb72a
DavidSheltonSF <david.faria012@gmail.com>
2026-05-15 15:46:37 -0300

    chore(Footer): update Footer to make the text white and remove unnecessary div

--- frontend/components/layout/Footer/Footer.tsx
+++ frontend/components/layout/Footer/Footer.tsx
@@ -1,37 +1,35 @@
 export function Footer() {
   return (
-    <footer>
-      <div className="flex flex-col h-[50vh] bg-color-primary">
-        <div className="flex justify-center items-center h-[65%]">
+    <footer className="flex flex-col h-[50vh] bg-color-primary text-color-white">
+      <div className="flex justify-center items-center h-[65%]">
+        <img
+          className="size-[240px]"
+          src="/website-logo.webp"
+          alt="Medeiros e Santiago"
+          loading="lazy"
+        />
+      </div>
+      <div className="flex justify-center items-center gap-[24px] lg:gap-[16px] h-[15%] border-y border-color-secondary">
+        <a href="#">
           <img
-            className="size-[240px]"
-            src="/website-logo.webp"
-            alt="Medeiros e Santiago"
+            className="size-[40px]"
+            src="/icons/instagram.svg"
+            alt="Vá para nossa página do Instagram"
             loading="lazy"
           />
-        </div>
-        <div className="flex justify-center items-center gap-[24px] lg:gap-[16px] h-[15%] border-y border-color-secondary">
-          <a href="#">
-            <img
-              className="size-[40px]"
-              src="/icons/instagram.svg"
-              alt="Vá para nossa página do Instagram"
-              loading="lazy"
-            />
-          </a>
-          <a href="#">
-            <img
-              className="size-[40px]"
-              src="/icons/facebook.png"
-              alt="Vá para nossa página do Facebook"
-              loading="lazy"
-            />
-          </a>
-        </div>
-        <div className="flex justify-center items-center gap-[24px] h-[20%] font-bold text-[var(--white-color)]">
-          <a href="#">Privacidade</a>
-          <a href="#">Contate-nos</a>
-        </div>
+        </a>
+        <a href="#">
+          <img
+            className="size-[40px]"
+            src="/icons/facebook.png"
+            alt="Vá para nossa página do Facebook"
+            loading="lazy"
+          />
+        </a>
+      </div>
+      <div className="flex justify-center items-center gap-[24px] h-[20%] font-bold text-[var(--white-color)]">
+        <a href="#">Privacidade</a>
+        <a href="#">Contate-nos</a>
       </div>
     </footer>
   );
