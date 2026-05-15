57c9ec22f78fc9c377347fbda3bc75e13de62b76
DavidSheltonSF <david.faria012@gmail.com>
2026-05-15 19:02:07 -0300

    feat(CasesList): implement case deletion functionality with loading state and error handling

--- frontend/components/features/cases/CasesList.tsx
+++ frontend/components/features/cases/CasesList.tsx
@@ -4,19 +4,41 @@ import { WithId } from '@/types/WithId';
 import { Activity, useState } from 'react';
 import { CardSkeleton } from '../../ui/Card/CardSkeleton';
 import { CaseWithRelations } from '@/types/CaseWithRelations';
+import { RequestState } from '@/types/RequestState';
+import { deleteCaseById } from '@/services/users/deleteCaseById';
+import { UnauthorizedError } from '@/errors/UnauthorizedError';
+import { handleLogout } from '@/lib/handleLogout';
 
 interface Props {
   cases: WithId<CaseWithRelations>[];
   loading: boolean;
-  loadCases: Function;
+  loadCases: () => void;
 }
 
 export function CasesList({ cases, loading, loadCases }: Props) {
   const [openedDropdownId, setOpenedDropdownId] = useState<string | null>(null);
+  const [requestState, setRequestState] = useState<RequestState | null>(null);
+
+  async function deleteCase(id: string) {
+    try {
+      setRequestState({ status: 'loading' });
+      await deleteCaseById(id);
+      setRequestState({ status: 'ok', message: 'Case deleted successfuly' });
+      // loadCases();
+    } catch (error: any) {
+      setRequestState({ status: 'error', message: error.message });
+      console.log(error);
+      if (error instanceof UnauthorizedError) {
+        handleLogout();
+      }
+    }
+  }
 
   const renderCases = cases?.map((cas, index) => {
     return (
       <CaseCard
+      loadCases={loadCases}
+        deleteCase={deleteCase}
         key={index}
         isDropdownOpen={openedDropdownId === cas.id}
         closeDropdown={() => setOpenedDropdownId(null)}
