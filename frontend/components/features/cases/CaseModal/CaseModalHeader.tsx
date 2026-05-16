f0bbb71b71d962af0ece5a39233537c5865be39e
DavidSheltonSF <david.faria012@gmail.com>
2026-05-16 16:21:45 -0300

    feat(frontend/components/features): add CaseModalHeader

---
+++ frontend/components/features/cases/CaseModal/CaseModalHeader.tsx
@@ -0,0 +1,15 @@
+interface Props {
+  title: string;
+  processNumber: string;
+}
+
+export function CaseModalHeader({ title, processNumber }: Props) {
+  return (
+    <header className="flex flex-col gap-[8px] w-full p-[24px] border-divider">
+      <h1 className="text-3xl font-bold ">{title}</h1>
+      <div className="flex flex-1">
+        <span className="text-muted">nº {processNumber}</span>
+      </div>
+    </header>
+  );
+}
