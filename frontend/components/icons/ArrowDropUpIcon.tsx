8a7802bfd2be79bc8dc4964e01508b2a8ad79c21
DavidSheltonSF <david.faria012@gmail.com>
2026-05-17 18:55:20 -0300

    fix(ArrowDropUpIcon): update stroke attributes to camelCase for consistency

--- frontend/components/icons/ArrowDropUpIcon.tsx
+++ frontend/components/icons/ArrowDropUpIcon.tsx
@@ -4,7 +4,7 @@ import { Icon } from './Icon';
 export function ArrowDropUpIcon(props: PropsWithClassName) {
   return (
     <Icon {...props} fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
-      <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 15.75 7.5-7.5 7.5 7.5" />
+      <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 15.75 7.5-7.5 7.5 7.5" />
     </Icon>
   );
 }
