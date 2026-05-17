4ad451e8440866f3c39f3bf710a019dc5f7ea1ad
DavidSheltonSF <david.faria012@gmail.com>
2026-05-17 12:51:50 -0300

    chore(CaseModalFiles): remove unused prop caseId and unused imports

--- frontend/components/features/cases/CaseModal/CaseModalFiles.tsx
+++ frontend/components/features/cases/CaseModal/CaseModalFiles.tsx
@@ -1,17 +1,14 @@
 import { WithId } from '@/types/WithId';
 import { CaseFile } from '@/types/CaseFile';
 import { CaseFilesTable } from '../CaseFilesTable';
-import { CaseFilesUploadModal } from '@/components/modals/CaseFilesUploadModal';
-import { useState } from 'react';
 import { OpenUploadModalButton } from '@/components/OpenUploadModalButton';
 
 interface Props {
-  caseId: string;
   caseFiles: WithId<CaseFile>[];
   openUploadModal: () => void;
 }
 
-export function CaseModalFiles({ caseId, caseFiles, openUploadModal }: Props) {
+export function CaseModalFiles({ caseFiles, openUploadModal }: Props) {
   return (
     <div className="flex flex-col gap-[24px] p-[24px]">
       <div className="flex justify-between  w-full">
