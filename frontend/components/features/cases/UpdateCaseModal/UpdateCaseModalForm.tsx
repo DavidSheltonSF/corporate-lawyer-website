d070ee93065e06d7a5ec452d2ab867b04d77f0fd
DavidSheltonSF <david.faria012@gmail.com>
2026-05-17 19:49:50 -0300

    refactor(UpdateCaseModalForm): improve button responsiveness

--- frontend/components/features/cases/UpdateCaseModal/UpdateCaseModalForm.tsx
+++ frontend/components/features/cases/UpdateCaseModal/UpdateCaseModalForm.tsx
@@ -81,7 +81,7 @@ export function UpdateCaseModalForm({ caseData, action }: Props) {
         defaultValue={caseData?.description}
       />
 
-      <Button className="w-full bg-color-primary text-color-white px-[16px] py-[8px]">
+      <Button className="w-full min-lg:w-fit min-lg:ml-auto bg-color-primary text-color-white px-[16px] py-[8px]">
         Confirmar Alterações
       </Button>
     </form>
