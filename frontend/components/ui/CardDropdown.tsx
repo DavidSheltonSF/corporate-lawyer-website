d50a50059f0515529b0527c4ea2de2782dc77ee8
DavidSheltonSF <david.faria012@gmail.com>
2026-05-17 19:37:59 -0300

    refactor(CardDropdown): update actions type to use PropsWithClassName for better type consistency

--- frontend/components/ui/CardDropdown.tsx
+++ frontend/components/ui/CardDropdown.tsx
@@ -1,13 +1,17 @@
 'use client';
-import { IconProps } from '@/components/icons/Icon';
 import { Button } from '@/components/ui/Button/Button';
+import { PropsWithClassName } from '@/types/PropsWithClassName';
 import { useEffect, useRef } from 'react';
 import { twMerge } from 'tailwind-merge';
 
 interface Props {
   close: Function;
   className?: string;
-  actions: { Icon: React.ComponentType<IconProps>; label: string; action: Function }[];
+  actions: {
+    Icon: React.ComponentType<PropsWithClassName>;
+    label: string;
+    action: Function;
+  }[];
 }
 
 export function CardDropdown({ close, className, actions }: Props) {
@@ -41,7 +45,8 @@ export function CardDropdown({ close, className, actions }: Props) {
     );
   });
 
-  const positionStyles = 'z-60 absolute top-[10px] min-lg:top-[-8px] right-1/2  min-lg:right-0 translate-x-1/2 min-lg:translate-x-[100%]';
+  const positionStyles =
+    'z-60 absolute top-[10px] min-lg:top-[-8px] right-1/2  min-lg:right-0 translate-x-1/2 min-lg:translate-x-[100%]';
   const sizeStyles = `w-[70%] min-lg:w-[300px] h-fit`;
   const transitionStyles = 'fade-in-animation-fast';
   const baseStyles = `bg-color-white py-[8px] rounded-[8px] text-color-black shadow-soft`;
