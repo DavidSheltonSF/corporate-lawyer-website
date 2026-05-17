b9e2d07a27a2d94626b1b0845b7d594633c0454f
DavidSheltonSF <david.faria012@gmail.com>
2026-05-17 17:58:02 -0300

    refactor(Icon): simplify component to accept SVGProps and remove custom props

--- frontend/components/icons/Icon.tsx
+++ frontend/components/icons/Icon.tsx
@@ -1,20 +1,5 @@
-import { WithChildren } from '@/types/WithChildren';
+import { SVGProps } from 'react';
 
-export interface IconProps {
-  className: string;
-}
-
-export function Icon({ className, children }: WithChildren<IconProps>) {
-  return (
-    <svg
-      className={className}
-      xmlns="http://www.w3.org/2000/svg"
-      width="100%"
-      height="100%"
-      viewBox="0 -960 960 960"
-      fill="currentColor"
-    >
-      {children}
-    </svg>
-  );
+export function Icon(props: SVGProps<SVGSVGElement>) {
+  return <svg xmlns="http://www.w3.org/2000/svg" {...props}></svg>;
 }
