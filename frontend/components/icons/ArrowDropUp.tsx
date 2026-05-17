394b5eecc2a790846eed81b00d84d37e1cef9743
DavidSheltonSF <david.faria012@gmail.com>
2026-05-17 18:53:22 -0300

    refactor(ArrowDropUp) to use PropsWithClassName, enhance SVG properties and update SVG path

--- frontend/components/icons/ArrowDropUp.tsx
+++ frontend/components/icons/ArrowDropUp.tsx
@@ -1,9 +1,13 @@
-import { Icon, IconProps } from './Icon';
+import { PropsWithClassName } from '@/types/PropsWithClassName';
+import { Icon } from './Icon';
 
-export function ArrowDropUp(props: IconProps) {
+export function ArrowDropUp(props: PropsWithClassName) {
   return (
-    <Icon {...props}>
-      <path d="m280-400 200-200 200 200H280Z" />
+    <Icon {...props}   fill="none"
+  viewBox="0 0 24 24"
+  strokeWidth="1.5"
+  stroke="currentColor">
+      <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 15.75 7.5-7.5 7.5 7.5" />
     </Icon>
   );
 }
