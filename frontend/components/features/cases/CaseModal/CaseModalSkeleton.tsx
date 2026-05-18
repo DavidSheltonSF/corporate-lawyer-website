c1620306bad4b1daec6caabebfbc7483088fa5c6
DavidSheltonSF <david.faria012@gmail.com>
2026-05-18 13:05:25 -0300

    refactor(CaseModalSkeleton): enhance layout ant replace static placeholders with SkeletonLine components for improved loading state

--- frontend/components/features/cases/CaseModal/CaseModalSkeleton.tsx
+++ frontend/components/features/cases/CaseModal/CaseModalSkeleton.tsx
@@ -1,27 +1,38 @@
+import { SkeletonLine } from '@/components/ui/SkeletonLine';
+
 export function CaseModalSkeleton() {
   return (
-    <div className="flex flex-col h-full w-full">
-      <div className="size-full bg-color-white">
-        <header className="flex bg-color-primary min-h-[72px] border-t border-white/50 pl-[24px] py-[16px]">
-          <span className="w-90 min-md:w-160 h-[24px] bg-gray-200 animate-pulse"></span>
-        </header>
-        <main className="flex flex-col h-full text-xl">
-          <div className="flex flex-col min-h-[30%] gap-[8px] pl-[24px] py-[24px]">
-            <span className="w-80 h-[24px] bg-gray-300 animate-pulse"></span>
-            <span className="w-90 h-[24px] bg-gray-300 animate-pulse"></span>
-            <span className="w-70 h-[24px] bg-gray-300 animate-pulse"></span>
-            <span className="w-100 h-[24px] bg-gray-300 animate-pulse"></span>
-            <span className="w-80 h-[24px] bg-gray-300 animate-pulse"></span>
-          </div>
-          <div className="flex flex-col min-h-[20%] gap-[8px] px-[24px] py-[24px] border-t border-black/30">
-            <h1 className="font-bold text-3xl">Resumo</h1>
-            <span className="w-180 h-[24px] bg-gray-300 animate-pulse"></span>
-            <span className="w-150 h-[24px] bg-gray-300 animate-pulse"></span>
-          </div>
-          <div className="flex relative items-center h-[56px] bg-color-primary border-t pl-[24px]">
-            <h1 className=" text-color-white text-3xl">Documentação</h1>
-          </div>
-        </main>
+    <div className="flex flex-col size-full bg-color-white">
+      <header className="flex flex-col gap-[8px] w-full p-[24px] border-divider">
+        <SkeletonLine className="w-110 h-[24px]" />
+        <div className="flex flex-1">
+          <SkeletonLine className="w-50 h-[8px]" />
+        </div>
+      </header>
+      <div className="flex flex-col justify-between font-size-md">
+        <div className="flex flex-col items-center min-md:flex-row min-md:items-start min-md:gap-[40px] border-b border-divider p-[24px]">
+          <SkeletonLine className="w-30" />
+          <SkeletonLine className="w-30" />
+          <SkeletonLine className="w-30" />
+        </div>
+        <div className="flex flex-col justify-center gap-[8px] p-[24px] borer-b border-divider">
+          <SkeletonLine className="w-50" />
+          <SkeletonLine className="w-50" />
+          <SkeletonLine className="w-50" />
+          <SkeletonLine className="w-50" />
+        </div>
+        <div className="flex flex-col gap-[8px] border-b border-black/50 p-[16px]">
+          <h1 className="text-2xl font-bold">Resumo</h1>
+          <SkeletonLine className="w-190" />
+          <SkeletonLine className="w-180" />
+          <SkeletonLine className="" />
+        </div>
+      </div>
+      <div className="flex flex-col gap-[24px] p-[24px]">
+        <div className="flex justify-between  w-full">
+          <h1>Arquivos</h1>
+        </div>
+        <div>{/* <CaseFilesTable documents={caseFiles} /> */}</div>
       </div>
     </div>
   );
