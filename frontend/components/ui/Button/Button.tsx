ca28f6376de330754279ebe3d18ac1a1b6d9679b
DavidSheltonSF <david.faria012@gmail.com>
2026-05-18 20:30:08 -0300

    refactor(Button): enhance props interface and improve styling variants, by adding 'primary', 'secondary' and 'generic' variants

--- frontend/components/ui/Button/Button.tsx
+++ frontend/components/ui/Button/Button.tsx
@@ -1,27 +1,33 @@
 'use client';
+import { ButtonHTMLAttributes } from 'react';
 import { twMerge } from 'tailwind-merge';
 
-interface Props {
-  type?: 'submit' | 'reset' | 'button';
-  children: React.ReactNode;
-  onclick?: React.MouseEventHandler<HTMLButtonElement>;
-  disabled?: boolean;
-  className?: string;
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: 'primary' | 'secondary';
 }
 
-export function Button(props: Props) {
-  const { type, children, onclick, disabled, className } = props;
+export function Button(props: ButtonProps) {
+  const { type, children, onClick, disabled, className, variant } = props;
 
-  const baseStyles = 'rounded-sm';
-  const activeStyles = 'hover:brightness-120 transition-[filter] duration-300 cursor-pointer';
-  const disabledStyles = 'cursor-default';
+  const baseStyles = 'rounded-sm font-bold py-[8px] px-[16px] duration-300 cursor-pointer';
+  const disabledStyles = 'cursor-default bg-gray/50 text-gray/50';
+
+  const variants: Record<string, string> = {
+    primary: 'bg-color-primary-light text-color-white hover:brightness-120',
+    secondary:
+      'border text-color-primary-light hover:shadow-[var(--inner-shadow-primary-soft)] transition-[box-shadow] transition-[background] hover:bg-[var(--color-primary-light)]/10',
+    generic: 'transition-[filter] hover:brightness-120 ',
+  };
 
   return (
     <button
       type={type}
       disabled={disabled}
-      className={twMerge(`${baseStyles} ${disabled ? disabledStyles : activeStyles}`, className)}
-      onClick={onclick}
+      className={twMerge(
+        `${baseStyles} ${disabled ? disabledStyles : variants[variant || 'generic']}`,
+        className
+      )}
+      onClick={onClick}
     >
       {children}
     </button>
