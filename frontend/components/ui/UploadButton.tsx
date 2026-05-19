067c0a3fe2464e81ff9c6c7aba2c8e4279f6e4ad
DavidSheltonSF <david.faria012@gmail.com>
2026-05-19 00:17:55 -0300

    feat(frontend/ui): add UploadButton

---
+++ frontend/components/ui/UploadButton.tsx
@@ -0,0 +1,29 @@
+import { ChangeEvent, Dispatch, SetStateAction, useRef } from 'react';
+import { Button } from './Button/Button';
+
+interface Props {
+  setFile: Dispatch<SetStateAction<File | null>>;
+}
+
+export function UploadButton({ setFile }: Props) {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  function handleOpenFilePicker() {
+    inputRef.current?.click();
+  }
+
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    const selectedFiles = Array.from(e.target.files || []);
+    const selectedFile = selectedFiles[0];
+    setFile(selectedFile);
+  }
+
+  return (
+    <div className="flex size-full">
+      <input name="file" onChange={handleChange} ref={inputRef} className="hidden" type="file" />
+      <Button onClick={handleOpenFilePicker} variant="secondary" className="w-full py-[4px]">
+        Selecionar arquivo
+      </Button>
+    </div>
+  );
+}
