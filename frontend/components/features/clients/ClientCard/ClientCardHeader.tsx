22f33a90b41898b2ba06a54484447c5334318750
DavidSheltonSF <david.faria012@gmail.com>
2026-05-19 01:40:56 -0300

    fix(ClientCardHeader): fix UserIcon props use

--- frontend/components/features/clients/ClientCard/ClientCardHeader.tsx
+++ frontend/components/features/clients/ClientCard/ClientCardHeader.tsx
@@ -10,7 +10,7 @@ export function ClientCardHeader({ clientName, cpf }: Props) {
     <header className="flex flex-col  gap-[8px] ">
       <div className="flex items-center gap-[32px]">
         <div className="flex justify-center items-center size-[5rem] border rounded-[8px]">
-          <UserIcon width="40px" height="40px" />
+          <UserIcon className='size-[40px]' />
         </div>
         <h2>{clientName}</h2>
       </div>
