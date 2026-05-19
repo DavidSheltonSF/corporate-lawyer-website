42f388398248e8d79d6c8abc364e024624451999
DavidSheltonSF <david.faria012@gmail.com>
2026-05-19 15:27:34 -0300

    fix(Card): make isDropdownOpen, openDropdown, closeDropdown, and actions props optional for better flexibility and make the more button hidden if one of these props is undefined

--- frontend/components/ui/Card/Card.tsx
+++ frontend/components/ui/Card/Card.tsx
@@ -7,10 +7,10 @@ import { twMerge } from 'tailwind-merge';
 interface Props {
   className?: string;
   onClick?: () => void;
-  isDropdownOpen: boolean;
-  openDropdown: () => void;
-  closeDropdown: () => void;
-  actions: CardAction[];
+  isDropdownOpen?: boolean;
+  openDropdown?: () => void;
+  closeDropdown?: () => void;
+  actions?: CardAction[];
   children: React.ReactNode;
 }
 
@@ -23,14 +23,24 @@ export function Card({
   children,
   className,
 }: Props) {
-  function openActions() {
-    openDropdown();
+  function renderMoreButton() {
+    if (!isDropdownOpen || !openDropdown || !closeDropdown || !actions) return;
+    return (
+      <Button
+        className="group flex justify-center items-center p-[4px] hover:bg-[var(--color-primary)] transition-[background] duration-300"
+        onClick={(e) => {
+          e.stopPropagation();
+          openDropdown && openDropdown();
+        }}
+      >
+        <VerticalMoreIcon className="size-[32px] stroke-color-black group-hover:invert" />
+      </Button>
+    );
   }
 
   function renderDropdown() {
-    if (isDropdownOpen) {
-      return <CardDropdown actions={actions} close={closeDropdown} />;
-    }
+    if (!isDropdownOpen || !closeDropdown || !actions) return;
+    return <CardDropdown actions={actions} close={closeDropdown} />;
   }
 
   const hoverStyles = onClick ? 'cursor-pointer' : '';
@@ -39,17 +49,7 @@ export function Card({
   return (
     <article className={twMerge(baseStyles, hoverStyles, className)} onClick={onClick}>
       {renderDropdown()}
-      <div className="absolute top-[8px] right-[8px]">
-        <Button
-          className="group flex justify-center items-center p-[4px] hover:bg-[var(--color-primary)] transition-[background] duration-300"
-          onClick={(e) => {
-            e.stopPropagation();
-            openActions();
-          }}
-        >
-          <VerticalMoreIcon className="size-[32px] stroke-color-black group-hover:invert" />
-        </Button>
-      </div>
+      <div className="absolute top-[8px] right-[8px]">{renderMoreButton()}</div>
       <div>{children}</div>
     </article>
   );
