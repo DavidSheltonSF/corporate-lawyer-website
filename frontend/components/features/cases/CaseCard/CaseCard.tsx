df157d05fd1f35793050018ef9dbed19ef1b78ba
DavidSheltonSF <david.faria012@gmail.com>
2026-05-19 20:55:38 -0300

    refactor(CaseCard):  use DANGER modal variant for delete modal.

--- frontend/components/features/cases/CaseCard/CaseCard.tsx
+++ frontend/components/features/cases/CaseCard/CaseCard.tsx
@@ -9,6 +9,7 @@ import { DeleteIcon } from '@/components/icons/DeleteIcon';
 import { useModal } from '@/hooks/useModal';
 import { makeCardAction } from '@/components/ui/CardDropdown/makeCardAction';
 import { CardActionType } from '@/components/ui/CardDropdown/types';
+import { ModalVariant } from '@/components/ui/Modal/PrimaryModal';
 
 interface Props {
   caseData: WithId<CaseWithRelations>;
@@ -37,6 +38,7 @@ export function CaseCard({ caseData, deleteCase, refetchCases }: Props) {
     openModal('confirm', {
       title: 'Excluir processo',
       message: 'Tem certeza que deseja excluir este processo? Essa ação não poderá ser desfeita.',
+      variant: ModalVariant.DANGER,
       onConfirm: async () => {
         await deleteCase(id);
         openModal(null);
