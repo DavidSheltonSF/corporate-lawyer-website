69ca9d284969d3f8a293dc624a5dfbff2673160b
DavidSheltonSF <david.faria012@gmail.com>
2026-05-19 20:28:38 -0300

    refactor(ConfirmModal): simplify modal structure and remove unnecessary buttons

--- frontend/components/ui/Modal/ConfirmModal.tsx
+++ frontend/components/ui/Modal/ConfirmModal.tsx
@@ -15,34 +15,14 @@ export function ConfirmModal({ data, close }: Props) {
   return (
     <PrimaryModal
       title={title}
+      onConfirm={onConfirm}
       className={
         'top-[2%] min-lg:top-[10%] left-1/2 translate-x-[-50%] w-[90%] min-lg:w-[400px] h-fit'
       }
       onClose={close}
     >
-      <div className="flex flex-col size-full px-[24px] pb-[24px]">
-        <div className="flex my-[24px] h-full text-center">
-          <h2>{message}</h2>
-        </div>
-        <div className="flex h-fit justify-between items-end w-full ">
-          <Button
-            className="w-[144px] h-[56px] bg-color-white text-color-black font-bold border border-black hover:brightness-95"
-            onClick={() => {
-              close();
-            }}
-          >
-            Cancelar
-          </Button>
-          <Button
-            className="w-[144px] h-[56px] bg-color-red text-color-white font-bold"
-            onClick={() => {
-              onConfirm();
-              close();
-            }}
-          >
-            Confirmar
-          </Button>
-        </div>
+      <div className="flex flex-col size-full px-[24px] p-[24px] text-sm">
+        <h3>{message}</h3>
       </div>
     </PrimaryModal>
   );
