f8156bbe7b02a2b9ce7253157386d531b77f3bf1
DavidSheltonSF <david.faria012@gmail.com>
2026-05-19 15:28:16 -0300

    feat(frontend/components/ui/CardDropdown): add CardActionType

--- frontend/components/ui/CardDropdown/types.ts
+++ frontend/components/ui/CardDropdown/types.ts
@@ -1,5 +1,10 @@
 import { PropsWithClassName } from '../../../types/PropsWithClassName';
 
+export enum CardActionType {
+  EDIT = 'EDIT',
+  DELETE = 'DELETE',
+}
+
 export interface CardAction {
   Icon: React.ComponentType<PropsWithClassName>;
   label: string;
