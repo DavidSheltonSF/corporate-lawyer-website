04222ecb0fb7e8f5a2ad7766c37c352a45dc70d2
DavidSheltonSF <david.faria012@gmail.com>
2026-05-19 00:18:25 -0300

    feat(frontend/components/modals): refactor CaseFilesUploadModal for improved file handling and UI consistency

--- frontend/components/modals/CaseFilesUploadModal.tsx
+++ frontend/components/modals/CaseFilesUploadModal.tsx
@@ -1,99 +1,93 @@
 'use client';
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { PrimaryModal } from '../ui/Modal/PrimaryModal';
 import { uploadCaseFile } from '@/services/cases/uploadCaseFile';
 import { RequestState } from '@/types/RequestState';
-import { DropArea } from '../DropArea';
 import { RequestFeedback } from '../ui/Feedback/RequestFeedback';
 import { handleLogout } from '@/lib/handleLogout';
 import { UnauthorizedError } from '@/errors/UnauthorizedError';
+import { Button } from '../ui/Button/Button';
+import { UploadButton } from '../ui/UploadButton';
+import { DocumentIcon } from '../icons/DocumentIcon';
+import { CloseIcon } from '../icons/CloseIcon';
+import { formatFileSize } from '@/lib/formatFileSize';
 
 interface Props {
   caseId: string;
   close: Function;
-  refetchCase: () => void
+  refetchCase: () => void;
 }
 
 export function CaseFilesUploadModal({ caseId, close, refetchCase }: Props) {
-  const [uploadState, setUploadState] = useState<null | RequestState>(null);
+  const [requestState, setRequestState] = useState<null | RequestState>(null);
+  const [file, setFile] = useState<File | null>(null);
 
   function closeModal() {
-    if (uploadState?.status === 'loading') return;
+    if (requestState?.status === 'loading') return;
     close();
-    setUploadState(null);
+    setRequestState(null);
   }
 
-  async function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+  async function handleUploadFile() {
     try {
-      setUploadState({ status: 'loading' });
-      const file = e.target.files?.[0];
       if (!file) return;
-
       const formData = new FormData();
-
       formData.append('file', file);
-
-      // REMOVE THIS AFTER TESTS
-      // REMOVE THIS AFTER TESTS
-      // REMOVE THIS AFTER TESTS
-      await new Promise((resolve) => setTimeout(resolve, 5000));
-
+      setRequestState({ status: 'loading' });
       await uploadCaseFile(formData, caseId);
+      setRequestState({ status: 'ok', message: `Arquivo '${file?.name}' foi enviado com sucesso` });
       refetchCase();
-      setUploadState({ status: 'ok', message: 'Arquivo adicionado com sucesso!' });
-    } catch (error) {
-      setUploadState({ status: 'error', message: 'Arquivo não adicionado' });
+    } catch (error: any) {
       console.log(error);
+      setRequestState({ status: 'error' });
+
       if (error instanceof UnauthorizedError) {
         handleLogout();
       }
     }
   }
 
-  async function handleDrop(e: React.DragEvent<HTMLLabelElement>) {
-    try {
-      const fileItems = e.dataTransfer.files;
-      const fileItem = fileItems[0];
-      if (!fileItem) return;
-
-      e.preventDefault();
-
-      if (fileItem.type !== 'application/pdf') {
-        setUploadState({ status: 'error', message: 'Apenas PDF(s) são permitidos' });
-        return;
-      }
-
-      if (fileItem.size > 10 * 1000000) {
-        setUploadState({ status: 'error', message: 'Arquivos devem ter no máximo 10mb' });
-        return;
-      }
-
-      setUploadState({ status: 'loading' });
-
-      const formData = new FormData();
-      formData.append('file', fileItem);
-
-      await uploadCaseFile(formData, caseId);
-
-      setUploadState({ status: 'ok', message: 'Arquivo adicionado com sucesso!' });
-    } catch (error) {
-      console.log(error);
-      setUploadState({ status: 'error', message: 'Arquivo não adicionado' });
-    }
+  function renderFileCard() {
+    if (!file) return;
+    return (
+      <article className="relative flex items-start border w-full h-fit p-[16px] gap-[16px] rounded-sm">
+        <Button onClick={() => setFile(null)} className="absolute top-[8px] right-[8px] p-[4px]">
+          <CloseIcon className="size-[16px] " />
+        </Button>
+        <div className="border size-[64px] rounded-sm">
+          <DocumentIcon className="fill-[var(--color-primary-light)]" />
+        </div>
+        <div className="flex flex-col items-start">
+          <p>{file.name}</p>
+          <p>{formatFileSize(file.size)}</p>
+        </div>
+      </article>
+    );
   }
 
   return (
     <PrimaryModal
-      additionalStyles="z-2 fixed top-[25%] left-1/2 translate-x-[-50%] w-[360px] h-[320px] rounded-lg overflow-hidden shadow-[0px_0px__3px_black]"
+      title="Envie um arquivo"
+      additionalStyles="z-2 fixed top-[25%] left-1/2 translate-x-[-50%] w-[440px] h-fit rounded-lg overflow-hidden shadow-[0px_0px__3px_black]"
       closeModal={() => {
         closeModal();
       }}
     >
-      <div className="size-full flex flex-col text-center items-center justify-end p-[4px]">
-        {(uploadState?.status === 'ok' || uploadState?.status === 'error') && (
-          <RequestFeedback requestState={uploadState} />
+      <div className="flex gap-[24px] size-full flex flex-col text-center items-center p-[24px]">
+        {(requestState?.status === 'ok' || requestState?.status === 'error') && (
+          <RequestFeedback requestState={requestState} />
         )}
-        <DropArea uploadState={uploadState} handleChange={handleChange} handleDrop={handleDrop} />
+        <UploadButton setFile={setFile} />
+        {renderFileCard()}
+        <p className="text-start text-size-sm">Apenas PDFs de tamanho máximo de 10MB</p>
+        <div className="flex gap-[24px] ml-auto">
+          <Button onClick={() => closeModal()} variant="secondary" className="px-[16px] py-[4px]">
+            Voltar
+          </Button>
+          <Button onClick={handleUploadFile} variant="primary" className="px-[16px] py-[4px]">
+            Confirmar
+          </Button>
+        </div>
       </div>
     </PrimaryModal>
   );
