2fc2239c40d64488b9defc169d30aae492fa75f0
DavidSheltonSF <david.faria012@gmail.com>
2026-05-19 16:12:05 -0300

    feat(PrimaryModal): add footer with 'Confirmar' and 'Voltar' buttons and their related props

--- frontend/components/ui/Modal/PrimaryModal.tsx
+++ frontend/components/ui/Modal/PrimaryModal.tsx
@@ -7,13 +7,24 @@ import { twMerge } from 'tailwind-merge';
 
 interface Props {
   title?: string;
+  onConfirm?: () => void;
   closeModal: Function;
+  confirmText?: string;
+  closeText?: string;
   children: React.ReactNode;
   additionalStyles: string;
 }
 
 export function PrimaryModal(props: Props) {
-  const { closeModal, children, title, additionalStyles } = props;
+  const {
+    onConfirm,
+    closeModal,
+    confirmText = 'Confirmar',
+    closeText = 'Voltar',
+    children,
+    title,
+    additionalStyles,
+  } = props;
   const modalRef = useRef<HTMLDivElement>(null);
 
   function close() {
@@ -35,6 +46,21 @@ export function PrimaryModal(props: Props) {
   const baseStyles =
     'flex flex-col z-20 fixed bg-color-white fade-in-animation-fast rounded-lg overflow-hidden shadow-[0px_0px__3px_black] text-color-black';
 
+  function renderFooterButtons() {
+    return (
+      <div className="flex justify-end items-center gap-[16px]">
+        <Button variant="secondary" onClick={() => closeModal()}>
+          {closeText}
+        </Button>
+        {onConfirm && (
+          <Button variant="primary" onClick={onConfirm}>
+            {confirmText}
+          </Button>
+        )}
+      </div>
+    );
+  }
+
   return (
     <div ref={modalRef} className={twMerge(baseStyles, additionalStyles)}>
       <div className="flex items-center w-full  border-divider bg-color-white px-[24px] py-[8px]">
@@ -48,7 +74,8 @@ export function PrimaryModal(props: Props) {
           <CloseIcon className="w-[32px] h-[32px]" />
         </Button>
       </div>
-      <div className="size-full overflow-y-auto">{children}</div>
+      <div className="size-full overflow-y-auto border-divider">{children}</div>
+      <footer className="py-[16px] px-[24px]">{renderFooterButtons()}</footer>
     </div>
   );
 }
