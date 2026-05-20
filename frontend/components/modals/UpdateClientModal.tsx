aaba1a31a4dae4646915acd698fa8fb937b70a61
DavidSheltonSF <david.faria012@gmail.com>
2026-05-19 22:28:40 -0300

    chore(UpdateClientModal): remove unnecessary styles

--- frontend/components/modals/UpdateClientModal.tsx
+++ frontend/components/modals/UpdateClientModal.tsx
@@ -72,7 +72,7 @@ export function UpdateClientModal({ data, close }: Props) {
     <PrimaryModal
     title='Editar cliente'
       className={
-        'fixed z-99999999999 top-[2%] min-lg:top-[10%] left-1/2 translate-x-[-50%] w-[90%] min-lg:w-[678px] h-fit rounded-lg overflow-hidden shadow-[0px_0px__3px_black] text-color-black'
+        'top-[2%] min-lg:top-[10%] left-1/2 translate-x-[-50%] w-[90%] min-lg:w-[678px] h-fit'
       }
       onClose={close}
     >
