a4b39fdf067165d57324c33a76b9b37a820b0a35
DavidSheltonSF <david.faria012@gmail.com>
2026-05-20 12:31:27 -0300

    refactor(Input): enhance props and style

--- frontend/components/ui/Input/Input.tsx
+++ frontend/components/ui/Input/Input.tsx
@@ -1,36 +1,18 @@
+import { PropsWithClassName } from '@/types/PropsWithClassName';
+import { twMerge } from 'tailwind-merge';
+
 interface Props {
   id: string;
   name: string;
+  placeholder?: string;
+  required?: boolean;
+  type?: string;
   defaultValue?: string;
-  width: string;
-  height: string;
-  paddingX: string;
-  paddingY: string;
 }
 
-export function Input(props: Props) {
-  const {
-    id,
-    name,
-    defaultValue,
-    width = 'auto',
-    height = '40px',
-    paddingX = '8px',
-    paddingY = '8px',
-  } = props;
-  return (
-    <input
-      className="border h-[40px] w-full rounded-sm px-[8px]"
-      style={{
-        width,
-        height,
-        paddingInline: paddingX,
-        paddingBlock: paddingY,
-      }}
-      id={id}
-      name={name}
-      type="text"
-      defaultValue={defaultValue}
-    />
-  );
+export function Input(props: PropsWithClassName<Props>) {
+  const { className } = props;
+
+  const baseStyles = 'border p-[8px] w-full rounded-sm p';
+  return <input className={twMerge(baseStyles, className)} {...props} />;
 }
