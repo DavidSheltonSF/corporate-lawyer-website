ca0baada6b7e38ab6e3809c1d1b02e5836471ffd
DavidSheltonSF <david.faria012@gmail.com>
2026-05-21 18:29:35 -0300

    refactor(AddIcon): update to use IconProps

--- frontend/components/icons/AddIcon.tsx
+++ frontend/components/icons/AddIcon.tsx
@@ -1,11 +1,10 @@
-import { PropsWithClassName } from '@/types/PropsWithClassName';
 import { Icon } from './Icon';
+import { IconProps } from './types';
 
-export function AddIcon(props: PropsWithClassName) {
+export function AddIcon(props: IconProps) {
   return (
     <Icon {...props} fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
       <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
     </Icon>
   );
 }
-
