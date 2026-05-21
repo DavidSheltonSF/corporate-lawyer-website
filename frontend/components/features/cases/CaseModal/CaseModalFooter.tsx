b70e027843ccb135d631b2d541f3fd1ad2faaed4
DavidSheltonSF <david.faria012@gmail.com>
2026-05-21 16:20:56 -0300

    refactor(CaseModal.Footer): wrap the 'Arquivos' button with Tooltip for better UX

--- frontend/components/features/cases/CaseModal/CaseModalFooter.tsx
+++ frontend/components/features/cases/CaseModal/CaseModalFooter.tsx
@@ -1,19 +1,19 @@
-import { DocumentIcon } from "@/components/icons/DocumentIcon";
-import { Button } from "@/components/ui/Button/Button";
+import { DocumentIcon } from '@/components/icons/DocumentIcon';
+import { Button } from '@/components/ui/Button/Button';
+import { Tooltip } from '@/components/ui/Toltip/Tooltip';
 
 interface Props {
-  openFilesModal: () => void
+  openFilesModal: () => void;
 }
 
-export function CaseModalFooter({openFilesModal}: Props) {
+export function CaseModalFooter({ openFilesModal }: Props) {
   return (
     <footer className="py-[8px] px-[24px]">
-      <Button
-        onClick={openFilesModal}
-        className="bg-color-white hover:brightness-95 p-[8px]"
-      >
-        <DocumentIcon label="Arquivos" className="size-[24px]" />
-      </Button>
+      <Tooltip label="Arquivos">
+        <Button onClick={openFilesModal} className="bg-color-white hover:brightness-95 p-[8px]">
+          <DocumentIcon className="size-[24px]" />
+        </Button>
+      </Tooltip>
     </footer>
   );
 }
