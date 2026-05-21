a8a165a39b8e8f6203b164d3c67e3c539be2c9c6
DavidSheltonSF <david.faria012@gmail.com>
2026-05-21 16:39:18 -0300

    refactor(CarouselButton): replace children prop with Icon component for improved structure

--- frontend/components/ui/Carousel/CarouselButton.tsx
+++ frontend/components/ui/Carousel/CarouselButton.tsx
@@ -1,20 +1,21 @@
 'use client';
+import { IconProps } from '@/components/icons/types';
 import { ReactEventHandler, ReactNode } from 'react';
 
 interface Props {
-  children: ReactNode;
+  Icon: React.ComponentType<IconProps>
   inert?: boolean;
   onClickHandler: ReactEventHandler;
 }
 
-export function CarouselButton({ onClickHandler, inert, children }: Props) {
+export function CarouselButton({ onClickHandler, inert, Icon }: Props) {
   return (
     <button
       inert={inert}
-      className={`rounded-md hidden md:block border-color-secondary bg-black/50 hover:bg-white transition-[background-color] duration-300 cursor-pointer inert:opacity-50 inert:saturate-0 size-[56px] md:size-[80px]`}
+      className={`rounded-md hidden md:block border border-color-primary-light bg-black/50 hover:bg-white transition-[background-color] duration-300 cursor-pointer inert:opacity-50 inert:saturate-0 size-[56px] md:size-[80px]`}
       onClick={onClickHandler}
     >
-      <div>{children}</div>
+      <Icon/>
     </button>
   );
 }
