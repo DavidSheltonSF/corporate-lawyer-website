f0de2f53f0d9be21d4782bab1e7b6b0873bbf317
DavidSheltonSF <david.faria012@gmail.com>
2026-05-21 13:44:28 -0300

    refactor(Tooltip): update TooltipContainer to use span for label and remove TooltipLabel component for simplicity

--- frontend/components/TooltipContainer.tsx
+++ frontend/components/TooltipContainer.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from 'react';
-import { TooltipLabel } from './TooltipLabel';
+import { twMerge } from 'tailwind-merge';
 
 interface Props {
   label: string;
@@ -8,10 +8,13 @@ interface Props {
   children: ReactNode;
 }
 export function TooltipContainer({ label, labelClassName, children }: Props) {
+  const baseStyles =
+    'absolute top-[-100%] left-1/2 -translate-x-1/2 hidden z-9999 group-hover:block fade-in-animation w-max p-[4px] rounded-md';
+
   return (
     <div className={'relative group w-fit h-fit'}>
       {children}
-      <TooltipLabel className={labelClassName}>{label}</TooltipLabel>
+      <span className={twMerge(baseStyles, labelClassName)}>{children}</span>{' '}
     </div>
   );
 }
