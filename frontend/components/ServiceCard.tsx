d226597b0fc7612ef70f5bf9b1680dce943aee97
DavidSheltonSF <david.faria012@gmail.com>
2026-05-21 16:27:04 -0300

    refactor(ServoceCard): remove tooltip for simplicity

--- frontend/components/ServiceCard.tsx
+++ frontend/components/ServiceCard.tsx
@@ -1,6 +1,5 @@
 import { IconCircle } from './IconCircle';
 import { ServiceModalProps } from '../types/ServiceModalProps';
-import { Tooltip } from './ui/Toltip/Tooltip';
 import { WithId } from '@/types/WithId';
 
 interface Props {
@@ -13,20 +12,7 @@ export function ServiceCard({ serviceDetails }: Props) {
   return (
     <article className="flex flex-col w-full h-[440px] lg:h-auto lg:w-[80%] shrink-0 group">
       <header className="flex justify-center">
-        <Tooltip
-          label={`Serviços - ${title}`}
-          tooltipLabelProps={{
-            position: {
-              bottom: '120%',
-              left: '50%',
-              translateX: '-50%',
-            },
-            fontSize: '24px',
-            color: 'var(--secondary-color)',
-          }}
-        >
           <IconCircle serviceAreaId={id} additionalStyles="size-[116px]" />
-        </Tooltip>
       </header>
       <main className="flex flex-col items-center text-center gap-[16px]">
         <h1 className="font-bold mt-[16px]">{title}</h1>
