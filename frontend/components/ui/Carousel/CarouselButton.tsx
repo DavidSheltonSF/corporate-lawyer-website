9483f8a5b3c10dd6bd918f1a1ed3dcc62d94fe05
DavidSheltonSF <david.faria012@gmail.com>
2026-05-21 16:45:43 -0300

    refactor(CarouselButton): replace button element with Button component for consistency and enhance styles

--- frontend/components/ui/Carousel/CarouselButton.tsx
+++ frontend/components/ui/Carousel/CarouselButton.tsx
@@ -1,21 +1,22 @@
 'use client';
 import { IconProps } from '@/components/icons/types';
-import { ReactEventHandler, ReactNode } from 'react';
+import { ReactEventHandler } from 'react';
+import { Button } from '../Button/Button';
 
 interface Props {
-  Icon: React.ComponentType<IconProps>
+  Icon: React.ComponentType<IconProps>;
   inert?: boolean;
   onClickHandler: ReactEventHandler;
 }
 
 export function CarouselButton({ onClickHandler, inert, Icon }: Props) {
   return (
-    <button
+    <Button
       inert={inert}
-      className={`rounded-md hidden md:block border border-color-primary-light bg-black/50 hover:bg-white transition-[background-color] duration-300 cursor-pointer inert:opacity-50 inert:saturate-0 size-[56px] md:size-[80px]`}
+      className={`rounded-md hidden md:block border border-color-primary-light  hover:bg-[var(--color-white)] transition-[background-color] duration-300  inert:opacity-50 inert:saturate-0 size-[56px] md:size-[80px]`}
       onClick={onClickHandler}
     >
-      <Icon/>
-    </button>
+      <Icon className="stroke-[var(--color-primary-light)]" />
+    </Button>
   );
 }
