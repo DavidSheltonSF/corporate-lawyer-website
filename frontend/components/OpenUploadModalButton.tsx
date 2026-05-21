826a1f9bf9a7aa975e765e83a4ccac16532ee1dd
DavidSheltonSF <david.faria012@gmail.com>
2026-05-20 23:30:16 -0300

    refactor(OpenUploadModalButton): enhance structure by merging styles and adding className prop

--- frontend/components/OpenUploadModalButton.tsx
+++ frontend/components/OpenUploadModalButton.tsx
@@ -1,19 +1,27 @@
 'use client';
+import { PropsWithClassName } from '@/types/PropsWithClassName';
 import { UploadIcon } from './icons/UploadIcon';
 import { Button } from './ui/Button/Button';
+import { twMerge } from 'tailwind-merge';
 
 interface Props {
   handleClick: Function;
   disabled?: boolean;
 }
-export function OpenUploadModalButton({ handleClick, disabled }: Props) {
+export function OpenUploadModalButton({
+  handleClick,
+  disabled,
+  className,
+}: PropsWithClassName<Props>) {
+  const baseStyles =
+    'flex items-center px-[16px] py-[8px] gap-[8px] bg-color-white hover:brightness-95 border';
   return (
     <Button
-      className="flex items-center px-[16px] py-[8px] gap-[8px] bg-color-white hover:brightness-95 border"
+      className={twMerge(baseStyles, className)}
       onClick={() => handleClick()}
       disabled={disabled}
     >
-      <UploadIcon className='size-[24px]'/>
+      <UploadIcon className="size-[24px]" />
       <span className="hidden min-md:block">Anexar arquivo</span>
     </Button>
   );
