cdac4b4e939c4ce8d0ac6dbf655060ea30b5d5db
DavidSheltonSF <david.faria012@gmail.com>
2026-05-20 23:14:57 -0300

    refactor(ModalFeedback): improve structure and semantics by updating heading elements

--- frontend/components/ui/Feedback/ModalFeedback.tsx
+++ frontend/components/ui/Feedback/ModalFeedback.tsx
@@ -5,9 +5,9 @@ interface Props {
 
 export function ModalFeedback({ title, message }: Props) {
   return (
-    <div className="flex flex-col items-center size-ful pt-[80px] px-[24px] text-center gap-[16px]">
-      <h1>{title}</h1>
-      <h3>{message}</h3>
+    <div className="flex flex-col  p-[24px] text-center gap-[16px]">
+      <h2>{title}</h2>
+      <p>{message}</p>
     </div>
   );
 }
