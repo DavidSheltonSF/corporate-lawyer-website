f63d0eebefa2fc6e8d4a2d13d51d3e68d4a8ffd4
DavidSheltonSF <david.faria012@gmail.com>
2026-05-21 16:16:53 -0300

    refactor(Tooltip): remove unused className prop and clean up tooltip positioning styles

--- frontend/components/ui/Toltip/Tooltip.tsx
+++ frontend/components/ui/Toltip/Tooltip.tsx
@@ -3,18 +3,17 @@ import { twMerge } from 'tailwind-merge';
 
 interface Props {
   label: string;
-  className?: string;
   labelClassName?: string;
   children: ReactNode;
 }
 export function Tooltip({ label, labelClassName, children }: Props) {
   const baseStyles =
-    'absolute top-[-100%] left-1/2 -translate-x-1/2 hidden z-9999 group-hover:block fade-in-animation w-max p-[4px] rounded-md';
+    'absolute left-1/2 -translate-x-1/2 hidden z-9999 group-hover:block fade-in-animation w-max p-[4px] rounded-md';
 
   return (
     <div className={'relative group w-fit h-fit'}>
       {children}
-      <span className={twMerge(baseStyles, labelClassName)}>{label}</span>{' '}
+      <span className={twMerge(baseStyles, labelClassName)}>{label}</span>
     </div>
   );
 }
