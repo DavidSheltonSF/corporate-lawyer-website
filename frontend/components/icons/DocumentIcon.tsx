863eb627c3a7195f37e5ce3fc15e537510af50fa
DavidSheltonSF <david.faria012@gmail.com>
2026-05-20 22:01:04 -0300

    fix(DocumentIcon): specify IconProps in PropsWithClassName for type safety

--- frontend/components/icons/DocumentIcon.tsx
+++ frontend/components/icons/DocumentIcon.tsx
@@ -1,7 +1,8 @@
 import { PropsWithClassName } from '@/types/PropsWithClassName';
 import { Icon } from './Icon';
+import { IconProps } from './types';
 
-export function DocumentIcon(props: PropsWithClassName) {
+export function DocumentIcon(props: PropsWithClassName<IconProps>) {
   return (
     <Icon {...props} viewBox="0 0 24 24">
       <path d="M5.625 1.5c-1.036 0-1.875.84-1.875 1.875v17.25c0 1.035.84 1.875 1.875 1.875h12.75c1.035 0 1.875-.84 1.875-1.875V12.75A3.75 3.75 0 0 0 16.5 9h-1.875a1.875 1.875 0 0 1-1.875-1.875V5.25A3.75 3.75 0 0 0 9 1.5H5.625Z" />
