8850a8584909303026db2b3e38c7017dc84e5672
DavidSheltonSF <david.faria012@gmail.com>
2026-05-21 19:40:05 -0300

    refactor(backend/configRouter): integrate deadline routes

--- backend/src/config/configRouter.ts
+++ backend/src/config/configRouter.ts
@@ -8,18 +8,22 @@ import { testRoutes } from '../routes/testRoutes';
 import { authRoutes } from '../routes/authRoutes';
 import { notificationsRoutes } from '../routes/notificationsRoutes';
 import { makeNotificationController } from '../factories/controllers/makeNotificationController';
+import { makeDeadlineController } from '../factories/controllers/makeDeadlineController';
+import { deadlineRoutes } from '../routes/deadlineRoutes';
 
 export function configRouter(app: Application) {
   const authController = makeAuthController();
   const caseController = makeCaseController();
   const userController = makeUserController();
   const notificationController = makeNotificationController();
+  const deadlineController = makeDeadlineController();
 
   const router = Router();
   authRoutes(router, authController);
   casesRoutes(router, caseController);
   usersRoutes(router, userController);
-  notificationsRoutes(router, notificationController)
+  notificationsRoutes(router, notificationController);
+  deadlineRoutes(router, deadlineController);
   testRoutes(router);
   app.use(router);
 }
