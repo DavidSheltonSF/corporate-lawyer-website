2998760d762ea40514eee6455424e9217d8a9848
DavidSheltonSF <david.faria012@gmail.com>
2026-05-20 23:54:04 -0300

    style(BaseModal): adjust modal position for improved visibility

--- frontend/components/ui/Modal/BaseModal.tsx
+++ frontend/components/ui/Modal/BaseModal.tsx
@@ -68,7 +68,7 @@ export function BaseModal(props: PropsWithClassName<Props>) {
     );
   }
 
-  const positionStyles = 'fixed top-[10vh] left-1/2 translate-x-[-50%] z-20';
+  const positionStyles = 'fixed top-[4vh] left-1/2 translate-x-[-50%] z-20';
   const sizeStyles = 'h-fit';
   const baseStyles =
     'flex flex-col bg-color-white fade-in-animation-fast rounded-lg overflow-hidden shadow-[0px_0px__3px_black] text-color-black';
