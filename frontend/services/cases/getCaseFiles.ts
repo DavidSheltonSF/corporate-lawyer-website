1b5d531291f9fec4d6d641869b6f144f3374d04f
DavidSheltonSF <david.faria012@gmail.com>
2026-05-22 20:48:53 -0300

    chore(frontend/services): update getCaseFiles t return a list of CaseFile with id

--- frontend/services/cases/getCaseFiles.ts
+++ frontend/services/cases/getCaseFiles.ts
@@ -1,8 +1,9 @@
 import { API_URL } from '@/config/api';
 import { CaseFile } from '@/types/CaseFile';
 import { apiFetch } from '../apiFetch';
+import { WithId } from '@/types/WithId';
 
-export async function getCaseFiles(id: string): Promise<CaseFile[]> {
+export async function getCaseFiles(id: string): Promise<WithId<CaseFile>[]> {
   const response = await apiFetch(`${API_URL}/cases/${id}/files`);
   const json = await response.json();
   return json.data;
