91292a5f98bcfb8fd5376f85d20ffcab39fe50ab
DavidSheltonSF <david.faria012@gmail.com>
2026-05-22 20:48:00 -0300

    chore(frontend/services): update routes

--- frontend/services/cases/getCaseFiles.ts
+++ frontend/services/cases/getCaseFiles.ts
@@ -3,7 +3,7 @@ import { CaseFile } from '@/types/CaseFile';
 import { apiFetch } from '../apiFetch';
 
 export async function getCaseFiles(id: string): Promise<CaseFile[]> {
-  const response = await apiFetch(`${API_URL}/my/cases/${id}/caseFiles`);
+  const response = await apiFetch(`${API_URL}/cases/${id}/files`);
   const json = await response.json();
   return json.data;
 }
--- frontend/services/cases/uploadCaseFile.ts
+++ frontend/services/cases/uploadCaseFile.ts
@@ -1,7 +1,7 @@
 import { apiFetch } from '../apiFetch';
 
 export async function uploadCaseFile(formData: FormData, id: string) {
-  await apiFetch(`/api/my/cases/${id}/caseFiles`, {
+  await apiFetch(`/api/my/cases/${id}/files`, {
     method: 'POST',
     body: formData,
   });
