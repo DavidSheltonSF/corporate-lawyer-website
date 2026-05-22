e3761d60a9e4d578e4f6f4bbfde7c8eed61bdc0d
DavidSheltonSF <david.faria012@gmail.com>
2026-05-22 20:42:41 -0300

    chore(frontend/services): rename getMyCaseFilesByCaseID to getCaseFIles for simpliciy

--- frontend/services/cases/getCaseFiles.ts
+++ frontend/services/cases/getCaseFiles.ts
@@ -2,7 +2,7 @@ import { API_URL } from '@/config/api';
 import { CaseFile } from '@/types/CaseFile';
 import { apiFetch } from '../apiFetch';
 
-export async function getMyCaseFilesByCaseId(id: string): Promise<CaseFile[]> {
+export async function getCaseFiles(id: string): Promise<CaseFile[]> {
   const response = await apiFetch(`${API_URL}/my/cases/${id}/caseFiles`);
   const json = await response.json();
   return json.data;
