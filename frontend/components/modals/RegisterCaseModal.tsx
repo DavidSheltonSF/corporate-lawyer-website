ff587a9f408087817b1bdb6a40170691f09654b8
DavidSheltonSF <david.faria012@gmail.com>
2026-05-22 16:46:56 -0300

    chore(RegisterCaseModal): remove unnecessary styles

--- frontend/components/modals/RegisterCaseModal.tsx
+++ frontend/components/modals/RegisterCaseModal.tsx
@@ -57,7 +57,7 @@ export function RegisterCaseModal({ isOpen, close, selectedClientId }: Props) {
     isOpen && (
       <BaseModal
         className={
-          'fixed z-99999999999 top-[2%] min-lg:top-[10%] left-1/2 translate-x-[-50%] w-[90%] min-lg:w-[678px] h-[90%] min-lg:h-fit rounded-lg overflow-hidden shadow-[0px_0px__3px_black] text-color-black'
+          'w-[90%] min-lg:w-[678px] h-[90%] min-lg:h-fit'
         }
         onClose={() => {
           close();
