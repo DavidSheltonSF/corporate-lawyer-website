88388e695219a8725b23d989e37b125fe72b27b0
DavidSheltonSF <david.faria012@gmail.com>
2026-05-23 19:36:48 -0300

    style(FormModal): enhance layout by adjusting height

--- frontend/components/ui/FormModal/FormModal.tsx
+++ frontend/components/ui/FormModal/FormModal.tsx
@@ -13,12 +13,10 @@ export function FormModal({ title, formId, close, children }: PropsWithChildren<
     <BaseModal
       title={title}
       formId={formId}
-      className={'h-[70vh] min-lg:h-fit w-[90%] min-md:w-[60%] min-lg:w-[680px]'}
+      className={'h-fit w-[90%] min-md:w-[60%] min-lg:w-[680px]'}
       onClose={close}
     >
-      <div className="flex flex-col h-[500px] min-lg:h-fit p-[24px] overflow-visible">
-        {children}
-      </div>
+      <div className="flex flex-col h-[500px] min-lg:h-fit overflow-visible">{children}</div>
     </BaseModal>
   );
 }
