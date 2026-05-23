ec40c90f29629f844c1de05828d620ec48a9beed
DavidSheltonSF <david.faria012@gmail.com>
2026-05-23 00:53:26 -0300

    refactor(DeadlineCard): enhnce card structure and layout by using RemainingDaysBadge and PriorityBadge, anso, improve card styles

--- frontend/components/features/deadlines/DeadLineCard/DeadlineCard.tsx
+++ frontend/components/features/deadlines/DeadLineCard/DeadlineCard.tsx
@@ -1,35 +1,32 @@
 import { Card } from '@/components/ui/Card/Card';
 import { formatDate } from '@/lib/formatDate';
 import { Deadline } from '@/types/Deadline';
-import { DeadlineStatus } from '@/types/DeadlineStatus';
 import { WithId } from '@/types/WithId';
+import { RemainingDaysBadge } from '../badges/RemainingDaysBadge';
+import { PriorityBadge } from '../badges/PriorityBadge';
 
 interface Props {
   deadline: WithId<Deadline>;
 }
 
 export function DeadlineCard({ deadline }: Props) {
-  function renderDeadlineStatus(deadline: WithId<Deadline>) {
-    const isExpired = deadline.status === DeadlineStatus.VENCIDO;
-    if (isExpired) {
-      return <p>Expirou</p>;
-    }
-
-    if (deadline.remainingDays === 0) {
-      return <p>Expira hoje</p>;
-    }
-
-    return <p>Expira em {deadline.remainingDays}</p>;
-  }
-
   return (
-    <Card key={deadline.id} className="border border-black w-full p-[24px]">
-      <p>{deadline.type}</p>
-      {renderDeadlineStatus(deadline)}
-      <p>
-        {formatDate(deadline.startDate)} - {formatDate(deadline.dueDate)}
-      </p>
-      <p>{deadline.priority} prioridate</p>
+    <Card key={deadline.id} className="shadow-soft w-full p-[24px]">
+      <div className="flex justify-between  items-end gap-[16px]">
+        <div className="flex flex-1 flex-col gap-[8px]">
+          <h3 className="">{deadline.type}</h3>
+          <RemainingDaysBadge remainingDays={deadline.remainingDays} />
+        </div>
+        <div className="flex-1 ">
+          <p>
+            {formatDate(deadline.startDate)} - {formatDate(deadline.dueDate)}
+          </p>
+          <p className="text-muted">Prazo final</p>
+        </div>
+        <div className="flex flex-1  flex-col gap-[8px]">
+          <PriorityBadge priority={deadline.priority} />
+        </div>
+      </div>
     </Card>
   );
 }
