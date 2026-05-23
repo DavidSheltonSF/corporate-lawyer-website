cc29093dde2e9afbb92bfcc7d0d584dbda537c31
DavidSheltonSF <david.faria012@gmail.com>
2026-05-23 19:36:12 -0300

    style(BaseModal.Footer): fix responsiveness

--- frontend/components/ui/Modal/BaseModalFooter.tsx
+++ frontend/components/ui/Modal/BaseModalFooter.tsx
@@ -16,14 +16,14 @@ export function BaseModalFooter(props: Props) {
   const showConfirmButton = onConfirm || formId;
 
   return (
-    <footer className="hidden min-lg:block py-[16px] px-[24px]">
+    <footer className="flex-1 py-[16px] px-[24px]">
       <div className="flex flex-col min-lg:flex-row justify-end items-center gap-[8px] min-lg:gap-[16px]">
-        <Button className="h-fit" variant={ButtonVariant.SECONDARY} onClick={() => onClose()}>
+        <Button className="hidden min-lg:block h-fit" variant={ButtonVariant.SECONDARY} onClick={() => onClose()}>
           {closeText}
         </Button>
         {showConfirmButton && (
           <Button
-            className="h-fit"
+            className="w-full min-lg:w-fit h-fit"
             form={formId}
             type={formId ? 'submit' : 'button'}
             variant={confirmButtonVariant}
