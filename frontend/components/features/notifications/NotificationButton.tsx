380a493cc4bd41fe9a0308b7f8d16a545b3d5fff
DavidSheltonSF <david.faria012@gmail.com>
2026-05-22 23:40:50 -0300

    fix(NotificationButton):  replace button tag with Button component for consistency

--- frontend/components/features/notifications/NotificationButton.tsx
+++ frontend/components/features/notifications/NotificationButton.tsx
@@ -1,21 +1,21 @@
 'use client';
-
 import { Activity } from 'react';
 import { NotificationIcon } from '../../icons/NotificationIcon';
 import { useNotificationsModalContext } from '@/hooks/useNotificationsModalContext';
+import { Button } from '@/components/ui/Button/Button';
 
 export function NotificationButton() {
   const { setIsOpen, unreadCount } = useNotificationsModalContext();
 
   return (
-    <button
+    <Button
       onClick={() => setIsOpen(true)}
-      className="flex justify-center items-center fixed bottom-[112px] right-[24px] size-[64px] rounded-full bg-color-primary-light cursor-pointer hover:brightness-110 inner-shadow-soft-white"
+      className="flex justify-center items-center fixed bottom-[112px] right-[24px] size-[64px] rounded-full bg-color-primary-light inner-shadow-soft-white p-0"
     >
-      <NotificationIcon className="size-[50%] stroke-color-white" />
+      <NotificationIcon className="size-[65%] stroke-color-white" />
       <Activity mode={unreadCount > 0 ? 'visible' : 'hidden'}>
         <div className="absolute size-[80%] rounded-full bg-color-primary-light top-1/2 translate-y-[-50%] animate-ping"></div>
       </Activity>
-    </button>
+    </Button>
   );
 }
