1c75790e53e53880791be1094bbed96ad845f1fa
DavidSheltonSF <david.faria012@gmail.com>
2026-05-25 19:03:23 -0300

    refactor(frontend/actions):  update Login to return ActionResponse

--- frontend/actions/login.ts
+++ frontend/actions/login.ts
@@ -1,10 +1,20 @@
 'use server';
 import { authenticateUser } from '@/services/users/authenticateUser';
+import { ActionResponse } from '@/types/ActionResponse';
 import { cookies } from 'next/headers';
 
-export async function login(formData: FormData): Promise<void> {
-  const token = await authenticateUser(formData);
+export async function login(formData: FormData): Promise<ActionResponse> {
+  const response = await authenticateUser(formData);
+
+  if (!response.success) {
+    return response;
+  }
+
+  const token = response.data;
+
   const cookiesStore = await cookies();
   //cookiesStore.set('authToken', token, { httpOnly: true, secure: true, sameSite: 'none' });
   cookiesStore.set('authToken', token, { httpOnly: true });
+
+  return response;
 }
