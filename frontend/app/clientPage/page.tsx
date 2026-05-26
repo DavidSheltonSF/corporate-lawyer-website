b72932ded79664eb5867d161692f25b709ee016d
DavidSheltonSF <david.faria012@gmail.com>
2026-05-26 19:31:51 -0300

    refactor(forntend/clientPage): enhance error handling

--- frontend/app/clientPage/page.tsx
+++ frontend/app/clientPage/page.tsx
@@ -3,38 +3,43 @@ import { ClientView } from '@/components/views/ClientView';
 import { LawyerView } from '@/components/views/LawyerView';
 import { AuthenticatedUserProvider } from '@/contexts/AuthenticatedUserProvider';
 import { getMe } from '@/services/users/getMe';
-import { User } from '@/types/User';
-import { WithId } from '@/types/WithId';
 import { redirect } from 'next/navigation';
 import { Notifications } from '@/components/features/notifications/Notifications';
 import { ModalProvider } from '@/contexts/modals/ModalProvider';
 import { ModalRenderer } from '@/components/renderer/ModalRenderer';
 
 export default async function ClientPage() {
-  let user: WithId<User> | null = null;
-
   try {
-    user = await getMe();
+    const response = await getMe();
+    if (!response.success) {
+      redirect('/clientPageLogin');
+    }
+
+    if (!response.data) {
+      throw new Error('Response was successful but user data was not provided');
+    }
+
+    const user = response.data;
+
+    return (
+      <AuthenticatedUserProvider userData={user}>
+        <ModalProvider>
+          <ModalRenderer />
+          <div className="bg-color-black min-h-[100vh]">
+            <HeroSection
+              background="var(--blue-gradient)"
+              title={`Bem vindo(a) ${user?.role === 'lawyer' ? 'Dra' : ''} ${user?.firstName}`}
+              additionalStyles="h-[280px]"
+            />
+            {user.role === 'lawyer' ? <LawyerView /> : <ClientView />}
+
+            <Notifications />
+          </div>
+        </ModalProvider>
+      </AuthenticatedUserProvider>
+    );
   } catch (error) {
     console.log(error);
     redirect('/clientPageLogin');
   }
-
-  return (
-    <AuthenticatedUserProvider userData={user}>
-      <ModalProvider>
-        <ModalRenderer />
-        <div className="bg-color-black min-h-[100vh]">
-          <HeroSection
-            background="var(--blue-gradient)"
-            title={`Bem vindo(a) ${user?.role === 'lawyer' ? 'Dra' : ''} ${user?.firstName}`}
-            additionalStyles="h-[280px]"
-          />
-          {user.role === 'lawyer' ? <LawyerView /> : <ClientView />}
-
-          <Notifications />
-        </div>
-      </ModalProvider>
-    </AuthenticatedUserProvider>
-  );
 }
