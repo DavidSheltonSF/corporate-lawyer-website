9938c878b66ca94d83b383fca62d04f6293f27e9
DavidSheltonSF <david.faria012@gmail.com>
2026-05-26 19:29:55 -0300

    refactor(frontend/services/getMe): update to return an ActionResponse of a user with id or null

--- frontend/services/users/getMe.ts
+++ frontend/services/users/getMe.ts
@@ -2,9 +2,15 @@ import { API_URL } from '@/config/api';
 import { User } from '@/types/User';
 import { WithId } from '@/types/WithId';
 import { apiFetch } from '../apiFetch';
+import { ActionResponse } from '@/types/ActionResponse';
 
-export async function getMe(): Promise<WithId<User>> {
+export async function getMe(): Promise<ActionResponse<WithId<User | null>>> {
   const response = await apiFetch(`${API_URL}/me`);
   const json = await response.json();
-  return json.data;
+
+  if (!response.ok) {
+    return { success: false, message: json.message, code: json.code };
+  }
+
+  return { success: true, data: json.data };
 }
