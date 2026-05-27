d82ade7e8683b609ae3c275c557a054c8a56307d
DavidSheltonSF <david.faria012@gmail.com>
2026-05-27 20:47:34 -0300

    refactor(frontend/CaseSearch): enhance request and error handling

--- frontend/components/features/cases/CaseSearch.tsx
+++ frontend/components/features/cases/CaseSearch.tsx
@@ -13,16 +13,18 @@ import { useAuthenticatedUserContext } from '@/hooks/useAuthenticatedUserContext
 import { MissingContextError } from '@/errors/MissingContextError';
 import { UserRole } from '@/types/UserRole';
 import { getCases } from '@/services/cases/getCases';
-import { UnauthorizedError } from '@/errors/UnauthorizedError';
 import { handleLogout } from '@/lib/handleLogout';
+import { RequestState } from '@/types/RequestState';
+import { Page } from '@/types/Page';
 
 export default function CaseSearch() {
   const [query, setQuery] = useState('');
   const [statusFilder, setStatusFilter] = useState<CaseStatusEnum | null>(null);
   const [page, setPage] = useState(1);
   const [totalPage, setTotalPage] = useState(0);
-  const [cases, setCases] = useState<WithId<CaseWithRelations>[]>([]);
-  const [casesLoading, setCasesLoading] = useState(false);
+  const [requestState, setRequestState] = useState<RequestState<Page<WithId<CaseWithRelations>>>>({
+    status: 'idle',
+  });
 
   const authUserContext = useAuthenticatedUserContext();
   if (!authUserContext) {
@@ -33,12 +35,12 @@ export default function CaseSearch() {
 
   async function fetchCases() {
     try {
-      setCasesLoading(true);
-      let casesPage = null;
+      setRequestState({ status: 'loading' });
+      let response = null;
 
       switch (userData.role) {
         case UserRole.lawyer:
-          casesPage = await getCases({
+          response = await getCases({
             page,
             limit: 4,
             query,
@@ -47,34 +49,49 @@ export default function CaseSearch() {
           break;
 
         case UserRole.client:
-          casesPage = await getMyCases({
+          response = await getMyCases({
             page,
             limit: 4,
             query,
             status: statusFilder || '',
           });
           break;
-
-        default:
-          break;
       }
 
-      const casesData = casesPage?.data;
-      setTotalPage(casesPage?.meta.totalPages || 0);
+      if (!response) {
+        throw new Error('Invalid role');
+      }
 
-      setCases(casesData || []);
-      setCasesLoading(false);
+      if (!response.success) {
+        const { message, code, details } = response;
+        return setRequestState({
+          status: 'error',
+          message: message || 'Unespected error',
+          code,
+          details,
+        });
+      }
+
+      const { data } = response;
+
+      setRequestState({ status: 'ok', data: response.data });
+      setPage(data?.meta.currentPage || 1);
+      setTotalPage(data?.meta.totalPages || 1);
     } catch (error: any) {
       console.log(error);
-      if (error instanceof UnauthorizedError) {
-        handleLogout();
-      }
+      handleLogout();
     }
   }
 
   useEffect(() => {
     fetchCases();
   }, [page]);
+
+   useEffect(() => {
+     if (requestState?.status === 'error') {
+       requestState.code === 'UNAUTHORIZED' && handleLogout();
+     }
+   }, [requestState]);
 
   return (
     <section className="flex flex-col items-center size-full">
@@ -90,7 +107,7 @@ export default function CaseSearch() {
           />
         </div>
       </div>
-      <CasesList loadCases={fetchCases} loading={casesLoading} cases={cases} />
+      <CasesList loadCases={fetchCases} requestState={requestState} />
       <Pagination page={page} setPage={setPage} totalPage={totalPage} />
     </section>
   );
