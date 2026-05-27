4ab5509219f1457becfb2161a28a3d75a68da47d
DavidSheltonSF <david.faria012@gmail.com>
2026-05-27 20:53:34 -0300

    refactor(CasesList): enhance error handling, request handling and conditional rendering

--- frontend/components/features/cases/CasesList.tsx
+++ frontend/components/features/cases/CasesList.tsx
@@ -1,52 +1,53 @@
 'use client';
 import { CaseCard } from './CaseCard/CaseCard';
 import { WithId } from '@/types/WithId';
-import { Activity, useState } from 'react';
 import { CardSkeleton } from '../../ui/Card/CardSkeleton';
 import { CaseWithRelations } from '@/types/CaseWithRelations';
 import { RequestState } from '@/types/RequestState';
 import { deleteCaseById } from '@/services/users/deleteCaseById';
-import { UnauthorizedError } from '@/errors/UnauthorizedError';
-import { handleLogout } from '@/lib/handleLogout';
+import { Page } from '@/types/Page';
 
 interface Props {
-  cases: WithId<CaseWithRelations>[];
-  loading: boolean;
+  requestState: RequestState<Page<WithId<CaseWithRelations>>>;
   loadCases: () => void;
 }
 
-export function CasesList({ cases, loading, loadCases }: Props) {
-  const [requestState, setRequestState] = useState<RequestState | null>(null);
-
+export function CasesList({ requestState, loadCases }: Props) {
   async function deleteCase(id: string) {
     try {
-      setRequestState({ status: 'loading' });
       await deleteCaseById(id);
-      setRequestState({ status: 'ok', message: 'Case deleted successfuly' });
       loadCases();
     } catch (error: any) {
-      setRequestState({ status: 'error', message: error.message });
       console.log(error);
-      if (error instanceof UnauthorizedError) {
-        handleLogout();
-      }
     }
   }
 
-  const renderCases = cases?.map((cas, index) => {
-    return <CaseCard refetchCases={loadCases} deleteCase={deleteCase} key={index} caseData={cas} />;
-  });
+  function renderContent() {
+    switch (requestState?.status) {
+      case 'loading':
+        const renderCaseSkeletons = Array.from({ length: 4 }).map((page, index) => {
+          return <CardSkeleton key={index} />;
+        });
+        return renderCaseSkeletons;
+      case 'ok':
+        const { data } = requestState;
+        if (data?.data.length === 0) {
+          return <h1>Nenhum processo encontrado</h1>;
+        }
+        const renderCases = data?.data?.map((cas, index) => {
+          return (
+            <CaseCard refetchCases={loadCases} deleteCase={deleteCase} key={index} caseData={cas} />
+          );
+        });
+        return renderCases;
 
-  const renderCaseSkeletons = Array.from({ length: 4 }).map((page, index) => {
-    return <CardSkeleton key={index} />;
-  });
+      case 'error':
+        return null;
 
-  return (
-    <div className="flex flex-col gap-[32px] mt-[88px] w-full">
-      <Activity mode={!loading && (!cases || cases.length === 0) ? 'visible' : 'hidden'}>
-        <h1 className="text-3xl">Nenhum caso encontrado</h1>
-      </Activity>
-      {loading ? renderCaseSkeletons : renderCases}
-    </div>
-  );
+      default:
+        return null;
+    }
+  }
+
+  return <div className="flex flex-col gap-[32px] mt-[88px] w-full">{renderContent()}</div>;
 }
