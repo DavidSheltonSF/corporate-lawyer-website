46f2bef1892839978b7899a42715ae71864d35d7
DavidSheltonSF <david.faria012@gmail.com>
2026-05-27 19:42:59 -0300

    refactor(Dashboard): uppdate to handle  conditional rendering

--- frontend/components/features/deashboard/Dashboard.tsx
+++ frontend/components/features/deashboard/Dashboard.tsx
@@ -1,19 +1,17 @@
 import { DashboardCard } from '@/components/DashboardCard';
 import { DashboardCardInfo } from '@/components/DashboardCardInfo';
+import { CasesStats } from '@/types/CasesStats';
 
 interface Props {
-  data: {
-    open: number;
-    closed: number;
-  };
+  data?: CasesStats;
 }
 
 export function Dashboard({ data }: Props) {
   return (
     <div className="flex flex-wrap gap-[40px]">
       <DashboardCard title="Processos" sectionIndex={1}>
-        <DashboardCardInfo name="Em andamento" value={data.open} />
-        <DashboardCardInfo name="Encerrados" value={data.closed} />
+        <DashboardCardInfo name="Em andamento" value={data?.open || 0} />
+        <DashboardCardInfo name="Encerrados" value={data?.closed || 0} />
       </DashboardCard>
       <DashboardCard title="Atendimento">
         <DashboardCardInfo name="Data" value="2 de jan. de 2026" />
