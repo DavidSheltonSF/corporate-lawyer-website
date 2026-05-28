bcb199acf4d6bc7b773b195d7f585d562a4b3aaf
DavidSheltonSF <david.faria012@gmail.com>
2026-05-27 23:49:12 -0300

    refactor(frontend/services): update deleteCaseById to return ActionResponse

--- frontend/services/users/deleteCaseById.ts
+++ frontend/services/users/deleteCaseById.ts
@@ -1,8 +1,12 @@
 import { API_URL } from '@/config/api';
 import { apiFetch } from '../apiFetch';
+import { makeActionResponse } from '@/factories/makeActionResponse';
+import { ActionResponse } from '@/types/ActionResponse';
 
-export async function deleteCaseById(id: string): Promise<void> {
-  await apiFetch(`${API_URL}/cases/${id}`, {
+export async function deleteCaseById(id: string): Promise<ActionResponse<null>> {
+  const response = await apiFetch(`${API_URL}/cases/${id}`, {
     method: 'DELETE',
   });
+
+  return makeActionResponse(response);
 }
