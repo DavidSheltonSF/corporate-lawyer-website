7dd4fc6eda9b1e8c5d4ca1a3a3cd0e9c9cd83f17
DavidSheltonSF <david.faria012@gmail.com>
2026-05-28 15:15:40 -0300

    refactor(frontend/RequestState): update to ensure data in 'ok' responses

--- frontend/types/RequestState.ts
+++ frontend/types/RequestState.ts
@@ -6,6 +6,6 @@ type ValidationFields = {
 export type RequestState<T = undefined> =
   | { status: 'idle' }
   | { status: 'loading' }
-  | { status: 'empty'}
-  | { status: 'ok'; data?: T }
+  | { status: 'empty' }
+  | { status: 'ok'; data: T }
   | { status: 'error'; code?: string; message: string; details?: { fields: ValidationFields } };
