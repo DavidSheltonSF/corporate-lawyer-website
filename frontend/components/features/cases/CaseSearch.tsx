27e380936587bf9e7453a0d132711196f6f59219
DavidSheltonSF <david.faria012@gmail.com>
2026-05-28 01:06:32 -0300

    refactor(CaseSearch): set request  status to 'empty' if no case is found.

--- frontend/components/features/cases/CaseSearch.tsx
+++ frontend/components/features/cases/CaseSearch.tsx
@@ -74,6 +74,10 @@ export default function CaseSearch() {
 
       const { data } = response;
 
+      if (data?.data.length === 0) {
+        return setRequestState({ status: 'empty' });
+      }
+
       setRequestState({ status: 'ok', data: response.data });
       setPage(data?.meta.currentPage || 1);
       setTotalPage(data?.meta.totalPages || 1);
