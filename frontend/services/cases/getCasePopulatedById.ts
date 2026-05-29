afd3b9c4801eb64f52b6bd290abc83b9a438d54c
DavidSheltonSF <david.faria012@gmail.com>
2026-05-28 23:42:47 -0300

    refactor(getCasePopulatedById): update to return ActionREsponse and remove error handling

--- frontend/services/cases/getCasePopulatedById.ts
+++ frontend/services/cases/getCasePopulatedById.ts
@@ -1,21 +1,19 @@
 import { API_URL } from '@/config/api';
-import { InvalidAPIResponseError } from '@/errors/InvalidAPIResponseError';
 import { MissingRequiredArgumentError } from '@/errors/MissingRequiredArgumentError';
 import { WithId } from '@/types/WithId';
 import { apiFetch } from '../apiFetch';
 import { CaseWithRelations } from '@/types/CaseWithRelations';
+import { ActionResponse } from '@/types/ActionResponse';
+import { makeActionResponse } from '@/factories/makeActionResponse';
 
-export async function getCasePopulatedById(id: string): Promise<WithId<CaseWithRelations>> {
+export async function getCasePopulatedById(
+  id: string
+): Promise<ActionResponse<WithId<CaseWithRelations>>> {
   if (!id) {
     throw new MissingRequiredArgumentError(getCasePopulatedById.name, 'id');
   }
 
   const response = await apiFetch(`${API_URL}/cases/${id}?populate=true`);
 
-  const json = await response.json();
-
-  if (!json?.data) {
-    throw new InvalidAPIResponseError('Missing data', json);
-  }
-  return json.data;
+  return await makeActionResponse(response);
 }
