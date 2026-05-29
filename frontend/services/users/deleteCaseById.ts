f6b95189e8374fc4cbdb71e3a36420e2fe71c3c0
DavidSheltonSF <david.faria012@gmail.com>
2026-05-28 23:13:44 -0300

    refactor(frontend/deleteCaseById): update to replace makeActionResponse with makeEmptyActionResponse for consistency

--- frontend/services/users/deleteCaseById.ts
+++ frontend/services/users/deleteCaseById.ts
@@ -2,11 +2,12 @@ import { API_URL } from '@/config/api';
 import { apiFetch } from '../apiFetch';
 import { makeActionResponse } from '@/factories/makeActionResponse';
 import { ActionResponse } from '@/types/ActionResponse';
+import { makeEmptyActionResponse } from '@/factories/makeEmptyActionResponse';
 
 export async function deleteCaseById(id: string): Promise<ActionResponse<null>> {
   const response = await apiFetch(`${API_URL}/cases/${id}`, {
     method: 'DELETE',
   });
 
-  return makeActionResponse(response);
+  return makeEmptyActionResponse(response);
 }
