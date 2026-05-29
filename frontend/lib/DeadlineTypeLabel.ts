67386f28f78c0a924e4bad4d5da1c27553008516
DavidSheltonSF <david.faria012@gmail.com>
2026-05-29 19:27:25 -0300

    feat(frontend/lib): add DeadlineTypeLabel

---
+++ frontend/lib/DeadlineTypeLabel.ts
@@ -0,0 +1,11 @@
+import { DeadlineType } from '@/types/DeadlineType';
+
+export const DeadlineTypeLabel: Record<DeadlineType, string> = {
+  AUDIENCIA: 'Audiência',
+  CONTESTACAO: 'Contestação',
+  MANIFESTACAO: 'Manifestação',
+  PAGAMENTO: 'Pagamento',
+  RECURSO: 'Recurso',
+  REPLICA: 'Réplica',
+  OUTRO: 'Outro',
+};
