5269315c56dbfb8faf6e5df4d6b761fab6a48c3e
DavidSheltonSF <david.faria012@gmail.com>
2026-05-28 23:56:07 -0300

    refactor(frontend/getCaseFiles): update to return ActionResponse

--- frontend/services/cases/getCaseFiles.ts
+++ frontend/services/cases/getCaseFiles.ts
@@ -2,9 +2,10 @@ import { API_URL } from '@/config/api';
 import { CaseFile } from '@/types/CaseFile';
 import { apiFetch } from '../apiFetch';
 import { WithId } from '@/types/WithId';
+import { ActionResponse } from '@/types/ActionResponse';
+import { makeActionResponse } from '@/factories/makeActionResponse';
 
-export async function getCaseFiles(id: string): Promise<WithId<CaseFile>[]> {
+export async function getCaseFiles(id: string): Promise<ActionResponse<WithId<CaseFile>[]>> {
   const response = await apiFetch(`${API_URL}/cases/${id}/files`);
-  const json = await response.json();
-  return json.data;
+  return makeActionResponse(response);
 }
