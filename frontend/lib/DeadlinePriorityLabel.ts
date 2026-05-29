3903de990c114e3e6dd74e325dab01c605794a96
DavidSheltonSF <david.faria012@gmail.com>
2026-05-29 19:27:40 -0300

    feat(frontend/lib): add DeadlinePriorityLabel

---
+++ frontend/lib/DeadlinePriorityLabel.ts
@@ -0,0 +1,7 @@
+import { DeadlinePriority } from '@/types/DeadlinePriority';
+
+export const DeadlinePriorityLabel: Record<DeadlinePriority, string> = {
+  ALTA: 'Alta',
+  MEDIA: 'Média',
+  BAIXA: 'Baixa',
+};
