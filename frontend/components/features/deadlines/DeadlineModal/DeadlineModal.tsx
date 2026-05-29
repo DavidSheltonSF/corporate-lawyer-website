679e598d13d90d578b19170faadf39f22249ebeb
DavidSheltonSF <david.faria012@gmail.com>
2026-05-29 17:10:31 -0300

    refactor(frontend/DeadlineModal): enhance structure by extracting DeadlineModalContent  and moving Loading rendering logic into the state machine

--- frontend/components/features/deadlines/DeadlineModal/DeadlineModal.tsx
+++ frontend/components/features/deadlines/DeadlineModal/DeadlineModal.tsx
@@ -7,12 +7,14 @@ import { Deadline } from '@/types/Deadline';
 import { Button } from '@/components/ui/Button/Button';
 import { GlobalModalProps } from '@/types/GlobalModalProps';
 import { LoadingModalScreeen } from '@/components/ui/Modal/LoadingModalScreen';
-import { DeadlineCard } from '../DeadLineCard/DeadlineCard';
 import { handleLogout } from '@/lib/handleLogout';
+import { DeadlineModalContent } from './DeadlineModalContent';
 
 interface Props {
   caseId: string;
 }
+
+DeadlineModal.Content = DeadlineModalContent;
 
 export function DeadlineModal({ payload, close }: GlobalModalProps<Props>) {
   const { caseId } = payload;
@@ -51,37 +53,14 @@ export function DeadlineModal({ payload, close }: GlobalModalProps<Props>) {
     onClose: close,
   };
 
-  if (isLoading) {
-    return (
-      <BaseModal {...BaseModalProps}>
-        <LoadingModalScreeen />
-      </BaseModal>
-    );
-  }
-
   function renderContent() {
     switch (requestState.status) {
       case 'loading':
-        return <LoadingModalScreeen />;
+        return <LoadingModalScreeen/>
 
       case 'ok':
         const { data } = requestState;
-        const renderDeadlines = data.map((deadline) => {
-          return <DeadlineCard key={deadline.id} deadline={deadline} />;
-        });
-        return (
-          <div className="flex flex-col max-h-[58vh]">
-            <div className="flex flex-col min-lg:flex-row min-lg:items-center p-[24px] border-divider gap-[16px] min-lg:gap-0">
-              <span className="font-bold">Quantidade: {data.length}</span>
-              <Button className="border border-black bg-color-white hover:brightness-95 min-lg:ml-auto">
-                Adicionar Prazo
-              </Button>
-            </div>
-            <div className="flex flex-col gap-[16px] border-divider p-[24px] overflow-y-auto">
-              {renderDeadlines}
-            </div>
-          </div>
-        );
+        return <DeadlineModal.Content data={data} />;
     }
   }
 
---
+++ frontend/components/features/deadlines/DeadlineModal/DeadlineModalContent.tsx
@@ -0,0 +1,28 @@
+import { Button } from '@/components/ui/Button/Button';
+import { Deadline } from '@/types/Deadline';
+import { WithId } from '@/types/WithId';
+import { DeadlineCard } from '../DeadLineCard/DeadlineCard';
+
+interface Props {
+  data: WithId<Deadline>[];
+}
+
+export function DeadlineModalContent({ data }: Props) {
+  const renderDeadlines = data.map((deadline) => {
+    return <DeadlineCard key={deadline.id} deadline={deadline} />;
+  });
+
+  return (
+    <div className="flex flex-col max-h-[58vh]">
+      <div className="flex flex-col min-lg:flex-row min-lg:items-center p-[24px] border-divider gap-[16px] min-lg:gap-0">
+        <span className="font-bold">Quantidade: {data.length}</span>
+        <Button className="border border-black bg-color-white hover:brightness-95 min-lg:ml-auto">
+          Adicionar Prazo
+        </Button>
+      </div>
+      <div className="flex flex-col gap-[16px] border-divider p-[24px] overflow-y-auto">
+        {renderDeadlines}
+      </div>
+    </div>
+  );
+}
