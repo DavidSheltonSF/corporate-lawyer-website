7304325d2fd14b645adab322067190be00457dd1
DavidSheltonSF <david.faria012@gmail.com>
2026-05-30 12:18:06 -0300

    refactor(frontend/services): update uploadCaseFile to retun ActionResponse

--- frontend/services/cases/uploadCaseFile.ts
+++ frontend/services/cases/uploadCaseFile.ts
@@ -1,8 +1,15 @@
+import { ActionResponse } from '@/types/ActionResponse';
 import { apiFetch } from '../apiFetch';
+import { makeEmptyActionResponse } from '@/factories/makeEmptyActionResponse';
 
-export async function uploadCaseFile(formData: FormData, id: string) {
-  await apiFetch(`/api/my/cases/${id}/files`, {
+export async function uploadCaseFile(
+  formData: FormData,
+  id: string
+): Promise<ActionResponse<null>> {
+  const response = await apiFetch(`/api/my/cases/${id}/files`, {
     method: 'POST',
     body: formData,
   });
+
+  return makeEmptyActionResponse(response);
 }
