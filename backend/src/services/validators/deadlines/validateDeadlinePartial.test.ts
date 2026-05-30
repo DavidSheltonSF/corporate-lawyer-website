f86238c5f5fe1c98e81aae6466244c5086707020
DavidSheltonSF <david.faria012@gmail.com>
2026-05-30 00:25:43 -0300

    refactor(validateDeadlinePartial): update to throw ValidationError containig all invalid fields if invalid data is provided and update related tests

--- backend/src/services/validators/deadlines/validateDeadlinePartial.test.ts
+++ backend/src/services/validators/deadlines/validateDeadlinePartial.test.ts
@@ -1,9 +1,7 @@
 import { validateDeadlinePartial } from './validateDeadlinePartial';
-import { InvalidDeadlineTypeError } from '../../../errors/domain/InvalidDeadlineTypeError';
-import { InvalidDeadlinePriorityError } from '../../../errors/domain/InvalidDeadlinePriorityError';
-import { InvalidDateError } from '../../../errors/domain/InvalidDateError';
 import { getThrownError } from '../../../tests/helpers/getThrownError';
 import { DeadlineMocker } from '../../../tests/mocks/entities/DeadlineMocker';
+import { ValidationError } from '../../../errors/presentation/ValidationError';
 
 describe(`Testing ${validateDeadlinePartial.name}`, () => {
   test('should not throw error if all deadline fields are  valid', () => {
@@ -16,20 +14,20 @@ describe(`Testing ${validateDeadlinePartial.name}`, () => {
     const deadlineDTO = DeadlineMocker.mockUpateDeadlineDTO();
     deadlineDTO.type = 'banana';
     const thrownError = getThrownError(() => validateDeadlinePartial(deadlineDTO));
-    expect(thrownError).toBeInstanceOf(InvalidDeadlineTypeError);
+    expect(thrownError).toBeInstanceOf(ValidationError);
   });
 
   test('should throw InvalidDeadlinePriorityError if type provided is invalid', () => {
     const deadlineDTO = DeadlineMocker.mockUpateDeadlineDTO();
     deadlineDTO.priority = 'banana';
     const thrownError = getThrownError(() => validateDeadlinePartial(deadlineDTO));
-    expect(thrownError).toBeInstanceOf(InvalidDeadlinePriorityError);
+    expect(thrownError).toBeInstanceOf(ValidationError);
   });
 
   test('should throw InvalidDateError if the start intimationDate provided is invalid', () => {
     const deadlineDTO = DeadlineMocker.mockUpateDeadlineDTO();
     deadlineDTO.intimationDate = 'banana';
     const thrownError = getThrownError(() => validateDeadlinePartial(deadlineDTO));
-    expect(thrownError).toBeInstanceOf(InvalidDateError);
+    expect(thrownError).toBeInstanceOf(ValidationError);
   });
 });
--- backend/src/services/validators/deadlines/validateDeadlinePartial.ts
+++ backend/src/services/validators/deadlines/validateDeadlinePartial.ts
@@ -1,20 +1,32 @@
 import { UpdateDeadlineDTO } from '../../../dtos/deadLine/UpdateDeadlineDTO';
+import { ValidationError } from '../../../errors/presentation/ValidationError';
 import { isValidDateString } from '../isValidDateString';
+import { isValidDeadlineCountingType } from './isValidDeadlineCountingType';
 import { isValidDeadlinePriority } from './isValidDeadlinePriority';
 import { isValidDeadlineType } from './isValidDeadlineType';
 
 export function validateDeadlinePartial(data: UpdateDeadlineDTO) {
-  const { type, priority, intimationDate } = data;
+  const { type, priority, intimationDate, countingType } = data;
 
-  if (type) {
-    isValidDeadlineType(type);
+  const invalidFields: Record<string, string> = {};
+
+  if (type && !isValidDeadlineType(type)) {
+    invalidFields.type = 'Invalid deadline type';
   }
 
-  if (priority) {
-    isValidDeadlinePriority(priority);
+  if (countingType && !isValidDeadlineCountingType(countingType)) {
+    invalidFields.countingType = 'Invalid deadline counting type';
   }
 
-  if (intimationDate) {
-    isValidDateString(intimationDate);
+  if (priority && !isValidDeadlinePriority(priority)) {
+    invalidFields.priority = 'Invalid deadline priority';
+  }
+
+  if (intimationDate && !isValidDateString(intimationDate)) {
+    invalidFields.intimationDate = 'Invalid date';
+  }
+
+  if (Object.keys(invalidFields).length > 0) {
+    throw new ValidationError('Invalid deadline data', { fields: invalidFields });
   }
 }
