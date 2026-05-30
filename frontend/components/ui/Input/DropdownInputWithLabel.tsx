22bf321bf6a0096a35d1fe9e753adc262c7813b7
DavidSheltonSF <david.faria012@gmail.com>
2026-05-30 18:17:13 -0300

    chore(frontend/DropdownInputWithLabel): remove unnecessary code and unused imports

--- frontend/components/ui/Input/DropdownInputWithLabel.tsx
+++ frontend/components/ui/Input/DropdownInputWithLabel.tsx
@@ -1,13 +1,10 @@
-import { useEffect, useRef, useState } from 'react';
-import { ArrowDropUpIcon } from '../../icons/ArrowDropUpIcon';
-import { Input } from './Input';
 import { InputProps } from './types';
 import { DropdownInput } from './DropdownInput';
 
 interface Props {
   label: string;
   itemLabel: Record<string, string>;
-  onSelectValue?: (value: string) => void
+  onSelectValue?: (value: string) => void;
 }
 
 export function DropdownInputWithLabel({
@@ -18,44 +15,6 @@ export function DropdownInputWithLabel({
   onSelectValue,
   ...inputProps
 }: Props & InputProps) {
-  const [selectedValue, setSelectedValue] = useState<string | null>(null);
-  const [listIsOpen, setListIsOpen] = useState(false);
-  const dropRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    function handleClickOutside(e: MouseEvent) {
-      if (dropRef.current && !dropRef.current.contains(e.target as Node)) {
-        setListIsOpen(false);
-      }
-    }
-
-    if (defaultValue) {
-      setSelectedValue(defaultValue);
-    }
-
-    document.addEventListener('click', handleClickOutside);
-
-    return () => {
-      document.removeEventListener('click', handleClickOutside);
-    };
-  }, [defaultValue]);
-
-  const items = Object.values(itemLabel);
-  const renderItems = items.map((item, index) => {
-    return (
-      <li
-        onClick={() => {
-          setSelectedValue(item);
-          setListIsOpen(false);
-        }}
-        className="bg-white hover:brightness-80 cursor-pointer px-[8px] h-[32px]"
-        key={index}
-      >
-        {item}
-      </li>
-    );
-  });
-
   return (
     <div className="w-full">
       <label className="text-[1.3rem]" htmlFor={id}>
