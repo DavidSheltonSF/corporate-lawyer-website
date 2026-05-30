12ca6772d17bc73e5bb3455c4d9a5f90a55300a8
DavidSheltonSF <david.faria012@gmail.com>
2026-05-30 12:47:50 -0300

    refactor(frontend/UploadButton): replace setFile with setUploadFile for consistency and validate the selected file size

--- frontend/components/ui/UploadButton.tsx
+++ frontend/components/ui/UploadButton.tsx
@@ -1,12 +1,13 @@
 import { ChangeEvent, Dispatch, SetStateAction, useRef } from 'react';
 import { Button } from './Button/Button';
 import { ButtonVariant } from './Button/ButtonVariant';
+import { UploadFileState } from '@/types/UploadFileState';
 
 interface Props {
-  setFile: Dispatch<SetStateAction<File | null>>;
+  setUploadFileState: Dispatch<SetStateAction<UploadFileState>>;
 }
 
-export function UploadButton({ setFile }: Props) {
+export function UploadButton({ setUploadFileState }: Props) {
   const inputRef = useRef<HTMLInputElement>(null);
 
   function handleOpenFilePicker() {
@@ -16,13 +17,27 @@ export function UploadButton({ setFile }: Props) {
   function handleChange(e: ChangeEvent<HTMLInputElement>) {
     const selectedFiles = Array.from(e.target.files || []);
     const selectedFile = selectedFiles[0];
-    setFile(selectedFile);
+
+    const MAX_FILE_SIZE = 9 * 1024 * 1024;
+
+    if (selectedFile.size > MAX_FILE_SIZE) {
+      return setUploadFileState({
+        status: 'error',
+        message: 'Arquivo ultrapassa o tamanho máximo de 9 MB.',
+      });
+    }
+
+    setUploadFileState({ status: 'ok', file: selectedFile });
   }
 
   return (
     <div className="flex size-full">
       <input name="file" onChange={handleChange} ref={inputRef} className="hidden" type="file" />
-      <Button onClick={handleOpenFilePicker} variant={ButtonVariant.SECONDARY} className="w-full py-[4px]">
+      <Button
+        onClick={handleOpenFilePicker}
+        variant={ButtonVariant.SECONDARY}
+        className="w-full py-[4px]"
+      >
         Selecionar arquivo
       </Button>
     </div>
