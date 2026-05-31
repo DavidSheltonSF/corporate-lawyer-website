f3fb5d64676b334845f79784c7ea5a500f6069a1
DavidSheltonSF <david.faria012@gmail.com>
2026-05-31 16:31:21 -0300

    chore(frontend/CreateDeadlineModal): remove unused imports

--- frontend/components/features/deadlines/CreateDeadlineModal/CreateDeadlineModal.tsx
+++ frontend/components/features/deadlines/CreateDeadlineModal/CreateDeadlineModal.tsx
@@ -1,16 +1,4 @@
-import { DropdownInputWithLabel } from '@/components/ui/Input/DropdownInputWithLabel';
-import { InputWithLabel } from '@/components/ui/Input/InputWithLabel';
 import { BaseModal } from '@/components/ui/Modal/BaseModal';
-import { useAuthenticatedUserContext } from '@/hooks/useAuthenticatedUserContext';
-import { DeadlineCountingTypeLabel } from '@/lib/DeadlineCountingTypeLabel';
-import { DeadlinePriorityLabel } from '@/lib/DeadlinePriorityLabel';
-import { DeadlineTypeLabel } from '@/lib/DeadlineTypeLabel';
-import { createDeadline } from '@/services/cases/createDeadline';
-import { Deadline } from '@/types/Deadline';
-import { DeadlineCountingType } from '@/types/DeadlineCountingType';
-import { RequestState } from '@/types/RequestState';
-import { WithId } from '@/types/WithId';
-import { useState } from 'react';
 import { CreateDeadlineModalForm } from './CreateDeadlineModalForm';
 
 interface Props {
