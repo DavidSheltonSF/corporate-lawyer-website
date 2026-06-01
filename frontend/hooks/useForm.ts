77af8f33cc2818995388aeee079e2a4347fd4b75
DavidSheltonSF <david.faria012@gmail.com>
2026-06-01 20:18:21 -0300

    chore(frontend/useForm): improve type safety

--- frontend/hooks/useForm.ts
+++ frontend/hooks/useForm.ts
@@ -1,28 +1,32 @@
 import { Dispatch, SetStateAction, useState } from 'react';
 
-interface UseFormReturn {
-  formState: Record<string, string>;
-  setFormState: Dispatch<SetStateAction<Record<string, string>>>;
+type FormState<T> = {
+  [K in keyof T]: string;
+};
+
+interface UseFormReturn<T> {
+  formState: FormState<T>;
+  setFormState: Dispatch<SetStateAction<FormState<T>>>;
   clearForm: () => void;
-  updateField: (name: string, value: string) => void;
+  updateField: (name: keyof T, value: string) => void;
   hasEmptyFields: () => boolean;
 }
 
-export function useForm(formData: Record<string, string>): UseFormReturn {
+export function useForm<T>(formData: FormState<T>): UseFormReturn<T> {
   const [formState, setFormState] = useState(formData);
 
   function clearForm() {
-    const aux: Record<string, string> = {};
-    Object.keys(formState).forEach((key) => (aux[key] = ''));
+    const aux = { ...formState };
+    (Object.keys(formState) as Array<keyof T>).forEach((key) => (aux[key] = ''));
     setFormState(aux);
   }
 
-  function updateField(name: string, value: string) {
+  function updateField(name: keyof T, value: string) {
     setFormState((prev) => ({ ...prev, [name]: value }));
   }
 
   function hasEmptyFields(): boolean {
-    return Object.entries(formState).some(([, value]) => value.trim() === '');
+    return (Object.values(formState) as string[]).some((value) => !value || value.trim() === '');
   }
 
   return { formState, setFormState, clearForm, updateField, hasEmptyFields };
