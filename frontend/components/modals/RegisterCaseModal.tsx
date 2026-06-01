6e7ef897407f01f2be791290b76a04ce44960c19
DavidSheltonSF <david.faria012@gmail.com>
2026-06-01 17:12:50 -0300

    refactor(RegiterCaseModal): remove invalid itemsRecord prop from DropdownInputWithLabel and update itemLabel prop

--- frontend/components/modals/RegisterCaseModal.tsx
+++ frontend/components/modals/RegisterCaseModal.tsx
@@ -63,56 +63,52 @@ export function RegisterCaseModal({ isOpen, close, selectedClientId }: Props) {
           setRequestState(null);
         }}
       >
-          <div className='flex flex-col overflow-y-auto p-[24px] h-[56vh]'>
-            <RequestFeedback requestState={requestState} />
-            <form className="flex flex-col gap-[16px] w-ful]" action={registerCase}>
-              <div className="flex flex-col gap-[16px] min-lg:flex-row w-full">
-                <InputWithLabel id="title-input" name="title" label="Título" />
-                <InputWithLabel
-                  id="process-number-input"
-                  name="processNumber"
-                  label="Número do Processo"
-                />
-              </div>
-              <div className="flex flex-col gap-[16px] min-lg:flex-row w-full">
-                <InputWithLabel id="court-input" name="court" label="Tribunal" />
-                <InputWithLabel id="court-division-input" name="courtDivision" label="Vara" />
-                <DropdownInputWithLabel
-                  id="status-input"
-                  name="status"
-                  label="Status"
-                  itemsRecord={CaseStatusEnum}
-                  itemLabel={(item: CaseStatusEnum) => CaseStatusLabel[item]}
-                />
-              </div>
-              <div className="flex flex-col gap-[16px] min-lg:flex-row w-full">
-                <DropdownInputWithLabel
-                  id="estado-input"
-                  name="state"
-                  label="Estado"
-                  itemsRecord={BrazilState}
-                  itemLabel={(item: BrazilState) => BrazilStateLabel[item]}
-                />
-                <DropdownInputWithLabel
-                  id="city-input"
-                  name="city"
-                  label="Cidade"
-                  itemsRecord={City}
-                  itemLabel={(item: City) => CityLabel[item]}
-                />
-              </div>
-              <div>
-                <InputWithLabel id="description-input" name="description" label="Description" />
-              </div>
+        <div className="flex flex-col overflow-y-auto p-[24px] h-[56vh]">
+          <RequestFeedback requestState={requestState} />
+          <form className="flex flex-col gap-[16px] w-ful]" action={registerCase}>
+            <div className="flex flex-col gap-[16px] min-lg:flex-row w-full">
+              <InputWithLabel id="title-input" name="title" label="Título" />
+              <InputWithLabel
+                id="process-number-input"
+                name="processNumber"
+                label="Número do Processo"
+              />
+            </div>
+            <div className="flex flex-col gap-[16px] min-lg:flex-row w-full">
+              <InputWithLabel id="court-input" name="court" label="Tribunal" />
+              <InputWithLabel id="court-division-input" name="courtDivision" label="Vara" />
+              <DropdownInputWithLabel
+                id="status-input"
+                name="status"
+                label="Status"
+                itemLabel={CaseStatusLabel}
+              />
+            </div>
+            <div className="flex flex-col gap-[16px] min-lg:flex-row w-full">
+              <DropdownInputWithLabel
+                id="estado-input"
+                name="state"
+                label="Estado"
+                itemLabel={BrazilStateLabel}
+              />
+              <DropdownInputWithLabel
+                id="city-input"
+                name="city"
+                label="Cidade"
+                itemLabel={CityLabel}
+              />
+            </div>
+            <div>
+              <InputWithLabel id="description-input" name="description" label="Description" />
+            </div>
 
-              <div className="flex justify-end w-full min-md:w-[200px]  min-md:ml-auto">
-                <Button variant={ButtonVariant.PRIMARY} className="w-full">
-                  Confirmar Alterações
-                </Button>
-              </div>
-            </form>
-          </div>
-
+            <div className="flex justify-end w-full min-md:w-[200px]  min-md:ml-auto">
+              <Button variant={ButtonVariant.PRIMARY} className="w-full">
+                Confirmar Alterações
+              </Button>
+            </div>
+          </form>
+        </div>
       </BaseModal>
     )
   );
