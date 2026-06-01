e8181b863df6211403e91e4fc969d0b665935eca
DavidSheltonSF <david.faria012@gmail.com>
2026-05-31 21:06:18 -0300

    refactor(FormModal): add confimDisabled prop and pass it as prop to BaseModal

--- frontend/components/ui/FormModal/FormModal.tsx
+++ frontend/components/ui/FormModal/FormModal.tsx
@@ -5,15 +5,17 @@ import { BaseModal } from '../Modal/BaseModal';
 interface Props {
   title?: string;
   formId: string;
+  confirmDisabled?: boolean;
   close: () => void;
 }
 
-export function FormModal({ title, formId, close, children }: PropsWithChildren<Props>) {
+export function FormModal({ title, formId, close, confirmDisabled, children }: PropsWithChildren<Props>) {
   return (
     <BaseModal
       title={title}
       formId={formId}
       className={'h-fit w-[90%] min-md:w-[60%] min-lg:w-[680px]'}
+      confirmDisabled={confirmDisabled}
       onClose={close}
     >
       <div className="flex flex-col h-[500px] min-lg:h-fit overflow-visible">{children}</div>
