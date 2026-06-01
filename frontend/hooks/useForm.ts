c53efaa831fa9fbe7ae88ddcd1a3f0fd13999a18
DavidSheltonSF <david.faria012@gmail.com>
2026-06-01 19:59:37 -0300

    feat(frontend/hooks): add useForm

---
+++ frontend/hooks/useForm.ts
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+interface UseFormReturn {
+  formState: Record<string, string>;
+  clearForm: () => void;
+  updateField: (name: string, value: string) => void;
+  hasEmptyFields: () => boolean;
+}
+
+export function useForm(formData: Record<string, string>): UseFormReturn {
+  const [formState, setFormState] = useState(formData);
+
+  function clearForm() {
+    const aux: Record<string, string> = {};
+    Object.keys(formState).forEach((key) => (aux[key] = ''));
+    setFormState(aux);
+  }
+
+  function updateField(name: string, value: string) {
+    setFormState((prev) => ({ ...prev, [name]: value }));
+  }
+
+  function hasEmptyFields(): boolean {
+    return Object.entries(formState).some(([, value]) => value.trim() === '');
+  }
+
+  return { formState, clearForm, updateField, hasEmptyFields };
+}
