03125aa57c26d365df6180c2d65c27fc729af0fc
DavidSheltonSF <david.faria012@gmail.com>
2026-05-31 22:36:32 -0300

    refactor(UpdateCaseModalForm): simplify request handling and form state management

--- frontend/components/features/cases/UpdateCaseModal/UpdateCaseModalForm.tsx
+++ frontend/components/features/cases/UpdateCaseModal/UpdateCaseModalForm.tsx
@@ -8,15 +8,12 @@ import { CityLabel } from '@/lib/CityLabel';
 import { handleLogout } from '@/lib/handleLogout';
 import { getCaseById } from '@/services/cases/getCaseById';
 import { updateCaseById } from '@/services/cases/updateCaseById';
-import { BrazilState } from '@/types/BrazilState';
 import { Case } from '@/types/Case';
-import { CaseStatusEnum } from '@/types/CaseStatusEnum';
-import { City } from '@/types/City';
 import { WithId } from '@/types/WithId';
 import { LoadingModalScreeen } from '@/components/ui/Modal/LoadingModalScreen';
 import { useEffect, useState } from 'react';
-import { useModal } from '@/hooks/useModal';
-import { ConfirmModalProps } from '@/components/ui/Modal/ConfirmModal';
+import { RequestState } from '@/types/RequestState';
+import { RequestFeedback } from '@/components/ui/Feedback/RequestFeedback';
 
 interface Props {
   formId: string;
@@ -25,127 +22,182 @@ interface Props {
 }
 
 export function UpdateCaseModalForm({ formId, caseId, refetchCases }: Props) {
-  const [caseData, setCaseData] = useState<WithId<Case> | null>(null);
-  const [isLoading, setIsLoading] = useState(false);
-  const { openModal } = useModal();
+  const [getRequestState, setGetRequestState] = useState<RequestState<WithId<Case>>>({
+    status: 'idle',
+  });
+  const [updateRequestState, setUpdateRequestState] = useState<RequestState<WithId<Case>>>({
+    status: 'idle',
+  });
+  const [formData, setFormData] = useState({
+    title: '',
+    processNumber: '',
+    court: '',
+    courtDivision: '',
+    status: '',
+    state: '',
+    city: '',
+    description: '',
+  });
 
-  function openConfirmDialog(message: string, onCloseGoBack: boolean = false) {
-    openModal('confirm', {
-      message,
-      onConfirm: () => {
-        if (onCloseGoBack) {
-          openModal('update-case', { formId, caseId, refetchCases });
-          return;
-        }
-        openModal(null);
-      },
+  function updateField(name: string, value: string) {
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  }
+
+  function resetForm() {
+    setFormData({
+      title: '',
+      processNumber: '',
+      court: '',
+      courtDivision: '',
+      status: '',
+      state: '',
+      city: '',
+      description: '',
     });
   }
 
   async function getUser() {
-    try {
-      setIsLoading(true);
-      const data = await getCaseById(caseId || '');
-      setCaseData(data);
-    } catch (error: any) {
-      console.log(error);
+    setGetRequestState({ status: 'loading' });
+    const response = await getCaseById(caseId || '');
 
-      if (error instanceof UnauthorizedError) {
-        handleLogout();
-      }
-      openConfirmDialog(error.message);
-    } finally {
-      setIsLoading(false);
+    if (!response.success) {
+      setGetRequestState({ ...response, status: 'error' });
+      return;
     }
+
+    fillForm(response.data);
+
+    setGetRequestState({ status: 'ok', data: response.data });
   }
 
   async function alterCase(formData: FormData) {
-    let message = '';
-    try {
-      const data = await updateCaseById(caseId || '', formData);
-      setCaseData(data);
-      refetchCases();
-      message = 'Processo atualizado com sucesso';
-    } catch (error: any) {
-      console.log(error);
-      message = error.message;
-    } finally {
-      openConfirmDialog(message, true);
+    setUpdateRequestState({ status: 'loading' });
+    const response = await updateCaseById(caseId || '', formData);
+
+    if (!response.success) {
+      if (response.code === 'UNAUTHORIZED') {
+        handleLogout();
+      }
+      return setUpdateRequestState({ ...response, status: 'error' });
     }
+
+    refetchCases();
+    resetForm();
+    setUpdateRequestState({ status: 'ok', data: response.data });
+  }
+
+  function fillForm(data: any) {
+    const { title, processNumber, court, courtDivision, status, location, description } = data;
+
+    setFormData({
+      title,
+      processNumber,
+      court,
+      courtDivision,
+      status,
+      state: location.state,
+      city: location.city,
+      description,
+    });
   }
 
   useEffect(() => {
-    if (!caseData) {
-      getUser();
-    }
+    getUser();
 
     return () => {
-      setCaseData(null);
+      setGetRequestState({ status: 'idle' });
+      setUpdateRequestState({ status: 'idle' });
     };
   }, []);
 
-  return (
-    <ShowSkeletonOnLoading isLoading={isLoading} Skeleton={LoadingModalScreeen}>
-      <form id={formId} className="flex flex-col gap-[16px] size-full p-[24px]" action={alterCase}>
-        <div className="flex flex-col gap-[16px] min-lg:flex-row w-full">
-          <InputWithLabel
-            id="title-input"
-            name="title"
-            label="Título"
-            defaultValue={caseData?.title}
-          />
-          <InputWithLabel
-            id="process-number-input"
-            name="processNumber"
-            label="Número do Processo"
-            defaultValue={caseData?.processNumber}
-          />
-        </div>
-        <div className="flex flex-col gap-[16px] min-lg:flex-row w-full">
-          <InputWithLabel
-            id="court-input"
-            name="court"
-            label="Tribunal"
-            defaultValue={caseData?.court}
-          />
-          <InputWithLabel
-            id="court-division-input"
-            name="courtDivision"
-            label="Vara"
-            defaultValue={caseData?.courtDivision}
-          />
-          <DropdownInputWithLabel
-            id="status-input"
-            name="status"
-            label="Status"
-            itemLabel={CaseStatusLabel}
-            defaultValue={caseData?.status}
-          />
-        </div>
-        <div className="flex flex-col gap-[16px] min-lg:flex-row w-full">
-          <DropdownInputWithLabel
-            id="estado-input"
-            name="state"
-            label="Estado"
-            itemLabel={BrazilStateLabel}
-            defaultValue={caseData?.location.state}
-          />
-          <DropdownInputWithLabel
-            id="city-input"
-            name="city"
-            label="Cidade"
-            itemLabel={CityLabel}
-            defaultValue={caseData?.location.city}
-          />
-        </div>
+  switch (getRequestState.status) {
+    case 'loading':
+      return <LoadingModalScreeen />;
 
-        <InputWithLabel
-          id="description-input"
-          name="description"
-          label="Description"
-          defaultValue={caseData?.description}
-        />
-      </form>
-    </ShowSkeletonOnLoading>
-  );
+    case 'ok':
+      const { title, processNumber, court, courtDivision, status, city, state, description } =
+        formData;
+
+      return (
+        <form
+          id={formId}
+          className="flex flex-col gap-[16px] size-full p-[24px]"
+          action={alterCase}
+        >
+          <RequestFeedback requestState={updateRequestState} />
+          <div className="flex flex-col gap-[16px] min-lg:flex-row w-full">
+            <InputWithLabel
+              id="title-input"
+              name="title"
+              label="Título"
+              value={title}
+              onChange={(e) => updateField('title', e.target.value)}
+            />
+            <InputWithLabel
+              id="process-number-input"
+              name="processNumber"
+              label="Número do Processo"
+              value={processNumber}
+              onChange={(e) => updateField('processNumber', e.target.value)}
+            />
+          </div>
+          <div className="flex flex-col gap-[16px] min-lg:flex-row w-full">
+            <InputWithLabel
+              id="court-input"
+              name="court"
+              label="Tribunal"
+              value={court}
+              onChange={(e) => updateField('court', e.target.value)}
+            />
+            <InputWithLabel
+              id="court-division-input"
+              name="courtDivision"
+              label="Vara"
+              value={courtDivision}
+              onChange={(e) => updateField('courtDivision', e.target.value)}
+            />
+            <DropdownInputWithLabel
+              id="status-input"
+              name="status"
+              label="Status"
+              itemLabel={CaseStatusLabel}
+              value={status}
+              onChange={(e) => updateField('status', e.target.value)}
+            />
+          </div>
+          <div className="flex flex-col gap-[16px] min-lg:flex-row w-full">
+            <DropdownInputWithLabel
+              id="estado-input"
+              name="state"
+              label="Estado"
+              itemLabel={BrazilStateLabel}
+              value={state}
+              onChange={(e) => updateField('state', e.target.value)}
+            />
+            <DropdownInputWithLabel
+              id="city-input"
+              name="city"
+              label="Cidade"
+              itemLabel={CityLabel}
+              value={city}
+              onChange={(e) => updateField('city', e.target.value)}
+            />
+          </div>
+
+          <InputWithLabel
+            id="description-input"
+            name="description"
+            label="Description"
+            value={description}
+            onChange={(e) => updateField('description', e.target.value)}
+          />
+        </form>
+      );
+
+    case 'error':
+      return <h3>{getRequestState.message}</h3>;
+
+    default:
+      return null;
+  }
 }
