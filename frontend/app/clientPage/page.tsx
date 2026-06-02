8d7879562a6e5223d3ba3108ed6ab64105cb289d
DavidSheltonSF <david.faria012@gmail.com>
2026-06-02 19:21:51 -0300

    refactor(clientaPage): use AuthHydrator to  hydrate AuthStore

--- frontend/app/clientPage/page.tsx
+++ frontend/app/clientPage/page.tsx
@@ -7,6 +7,7 @@ import { redirect } from 'next/navigation';
 import { Notifications } from '@/components/features/notifications/Notifications';
 import { ModalProvider } from '@/contexts/modals/ModalProvider';
 import { ModalRenderer } from '@/components/renderer/ModalRenderer';
+import { AuthHydrator } from '@/components/AuthHydrator';
 
 export default async function ClientPage() {
   try {
@@ -18,24 +19,25 @@ export default async function ClientPage() {
     if (!response.data) {
       throw new Error('Response was successful but user data was not provided');
     }
-
     const user = response.data;
 
     return (
       <AuthenticatedUserProvider userData={user}>
-        <ModalProvider>
-          <ModalRenderer />
-          <div className="bg-color-black min-h-[100vh]">
-            <HeroSection
-              background="var(--blue-gradient)"
-              title={`Bem vindo(a) ${user?.role === 'lawyer' ? 'Dra' : ''} ${user?.firstName}`}
-              additionalStyles="h-[280px]"
-            />
-            {user.role === 'lawyer' ? <LawyerView /> : <ClientView />}
+        <AuthHydrator user={user}>
+          <ModalProvider>
+            <ModalRenderer />
+            <div className="bg-color-black min-h-[100vh]">
+              <HeroSection
+                background="var(--blue-gradient)"
+                title={`Bem vinda(a) ${user?.role === 'lawyer' ? 'Dra' : ''} ${user?.firstName}`}
+                additionalStyles="h-[280px]"
+              />
+              {user.role === 'lawyer' ? <LawyerView /> : <ClientView />}
 
-            <Notifications />
-          </div>
-        </ModalProvider>
+              <Notifications />
+            </div>
+          </ModalProvider>
+        </AuthHydrator>
       </AuthenticatedUserProvider>
     );
   } catch (error) {
