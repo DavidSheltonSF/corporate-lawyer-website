9e2b84bf93ed2fa4a7bada6133ce5e0c9513301c
DavidSheltonSF <david.faria012@gmail.com>
2026-06-02 20:32:38 -0300

    fix(RegisterCaseModalForm): render content even if the request fails

--- frontend/components/modals/RegisterCaseModalForm.tsx
+++ frontend/components/modals/RegisterCaseModalForm.tsx
@@ -49,85 +49,78 @@ export function RegisterCaseModalForm({ formId, clientId }: Props) {
     });
   }
 
-  switch (requestState.status) {
-    case 'idle':
-    case 'ok':
-      return (
-        <div className="flex flex-col overflow-y-auto p-[24px] h-[56vh]">
-          <RequestFeedback requestState={requestState} />
-          <form id={formId} className="flex flex-col gap-[16px] w-ful]" onSubmit={registerCase}>
-            <div className="flex flex-col gap-[16px] min-lg:flex-row w-full">
-              <InputWithLabel
-                id="title-input"
-                name="title"
-                label="Título"
-                value={formState.title}
-                onChange={(e) => updateField('title', e.target.value)}
-              />
-              <InputWithLabel
-                id="process-number-input"
-                name="processNumber"
-                label="Número do Processo"
-                value={formState.processNumber}
-                onChange={(e) => updateField('processNumber', e.target.value)}
-              />
-            </div>
-            <div className="flex flex-col gap-[16px] min-lg:flex-row w-full">
-              <InputWithLabel
-                id="court-input"
-                name="court"
-                label="Tribunal"
-                value={formState.court}
-                onChange={(e) => updateField('court', e.target.value)}
-              />
-              <InputWithLabel
-                id="court-division-input"
-                name="courtDivision"
-                label="Vara"
-                value={formState.courtDivision}
-                onChange={(e) => updateField('courtDivision', e.target.value)}
-              />
-              <DropdownInputWithLabel
-                id="status-input"
-                name="status"
-                label="Status"
-                itemLabel={CaseStatusLabel}
-                value={formState.status}
-                setSelectedValue={(value) => updateField('status', value)}
-              />
-            </div>
-            <div className="flex flex-col gap-[16px] min-lg:flex-row w-full">
-              <DropdownInputWithLabel
-                id="estado-input"
-                name="state"
-                label="Estado"
-                itemLabel={BrazilStateLabel}
-                value={formState.state}
-                setSelectedValue={(value) => updateField('state', value)}
-              />
-              <DropdownInputWithLabel
-                id="city-input"
-                name="city"
-                label="Cidade"
-                itemLabel={CityLabel}
-                value={formState.city}
-                setSelectedValue={(value) => updateField('city', value)}
-              />
-            </div>
-            <div>
-              <InputWithLabel
-                id="description-input"
-                name="description"
-                label="Description"
-                value={formState.description}
-                onChange={(e) => updateField('description', e.target.value)}
-              />
-            </div>
-          </form>
+  return (
+    <div className="flex flex-col overflow-y-auto p-[24px] h-[56vh]">
+      <RequestFeedback requestState={requestState} />
+      <form id={formId} className="flex flex-col gap-[16px] w-ful]" onSubmit={registerCase}>
+        <div className="flex flex-col gap-[16px] min-lg:flex-row w-full">
+          <InputWithLabel
+            id="title-input"
+            name="title"
+            label="Título"
+            value={formState.title}
+            onChange={(e) => updateField('title', e.target.value)}
+          />
+          <InputWithLabel
+            id="process-number-input"
+            name="processNumber"
+            label="Número do Processo"
+            value={formState.processNumber}
+            onChange={(e) => updateField('processNumber', e.target.value)}
+          />
         </div>
-      );
-
-    default:
-      return null;
-  }
+        <div className="flex flex-col gap-[16px] min-lg:flex-row w-full">
+          <InputWithLabel
+            id="court-input"
+            name="court"
+            label="Tribunal"
+            value={formState.court}
+            onChange={(e) => updateField('court', e.target.value)}
+          />
+          <InputWithLabel
+            id="court-division-input"
+            name="courtDivision"
+            label="Vara"
+            value={formState.courtDivision}
+            onChange={(e) => updateField('courtDivision', e.target.value)}
+          />
+          <DropdownInputWithLabel
+            id="status-input"
+            name="status"
+            label="Status"
+            itemLabel={CaseStatusLabel}
+            value={formState.status}
+            setSelectedValue={(value) => updateField('status', value)}
+          />
+        </div>
+        <div className="flex flex-col gap-[16px] min-lg:flex-row w-full">
+          <DropdownInputWithLabel
+            id="estado-input"
+            name="state"
+            label="Estado"
+            itemLabel={BrazilStateLabel}
+            value={formState.state}
+            setSelectedValue={(value) => updateField('state', value)}
+          />
+          <DropdownInputWithLabel
+            id="city-input"
+            name="city"
+            label="Cidade"
+            itemLabel={CityLabel}
+            value={formState.city}
+            setSelectedValue={(value) => updateField('city', value)}
+          />
+        </div>
+        <div>
+          <InputWithLabel
+            id="description-input"
+            name="description"
+            label="Description"
+            value={formState.description}
+            onChange={(e) => updateField('description', e.target.value)}
+          />
+        </div>
+      </form>
+    </div>
+  );
 }
