6c9f803acbd4c089a5f5a149780252fc1e7cc9e3
DavidSheltonSF <david.faria012@gmail.com>
2026-06-02 19:24:14 -0300

    refactor(DashboardSection): replace context with AuthStore to get the current user data

--- frontend/components/DashboardSection.tsx
+++ frontend/components/DashboardSection.tsx
@@ -5,8 +5,6 @@ import { useEffect, useState } from 'react';
 import { mockPromise } from '@/test/mockPromise';
 import { DashboardCardSkeleton } from './DashboardCardSkeleton';
 import { getMyCasesStats } from '@/services/cases/getMyCasesStats';
-import { useAuthenticatedUserContext } from '@/hooks/useAuthenticatedUserContext';
-import { MissingContextError } from '@/errors/MissingContextError';
 import { UserRole } from '@/types/UserRole';
 import { getCasesStats } from '@/services/cases/getCasesStats';
 import { RequestState } from '@/types/RequestState';
@@ -14,58 +12,56 @@ import { handleLogout } from '@/lib/handleLogout';
 import { CasesStats } from '@/types/CasesStats';
 import { DashboardSkeleton } from './features/deashboard/DashboardSkeleton';
 import { Dashboard } from './features/deashboard/Dashboard';
+import { useAuthStore } from '@/stores/useAuthStore';
 
 export function DashboardSection() {
-  const [requestState, setRequestState] = useState<RequestState<CasesStats>>({ status: 'idle' });
+  const [requestState, setRequestState] = useState<RequestState<CasesStats>>({ status: 'loading' });
   const isLoading = requestState?.status === 'loading';
-
-  const context = useAuthenticatedUserContext();
-  if (!context) {
-    throw new MissingContextError('AuthenticatedUserContext');
-  }
-
-  const authUserData = context.userData;
-
-  const userRole = authUserData.role;
-
-  async function fetchCases() {
-    let response = null;
-
-    setRequestState({ status: 'loading' });
-
-    switch (userRole) {
-      case UserRole.admin:
-      case UserRole.lawyer:
-        response = await getCasesStats();
-        break;
-
-      case UserRole.client:
-        response = await getMyCasesStats();
-        break;
-    }
-
-    if (!response) {
-      return setRequestState({ status: 'error', message: 'Invalid role' });
-    }
-
-    if (!response.success) {
-      const { code, message, details } = response;
-      setRequestState({ status: 'error', code, message, details });
-      return;
-    }
-
-    setRequestState({ status: 'ok', data: response.data });
-  }
+  const user = useAuthStore((state) => state.user);
 
   useEffect(() => {
+    async function fetchCases() {
+      if (!user) return;
+      let response = null;
+
+      setRequestState({ status: 'loading' });
+
+      switch (user.role) {
+        case UserRole.admin:
+        case UserRole.lawyer:
+          response = await getCasesStats();
+          break;
+
+        case UserRole.client:
+          response = await getMyCasesStats();
+          break;
+      }
+
+      if (!response) {
+        return setRequestState({ status: 'error', message: 'Invalid role' });
+      }
+
+      if (!response.success) {
+        const { code, message, details } = response;
+        setRequestState({ status: 'error', code, message, details });
+        return;
+      }
+
+      setRequestState({ status: 'ok', data: response.data });
+    }
+
     fetchCases();
-  }, []);
+  }, [user]);
 
   useEffect(() => {
     if (requestState?.status === 'error') {
       requestState.code === 'UNAUTHORIZED' && handleLogout();
     }
   }, [requestState]);
+
+  if (!user) {
+    return <DashboardSkeleton />;
+  }
 
   switch (requestState.status) {
     case 'idle':
