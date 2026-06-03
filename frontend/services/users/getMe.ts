5a11f5a8abe68da791eaad484d4a0d4a42e41fbc
DavidSheltonSF <david.faria012@gmail.com>
2026-06-03 18:29:46 -0300

    chore(frontend/getMe): make the response with makeActionResponse for consistency

--- frontend/services/users/getMe.ts
+++ frontend/services/users/getMe.ts
@@ -3,14 +3,9 @@ import { User } from '@/types/User';
 import { WithId } from '@/types/WithId';
 import { apiFetch } from '../apiFetch';
 import { ActionResponse } from '@/types/ActionResponse';
+import { makeActionResponse } from '@/factories/makeActionResponse';
 
-export async function getMe(): Promise<ActionResponse<WithId<User | null>>> {
+export async function getMe(): Promise<ActionResponse<WithId<User>>> {
   const response = await apiFetch(`${API_URL}/me`);
-  const json = await response.json();
-
-  if (!response.ok) {
-    return { success: false, message: json.message, code: json.code };
-  }
-
-  return { success: true, data: json.data };
+  return makeActionResponse(response);
 }
