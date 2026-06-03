f3713223e5b157a95f4bd0a748dd30e2f443abf3
DavidSheltonSF <david.faria012@gmail.com>
2026-06-02 23:28:33 -0300

    refactor(frontend/getUserById): update to return ActionResponse for consistency

--- frontend/services/users/getUserById.ts
+++ frontend/services/users/getUserById.ts
@@ -1,20 +1,16 @@
 import { API_URL } from '@/config/api';
-import { InvalidAPIResponseError } from '@/errors/InvalidAPIResponseError';
 import { MissingRequiredArgumentError } from '@/errors/MissingRequiredArgumentError';
 import { User } from '@/types/User';
 import { WithId } from '@/types/WithId';
 import { apiFetch } from '../apiFetch';
+import { ActionResponse } from '@/types/ActionResponse';
+import { makeActionResponse } from '@/factories/makeActionResponse';
 
-export async function getUserById(id: string): Promise<WithId<User>> {
+export async function getUserById(id: string): Promise<ActionResponse<WithId<User>>> {
   if (!id) {
     throw new MissingRequiredArgumentError('getUserById', 'id');
   }
 
   const response = await apiFetch(`${API_URL}/users/${id}`);
-  const json = await response.json();
-
-  if (!json?.data) {
-    throw new InvalidAPIResponseError('Missing data', json);
-  }
-  return json.data;
+  return makeActionResponse(response);
 }
