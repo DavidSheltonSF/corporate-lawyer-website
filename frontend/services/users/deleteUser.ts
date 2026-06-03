87ccf8d7adc532ae6f47d876021a5f27af3178ec
DavidSheltonSF <david.faria012@gmail.com>
2026-06-03 17:25:09 -0300

    refactor(frontend/deleteUser): update to return ActionResponse for consistency

--- frontend/services/users/deleteUser.ts
+++ frontend/services/users/deleteUser.ts
@@ -2,11 +2,12 @@ import { API_URL } from '@/config/api';
 import { SafeUser } from '@/types/SafeUser';
 import { WithId } from '@/types/WithId';
 import { apiFetch } from '../apiFetch';
+import { makeActionResponse } from '@/factories/makeActionResponse';
+import { ActionResponse } from '@/types/ActionResponse';
 
-export async function deleteUser(id: string): Promise<WithId<SafeUser>> {
+export async function deleteUser(id: string): Promise<ActionResponse<WithId<SafeUser>>> {
   const response = await apiFetch(`${API_URL}/users/${id}`, {
     method: 'DELETE',
   });
-  const json = await response.json();
-  return json.data;
+  return makeActionResponse(response);
 }
