20bf121df7c623f08cc84349e0118cfb1496a499
DavidSheltonSF <david.faria012@gmail.com>
2026-06-02 23:13:03 -0300

    refactor(ClientModal): handle action responses and render UI based on state

--- frontend/components/features/clients/ClientModal/ClientModal.tsx
+++ frontend/components/features/clients/ClientModal/ClientModal.tsx
@@ -26,39 +26,29 @@ ClientModal.Info = ClientModalInfo;
 ClientModal.Cases = ClientModalCases;
 
 export function ClientModal({ payload, close }: GlobalModalProps<Props>) {
-  const [clientData, setClientData] = useState<(SafeUser & { cases: WithId<Case>[] }) | null>(null);
   const [registerCaseModalIsOpen, setRegisterCaseModalIsOpen] = useState(false);
-  const [requestState, setRequestState] = useState<RequestState | null>(null);
+  const [requestState, setRequestState] = useState<
+    RequestState<SafeUser & { cases: WithId<Case>[] }>
+  >({ status: 'idle' });
   const isLoading = requestState?.status === 'loading';
   const error = requestState?.status === 'error';
   const { clientId } = payload;
 
   useEffect(() => {
     async function fetchClientData() {
-      try {
-        if (!clientId) return;
-        setRequestState({ status: 'loading' });
-        const clientFound = await getClientWithCases(clientId);
-        setClientData(clientFound);
-        setRequestState({ status: 'ok' });
-      } catch (error: any) {
-        console.log(error);
-        setRequestState({ status: 'error', message: error.message });
+      const response = await getClientWithCases(clientId);
 
-        if (error instanceof UnauthorizedError) {
-          handleLogout();
-        }
+      if (!response.success) {
+        setRequestState({ ...response, status: 'error' });
+        return;
       }
-    }
 
-    function resetStates() {
-      setClientData(null);
-      close;
+      setRequestState({ status: 'ok', data: response.data });
     }
 
     fetchClientData();
     return () => {
-      resetStates;
+      setRequestState({ status: 'idle' });
     };
   }, []);
 
@@ -75,35 +65,40 @@ export function ClientModal({ payload, close }: GlobalModalProps<Props>) {
   }
 
   function renderContent() {
-    if (isLoading) {
-      return <CaseModalSkeleton />;
-    }
+    switch (requestState.status) {
+      case 'loading':
+        return <CaseModalSkeleton />;
 
-    if (error || !clientData) {
-      return (
-        <div className="flex flex-col items-center size-ful pt-[80px] px-[24px] text-center gap-[16px]">
-          <h1>Cliente não encontrado</h1>
-          <h3>O cliente procurado foi removido do sistema ou não existe</h3>
-        </div>
-      );
-    }
+      case 'ok':
+        const { data } = requestState;
+        return (
+          <div className="flex flex-col size-full">
+            <ClientModal.Header
+              firstName={data.firstName}
+              lastName={data.lastName}
+              cpf={data.cpf}
+            />
+            <ClientModal.Info clientData={data} />
+            <ClientModal.Cases
+              cases={data.cases}
+              openRegisterCaseModal={() => {
+                setRegisterCaseModalIsOpen(true);
+              }}
+            />
+          </div>
+        );
 
-    return (
-      <div className="flex flex-col size-full">
-        <ClientModal.Header
-          firstName={clientData.firstName}
-          lastName={clientData.lastName}
-          cpf={clientData.cpf}
-        />
-        <ClientModal.Info clientData={clientData} />
-        <ClientModal.Cases
-          cases={clientData.cases}
-          openRegisterCaseModal={() => {
-            setRegisterCaseModalIsOpen(true);
-          }}
-        />
-      </div>
-    );
+      case 'error':
+        return (
+          <div className="flex flex-col items-center size-ful pt-[80px] px-[24px] text-center gap-[16px]">
+            <h1>Cliente não encontrado</h1>
+            <h3>O cliente procurado foi removido do sistema ou não existe</h3>
+          </div>
+        );
+
+      default:
+        break;
+    }
   }
 
   return (
