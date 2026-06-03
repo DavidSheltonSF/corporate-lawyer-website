3fcb63cbe380759929a36dff8701478084e5bbad
DavidSheltonSF <david.faria012@gmail.com>
2026-06-02 23:12:05 -0300

    refactor(frontend/getClientWithCases): update to return ActionResponse for consistency

--- frontend/services/users/getClientWithCases.ts
+++ frontend/services/users/getClientWithCases.ts
@@ -3,11 +3,12 @@ import { Case } from '@/types/Case';
 import { SafeUser } from '@/types/SafeUser';
 import { apiFetch } from '../apiFetch';
 import { WithId } from '@/types/WithId';
+import { ActionResponse } from '@/types/ActionResponse';
+import { makeActionResponse } from '@/factories/makeActionResponse';
 
 export async function getClientWithCases(
   id: string
-): Promise<SafeUser & { cases: WithId<Case>[] }> {
+): Promise<ActionResponse<SafeUser & { cases: WithId<Case>[] }>> {
   const response = await apiFetch(`${API_URL}/clients/${id}?include=cases`);
-  const json = await response.json();
-  return json.data;
+  return makeActionResponse(response);
 }
