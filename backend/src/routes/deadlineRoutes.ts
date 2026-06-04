c46e7dbb954d364c5cd80f9b1f14388574ba4656
DavidSheltonSF <david.faria012@gmail.com>
2026-06-03 23:05:45 -0300

    feat(backend/deadlineRoutes): add  route to delete deadline by id

--- backend/src/routes/deadlineRoutes.ts
+++ backend/src/routes/deadlineRoutes.ts
@@ -10,4 +10,5 @@ export function deadlineRoutes(router: Router, deadlineController: IDeadlineCont
     requireAuth,
     expressHttpAdapter(deadlineController.findByCaseId)
   );
+  router.delete('/api/deadlines/:id', expressHttpAdapter(deadlineController.deleteById));
 }
