372f3610b8846ae174ded142c8772c0859e8c80b
DavidSheltonSF <david.faria012@gmail.com>
2026-06-03 21:25:41 -0300

    refactor(frontend/getMyNotifications): return ActionResponse for consistency

--- frontend/services/notifications/getMyNotifications.ts
+++ frontend/services/notifications/getMyNotifications.ts
@@ -3,10 +3,14 @@ import { WithId } from '@/types/WithId';
 import { apiFetch } from '../apiFetch';
 import { Notification } from '@/types/Notification';
 import { Page } from '@/types/Page';
+import { ActionResponse } from '@/types/ActionResponse';
+import { makeActionResponse } from '@/factories/makeActionResponse';
 
-export async function getMyNotifications(page: number, limit: number): Promise<Page<WithId<Notification>>> {
+export async function getMyNotifications(
+  page: number,
+  limit: number
+): Promise<ActionResponse<Page<WithId<Notification>>>> {
   const baseRoute = `${API_URL}/my/notifications`;
   const response = await apiFetch(`${baseRoute}?page=${page}&limit=${limit}`);
-  const responseJson = await response.json();
-  return responseJson.data;
+  return makeActionResponse(response);
 }
