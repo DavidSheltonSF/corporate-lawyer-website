7767d1bb2734fe03314cb49b7a78181bc66e9436
DavidSheltonSF <david.faria012@gmail.com>
2026-06-03 22:17:32 -0300

    refactor(DeadlinesModal): simplify deadlines counter label

--- frontend/components/features/deadlines/DeadlineModal/DeadlineModalContent.tsx
+++ frontend/components/features/deadlines/DeadlineModal/DeadlineModalContent.tsx
@@ -16,7 +16,7 @@ export function DeadlineModalContent({ data, openCreateModal }: Props) {
   return (
     <div className="flex flex-col h-full">
       <div className="flex flex-col min-lg:flex-row min-lg:items-center p-[24px] border-divider gap-[16px] min-lg:gap-0">
-        <span className="font-bold">Quantidade: {data.length}</span>
+        <span className="font-bold">Prazos ({data.length})</span>
         <Button
           onClick={openCreateModal}
           className="border border-black bg-color-white hover:brightness-95 min-lg:ml-auto"
