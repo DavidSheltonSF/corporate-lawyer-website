26f80dc2f469ecff9bb3f784e0ae3c3dec55aa46
DavidSheltonSF <david.faria012@gmail.com>
2026-06-05 00:39:44 -0300

    fix(useDeleteClientModal): add refetchClients param and pass it to openModal

--- frontend/hooks/modals/useDeleteClientModal.ts
+++ frontend/hooks/modals/useDeleteClientModal.ts
@@ -4,8 +4,8 @@ import { WithId } from '@/types/WithId';
 
 export function useDeleteClientModal() {
   const { openModal } = useModalStore();
-  function openDeleteClientModal(clientSlice: WithId<UserSlice>) {
-    openModal({ type: 'delete-client', payload: { clientSlice } });
+  function openDeleteClientModal(clientSlice: WithId<UserSlice>, refetchClients: () => void) {
+    openModal({ type: 'delete-client', payload: { clientSlice, refetchClients } });
   }
 
   return { openDeleteClientModal };
