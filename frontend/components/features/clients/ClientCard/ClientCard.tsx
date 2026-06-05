aac24e2f7dc9f83fa9ee6e8727c75442bda64682
DavidSheltonSF <david.faria012@gmail.com>
2026-06-05 00:34:05 -0300

    refactor(ClientCard): migrate modal handling from context to store hooks

--- frontend/components/features/clients/ClientCard/ClientCard.tsx
+++ frontend/components/features/clients/ClientCard/ClientCard.tsx
@@ -3,10 +3,12 @@ import { SafeUser } from '@/types/SafeUser';
 import { Card } from '../../../ui/Card/Card';
 import { ClientCardHeader } from './ClientCardHeader';
 import { ClientCardFooter } from './ClientCardFooter';
-import { useModal } from '@/hooks/useModal';
 import { makeCardAction } from '@/components/ui/CardDropdown/makeCardAction';
 import { CardActionType } from '@/components/ui/CardDropdown/types';
 import { ButtonVariant } from '@/components/ui/Button/ButtonVariant';
+import { useUpdateClientModal } from '@/hooks/modals/useUpdateClientModal';
+import { useDeleteClientModal } from '@/hooks/modals/useDeleteClientModal';
+import { useClientModal } from '@/hooks/modals/useClientModal';
 
 interface Props {
   clientData: WithId<SafeUser>;
@@ -18,33 +20,21 @@ ClientCard.Footer = ClientCardFooter;
 
 export function ClientCard({ clientData, fetchClients }: Props) {
   const { id, firstName, lastName, email, phone, cpf } = clientData;
-  const { openModal } = useModal();
 
-  function openEditModal() {
-    openModal('update-client', {
-      clientId: id,
-      refetchClients: fetchClients,
-    });
-  }
-
-  function openDeleteModal() {
-    openModal('delete-client', {
-      clientSlice: { id, firstName, lastName },
-      refetchClients: fetchClients,
-      variant: ButtonVariant.DANGER,
-    });
-  }
+  const { openUpdateClientModal } = useUpdateClientModal();
+  const { openDeleteClientModal } = useDeleteClientModal();
+  const { openClientModal } = useClientModal();
 
   return (
     <Card
       className="w-full h-fit min-md:w-[720px]"
       actions={[
-        makeCardAction(CardActionType.EDIT, openEditModal),
-        makeCardAction(CardActionType.DELETE, openDeleteModal),
+        makeCardAction(CardActionType.EDIT, () => openUpdateClientModal(id, fetchClients)),
+        makeCardAction(CardActionType.DELETE, () =>
+          openDeleteClientModal({ id, firstName, lastName })
+        ),
       ]}
-      onClick={() => {
-        openModal('client', { clientId: id });
-      }}
+      onClick={() => openClientModal(id)}
     >
       <div className="flex flex-col text-color-black p-[24px] gap-[32px]">
         <ClientCard.Header clientName={`${firstName} ${lastName}`} cpf={cpf} />
