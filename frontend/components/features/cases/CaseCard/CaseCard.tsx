e88751f5fc7a8859f71b78b157d1963194433589
DavidSheltonSF <david.faria012@gmail.com>
2026-06-05 00:29:30 -0300

    refactor(CaseCard): migrate modal handling from context to store hooks

--- frontend/components/features/cases/CaseCard/CaseCard.tsx
+++ frontend/components/features/cases/CaseCard/CaseCard.tsx
@@ -6,10 +6,14 @@ import { CaseCardHeader } from './CaseCardHeader';
 import { CaseCardFooter } from './CaseCardFooter';
 import { EditIcon } from '@/components/icons/EditIcon';
 import { DeleteIcon } from '@/components/icons/DeleteIcon';
-import { useModal } from '@/hooks/useModal';
 import { makeCardAction } from '@/components/ui/CardDropdown/makeCardAction';
 import { CardActionType } from '@/components/ui/CardDropdown/types';
 import { ButtonVariant } from '@/components/ui/Button/ButtonVariant';
+import { useCaseModal } from '@/hooks/modals/useCaseModal';
+import { useDeadlinesModal } from '@/hooks/modals/useDeadlinesModal';
+import { useCaseFilesModal } from '@/hooks/modals/useCaseFilesModal';
+import { useConfirmModal } from '@/hooks/modals/useConfirmModal';
+import { useUpdateCaseModal } from '@/hooks/modals/useUpdateCaseModal';
 
 interface Props {
   caseData: WithId<CaseWithRelations>;
@@ -24,46 +28,37 @@ export function CaseCard({ caseData, deleteCase, refetchCases }: Props) {
   const clientData = caseData.client;
   const { firstName, lastName } = clientData;
   const { id, status } = caseData;
-  const { openModal } = useModal();
+  const { openCaseModal } = useCaseModal();
+  const { openDeadlinesModal } = useDeadlinesModal();
+  const { openCaseFilesModal } = useCaseFilesModal();
+  const { openConfirmModal } = useConfirmModal();
+  const { openUpdateCaseModal } = useUpdateCaseModal();
 
   const lawyersNames = caseData.lawyers?.map((lawyer) => {
     return `${lawyer.firstName} ${lawyer.lastName}`;
   });
 
-  function openUpdateCaseModal() {
-    openModal('update-case', { caseId: id, refetchCases });
-  }
-
-  function openConfirmModal() {
-    openModal('confirm', {
-      title: 'Excluir processo',
-      message: 'Tem certeza que deseja excluir este processo? Essa ação não poderá ser desfeita.',
-      confirmButtonVariant: ButtonVariant.DANGER,
-      onConfirm: async () => {
-        await deleteCase(id);
-        refetchCases();
-      },
-    });
-  }
-
-  function openDeadlinesModal() {
-    openModal('deadlines', { caseId: caseData.id });
-  }
-
-  function openFilesModal() {
-    openModal('case-files', { caseId: caseData.id });
-  }
-
   return (
     <Card
       actions={[
-        makeCardAction(CardActionType.EDIT, openUpdateCaseModal),
-        makeCardAction(CardActionType.DELETE, openConfirmModal),
-        makeCardAction(CardActionType.CHECK_DEADLINES, openDeadlinesModal),
-        makeCardAction(CardActionType.CHECK_FILES, openFilesModal),
+        makeCardAction(CardActionType.EDIT, () => openUpdateCaseModal(id, refetchCases)),
+        makeCardAction(CardActionType.DELETE, () =>
+          openConfirmModal({
+            title: 'Excluir processo',
+            message:
+              'Tem certeza que deseja excluir este processo? Essa ação não poderá ser desfeita.',
+            confirmButtonVariant: ButtonVariant.DANGER,
+            onConfirm: async () => {
+              deleteCase(id);
+              refetchCases();
+            },
+          })
+        ),
+        makeCardAction(CardActionType.CHECK_DEADLINES, () => openDeadlinesModal(id)),
+        makeCardAction(CardActionType.CHECK_FILES, () => openCaseFilesModal(id)),
       ]}
       className="relative w-full h-fit min-md:w-[720px]"
-      onClick={() => openModal('case', { caseId: id })}
+      onClick={() => openCaseModal(id)}
     >
       <div className="flex flex-col text-color-black p-[24px] gap-[32px]">
         <CaseCard.Header title={caseData.title} processNumber={caseData.processNumber} />
