b7a45402a5a19b37832e59185187355d16016eed
DavidSheltonSF <david.faria012@gmail.com>
2026-06-05 23:43:20 -0300

    refactor(DropdownInput): migrate custom dropdown positioning logic to Floating UI

--- frontend/components/ui/Input/DropdownInput.tsx
+++ frontend/components/ui/Input/DropdownInput.tsx
@@ -2,6 +2,7 @@ import { useEffect, useRef, useState } from 'react';
 import { ArrowDropUpIcon } from '../../icons/ArrowDropUpIcon';
 import { Input } from './Input';
 import { InputProps } from './types';
+import { useFloating, size, offset, autoUpdate } from '@floating-ui/react';
 
 interface Props {
   setSelectedValue: (value: string) => void;
@@ -16,6 +17,20 @@ export function DropdownInput({
 }: Props & InputProps) {
   const [listIsOpen, setListIsOpen] = useState(false);
   const dropRef = useRef<HTMLDivElement>(null);
+  const { refs, floatingStyles } = useFloating({
+    placement: 'bottom',
+    whileElementsMounted: autoUpdate,
+    middleware: [
+      offset(0),
+      size({
+        apply({ rects, elements }) {
+          Object.assign(elements.floating.style, {
+            width: `${rects.reference.width}px`,
+          });
+        },
+      }),
+    ],
+  });
 
   useEffect(() => {
     function handleClickOutside(e: MouseEvent) {
@@ -47,51 +62,52 @@ export function DropdownInput({
   });
 
   return (
-    <div className="w-full">
-      <div ref={dropRef} className="flex flex-col relative">
-        <div
-          className="flex jusfity-center items-center border h-fit w-full rounded-sm "
-          onClick={() => setListIsOpen(true)}
-        >
-          <Input
-            className="w-full  border-none"
-            type="text"
-            value={itemLabel[value || ''] ?? ''}
-            readOnly
-            {...inputProps}
-          />
+    <div ref={dropRef}>
+      <div
+        ref={refs.setReference}
+        className="flex jusfity-center items-center border h-fit w-full rounded-sm "
+        onClick={() => setListIsOpen(true)}
+      >
+        <Input
+          className="w-full  border-none"
+          type="text"
+          value={itemLabel[value || ''] ?? ''}
+          readOnly
+          {...inputProps}
+        />
 
-          <button
-            type="button"
-            className="flex items-center justify-center rounded-md size-[32px] bg-white hover:brightness-90 cursor-pointer"
-            onClick={() => setListIsOpen(!listIsOpen)}
-          >
-            <span className={`transition-[rotate] duration-300 ${!listIsOpen && 'rotate-180'}`}>
-              <ArrowDropUpIcon className="size-[24px]" />
-            </span>
-          </button>
-        </div>
-        <div
-          className={`transition-[height] duration-300 absolute z-1 w-full bg-white overflow-hidden w-full top-[100%]  ${
-            listIsOpen && 'py-[8px] overflow-y-auto shadow-soft'
-          }`}
-          style={{
-            maxHeight: listIsOpen ? 5 * 32 : 0,
-          }}
+        <button
+          type="button"
+          className="flex items-center justify-center rounded-md size-[32px] bg-white hover:brightness-90 cursor-pointer"
+          onClick={() => setListIsOpen(!listIsOpen)}
         >
-          <ul>
-            <li
-              onClick={() => {
-                setSelectedValue('');
-                setListIsOpen(false);
-              }}
-              className="bg-white hover:brightness-80 cursor-pointer px-[8px]"
-            >
-              ...
-            </li>
-            {renderItems}
-          </ul>
-        </div>
+          <span className={`transition-[rotate] duration-300 ${!listIsOpen && 'rotate-180'}`}>
+            <ArrowDropUpIcon className="size-[24px]" />
+          </span>
+        </button>
+      </div>
+      <div
+        ref={refs.setFloating}
+        className={`transition-[max-height] transition-[padding] duration-300 bg-white overflow-hidden rounded-sm ${
+          listIsOpen && 'overflow-y-auto shadow-soft py-[8px] z-10'
+        }`}
+        style={{
+          maxHeight: listIsOpen ? 5 * 32 : 0,
+          ...floatingStyles,
+        }}
+      >
+        <ul>
+          <li
+            onClick={() => {
+              setSelectedValue('');
+              setListIsOpen(false);
+            }}
+            className="bg-white hover:brightness-80 cursor-pointer px-[8px]"
+          >
+            ...
+          </li>
+          {renderItems}
+        </ul>
       </div>
     </div>
   );
