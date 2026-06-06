e3b4eff4254fdb7bfd22778f2d88e266abecff32
DavidSheltonSF <david.faria012@gmail.com>
2026-06-06 18:21:19 -0300

    refactor(CardMoreButton): add ref optional props

--- frontend/components/ui/Card/CardMoreButton.tsx
+++ frontend/components/ui/Card/CardMoreButton.tsx
@@ -1,18 +1,16 @@
 import { VerticalMoreIcon } from '@/components/icons/VerticalMoreIcon';
 import { Button } from '../Button/Button';
-import { MouseEvent } from 'react';
+import { MouseEvent, Ref } from 'react';
 
 interface Props {
-  className: string
+  ref: Ref<HTMLButtonElement>;
+  className: string;
   onClick: (e: MouseEvent<HTMLButtonElement>) => void;
 }
 
-export function CardMoreButton({ onClick, className }: Props) {
+export function CardMoreButton({ ref, onClick, className }: Props) {
   return (
-    <Button
-      className={className}
-      onClick={onClick}
-    >
+    <Button ref={ref} className={className} onClick={onClick}>
       <VerticalMoreIcon className="size-[32px] stroke-color-black group-hover:invert" />
     </Button>
   );
