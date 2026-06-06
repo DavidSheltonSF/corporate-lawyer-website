cbb9bc8fab40a641d11199a30f84c9833677138f
DavidSheltonSF <david.faria012@gmail.com>
2026-06-06 18:46:56 -0300

    refactor(DeadlineCard): allow deadline deletion

--- frontend/components/features/deadlines/DeadLineCard/DeadlineCard.tsx
+++ frontend/components/features/deadlines/DeadLineCard/DeadlineCard.tsx
@@ -4,14 +4,52 @@ import { Deadline } from '@/types/Deadline';
 import { WithId } from '@/types/WithId';
 import { RemainingDaysBadge } from '../badges/RemainingDaysBadge';
 import { PriorityBadge } from '../badges/PriorityBadge';
+import { makeCardAction } from '@/components/ui/CardDropdown/makeCardAction';
+import { CardActionType } from '@/components/ui/CardDropdown/types';
+import { deleteDeadline } from '@/services/cases/deleteDeadline';
+import { useErrorModal } from '@/hooks/modals/useErrorModal';
+import { useSuccessModal } from '@/hooks/modals/useSuccessModal';
+import { useConfirmModal } from '@/hooks/modals/useConfirmModal';
+import { ButtonVariant } from '@/components/ui/Button/ButtonVariant';
+import { handleLogout } from '@/lib/handleLogout';
 
 interface Props {
   deadline: WithId<Deadline>;
 }
 
 export function DeadlineCard({ deadline }: Props) {
+  const { openErrorModal } = useErrorModal();
+  const { openSuccessModal } = useSuccessModal();
+  const { openConfirmModal } = useConfirmModal();
+  async function handleDeleteDeadline() {
+    const response = await deleteDeadline(deadline.id);
+
+    if (!response.success) {
+      if (response.code === 'UNAUTHORIZED') {
+        handleLogout();
+      }
+      openErrorModal(response.message);
+      return;
+    }
+
+    openSuccessModal('Prazo removido com sucesso!');
+  }
+
   return (
-    <Card key={deadline.id} className="border-divider rounded-none w-full p-[24px]">
+    <Card
+      key={deadline.id}
+      className="border-divider rounded-none w-full p-[24px]"
+      actions={[
+        makeCardAction(CardActionType.DELETE, () =>
+          openConfirmModal({
+            title: 'Remover prazo',
+            message: 'Deseja realmente remover este prazo? Esta ação não poderá ser desfeita',
+            onConfirm: handleDeleteDeadline,
+            confirmButtonVariant: ButtonVariant.DANGER,
+          })
+        ),
+      ]}
+    >
       <div className="flex flex-col min-md:flex-row min-md:justify-between min-md:items-end gap-[16px] text-sm min-md:text-md">
         <div className="flex flex-col gap-[8px]">
           <p className="font-bold text-base">{deadline.type}</p>
