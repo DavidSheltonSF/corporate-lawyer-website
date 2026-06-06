504234d5e4818933d2064d9af31499e35be206ca
DavidSheltonSF <david.faria012@gmail.com>
2026-06-06 18:46:31 -0300

    fix(deadlineRoutes): use requireAuth middleware in the DELETE route

--- backend/src/routes/deadlineRoutes.ts
+++ backend/src/routes/deadlineRoutes.ts
@@ -10,5 +10,5 @@ export function deadlineRoutes(router: Router, deadlineController: IDeadlineCont
     requireAuth,
     expressHttpAdapter(deadlineController.findByCaseId)
   );
-  router.delete('/api/deadlines/:id', expressHttpAdapter(deadlineController.deleteById));
+  router.delete('/api/deadlines/:id', requireAuth, expressHttpAdapter(deadlineController.deleteById));
 }
