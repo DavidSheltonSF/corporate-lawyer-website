18923de658a6309bcde3edef68717af3b2b79cb6
DavidSheltonSF <david.faria012@gmail.com>
2026-06-09 17:36:17 -0300

    feat(hooks): add usePermissions

---
+++ frontend/hooks/auth/usePermissions.ts
@@ -0,0 +1,28 @@
+import { UserPermissions } from '@/types/UserPermissions';
+import { useUserRole } from './useUserRole';
+
+export function usePermissions(): UserPermissions {
+  const role = useUserRole();
+
+  const permittionsByRole: Record<string, UserPermissions> = {
+    admin: {
+      canDeleteCase: true,
+      canUpdateCase: true,
+      canSeeDeadlines: true,
+    },
+
+    lawyer: {
+      canDeleteCase: true,
+      canUpdateCase: true,
+      canSeeDeadlines: true,
+    },
+
+    client: {
+      canDeleteCase: false,
+      canUpdateCase: false,
+      canSeeDeadlines: false,
+    },
+  };
+
+  return permittionsByRole[role];
+}
