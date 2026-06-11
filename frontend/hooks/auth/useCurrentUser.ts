ad77ac3a1ff08e94dd138059de356ad30b51da1c
DavidSheltonSF <david.faria012@gmail.com>
2026-06-11 18:20:54 -0300

    refactor(useCurrentUser): update to accept and return a null user

--- frontend/hooks/auth/useCurrentUser.ts
+++ frontend/hooks/auth/useCurrentUser.ts
@@ -2,12 +2,6 @@ import { useAuthStore } from '@/stores/useAuthStore';
 import { User } from '@/types/User';
 import { WithId } from '@/types/WithId';
 
-export function useCurrentUser(): WithId<User> {
-  const user = useAuthStore((state) => state.user);
-
-  if (!user) {
-    throw Error('User not authenticated');
-  }
-
-  return user;
+export function useCurrentUser(): WithId<User> | null {
+  return useAuthStore((state) => state.user);
 }
