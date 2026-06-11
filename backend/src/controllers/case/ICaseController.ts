9318d4a401933009e10453c0e2f35fd0a678be84
DavidSheltonSF <david.faria012@gmail.com>
2026-06-10 21:52:39 -0300

    feat(CaseController): add findByClientId

--- backend/src/controllers/case/ICaseController.ts
+++ backend/src/controllers/case/ICaseController.ts
@@ -13,6 +13,7 @@ export interface ICaseController {
   create: (httpRequest: HttpRequest) => Promise<CreateResponse>;
   updateById: (httpRequest: HttpRequest) => Promise<CreateResponse>;
   findById: (httpRequest: HttpRequest) => Promise<FindByIdResponse>;
+  findByClientId: (httpRequest: HttpRequest) => Promise<FindByIdResponse>;
   findAll: (httpRequest: HttpRequest) => Promise<FindByClientResponse>;
   findMyCases: (httpRequest: HttpRequest) => Promise<FindByClientResponse>;
   getMyStats: (httpRequest: HttpRequest) => Promise<GetMyStatsResponse>;
