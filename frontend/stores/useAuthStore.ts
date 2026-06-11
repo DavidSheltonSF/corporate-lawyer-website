15c1884f7a9cb6d09556fba8f8152b64d63f36e0
DavidSheltonSF <david.faria012@gmail.com>
2026-06-11 18:18:02 -0300

    refactor(useAuthStore): update to not persist user anymore

--- frontend/stores/useAuthStore.ts
+++ frontend/stores/useAuthStore.ts
@@ -1,24 +1,18 @@
 import { User } from '@/types/User';
 import { WithId } from '@/types/WithId';
 import { create } from 'zustand';
-import { persist } from 'zustand/middleware';
 
 interface AuthStore {
   user: WithId<User> | null;
   setUser: (user: WithId<User>) => void;
 }
 
-export const useAuthStore = create<AuthStore>()(
-  persist(
-    (set) => ({
-      user: null,
+export const useAuthStore = create<AuthStore>((set) => ({
+  user: null,
 
-      setUser: (user: WithId<User> | null) => {
-        set({
-          user,
-        });
-      },
-    }),
-    { name: 'auth-storage' }
-  )
-);
+  setUser: (user: WithId<User> | null) => {
+    set({
+      user,
+    });
+  },
+}));
