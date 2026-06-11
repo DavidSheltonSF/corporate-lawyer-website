65a2bbb4fac6d4c21af849c99eb3329dccb13ab6
DavidSheltonSF <david.faria012@gmail.com>
2026-06-11 20:40:17 -0300

    refactor(Searchbar): replace defaultValue with value to make search inpu controlled

--- frontend/components/ui/Search/SearchBar.tsx
+++ frontend/components/ui/Search/SearchBar.tsx
@@ -3,23 +3,17 @@ import { SearchInput } from './SearchInput';
 import { SearchIcon } from '@/components/icons/SearchIcon';
 
 interface Props {
-  defaultValue: string;
+  value: string;
   onChange: ChangeEventHandler<HTMLInputElement>;
 }
 
 SearchBar.Input = SearchInput;
 
-export function SearchBar({ onChange, defaultValue }: Props) {
-
-
+export function SearchBar({ onChange, value }: Props) {
   return (
     <div className="flex items-center gap-[8px] bg-color-white w-full min-md:w-[70%] min-lg:w-[520px] h-[48px] rounded-full px-[8px]">
       <SearchIcon className="size-[32px] stroke-color-black" />
-      <SearchBar.Input
-      defaultValue={defaultValue}
-        placeholder="Pesquisar processo..."
-        onChange={onChange}
-      />
+      <SearchBar.Input value={value} placeholder="Pesquisar processo..." onChange={onChange} />
     </div>
   );
 }
--- frontend/components/ui/Search/SearchInput.tsx
+++ frontend/components/ui/Search/SearchInput.tsx
@@ -1,17 +1,17 @@
 interface Props {
-  defaultValue: string;
+  value: string
   placeholder: string;
   onChange: React.ChangeEventHandler<HTMLInputElement>;
 }
 
-export function SearchInput({ placeholder, defaultValue, onChange }: Props) {
+export function SearchInput({ placeholder, value, onChange }: Props) {
   return (
     <input
       name="caseSearchBar"
       className="flex flex-1 size-full placeholder:text-black/65 text-black"
       type="text"
       placeholder={placeholder}
-      defaultValue={defaultValue}
+      value={value}
       onChange={onChange}
     />
   );
