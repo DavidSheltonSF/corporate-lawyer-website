75dce5b85417e89d61e8c95cabf4ad7c2c874fc9
DavidSheltonSF <david.faria012@gmail.com>
2026-06-11 22:04:57 -0300

    feat(hooks): add useUrlParams

---
+++ frontend/hooks/url/useUrlParams.ts
@@ -0,0 +1,25 @@
+import { usePathname, useSearchParams } from 'next/navigation';
+import { useRouter } from 'next/navigation';
+
+export function useUrlParams() {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  function updateParam(key: string, value: string) {
+    const params = new URLSearchParams(searchParams);
+
+    if (value) {
+      params.set(key, value);
+    } else {
+      params.delete(key);
+    }
+
+    router.replace(`${pathname}?${params.toString()}`);
+  }
+
+  return {
+    searchParams,
+    updateParam,
+  };
+}
