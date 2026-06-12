ebe01ca22c33d0605b251f088c7d8fff8fc12363
DavidSheltonSF <david.faria012@gmail.com>
2026-06-11 22:26:07 -0300

    refactor(backend/types): add clientId to CaseQuery

--- backend/src/types/CaseQuery.ts
+++ backend/src/types/CaseQuery.ts
@@ -2,5 +2,6 @@ export interface CaseQuery {
   limit?: number | undefined;
   page?: number | undefined;
   query?: string | undefined;
+  clientId?: string | undefined;
   status?: string | undefined;
 }
