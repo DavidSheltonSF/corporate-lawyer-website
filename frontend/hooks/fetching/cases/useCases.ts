fe777f01a61bc27e633fd30353a8aacfde482330
DavidSheltonSF <david.faria012@gmail.com>
2026-06-14 19:51:52 -0300

    feat(hooks): add useCases TanStack Query hook

---
+++ frontend/hooks/fetching/cases/useCases.ts
@@ -0,0 +1,18 @@
+import { getCases } from '@/services/cases/getCases';
+import { GetCasesParams } from '@/services/cases/types';
+import { useQuery } from '@tanstack/react-query';
+
+
+export function useCases(userRole: string | null, params: GetCasesParams) {
+  console.log(userRole)
+  const { page, status, search, clientId } = params;
+
+  return useQuery({
+    queryKey: ['cases', page, status, search, clientId],
+    queryFn: () => {
+      return getCases(userRole!, params);
+    },
+    staleTime: 1000 * 60,
+    enabled: !!userRole,
+  });
+}
