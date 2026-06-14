f63e98db6ea0946354ef60aac4e8d7fe31537cbc
DavidSheltonSF <david.faria012@gmail.com>
2026-06-14 20:21:49 -0300

    refactor(CaseSearch):  migrate fetching logic to TanStack Query and update CasesList

--- frontend/components/features/cases/CaseSearch.tsx
+++ frontend/components/features/cases/CaseSearch.tsx
@@ -1,82 +1,34 @@
 'use client';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { SearchBar } from '../../ui/Search/SearchBar';
 import { CasesList } from './CasesList/CasesList';
-import { getMyCases } from '@/services/cases/getMyCases';
-import { WithId } from '@/types/WithId';
 import { Pagination } from '../../Pagination';
 import { CaseStatusLabel } from '@/lib/CaseStatusLabel';
-import { CaseWithRelations } from '@/types/CaseWithRelations';
-import { getCases } from '@/services/cases/getCases';
-import { handleLogout } from '@/lib/handleLogout';
-import { RequestState } from '@/types/RequestState';
-import { Page } from '@/types/Page';
 import { useUserRole } from '@/hooks/auth/useUserRole';
-import { CasesFetcher } from '@/services/cases/types';
 import { useCaseFilters } from '@/hooks/url/useCaseFilters';
 import { SearchFilter } from './SearchFilter';
 import { FilterTag } from '@/components/ui/FilterTag';
+import { useCases } from '@/hooks/fetching/cases/useCases';
 
 export default function CaseSearch() {
   const { search, setSearch, clientId, clearClientFilter, clientName } = useCaseFilters();
   const [searchText, setSearchText] = useState(search);
   const [status, setStatusFilter] = useState<string | null>(null);
   const [page, setPage] = useState(1);
-  const [totalPage, setTotalPage] = useState(0);
-  const [requestState, setRequestState] = useState<RequestState<Page<WithId<CaseWithRelations>>>>({
-    status: 'idle',
-  });
 
   const userRole = useUserRole();
 
-  async function fetchCases() {
-    setRequestState({ status: 'loading' });
-    if (!userRole) return;
+  const { data, error, isLoading } = useCases(userRole, {
+    page,
+    status: status || '',
+    search,
+    clientId,
+    limit: 4,
+  });
 
-    let casesFetcher: Record<string, CasesFetcher> = {
-      admin: getCases,
-      lawyer: getCases,
-      client: getMyCases,
-    };
-
-    const response = await casesFetcher[userRole]({
-      page,
-      limit: 4,
-      search,
-      status: status || '',
-      clientId,
-    });
-
-    if (!response.success) {
-      const { message, code, details } = response;
-      return setRequestState({
-        status: 'error',
-        message: message || 'Unespected error',
-        code,
-        details,
-      });
-    }
-
-    const { data } = response;
-
-    setRequestState({ status: 'ok', data: response.data });
-    setPage(data?.meta.currentPage || 1);
-    setTotalPage(data?.meta.totalPages || 1);
+  if (error) {
+    return null;
   }
-
-  useEffect(() => {
-    fetchCases();
-  }, [page, userRole, search, clientId, status, fetchCases]);
-
-  useEffect(() => {
-    setPage(1);
-  }, [search, clientId, status]);
-
-  useEffect(() => {
-    if (requestState?.status === 'error') {
-      requestState.code === 'UNAUTHORIZED' && handleLogout();
-    }
-  }, [requestState]);
 
   return (
     <section className="flex flex-col items-center size-full">
@@ -100,8 +52,12 @@ export default function CaseSearch() {
           {clientId && <FilterTag label={clientName} onClear={clearClientFilter} />}
         </div>
       </div>
-      <CasesList loadCases={fetchCases} requestState={requestState} />
-      <Pagination page={page} setPage={setPage} totalPage={totalPage} />
+      {isLoading || !data ? (
+        <CasesList.Skeleton />
+      ) : (
+        <CasesList loadCases={() => {}} cases={data.items} />
+      )}
+      <Pagination page={page} setPage={setPage} totalPage={data?.meta.totalPages || 0} />
     </section>
   );
 }
--- frontend/components/features/cases/CasesList/CasesList.tsx
+++ frontend/components/features/cases/CasesList/CasesList.tsx
@@ -1,23 +1,21 @@
 'use client';
 import { WithId } from '@/types/WithId';
 import { CaseWithRelations } from '@/types/CaseWithRelations';
-import { RequestState } from '@/types/RequestState';
 import { deleteCaseById } from '@/services/users/deleteCaseById';
-import { Page } from '@/types/Page';
 import { CasesListSkeleton } from './CasesListSkeleton';
 import { CasesListData } from './CasesListData';
 import { useErrorModal } from '@/hooks/modals/useErrorModal';
 import { useSuccessModal } from '@/hooks/modals/useSuccessModal';
 
 interface Props {
-  requestState: RequestState<Page<WithId<CaseWithRelations>>>;
+  cases: WithId<CaseWithRelations>[];
   loadCases: () => void;
 }
 
 CasesList.Skeleton = CasesListSkeleton;
 CasesList.Data = CasesListData;
 
-export function CasesList({ requestState, loadCases }: Props) {
+export function CasesList({ cases, loadCases }: Props) {
   const { openSuccessModal } = useSuccessModal();
   const { openErrorModal } = useErrorModal();
 
@@ -34,31 +32,9 @@ export function CasesList({ requestState, loadCases }: Props) {
     loadCases();
   }
 
-  function renderContent() {
-    switch (requestState?.status) {
-      case 'loading':
-        return <CasesList.Skeleton />;
-
-      case 'ok':
-        const { items } = requestState.data;
-        if (items.length === 0) {
-          return <h1>Nenhum processo encontrado</h1>;
-        }
-        return (
-          <CasesList.Data
-            data={items}
-            refetchCases={loadCases}
-            handleDeleteCase={handleDeleteCase}
-          />
-        );
-
-      case 'error':
-        return null;
-
-      default:
-        return null;
-    }
-  }
-
-  return <div className="flex flex-col gap-[32px] mt-[88px] w-full">{renderContent()}</div>;
+  return (
+    <div className="flex flex-col gap-[32px] mt-[88px] w-full">
+      <CasesList.Data data={cases} refetchCases={loadCases} handleDeleteCase={handleDeleteCase} />
+    </div>
+  );
 }
