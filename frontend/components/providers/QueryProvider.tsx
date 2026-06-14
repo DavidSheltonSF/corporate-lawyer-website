f86bd73ec10256136c855907176553babd1caa31
DavidSheltonSF <david.faria012@gmail.com>
2026-06-14 19:18:40 -0300

    feat(frontend/providers): add QueryProvider to provide QueryClient

---
+++ frontend/components/providers/QueryProvider.tsx
@@ -0,0 +1,18 @@
+'use client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { PropsWithChildren, useState } from 'react';
+
+export function QueryProvider({ children }: PropsWithChildren) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            retry: 1,
+          },
+        },
+      })
+  );
+
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+}
