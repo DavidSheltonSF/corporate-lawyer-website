bd23e96b36383729f13918e57abf14c0d5fea24e
DavidSheltonSF <david.faria012@gmail.com>
2026-06-14 20:50:50 -0300

    refactor(deleteCaseById): simplify function to return void and remove unused imports

--- frontend/services/users/deleteCaseById.ts
+++ frontend/services/users/deleteCaseById.ts
@@ -1,13 +1,8 @@
 import { API_URL } from '@/config/api';
 import { apiFetch } from '../apiFetch';
-import { makeActionResponse } from '@/factories/makeActionResponse';
-import { ActionResponse } from '@/types/ActionResponse';
-import { makeEmptyActionResponse } from '@/factories/makeEmptyActionResponse';
 
-export async function deleteCaseById(id: string): Promise<ActionResponse<null>> {
-  const response = await apiFetch(`${API_URL}/cases/${id}`, {
+export async function deleteCaseById(id: string): Promise<void> {
+  await apiFetch(`${API_URL}/cases/${id}`, {
     method: 'DELETE',
   });
-
-  return makeEmptyActionResponse(response);
 }
