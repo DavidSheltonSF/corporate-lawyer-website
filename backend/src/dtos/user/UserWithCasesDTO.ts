a05b8f9000cda81c6fe2a5892f782fe20ba9d4e9
DavidSheltonSF <david.faria012@gmail.com>
2026-06-14 16:26:57 -0300

    chore(dtos): remove unused UserWithCasesDTO and UserWithCasesResponseDTO

--- backend/src/dtos/user/UserWithCasesDTO.ts
+++
@@ -1,7 +0,0 @@
-import { WithId } from '../../types/WithId';
-import { CaseDTO } from '../case/CaseDTO';
-import { UserDTO } from './UserDTO';
-
-export type UserWithCasesDTO = UserDTO & {
-  cases: WithId<CaseDTO>[];
-};
--- backend/src/dtos/user/UserWithCasesResponseDTO.ts
+++
@@ -1,7 +0,0 @@
-import { WithId } from '../../types/WithId';
-import { CaseDTO } from '../case/CaseDTO';
-import { UserDTO } from './UserDTO';
-
-export type UserWithCasesResponseDTO = Omit<UserDTO, 'password'> & {
-  cases: WithId<CaseDTO>[];
-};
