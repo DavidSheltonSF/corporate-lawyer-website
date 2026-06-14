cfed87cf226c46c9b04cfb13a1ab6498de2f96ee
DavidSheltonSF <david.faria012@gmail.com>
2026-06-14 19:19:57 -0300

    chore(userRoutes): remove unused get clients by id route

--- backend/src/routes/usersRoutes.ts
+++ backend/src/routes/usersRoutes.ts
@@ -7,7 +7,6 @@ export function usersRoutes(router: Router, userController: IUserController) {
   router.get('/api/clients', requireAuth, expressHttpAdapter(userController.findClients));
   router.post('/api/clients', requireAuth, expressHttpAdapter(userController.createClient));
   router.get('/api/users/:id', requireAuth, expressHttpAdapter(userController.findById));
-  router.get('/api/clients/:id', requireAuth, expressHttpAdapter(userController.findClientById));
   router.put('/api/users/:id', requireAuth, expressHttpAdapter(userController.updateById));
   router.delete('/api/users/:id', requireAuth, expressHttpAdapter(userController.deleteById));
 }
