a286c7eb0d81814a6bade4c2c0159d9981e3bf2c
DavidSheltonSF <david.faria012@gmail.com>
2026-06-16 13:04:41 -0300

    refactor(updateCaseById): simplify function to return void and remove unused imports

--- frontend/services/cases/updateCaseById.ts
+++ frontend/services/cases/updateCaseById.ts
@@ -1,14 +1,8 @@
 import { API_URL } from '@/config/api';
-import { Case } from '@/types/Case';
-import { WithId } from '@/types/WithId';
 import { apiFetch } from '../apiFetch';
-import { ActionResponse } from '@/types/ActionResponse';
-import { makeActionResponse } from '@/factories/makeActionResponse';
 
-export async function updateCaseById(
-  id: string,
-  formData: Record<string, string>
-): Promise<ActionResponse<WithId<Case>>> {
+export async function updateCaseById( id: string,
+  formData: Record<string, string>): Promise<void> {
   const title = formData.title;
   const description = formData.description;
   const processNumber = formData.processNumber;
@@ -18,8 +12,7 @@ export async function updateCaseById(
   const state = formData.state;
   const city = formData.city;
 
-
-  const response = await apiFetch(`${API_URL}/cases/${id}`, {
+  await apiFetch(`${API_URL}/cases/${id}`, {
     headers: {
       'Content-Type': 'application/json',
     },
@@ -37,6 +30,4 @@ export async function updateCaseById(
       },
     }),
   });
-
-  return makeActionResponse(response);
 }
