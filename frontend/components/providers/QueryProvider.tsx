ececc9343f2400cd5a7ab66cff4d8360312b2e19
DavidSheltonSF <david.faria012@gmail.com>
2026-06-16 14:21:19 -0300

    refactor(QueryProvider): handle UnauthorizedError

--- frontend/components/providers/QueryProvider.tsx
+++ frontend/components/providers/QueryProvider.tsx
@@ -1,11 +1,27 @@
 'use client';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { UnauthorizedError } from '@/errors/UnauthorizedError';
+import { handleLogout } from '@/lib/handleLogout';
+import { MutationCache, QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { PropsWithChildren, useState } from 'react';
 
 export function QueryProvider({ children }: PropsWithChildren) {
   const [queryClient] = useState(
     () =>
       new QueryClient({
+        mutationCache: new MutationCache({
+          onError: (error) => {
+            if (error instanceof UnauthorizedError) {
+              handleLogout();
+            }
+          },
+        }),
+        queryCache: new QueryCache({
+          onError: (error) => {
+            if (error instanceof UnauthorizedError) {
+              handleLogout();
+            }
+          },
+        }),
         defaultOptions: {
           queries: {
             retry: 1,
