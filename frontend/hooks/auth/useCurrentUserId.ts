fed8de5a6cc694fdd9659a17a4bc834a7058f441
DavidSheltonSF <david.faria012@gmail.com>
2026-06-16 14:57:26 -0300

    refactor(useCurrentUserId): return null if user is null

--- frontend/hooks/auth/useCurrentUserId.ts
+++ frontend/hooks/auth/useCurrentUserId.ts
@@ -2,5 +2,8 @@ import { useCurrentUser } from './useCurrentUser';
 
 export function useCurrentUserId() {
   const user = useCurrentUser();
+  if (!user) {
+    return null;
+  }
   return user.id;
 }
