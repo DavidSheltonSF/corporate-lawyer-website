e3512e1918050e7454ec7feaa8bf5ed7e4b83abb
DavidSheltonSF <david.faria012@gmail.com>
2026-06-16 21:20:50 -0300

    refactor(ClientsList): assign  ClientsListSkeleton to ClienStList.Skeleton

--- frontend/components/features/clients/ClientsList.tsx
+++ frontend/components/features/clients/ClientsList.tsx
@@ -4,17 +4,20 @@ import { CardSkeleton } from '../../ui/Card/CardSkeleton';
 import { ClientCard } from './ClientCard/ClientCard';
 import { SafeUser } from '@/types/SafeUser';
 import { RequestState } from '@/types/RequestState';
+import { ClientsListSkeleton } from './ClientsListSkeleton';
 
 interface Props {
   fetchClients: () => void;
   requestState: RequestState<WithId<SafeUser>[]>;
 }
 
+ClientsList.Skeleton = ClientsListSkeleton;
+
 export function ClientsList({ requestState, fetchClients }: Props) {
   function renderContent() {
     switch (requestState?.status) {
       case 'loading':
-        return null
+        return null;
 
       case 'ok':
         const { data } = requestState;
