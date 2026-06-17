191e6e15616b7ff6a226c3b5cc4a7df052ab3f7f
DavidSheltonSF <david.faria012@gmail.com>
2026-06-16 22:38:37 -0300

    feat(types): add user-related types for pagination and search parameters

---
+++ frontend/services/users/types.ts
@@ -0,0 +1,13 @@
+import { Page } from '@/types/Page';
+import { SafeUser } from '@/types/SafeUser';
+import { User } from '@/types/User';
+import { WithId } from '@/types/WithId';
+
+export type UserPage = Page<WithId<User>>;
+export type SafeUserPage = Page<WithId<SafeUser>>;
+export interface GetUsersParams {
+  search?: string;
+  page: number;
+  limit: number;
+  status?: string;
+}
