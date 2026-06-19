0a1e73eb62c4b41de6072c97561c90cb8b9e7a9b
DavidSheltonSF <david.faria012@gmail.com>
2026-06-18 23:57:51 -0300

    refactor(uploadCaseFile): update function signature to use structured parameter for clarity

--- frontend/services/cases/uploadCaseFile.ts
+++ frontend/services/cases/uploadCaseFile.ts
@@ -2,11 +2,16 @@ import { ActionResponse } from '@/types/ActionResponse';
 import { apiFetch } from '../apiFetch';
 import { makeEmptyActionResponse } from '@/factories/makeEmptyActionResponse';
 
-export async function uploadCaseFile(
-  formData: FormData,
-  id: string
-): Promise<ActionResponse<null>> {
-  const response = await apiFetch(`/api/my/cases/${id}/files`, {
+interface UploadFileParam {
+  formData: FormData;
+  ownerId: string;
+}
+
+export async function uploadCaseFile({
+  formData,
+  ownerId,
+}: UploadFileParam): Promise<ActionResponse<null>> {
+  const response = await apiFetch(`/api/my/cases/${ownerId}/files`, {
     method: 'POST',
     body: formData,
   });
