2ec95b9ca719b98eb639e4cb71d4f5cd016d358e
DavidSheltonSF <david.faria012@gmail.com>
2026-06-18 23:33:22 -0300

    feat(backend/services): add FileService

---
+++ backend/src/services/files/FileService.ts
@@ -0,0 +1,28 @@
+import { FileRepository } from '../../repositories/FileRepository';
+import { WithId } from '../../types/WithId';
+import { IFileService } from './IFileService';
+import { CreateFileDTO } from '../../dtos/caseFile/CreateFileDTO';
+import { FileDTO } from '../../dtos/caseFile/FileDTO';
+import { validateFile } from '../validators/files/validateFile';
+import { Page } from '../../types/Page';
+import { PageParams } from '../../types/PageParams';
+
+export class FileService implements IFileService {
+  constructor(private fileRepository: FileRepository) {}
+  async create(data: CreateFileDTO, buffer: Buffer): Promise<WithId<FileDTO>> {
+    await validateFile(data, buffer);
+    return await this.fileRepository.create(data);
+  }
+
+  async findByOwnerId(ownerId: string, pageParams: PageParams): Promise<Page<WithId<FileDTO>>> {
+    return await this.fileRepository.findByOwnerId(ownerId, pageParams);
+  }
+
+  async rename(fileId: string, name: string): Promise<WithId<FileDTO> | null> {
+    return await this.fileRepository.rename(fileId, name);
+  }
+
+  async deleteById(fileId: string): Promise<WithId<FileDTO> | null> {
+    return await this.fileRepository.deleteById(fileId);
+  }
+}
---
+++ backend/src/services/files/IFileService.ts
@@ -0,0 +1,12 @@
+import { CreateFileDTO } from '../../dtos/caseFile/CreateFileDTO';
+import { FileDTO } from '../../dtos/caseFile/FileDTO';
+import { Page } from '../../types/Page';
+import { PageParams } from '../../types/PageParams';
+import { WithId } from '../../types/WithId';
+
+export interface IFileService {
+  create(data: CreateFileDTO, buffer: Buffer): Promise<WithId<FileDTO>>;
+  findByOwnerId(ownerId: string, pageParams: PageParams): Promise<Page<WithId<FileDTO>>>;
+  rename(fileId: string, name: string): Promise<WithId<FileDTO> | null>;
+  deleteById(id: string): Promise<WithId<FileDTO> | null>;
+}
