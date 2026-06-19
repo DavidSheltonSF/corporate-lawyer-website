9c96096aad57ce70390930956e62fde295f39905
DavidSheltonSF <david.faria012@gmail.com>
2026-06-19 14:18:05 -0300

    refactor(CaseFile): add downloadUrl

--- frontend/types/CaseFile.ts
+++ frontend/types/CaseFile.ts
@@ -4,6 +4,7 @@ import { WithId } from './WithId';
 export interface CaseFile {
   name: string;
   url: string;
+  downloadUrl: string;
   size: string;
   mimeType: string;
   uploadedBy: WithId<UserSlice>;
