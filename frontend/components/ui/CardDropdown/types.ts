242b50318a4f867e71e7e9b2c7f7f3db3e4895b2
DavidSheltonSF <david.faria012@gmail.com>
2026-07-07 15:41:55 -0300

    refactor(CardDropdown/types): remove unused CardActionType

--- frontend/components/ui/CardDropdown/types.ts
+++ frontend/components/ui/CardDropdown/types.ts
@@ -1,12 +1,5 @@
 import { PropsWithClassName } from '../../../types/PropsWithClassName';
 
-export enum CardActionType {
-  EDIT = 'EDIT',
-  DELETE = 'DELETE',
-  CHECK_DEADLINES = 'CHECK_DEADLINES',
-  CHECK_FILES = 'CHECK_FILES',
-}
-
 export interface CardAction {
   Icon: React.ComponentType<PropsWithClassName>;
   label: string;
