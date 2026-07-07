93d8d7fd663a50db7773c6fc571c5cee1a651557
DavidSheltonSF <david.faria012@gmail.com>
2026-07-07 17:50:48 -0300

    refactor(UserPermissions): update to include canDownloadFiles, candDeleteFiles and canRename files.

--- frontend/types/UserPermissions.ts
+++ frontend/types/UserPermissions.ts
@@ -2,4 +2,8 @@ export interface UserPermissions {
   canDeleteCase: boolean;
   canUpdateCase: boolean;
   canSeeDeadlines: boolean;
+  canUploadFiles: boolean;
+  canDownloadFiles: boolean;
+  canDeleteFiles: boolean;
+  canRenameFiles: boolean;
 }
