49a651ed897337d25d66d41c3e9344a4bd577fbd
DavidSheltonSF <david.faria012@gmail.com>
2026-07-09 17:55:05 -0300

    feat(hookis): add useRenameFile

---
+++ frontend/hooks/fetching/files/useRenameFile.ts
@@ -0,0 +1,16 @@
+import { renameFile } from "@/services/files/renameFile";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+export function useRenameFile() {
+  const queryClint = useQueryClient();
+  return useMutation({
+    mutationFn: renameFile,
+    onSuccess: () => {
+      queryClint.invalidateQueries({
+        queryKey: ['files']
+      })
+    }
+  });
+
+
+}
