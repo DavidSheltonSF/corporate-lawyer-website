39a033125dc62868c802d0ef0299b546182f6896
DavidSheltonSF <david.faria012@gmail.com>
2026-07-09 17:59:31 -0300

    feat(FileList): add onRename prop and pass it to FileCard

--- frontend/components/features/files/FilesList/FilesList.tsx
+++ frontend/components/features/files/FilesList/FilesList.tsx
@@ -4,13 +4,14 @@ import { FileCard } from '../FileCard/FileCard';
 
 interface Props {
   files: WithId<CaseFile>[];
-  onDelete: (fileId: string, fileName: string) => void
+  onRename: (fileId: string, fileName: string) => void;
+  onDelete: (fileId: string, fileName: string) => void;
 }
 
-export function FilesList({ files, onDelete }: Props) {
+export function FilesList({ files, onRename, onDelete }: Props) {
   const renderFiles = files.map((file) => {
-    return <FileCard key={file.id} file={file} onDelete={onDelete} />;
+    return <FileCard key={file.id} file={file} onRename={onRename} onDelete={onDelete} />;
   });
 
-  return <div className='flex flex-col size-full gap-[16px]'>{renderFiles}</div>;
+  return <div className="flex flex-col size-full gap-[16px]">{renderFiles}</div>;
 }
