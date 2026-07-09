7d1cfe32d6c5460426ff19a642a56d4578bcaf7f
DavidSheltonSF <david.faria012@gmail.com>
2026-07-09 17:56:32 -0300

    refactor(renameFile): update function signature to use params object

--- frontend/services/files/renameFile.ts
+++ frontend/services/files/renameFile.ts
@@ -3,10 +3,19 @@ import { WithId } from '@/types/WithId';
 import { apiFetch } from '../apiFetch';
 import { API_URL } from '@/config/api';
 
-export async function renameFile(id: string, name: string): Promise<WithId<CaseFile>> {
-  const response = await apiFetch(`${API_URL}/files/:id`, { method: 'PATCH' , body: {
-    name,
-  }});
+interface RenameFileParams {
+  fileName: string;
+  fileId: string;
+}
+
+export async function renameFile(params: RenameFileParams): Promise<WithId<CaseFile>> {
+  const response = await apiFetch(`${API_URL}/files/${params.fileId}`, {
+
+    method: 'PATCH',
+    body: JSON.stringify({
+      name: params.fileName,
+    }),
+  });
 
   return response.json()
 }
