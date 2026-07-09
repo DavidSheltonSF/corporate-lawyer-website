92bd98a42f9a75c18199f0f3e1a8b0751f49d086
DavidSheltonSF <david.faria012@gmail.com>
2026-07-09 17:54:23 -0300

    feat(useFileMenuItems): add rename functionality to file menu items

--- frontend/hooks/cards/useFileMenuItems.ts
+++ frontend/hooks/cards/useFileMenuItems.ts
@@ -2,16 +2,26 @@ import { DeleteIcon } from '@/components/icons/DeleteIcon';
 import { MenuItem } from '@/types/MenuItem';
 import { usePermissions } from '../auth/usePermissions';
 import { DownloadIcon } from '@/components/icons/DownloadIcon';
+import { EditIcon } from '@/components/icons/EditIcon';
 
 interface Props {
+  onRename: () => void;
   onDelete: () => void;
   onDownload: () => void;
 }
 
-export function useFileMenuItems({ onDelete, onDownload }: Props): MenuItem[] {
+export function useFileMenuItems({ onRename, onDelete, onDownload }: Props): MenuItem[] {
   const permissions = usePermissions();
 
   const items = [];
+
+  if (permissions?.canRenameFiles) {
+    items.push({
+      label: 'Renomear',
+      Icon: EditIcon,
+      action: onRename,
+    });
+  }
 
   if (permissions?.canDeleteFiles) {
     items.push({
