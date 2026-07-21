a50106d8cf9a0129742cd9ec1125655249e1e04a
DavidSheltonSF <david.faria012@gmail.com>
2026-07-21 17:27:45 -0300

    test(AuthController): improve test setup with createMockObject and improve first test

--- backend/src/controllers/auth/AuthController.test.ts
+++ backend/src/controllers/auth/AuthController.test.ts
@@ -1,42 +1,46 @@
 import { MissingAuthenticatedUserError } from '../../errors/presentation/MissingAuthenticatedUserError';
 import { IAuthService } from '../../services/auth/IAuthService';
 import { IUserService } from '../../services/user/IUserService';
+import { UserMocker } from '../../tests/mocks/entities/UserMocker';
+import { createMockObject } from '../../tests/mocks/createMockObject';
 import { HttpRequest } from '../types/HttpRequest';
 import { HttpStatusCode } from '../types/HttpStatusCode';
 import { AuthController } from './AuthController';
 
 describe(`Test ${AuthController.name}`, () => {
   function makeSut() {
-    const user = {
+    const authUser = {
       id: 'user-id',
       email: 'user@example.com',
     };
-    const findByEmail = jest.fn().mockResolvedValue(user);
-    const controller = new AuthController(
-      {} as IAuthService,
-      { findByEmail } as unknown as IUserService
-    );
+    const mockAuthService = createMockObject<IAuthService>(['authenticate']);
+    const mockUserService = createMockObject<IUserService>(['findByEmail']);
+    const controller = new AuthController(mockAuthService, mockUserService);
 
     return {
       controller,
-      findByEmail,
-      user,
+      mockUserService,
+      authUser,
     };
   }
 
   test('returns the authenticated user for the request email', async () => {
-    const { controller, findByEmail, user } = makeSut();
+    const { controller, mockUserService, authUser } = makeSut();
 
     const httpRequest: HttpRequest = {
-      user,
+      user: authUser,
     };
+
+    const mockUser = UserMocker.mockUserDTOWithId();
+
+    mockUserService.findByEmail.mockResolvedValue(mockUser);
 
     const response = await controller.getMe(httpRequest);
 
-    expect(findByEmail).toHaveBeenCalledWith(user.email);
+    expect(mockUserService.findByEmail).toHaveBeenCalledWith(authUser.email);
     expect(response).toMatchObject({
       status: HttpStatusCode.ok,
-      data: user,
+      data: mockUser,
     });
   });
 
