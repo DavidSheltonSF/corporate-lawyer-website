037fd262b24a186639d9491050f4474f62a24a6c
DavidSheltonSF <david.faria012@gmail.com>
2026-07-22 18:38:00 -0300

    refactor(casesRoutes): integrate requireLawyer middleware

--- backend/src/routes/casesRoutes.ts
+++ backend/src/routes/casesRoutes.ts
@@ -1,15 +1,29 @@
-import { Router } from 'express';
+import { RequestHandler, Router } from 'express';
 import { ICaseController } from '../controllers/case/ICaseController';
 import { requireAuth } from '../middlewares/requireAuth';
 import { upload } from '../middlewares/uploadFile';
 import { expressHttpAdapter } from './adapters/expressHttpAdapter';
 
-export function casesRoutes(router: Router, caseController: ICaseController) {
-  router.post('/api/cases', requireAuth, expressHttpAdapter(caseController.create));
-  router.put('/api/cases/:id', requireAuth, expressHttpAdapter(caseController.updateById));
+export function casesRoutes(
+  router: Router,
+  caseController: ICaseController,
+  requireLawyer: RequestHandler
+) {
+  router.post('/api/cases', requireAuth, requireLawyer, expressHttpAdapter(caseController.create));
+  router.put(
+    '/api/cases/:id',
+    requireAuth,
+    requireLawyer,
+    expressHttpAdapter(caseController.updateById)
+  );
   router.get('/api/my/cases/stats', requireAuth, expressHttpAdapter(caseController.getMyStats));
-  router.get('/api/cases/stats', requireAuth, expressHttpAdapter(caseController.getStats));
-  router.get('/api/cases', requireAuth, expressHttpAdapter(caseController.findAll));
+  router.get(
+    '/api/cases/stats',
+    requireAuth,
+    requireLawyer,
+    expressHttpAdapter(caseController.getStats)
+  );
+  router.get('/api/cases', requireAuth, requireLawyer, expressHttpAdapter(caseController.findAll));
   router.get('/api/cases/:id', expressHttpAdapter(caseController.findById));
   router.get('/api/my/cases', requireAuth, expressHttpAdapter(caseController.findMyCases));
   router.get(
@@ -23,5 +37,10 @@ export function casesRoutes(router: Router, caseController: ICaseController) {
     upload.single('file'),
     expressHttpAdapter(caseController.uploadMyFile)
   );
-  router.delete('/api/cases/:id', requireAuth, expressHttpAdapter(caseController.deleteById));
+  router.delete(
+    '/api/cases/:id',
+    requireAuth,
+    requireLawyer,
+    expressHttpAdapter(caseController.deleteById)
+  );
 }
