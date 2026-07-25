82487c823e5d5724fb344d2f8e04276faf3fe247
DavidSheltonSF <david.faria012@gmail.com>
2026-07-24 21:43:12 -0300

    refactor(userRoutes): integrate requireUser and requireLawyer middlewares

--- backend/src/routes/usersRoutes.ts
+++ backend/src/routes/usersRoutes.ts
@@ -1,12 +1,47 @@
-import { Router } from 'express';
+import { RequestHandler, Router } from 'express';
 import { IUserController } from '../controllers/user/IUserController';
 import { expressHttpAdapter } from './adapters/expressHttpAdapter';
 import { requireAuth } from '../middlewares/requireAuth';
+import { requireLawyer } from '../middlewares/requireLawyer';
 
-export function usersRoutes(router: Router, userController: IUserController) {
-  router.get('/api/clients', requireAuth, expressHttpAdapter(userController.findClients));
-  router.post('/api/clients', requireAuth, expressHttpAdapter(userController.createClient));
-  router.get('/api/users/:id', requireAuth, expressHttpAdapter(userController.findById));
-  router.put('/api/users/:id', requireAuth, expressHttpAdapter(userController.updateById));
-  router.delete('/api/users/:id', requireAuth, expressHttpAdapter(userController.deleteById));
+export function usersRoutes(
+  router: Router,
+  userController: IUserController,
+  requireUser: RequestHandler
+) {
+  router.get(
+    '/api/clients',
+    requireAuth,
+    requireUser,
+    requireLawyer,
+    expressHttpAdapter(userController.findClients)
+  );
+  router.post(
+    '/api/clients',
+    requireAuth,
+    requireUser,
+    requireLawyer,
+    expressHttpAdapter(userController.createClient)
+  );
+  router.get(
+    '/api/users/:id',
+    requireAuth,
+    requireUser,
+    requireLawyer,
+    expressHttpAdapter(userController.findById)
+  );
+  router.put(
+    '/api/users/:id',
+    requireAuth,
+    requireUser,
+    requireLawyer,
+    expressHttpAdapter(userController.updateById)
+  );
+  router.delete(
+    '/api/users/:id',
+    requireAuth,
+    requireUser,
+    requireLawyer,
+    expressHttpAdapter(userController.deleteById)
+  );
 }
