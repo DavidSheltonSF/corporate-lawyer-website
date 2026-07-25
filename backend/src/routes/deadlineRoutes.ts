8bc0080bdd0e70f111f01da97801b8bf8d93efc7
DavidSheltonSF <david.faria012@gmail.com>
2026-07-24 21:48:40 -0300

    refactor(deadlineRoutes): integrate requireUser and requireLawyer middlewares

--- backend/src/routes/deadlineRoutes.ts
+++ backend/src/routes/deadlineRoutes.ts
@@ -1,14 +1,33 @@
-import { Router } from 'express';
+import { RequestHandler, Router } from 'express';
 import { expressHttpAdapter } from './adapters/expressHttpAdapter';
 import { requireAuth } from '../middlewares/requireAuth';
 import { IDeadlineController } from '../controllers/deadline/IDeadlineController';
+import { requireLawyer } from '../middlewares/requireLawyer';
 
-export function deadlineRoutes(router: Router, deadlineController: IDeadlineController) {
-  router.post('/api/deadlines', requireAuth, expressHttpAdapter(deadlineController.create));
+export function deadlineRoutes(
+  router: Router,
+  deadlineController: IDeadlineController,
+  requireUser: RequestHandler
+) {
+  router.post(
+    '/api/deadlines',
+    requireAuth,
+    requireUser,
+    requireLawyer,
+    expressHttpAdapter(deadlineController.create)
+  );
   router.get(
     '/api/cases/:id/deadlines',
     requireAuth,
+    requireUser,
+    requireLawyer,
     expressHttpAdapter(deadlineController.findByCaseId)
   );
-  router.delete('/api/deadlines/:id', requireAuth, expressHttpAdapter(deadlineController.deleteById));
+  router.delete(
+    '/api/deadlines/:id',
+    requireAuth,
+    requireUser,
+    requireLawyer,
+    expressHttpAdapter(deadlineController.deleteById)
+  );
 }
