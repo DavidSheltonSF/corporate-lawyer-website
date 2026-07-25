1bc6d1796c371787b6a72e66298148d395574ad4
DavidSheltonSF <david.faria012@gmail.com>
2026-07-24 21:28:48 -0300

    refactor(backend/types): use AuthenticatedUser type in express.d.ts

--- backend/src/types/express.d.ts
+++ backend/src/types/express.d.ts
@@ -1,12 +1,10 @@
 import 'express';
+import { AuthenticatedUser } from './AuthenticatedUser';
 
 declare global {
   namespace Express {
     interface Request {
-      user?: {
-        id: string;
-        email: string;
-      };
+      user?: AuthenticatedUser
     }
   }
 }
