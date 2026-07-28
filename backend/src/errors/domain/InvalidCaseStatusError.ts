18d54ed4545bda22f6edc5ad118d19bf5486c986
DavidSheltonSF <david.faria012@gmail.com>
2026-07-28 16:40:29 -0300

    refactor(backend/error): remove unused InvalidCaseStatusError, InvalidCaseTitleError and InvalidProcessNumberError

--- backend/src/errors/domain/InvalidCaseStatusError.ts
+++
@@ -1,9 +0,0 @@
-import { CasesStatus } from '../../types/CasesStatus';
-import { DomainError } from './DomainError';
-
-export class InvalidCaseStatusError extends DomainError {
-  constructor(status: string) {
-    super(`Status "${status}" is invalid. Expected ${Object.values(CasesStatus).toString()}`);
-    this.name = InvalidCaseStatusError.name;
-  }
-}
--- backend/src/errors/domain/InvalidCaseTitleError.ts
+++
@@ -1,8 +0,0 @@
-import { DomainError } from './DomainError';
-
-export class InvalidCaseTitleError extends DomainError {
-  constructor(title: string) {
-    super(`Title "${title}" is invalid. Expected a string with between 15 and 100 characters.`);
-    this.name = InvalidCaseTitleError.name;
-  }
-}
