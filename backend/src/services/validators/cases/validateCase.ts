4e74c737d4f46b3369331cbdc9e3d17db3f0b2af
DavidSheltonSF <david.faria012@gmail.com>
2026-07-28 16:41:51 -0300

    refactor(backend/validators): enhance validateCase function to collect and report multiple validation errors

--- backend/src/services/validators/cases/validateCase.ts
+++ backend/src/services/validators/cases/validateCase.ts
@@ -1,12 +1,27 @@
 import { CreateCaseDTO } from '../../../dtos/case/CreateCaseDTO';
+import { ValidationError } from '../../../errors/presentation/ValidationError';
+import { CasesStatus } from '../../../types/CasesStatus';
 import { validateCaseStatus } from './validateCaseStatus';
 import { validateCaseTitle } from './validateCaseTitle';
 import { validateProcessNumber } from './validateProcessNumber';
 
 export function validateCase(data: CreateCaseDTO) {
   const { title, processNumber, status } = data;
+  const invalidFields: Partial<Record<keyof CreateCaseDTO, string>> = {};
 
-  validateCaseTitle(title);
-  validateProcessNumber(processNumber);
-  validateCaseStatus(status);
+  if (!validateCaseTitle(title)) {
+    invalidFields.title = `Title "${title}" is invalid. Expected a string with between 15 and 100 characters.`;
+  }
+
+  if (!validateProcessNumber(processNumber)) {
+    invalidFields.processNumber = `Process number '${processNumber}' is invalid. Expected format: NNNNNNN-DD.AAAA.J.TR.OOOO`;
+  }
+
+  if (!validateCaseStatus(status)) {
+    invalidFields.status = `Status "${status}" is invalid. Expected ${Object.values(CasesStatus).toString()}`;
+  }
+
+  if (Object.keys(invalidFields).length > 0) {
+    throw new ValidationError('Invalid case data', invalidFields);
+  }
 }
