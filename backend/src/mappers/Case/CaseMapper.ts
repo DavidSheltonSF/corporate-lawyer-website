135e6a520e7c4552097122c48ab97894c709d246
DavidSheltonSF <david.faria012@gmail.com>
2026-08-02 18:00:00 -0300

    refactor(CaseMapper): update persistenceToPopulatedPresentatio to return CaseDTO with id

--- backend/src/mappers/Case/CaseMapper.ts
+++ backend/src/mappers/Case/CaseMapper.ts
@@ -1,18 +1,12 @@
 import { CaseDTO } from '../../dtos/case/CaseDTO';
-import { CasePopulatedResponseDTO } from '../../dtos/case/CasePopulatedResponseDTO';
-import { Case } from '../../entities/Case';
 import { WithId } from '../../types/WithId';
-import { FileMapper } from '../CaseFile/FileMapper';
-import { HearingMapper } from '../Hearing/HearingMapper';
 import { toUserIdentity } from '../toUserIdentity';
 
 export class CaseMapper {
-  static persistenceToPopulatedPresentation(cas: any): WithId<CasePopulatedResponseDTO> {
+  static persistenceToPopulatedPresentation(cas: any): WithId<CaseDTO> {
     const { title, processNumber, court, courtDivision, status, description, location } = cas;
     const client = toUserIdentity(cas.client);
     const lawyers = cas.lawyers.map(toUserIdentity);
-    const files = cas.files.map(FileMapper.persistenceToPresentation);
-    const hearings = cas.hearings.map(HearingMapper.persistenceToPresentation);
 
     return {
       id: cas._id.toString(),
@@ -24,11 +18,10 @@ export class CaseMapper {
       description,
       client,
       lawyers,
-      files,
-      hearings,
       location,
       createdAt: cas.createdAt.toISOString(),
       updatedAt: cas.updatedAt.toISOString(),
+      populated: true,
     };
   }
 
@@ -49,8 +42,6 @@ export class CaseMapper {
       description,
       client: cas.client.toString(),
       lawyers,
-      files,
-      hearings,
       location,
     };
   }
