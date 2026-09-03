c1af1e9951c879e5ed62b84d0cefb2bc5b6d0c53
DavidSheltonSF <david.faria012@gmail.com>
2026-09-03 12:10:11 -0300

    chore(test): migrate createMockCaseService to vitest

--- backend/src/tests/mocks/services/createMockCaseServiceTest.ts
+++ backend/src/tests/mocks/services/createMockCaseServiceTest.ts
@@ -1,9 +1,10 @@
+import { Mocked } from 'vitest';
 import { CaseService } from '../../../services/case/CaseService';
 import { ICaseService } from '../../../services/case/ICaseService';
 import { getClassMethods } from '../../helpers/getClassMethods';
 import { createMockObject } from '../createMockObject';
 
-export function createMockCaseService(): jest.Mocked<ICaseService> {
+export function createMockCaseService(): Mocked<ICaseService> {
   const methods = getClassMethods(CaseService);
   return createMockObject(methods);
 }
