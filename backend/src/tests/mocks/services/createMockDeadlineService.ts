f99240a004b7e514d8982b8777b46d1fd76b13ba
DavidSheltonSF <david.faria012@gmail.com>
2026-09-03 12:13:08 -0300

    chore(test): migrate createMockDeadlineService to vitest

--- backend/src/tests/mocks/services/createMockDeadlineService.ts
+++ backend/src/tests/mocks/services/createMockDeadlineService.ts
@@ -1,9 +1,10 @@
+import { Mocked } from 'vitest';
 import { DeadlineService } from '../../../services/deadline/DeadlineService';
 import { IDeadlineService } from '../../../services/deadline/IDeadlineService';
 import { getClassMethods } from '../../helpers/getClassMethods';
 import { createMockObject } from '../createMockObject';
 
-export function createMockDeadlineService(): jest.Mocked<IDeadlineService> {
+export function createMockDeadlineService(): Mocked<IDeadlineService> {
   const methods = getClassMethods(DeadlineService);
   return createMockObject(methods);
 }
