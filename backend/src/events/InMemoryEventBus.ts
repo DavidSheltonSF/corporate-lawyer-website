693214b88c03df8c680e1239e1cd3cf665b53e29
DavidSheltonSF <david.faria012@gmail.com>
2026-09-04 16:07:31 -0300

    refactor(event-bus): convert subscribe and publish methods to standard function syntax

--- backend/src/events/InMemoryEventBus.ts
+++ backend/src/events/InMemoryEventBus.ts
@@ -3,18 +3,18 @@ import { IEventBus } from './IEventBus';
 
 export class InMemoryEventBus implements IEventBus {
   private listeners = new Map();
-  subscribe = (eventName: string, listener: EventListener) => {
+  subscribe(eventName: string, listener: EventListener) {
     const listeners: EventListener[] = this.listeners.get(eventName) || [];
 
     listeners.push(listener);
 
     this.listeners.set(eventName, listeners);
-  };
+  }
 
-  publish = (eventName: string, payload: unknown) => {
+  publish(eventName: string, payload: unknown) {
     const listeners = this.listeners.get(eventName) as EventListener[];
     listeners.forEach((listener) => {
       listener.handle(payload);
     });
-  };
+  }
 }
