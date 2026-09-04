36d5eb80d3dc041be882d2960a84a112c5e7458a
DavidSheltonSF <david.faria012@gmail.com>
2026-09-04 18:23:27 -0300

    fix(validateCasePartial): fix and enhance validation logic and error handling for case data

--- backend/src/services/validators/cases/validateCasePartial.ts
+++ backend/src/services/validators/cases/validateCasePartial.ts
@@ -1,4 +1,6 @@
 import { UpdateCaseDTO } from '../../../dtos/case/UpdateCaseDTO';
+import { ValidationError } from '../../../errors/presentation/ValidationError';
+import { CasesStatus } from '../../../types/CasesStatus';
 import { validateCaseStatus } from './validateCaseStatus';
 import { validateCaseTitle } from './validateCaseTitle';
 import { validateProcessNumber } from './validateProcessNumber';
@@ -6,15 +8,21 @@ import { validateProcessNumber } from './validateProcessNumber';
 export function validateCasePartial(data: UpdateCaseDTO) {
   const { title, processNumber, status } = data;
 
-  if (title) {
-    validateCaseTitle(title);
+  const invalidFields: Partial<Record<keyof UpdateCaseDTO, string>> = {};
+
+  if (title && !validateCaseTitle(title)) {
+    invalidFields.title = `Title "${title}" is invalid. Expected a string with between 15 and 100 characters.`;
   }
 
-  if (processNumber) {
-    validateProcessNumber(processNumber);
+  if (processNumber && !validateProcessNumber(processNumber)) {
+    invalidFields.processNumber = `Process number '${processNumber}' is invalid. Expected format: NNNNNNN-DD.AAAA.J.TR.OOOO`;
   }
 
-  if (status) {
-    validateCaseStatus(status);
+  if (status && !validateCaseStatus(status)) {
+    invalidFields.status = `Status "${status}" is invalid. Expected ${Object.values(CasesStatus).toString()}`;
+  }
+
+  if (Object.keys(invalidFields).length > 0) {
+    throw new ValidationError('Invalid case data', invalidFields);
   }
 }
