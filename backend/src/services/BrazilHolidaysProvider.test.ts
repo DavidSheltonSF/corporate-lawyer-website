edfbfe48ff13120439bbedacc9999435b9b6e963
DavidSheltonSF <david.faria012@gmail.com>
2026-09-05 21:49:04 -0300

    refactor(BrazilianHolidaysProvider.test): rewrite and migrate to vitest

--- backend/src/services/BrazilHolidaysProvider.test.ts
+++ backend/src/services/BrazilHolidaysProvider.test.ts
@@ -1,19 +1,26 @@
+import { describe, expect, it } from 'vitest';
 import { BrazilState } from '../types/BrazilState';
 import { City } from '../types/City';
-import { BrazilHolidaysProvider } from './BrazilHolidaysProvider';
+import { BrazilHolidaysProvider, brazilHolidaysRecord } from './BrazilHolidaysProvider';
 
 describe(`Testing ${BrazilHolidaysProvider.name}`, () => {
   function makeSut() {
     const brazilHolidaysProvider = new BrazilHolidaysProvider();
     return { brazilHolidaysProvider };
   }
-  test(`should return Belford Roxo's holidays, Rio de Janeiro's (STATE) Holidays and Brazil's holidays`, () => {
-    const { brazilHolidaysProvider } = makeSut();
+  describe('getLocalHolidays', () => {
+    it(`should return the Brazilian holidays applicable for the provided state and city`, () => {
+      const { brazilHolidaysProvider } = makeSut();
 
-    const holidays = brazilHolidaysProvider.getLocalHolidays(
-      BrazilState.RIO_DE_JANEIRO,
-      City.RIO_DE_JANEIRO
-    );
-    console.log(holidays);
+      const state = BrazilState.RIO_DE_JANEIRO;
+      const city = City.DUQUE_DE_CAXIAS;
+      const holidays = brazilHolidaysProvider.getLocalHolidays(state, city);
+
+      expect(holidays).toEqual([
+        ...brazilHolidaysRecord.national,
+        ...brazilHolidaysRecord.states[state],
+        ...brazilHolidaysRecord.cities[city],
+      ]);
+    });
   });
 });
