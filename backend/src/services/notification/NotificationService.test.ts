43c6c65bb6d207b9118f1aabbfd9e6ef75f4328b
DavidSheltonSF <david.faria012@gmail.com>
2026-09-05 22:00:27 -0300

    refactor(NotificationRepository.test): rewrite and migrate to vitest

--- backend/src/services/notification/NotificationService.test.ts
+++ backend/src/services/notification/NotificationService.test.ts
@@ -1,37 +1,60 @@
-import { mockNotificationRepository } from '../../tests/mocks/repositories/mockNotificationRepository';
+import { describe, expect, it } from 'vitest';
 import { NotificationMocker } from '../../tests/mocks/entities/NotificationMocker';
 import { NotificationService } from './NotificationService';
+import { createMockNotificationRepository } from '../../tests/mocks/repositories/createMockNotificationRepository';
 
 describe(`Test ${NotificationService.name}`, () => {
   function makeSut() {
-    const notificationRepository = mockNotificationRepository();
+    const notificationRepository = createMockNotificationRepository();
     const notificationService = new NotificationService(notificationRepository);
+    const fakeId = 'fakeId';
 
     return {
       notificationRepository,
       notificationService,
+      fakeId,
     };
   }
 
-  test('should call NotificationRepository.create', async () => {
-    const { notificationService, notificationRepository } = makeSut();
-    const createNotificationDTO = NotificationMocker.mockCreateNotificationDTO();
-    await notificationService.create(createNotificationDTO);
-    expect(notificationRepository.create).toHaveBeenCalled();
+  describe('create', () => {
+    it('should create a new notification', async () => {
+      const { notificationService, notificationRepository } = makeSut();
+      const notificationData = NotificationMocker.mockCreateNotificationDTO();
+      await notificationService.create(notificationData);
+      expect(notificationRepository.create).toHaveBeenCalledWith(notificationData);
+    });
   });
 
-  test('should find notification by id', async () => {
-    const { notificationService, notificationRepository } = makeSut();
-    const id = 'testid--fnsianf';
-    await notificationService.findById(id);
-    expect(notificationRepository.findById).toHaveBeenCalledWith(id);
+  describe('findById', () => {
+    it('should find notification by id', async () => {
+      const { notificationService, notificationRepository, fakeId } = makeSut();
+
+      const expectedNotification = NotificationMocker.mockNotificationDTOWithId();
+      notificationRepository.findById.mockResolvedValue(expectedNotification);
+
+      const notification = await notificationService.findById(fakeId);
+
+      expect(notification).toMatchObject(expectedNotification);
+      expect(notificationRepository.findById).toHaveBeenCalledWith(fakeId);
+    });
+
+    it('should return null if notification is not found', async () => {
+      const { notificationService, notificationRepository, fakeId } = makeSut();
+
+      notificationRepository.findById.mockResolvedValue(null);
+      const notification = await notificationService.findById(fakeId);
+
+      expect(notification).toBeNull();
+      expect(notificationRepository.findById).toHaveBeenCalledWith(fakeId);
+    });
   });
 
-  test('should find notifications by user id', async () => {
-    const { notificationService, notificationRepository } = makeSut();
-    const userId = 'testid--fnsianf';
-    const query = { page: 1, limit: 4 };
-    await notificationService.findByUserId(userId, query);
-    expect(notificationRepository.findByUserId).toHaveBeenCalledWith(userId, query);
+  describe('findByUserId', () => {
+    it('should find notifications by user id', async () => {
+      const { notificationService, notificationRepository, fakeId } = makeSut();
+      const query = { page: 1, limit: 4 };
+      await notificationService.findByUserId(fakeId, query);
+      expect(notificationRepository.findByUserId).toHaveBeenCalledWith(fakeId, query);
+    });
   });
 });
