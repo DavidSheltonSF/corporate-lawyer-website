21259cf536b57cb565e1580300b5565ca8e4ad14
DavidSheltonSF <david.faria012@gmail.com>
2026-09-05 21:42:46 -0300

    feat(UserMapper): add toResponse method

--- backend/src/mappers/User/UserMapper.ts
+++ backend/src/mappers/User/UserMapper.ts
@@ -1,9 +1,15 @@
 import { UserDTO } from '../../dtos/user/UserDTO';
+import { UserResponseDTO } from '../../dtos/user/UserResponseDTO';
 import { WithId } from '../../types/WithId';
 
 export class UserMapper {
   static persistenceToPresentation(data: any): WithId<UserDTO> {
     const { firstName, lastName, email, phone, cpf, role, password } = data;
     return { id: data._id.toString(), firstName, lastName, email, phone, cpf, role, password };
+  }
+
+  static toResponse(user: WithId<UserDTO>): WithId<UserResponseDTO> {
+    const { password, ...safeUser } = user;
+    return safeUser;
   }
 }
