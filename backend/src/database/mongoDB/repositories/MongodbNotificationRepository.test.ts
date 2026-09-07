32bcaa1a576d73c3dffabcfb3405bcb06708851f
DavidSheltonSF <david.faria012@gmail.com>
2026-09-07 20:26:46 -0300

    refactor(MongodbNotificationRepository.tests): rewrite and migrate from jest to vitest

--- backend/src/database/mongoDB/repositories/MongodbNotificationRepository.test.ts
+++ backend/src/database/mongoDB/repositories/MongodbNotificationRepository.test.ts
@@ -1,13 +1,14 @@
+import { describe, beforeAll, beforeEach, afterAll, it, expect } from 'vitest';
 import { NotificationModel } from '../../../models/NotificationModel';
 import { NotificationMocker } from '../../../tests/mocks/entities/NotificationMocker';
 import { GenericMocker } from '../../../tests/mocks/fields/GenericMocker';
-import { MongodbTestConnector } from '../MongodbTestConnector';
+import { MongodbConnector } from '../MongodbConnector';
 import { MongodbNotificationRepository } from './MongodbNotificationRepository';
 
 describe(`Testing ${MongodbNotificationRepository.name}`, () => {
-  let connection: MongodbTestConnector | null = null;
+  let connection: MongodbConnector | null = null;
   beforeAll(async () => {
-    connection = await MongodbTestConnector.connectAndReturn('deadline_repository_test');
+    connection = await MongodbConnector.connectAndReturn();
   });
 
   beforeEach(async () => {
@@ -15,7 +16,6 @@ describe(`Testing ${MongodbNotificationRepository.name}`, () => {
   });
 
   afterAll(async () => {
-    await connection?.deleteDatabase();
     await connection?.disconnect();
   });
 
@@ -27,50 +27,63 @@ describe(`Testing ${MongodbNotificationRepository.name}`, () => {
     };
   }
 
-  test('should crate a notification', async () => {
-    const { notificationRepository } = makeSut();
-    const notificationDTO = NotificationMocker.mockCreateNotificationDTO();
-    const result = await notificationRepository.create(notificationDTO);
-    expect(result).toMatchObject(notificationDTO);
+  describe('should create a new notification', () => {
+    it('should crate a notification', async () => {
+      const { notificationRepository } = makeSut();
+      const notification = NotificationMocker.mockCreateNotificationDTO();
+      const created = await notificationRepository.create(notification);
+      expect(created).toMatchObject(notification);
+    });
   });
 
-  test('should find a notification by id', async () => {
-    const { notificationRepository } = makeSut();
-    const notificationDTO = NotificationMocker.mockCreateNotificationDTO();
-    const notificationId = (await NotificationModel.create(notificationDTO))._id;
-    const result = await notificationRepository.findById(notificationId.toString());
-    expect(result).toMatchObject(notificationDTO);
+  describe('findById', () => {
+    it('should find a notification by id', async () => {
+      const { notificationRepository } = makeSut();
+      const notification = NotificationMocker.mockCreateNotificationDTO();
+      const notificationId = (await NotificationModel.create(notification))._id;
+      const found = await notificationRepository.findById(notificationId.toString());
+      expect(found).toMatchObject(notification);
+    });
   });
 
-  test('should find all notifications', async () => {
-    const { notificationRepository } = makeSut();
-    const notification1 = NotificationMocker.mockCreateNotificationDTO();
-    const notification2 = NotificationMocker.mockCreateNotificationDTO();
-    await NotificationModel.create([notification1, notification2]);
-    const result = await notificationRepository.findAll();
-    expect(result).toContainEqual(expect.objectContaining(notification1));
-    expect(result).toContainEqual(expect.objectContaining(notification2));
+  describe('findAll', () => {
+    it('should find all notifications', async () => {
+      const { notificationRepository } = makeSut();
+      const notification1 = NotificationMocker.mockCreateNotificationDTO();
+      const notification2 = NotificationMocker.mockCreateNotificationDTO();
+      await NotificationModel.create([notification2, notification1]);
+      const result = await notificationRepository.findAll();
+      expect(result).toEqual(expect.arrayContaining([expect.objectContaining(notification1)]));
+      expect(result).toEqual(expect.arrayContaining([expect.objectContaining(notification2)]));
+    });
   });
+  describe('findByUserId', () => {
+    it('should find a notifications by user id', async () => {
+      const { notificationRepository } = makeSut();
+      const userId = GenericMocker.mockMongoId().toString();
+      const notification1 = NotificationMocker.mockCreateNotificationDTO();
+      const notification2 = NotificationMocker.mockCreateNotificationDTO();
+      const notification3 = NotificationMocker.mockCreateNotificationDTO();
 
-  test('should find a notifications by user id', async () => {
-    const { notificationRepository } = makeSut();
-    const userId = GenericMocker.mockMongoId().toString();
-    const notification1 = NotificationMocker.mockCreateNotificationDTO();
-    const notification2 = NotificationMocker.mockCreateNotificationDTO();
-    const notification3 = NotificationMocker.mockCreateNotificationDTO();
+      // Notifications with the same user
+      notification1.userId = userId;
+      notification3.userId = userId;
 
-    notification1.userId = userId;
-    notification3.userId = userId;
+      await NotificationModel.create([notification1, notification2, notification3]);
 
-    await NotificationModel.create([notification1, notification2, notification3]);
-    const query = { page: 1, limit: 4 };
-    const result = await notificationRepository.findByUserId(userId, query);
-    const { currentPage } = result.meta;
-    const data = result.data;
+      const limit = 4;
+      const page = 1;
 
-    expect(data).toContainEqual(expect.objectContaining(notification1));
-    expect(data).toContainEqual(expect.objectContaining(notification3));
-    expect(data).not.toContainEqual(expect.objectContaining(notification2));
-    expect(currentPage).toBe(query.page);
+      const { items, meta } = await notificationRepository.findByUserId(userId, { limit, page });
+
+      expect(items).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining(notification1),
+          expect.objectContaining(notification3),
+        ])
+      );
+
+      expect(meta.currentPage).toBe(page);
+    });
   });
 });
