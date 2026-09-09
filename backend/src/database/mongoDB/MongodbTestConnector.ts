dc5227c02756ca5dc519d2195e609659efc910dd
DavidSheltonSF <david.faria012@gmail.com>
2026-09-09 12:55:25 -0300

    feat(tests): add MongodbTestConnector

---
+++ backend/src/database/mongoDB/MongodbTestConnector.ts
@@ -0,0 +1,49 @@
+import mongoose from 'mongoose';
+import { config } from 'dotenv';
+config();
+
+function getTestURI(workerId: string): string {
+  const URI = process.env.MONGODB_TEST_URI as string;
+  return URI.replace('corporate_website_test', `${workerId}`);
+}
+
+export class MongodbTestConnector {
+  static instance: MongodbTestConnector | null = null;
+
+  private constructor(private connection: mongoose.Connection | null = null) {}
+
+  static async connect(workerId: string): Promise<void> {
+    try {
+      const connection = await mongoose.connect(getTestURI(workerId));
+    } catch (error) {
+      console.log('Database error');
+      process.exit(1);
+    }
+  }
+
+  static async connectAndReturn(workerId: string): Promise<MongodbTestConnector> {
+    if (this.instance) return this.instance;
+    await this.connect(workerId);
+    this.instance = new MongodbTestConnector(mongoose.connection);
+    return this.instance;
+  }
+
+  async disconnect() {
+    try {
+      if (!this.connection) return;
+      await this.connection.close();
+      this.connection = null;
+      MongodbTestConnector.instance = null;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async deleteDatabase() {
+    await this.connection?.dropDatabase();
+  }
+
+  createModel(name: string, schema: any) {
+    return this.connection?.model(name, schema);
+  }
+}
