fbb630b72d6a260b18835708bfbca3bd5fad93bc
DavidSheltonSF <david.faria012@gmail.com>
2026-09-08 22:23:01 -0300

    refactor(MongodbConnector.test): migrate test from jest to vitest and update test syntax

--- backend/src/database/mongoDB/MongodbConnector.test.ts
+++ backend/src/database/mongoDB/MongodbConnector.test.ts
@@ -1,8 +1,8 @@
+import { describe, expect, it } from 'vitest';
 import { MongodbConnector } from './MongodbConnector';
-jest.setTimeout(99999);
 
 describe('Testing MongodbConnector', () => {
-  test('should be returned the same MongodbConnector instance', async () => {
+  it('should be returned the same MongodbConnector instance', async () => {
     const connection1 = await MongodbConnector.connectAndReturn();
     const connection2 = await MongodbConnector.connectAndReturn();
 
