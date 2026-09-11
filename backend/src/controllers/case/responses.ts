1582a579b4e9b0ff3a8eed0d29f32e6ad790ab2e
DavidSheltonSF <david.faria012@gmail.com>
2026-09-10 22:18:41 -0300

    fix(responses): update response types to use CaseDTO instead of CasePopulatedResponseDTO

--- backend/src/controllers/case/responses.ts
+++ backend/src/controllers/case/responses.ts
@@ -1,14 +1,13 @@
 import { CaseDTO } from '../../dtos/case/CaseDTO';
-import { CasePopulatedResponseDTO } from '../../dtos/case/CasePopulatedResponseDTO';
 import { FileDTO } from '../../dtos/caseFile/FileDTO';
 import { CasesStats } from '../../types/CasesStats';
 import { Page } from '../../types/Page';
 import { WithId } from '../../types/WithId';
 import { HttpResponse } from '../types/HttpResponse';
 
-export type FindAllResponse = HttpResponse<Page<WithId<CasePopulatedResponseDTO>> | null>;
-export type FindByClientResponse = HttpResponse<Page<WithId<CasePopulatedResponseDTO>> | null>;
-export type FindByIdResponse = HttpResponse<WithId<CaseDTO | CasePopulatedResponseDTO> | null>;
+export type FindAllResponse = HttpResponse<Page<WithId<CaseDTO>> | null>;
+export type FindByClientResponse = HttpResponse<Page<WithId<CaseDTO>> | null>;
+export type FindByIdResponse = HttpResponse<WithId<CaseDTO | CaseDTO> | null>;
 export type CreateResponse = HttpResponse<WithId<CaseDTO> | null>;
 export type UpdateResponse = HttpResponse<WithId<CaseDTO> | null>;
 export type GetMyStatsResponse = HttpResponse<CasesStats>;
