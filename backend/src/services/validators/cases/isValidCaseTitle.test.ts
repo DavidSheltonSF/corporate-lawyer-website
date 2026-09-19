f94e3cec6c240ccd5cddd5cbe75c3eb1beb528d9
DavidSheltonSF <david.faria012@gmail.com>
2026-09-18 22:07:01 -0300

    test(isValidCaseTitle): add vitest tests

---
+++ backend/src/services/validators/cases/isValidCaseTitle.test.ts
@@ -0,0 +1,22 @@
+import { describe, expect, it } from 'vitest';
+import { isValidCaseTitle } from './isValidCaseTitle.js';
+
+describe(`Test ${isValidCaseTitle}`, () => {
+  it('should return true if the case title is valid', () => {
+    expect(isValidCaseTitle('Ação de Usucapião Urbano')).toBeTruthy();
+    expect(isValidCaseTitle('Ação de Teste e Danos Morais')).toBeTruthy();
+  });
+
+  it('should return false if the case title is less than 15 characters', () => {
+    expect(isValidCaseTitle('Ano')).toBeFalsy();
+    expect(isValidCaseTitle('Ação de Teste')).toBeFalsy();
+  });
+
+  it('should return false if the case title is more than 100 characters', () => {
+    let bigString = '';
+    for (let i = 0; i < 101; i ++) {
+      bigString += 'a'
+    }
+    expect(isValidCaseTitle(bigString)).toBeFalsy();
+  });
+});
